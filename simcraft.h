9dfafa19e05d399891b1f26202f0f9a7dd5cca82
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-04-20 20:49:32 +0000

    ongoing T7/T8 work....  current state bad....  hold on
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@2202 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- simcraft.h
+++ simcraft.h
@@ -50,7 +50,7 @@ struct patch_t
     *version  = (int) m % 100; m /= 100;
     *arch     = (int) m % 100;
   }
-  patch_t() { mask = encode( 3, 0, 9 ); }
+  patch_t() { mask = encode( 3, 1, 0 ); }
 };
 
 // Forward Declarations ======================================================
