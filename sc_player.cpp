f40a58334c30d7f23173882ebd25964167e069e3
delmortis1 <delmortis1@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-07-04 07:19:35 +0000

    Allow for empty talent strings.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@2777 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- sc_player.cpp
+++ sc_player.cpp
@@ -2564,10 +2564,17 @@ bool player_t::parse_talents( const std::string& talent_string )
 
   int size1 = talent_tree1.size();
   int size2 = talent_tree2.size();
+  std::string temp_talent_string = talent_string;
 
-  std::string talent_string1( talent_string,     0,  size1 );
-  std::string talent_string2( talent_string, size1,  size2 );
-  std::string talent_string3( talent_string, size1 + size2 );
+  /* 0/0/0 talents returns an empty string from the Armory rather than a string of zero's. */
+  if ( temp_talent_string.empty() )
+  {
+    temp_talent_string.resize( size1 + size2, '0' );
+  }
+
+  std::string talent_string1( temp_talent_string,     0,  size1 );
+  std::string talent_string2( temp_talent_string, size1,  size2 );
+  std::string talent_string3( temp_talent_string, size1 + size2 );
 
   parse_talents( talent_tree1, talent_string1 );
   parse_talents( talent_tree2, talent_string2 );
