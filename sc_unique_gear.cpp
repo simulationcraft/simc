653f0580ba418ae165964988bb86f31cb1cfeceb
delmortis1 <delmortis1@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-08-15 12:31:09 +0000

    Update some enchants for 3.2
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@3119 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- sc_unique_gear.cpp
+++ sc_unique_gear.cpp
@@ -361,8 +361,8 @@ bool unique_gear_t::get_use_encoding( std::string&       encoding,
   else if ( name == "victors_call"               ) e = ( id == "47725" ? "OnAttackHit_215AP_5Stack_20Dur_120Cd"  : "OnAttackHit_250AP_5Stack_20Dur_120Cd"  );
 
   // Enchants
-  else if ( name == "pyrorocket"               ) e = "1937Fire_45Cd";  // temporary for backwards compatibility
-  else if ( name == "hand_mounted_pyro_rocket" ) e = "1937Fire_45Cd";
+  else if ( name == "pyrorocket"               ) e = "1837Fire_45Cd";  // temporary for backwards compatibility
+  else if ( name == "hand_mounted_pyro_rocket" ) e = "1837Fire_45Cd";
   else if ( name == "hyperspeed_accelerators"  ) e = "340Haste_12Dur_60Cd";
 
   if ( e.empty() ) return false;
