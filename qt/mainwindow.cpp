efc7ebefeef92e1c2e999adeed89d4ddeceef708
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-09-15 23:50:33 +0000

    a few more bones on the skeleton
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@3330 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- qt/mainwindow.cpp
+++ qt/mainwindow.cpp
@@ -2,62 +2,124 @@
 #include <QtGui>
 #include <QtWebKit>
 
+static QComboBox* create_choice( int count, ... )
+{
+    QComboBox* choice = new QComboBox();
+    va_list vap;
+    va_start( vap, count );
+    for ( int i=0; i < count; i++ )
+    {
+      const char* s = ( char* ) va_arg( vap, char* );
+      choice->addItem( s );
+    }
+    va_end( vap );
+    return choice;
+}
+
+void MainWindow::updateProgress()
+{
+}
+
+void MainWindow::mainTabChanged( int index )
+{
+    switch( index )
+    {
+        case 0: main_button->setText( "Go!"       ); break;
+        case 1: main_button->setText( "Save!"     ); break;
+        case 2: main_button->setText( "Import!"   ); break;
+        case 3: main_button->setText( "Simulate!" ); break;
+        case 4: main_button->setText( "Simulate!" ); break;
+        case 5: main_button->setText( "Simulate!" ); break;
+        case 6: main_button->setText( "Save!"     ); break;
+    }
+}
+
+void MainWindow::importTabChanged( int index )
+{
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QWidget(parent)
 {
-    cmd_line = new QLineEdit();
-    main_button = new QPushButton( "Go!" );
-    QHBoxLayout* h_layout = new QHBoxLayout();
-    h_layout->addWidget( cmd_line );
-    h_layout->addWidget( main_button );
-    QGroupBox* h_groupbox = new QGroupBox();
-    h_groupbox->setLayout( h_layout );
+    QHBoxLayout* cmd_line_layout = new QHBoxLayout();
+    cmd_line_layout->addWidget( cmd_line = new QLineEdit() );
+    cmd_line_layout->addWidget( progress_bar = new QProgressBar() );
+    cmd_line_layout->addWidget( main_button = new QPushButton( "Go!" ) );
+    progress_bar->setMaximum( 100 );
+    progress_bar->setMaximumWidth( 100 );
+    QGroupBox* cmd_line_groupbox = new QGroupBox();
+    cmd_line_groupbox->setLayout( cmd_line_layout );
 
-    tab_widget = new QTabWidget();
+    main_tab = new QTabWidget();
+    connect( main_tab, SIGNAL(currentChanged(int)), this, SLOT(mainTabChanged(int)) );
+
     QLabel* welcome_label = new QLabel( "\n  Welcome to SimulationCraft!\n\n  Help text will be here.\n" );
     welcome_label->setAlignment( Qt::AlignLeft|Qt::AlignTop );
-    tab_widget->addTab( welcome_label, "Welcome" );
+    main_tab->addTab( welcome_label, "Welcome" );
 
-    QLabel* globals_label = new QLabel( "\n Global options here: Region, Patch, Iterations, MaxTime, Latency, FightStyle" );
-    globals_label->setAlignment( Qt::AlignLeft|Qt::AlignTop );
-    tab_widget->addTab( globals_label, "Globals" );
+    QFormLayout* globals_layout = new QFormLayout();
+    globals_layout->addRow( "Region", region_choice = create_choice( 2, "US", "EU" ) );
+    globals_layout->addRow( "Patch", patch_choice = create_choice( 2, "3.2.0", "3.2.2" ) );
+    globals_layout->addRow( "Latency", latency_choice = create_choice( 2, "Low", "High" ) );
+    globals_layout->addRow( "Iterations", iterations_choice = create_choice( 3, "100", "1000", "10000" ) );
+    globals_layout->addRow( "Length (sec)", fight_length_choice = create_choice( 3, "100", "300", "500" ) );
+    globals_layout->addRow( "Fight Style", fight_style_choice = create_choice( 2, "Patchwerk", "Helter Skelter" ) );
+    globals_layout->addRow( "Scale Factors", scale_factors_choice = create_choice( 2, "No", "Yes" ) );
+    globals_layout->addRow( "Threads", threads_choice = create_choice( 4, "1", "2", "4", "8" ) );
+    iterations_choice->setCurrentIndex( 1 );
+    fight_length_choice->setCurrentIndex( 1 );
+    threads_choice->setCurrentIndex( 1 );
+    QGroupBox* globals_groupbox = new QGroupBox( "Global Options" );
+    globals_groupbox->setLayout( globals_layout );
+    main_tab->addTab( globals_groupbox, "Globals" );
+
+    import_tab = new QTabWidget();
+    connect( import_tab, SIGNAL(currentChanged(int)), this, SLOT(importTabChanged(int)) );
+    main_tab->addTab( import_tab, "Import" );
 
     armory_view = new QWebView();
     armory_view->setUrl( QUrl( "http://www.wowarmory.com" ) );
-    tab_widget->addTab( armory_view, "Armory" );
+    import_tab->addTab( armory_view, "Armory" );
 
     wowhead_view = new QWebView();
     wowhead_view->setUrl( QUrl( "http://www.wowhead.com/?profiles" ) );
-    tab_widget->addTab( wowhead_view, "Wowhead" );
+    import_tab->addTab( wowhead_view, "Wowhead" );
 
     chardev_view = new QWebView();
     chardev_view->setUrl( QUrl( "http://www.chardev.org" ) );
-    tab_widget->addTab( chardev_view, "CharDev" );
+    import_tab->addTab( chardev_view, "CharDev" );
 
     warcrafter_view = new QWebView();
     warcrafter_view->setUrl( QUrl( "http://www.warcrafter.net" ) );
-    tab_widget->addTab( warcrafter_view, "Warcrafter" );
+    import_tab->addTab( warcrafter_view, "Warcrafter" );
 
-    script_text = new QPlainTextEdit();
-    script_text->document()->setPlainText( "# Profiles will be downloaded into here.  Right-Click menu will Open/Save scripts." );
-    tab_widget->addTab( script_text, "Script" );
+    simulate_text = new QPlainTextEdit();
+    simulate_text->document()->setPlainText( "# Profiles will be downloaded into here.  Right-Click menu will Open/Save scripts." );
+    main_tab->addTab( simulate_text, "Simulate" );
 
     overrides_text = new QPlainTextEdit();
     overrides_text->document()->setPlainText( "# Examples of all available global and player parms will shown here." );
-    tab_widget->addTab( overrides_text, "Overrides" );
+    main_tab->addTab( overrides_text, "Overrides" );
 
     log_text = new QPlainTextEdit();
     log_text->setReadOnly(true);
     log_text->document()->setPlainText( "Standard-Out will end up here....." );
-    tab_widget->addTab( log_text, "Log" );
+    main_tab->addTab( log_text, "Log" );
+
+    results_tab = new QTabWidget();
+    results_tab->setTabsClosable( true );
+    main_tab->addTab( results_tab, "Results" );
 
     QVBoxLayout* v_layout = new QVBoxLayout();
-    v_layout->addWidget( tab_widget );
-    v_layout->addWidget( h_groupbox );
+    v_layout->addWidget( main_tab );
+    v_layout->addWidget( cmd_line_groupbox );
     setLayout( v_layout );
+
+    timer = new QTimer();
+    connect( timer, SIGNAL(timeout()), this, SLOT(updateProgress()) );
 }
 
 MainWindow::~MainWindow()
 {
-
+    exit(0);
 }
--- qt/mainwindow.h
+++ qt/mainwindow.h
@@ -4,25 +4,45 @@
 #include <QtGui/QWidget>
 #include <simcraft.h>
 
+class QComboBox;
 class QLineEdit;
 class QPlainTextEdit;
+class QProgressBar;
 class QPushButton;
 class QTabWidget;
+class QTimer;
 class QWebView;
 
 class MainWindow : public QWidget
 {
     Q_OBJECT
-    QTabWidget* tab_widget;
+    QTabWidget* main_tab;
+    QComboBox* region_choice;
+    QComboBox* patch_choice;
+    QComboBox* latency_choice;
+    QComboBox* iterations_choice;
+    QComboBox* fight_length_choice;
+    QComboBox* fight_style_choice;
+    QComboBox* scale_factors_choice;
+    QComboBox* threads_choice;
+    QTabWidget* import_tab;
     QWebView* armory_view;
     QWebView* wowhead_view;
     QWebView* chardev_view;
     QWebView* warcrafter_view;
-    QPlainTextEdit* script_text;
+    QPlainTextEdit* simulate_text;
     QPlainTextEdit* overrides_text;
     QPlainTextEdit* log_text;
+    QTabWidget* results_tab;
     QLineEdit* cmd_line;
+    QProgressBar* progress_bar;
     QPushButton* main_button;
+    QTimer* timer;
+
+private slots:
+    void updateProgress();
+    void mainTabChanged( int index );
+    void importTabChanged( int index );
 
 public:
     MainWindow(QWidget *parent = 0);
