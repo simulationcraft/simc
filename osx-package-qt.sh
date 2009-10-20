0112225d249cf43c9e3b3f8def92b2b0712a41fa
craig.hughes <craig.hughes@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-10-20 19:29:57 +0000

    * Create disk image bundling Qt app, CLI app, profiles, and README * Copy version number from app into disk image volume name * Remove spurious osx-package-qt.sh script
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@3603 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- osx-package-qt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/bin/mkdir $PROJECT_DIR/simcqt.app/Contents/Frameworks
-/bin/cp -a /Library/Frameworks/Sparkle.framework $PROJECT_DIR/simcqt.app/Contents/Frameworks
-/usr/bin/macdeployqt $PROJECT_DIR/simcqt.app -dmg
