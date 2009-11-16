92dae8b23c93a56609d8f9d0233f49ae7baecbfd
msjogren <msjogren@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-11-16 22:51:31 +0000

    Ugly workaround for double partial resists penalty on chimera-serpent
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@3803 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_hunter.cpp
+++ engine/sc_hunter.cpp
@@ -2126,7 +2126,11 @@ struct chimera_shot_t : public hunter_attack_t
       sting -> refresh_duration();
       sting -> result = RESULT_HIT;
 
+      // ugly ugly hack to get rid of double penalty from partial resists
+      bool binary = sting -> binary;
+      sting -> binary = true;
       double sting_dmg = sting -> num_ticks * sting -> calculate_tick_damage();
+      sting -> binary = binary;
 
       if ( p -> active_serpent_sting )
       {
