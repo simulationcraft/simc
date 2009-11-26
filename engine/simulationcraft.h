cbd0d444d30fdab806079715672b3f09d3978703
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-11-26 20:54:53 +0000

    tick release to get paste ill-fated number-10
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@3997 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/simulationcraft.h
+++ engine/simulationcraft.h
@@ -87,7 +87,7 @@ struct patch_t
 };
 
 #define SC_MAJOR_VERSION "322"
-#define SC_MINOR_VERSION "10"
+#define SC_MINOR_VERSION "11"
 
 // Forward Declarations ======================================================
 
