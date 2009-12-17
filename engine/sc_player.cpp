b4160eba7cb11e0c0be017a091d838d13e7d1434
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-12-17 14:39:21 +0000

    quote origin in generated profile to handle spaces in dir/file names
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@4137 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_player.cpp
+++ engine/sc_player.cpp
@@ -3792,7 +3792,7 @@ bool player_t::create_profile( std::string& profile_str, int save_type )
   if ( save_type == SAVE_ALL )
   {
     profile_str += util_t::player_type_string( type ); profile_str += "=" + name_str + "\n";
-    profile_str += "origin=" + origin_str + "\n";
+    profile_str += "origin=\"" + origin_str + "\"\n";
     profile_str += "level=" + util_t::to_string( level ) + "\n";
     profile_str += "race=" + race_str + "\n";
 
