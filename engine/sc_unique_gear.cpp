4abd5a9d27ee31cc3ebc17d48931ad3f260b13e6
chip.turner <chip.turner@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2009-12-31 03:06:46 +0000

    format with astyle --style=ansi --indent=spaces=2 -O -o -D  sc_unique_gear.cpp
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@4173 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_unique_gear.cpp
+++ engine/sc_unique_gear.cpp
@@ -15,9 +15,9 @@ struct stat_proc_callback_t : public action_callback_t
   double tick;
   stat_buff_t* buff;
 
-  stat_proc_callback_t( const std::string& n, player_t* p, int s, int max_stacks, double a, 
-			double proc_chance, double duration, double cooldown, 
-			double t=0, bool reverse=false, int rng_type=RNG_DEFAULT ) :
+  stat_proc_callback_t( const std::string& n, player_t* p, int s, int max_stacks, double a,
+                        double proc_chance, double duration, double cooldown,
+                        double t=0, bool reverse=false, int rng_type=RNG_DEFAULT ) :
       action_callback_t( p -> sim, p ),
       name_str( n ), stat( s ), amount( a ), tick( t )
   {
@@ -58,14 +58,14 @@ struct stat_proc_callback_t : public action_callback_t
           {
             stat_buff_t* b = callback -> buff;
             if ( b -> current_stack > 0 &&
-		 b -> current_stack < b -> max_stack )
+                 b -> current_stack < b -> max_stack )
             {
               b -> bump();
               new ( sim ) tick_stack_t( sim, player, callback );
             }
           }
         };
-        
+
         new ( sim ) tick_stack_t( sim, a -> player, this );
       }
     }
@@ -93,7 +93,7 @@ struct discharge_proc_callback_t : public action_callback_t
     struct discharge_spell_t : public spell_t
     {
       discharge_spell_t( const char* n, player_t* p, double min, double max, int s ) :
-        spell_t( n, p, RESOURCE_NONE, ( s == SCHOOL_DRAIN ) ? SCHOOL_SHADOW : s )
+          spell_t( n, p, RESOURCE_NONE, ( s == SCHOOL_DRAIN ) ? SCHOOL_SHADOW : s )
       {
         trigger_gcd = 0;
         base_dd_min = min;
@@ -173,7 +173,7 @@ static void register_black_bruise( item_t* item )
   struct black_bruise_trigger_t : public action_callback_t
   {
     buff_t* buff;
-    black_bruise_trigger_t( player_t* p, buff_t* b ) : action_callback_t( p -> sim, p ), buff(b) {}
+    black_bruise_trigger_t( player_t* p, buff_t* b ) : action_callback_t( p -> sim, p ), buff( b ) {}
     virtual void trigger( action_t* a )
     {
       // FIXME! Can specials trigger the proc?
@@ -187,7 +187,7 @@ static void register_black_bruise( item_t* item )
   {
     buff_t* buff;
     spell_t* spell;
-    black_bruise_damage_t( player_t* p, buff_t* b, spell_t* s ) : action_callback_t( p -> sim, p ), buff(b), spell(s) {}
+    black_bruise_damage_t( player_t* p, buff_t* b, spell_t* s ) : action_callback_t( p -> sim, p ), buff( b ), spell( s ) {}
     virtual void trigger( action_t* a )
     {
       // FIXME! Can specials trigger the damage?
@@ -248,7 +248,7 @@ static void register_deaths_choice( item_t* item )
   int stat = ( p -> attribute[ ATTR_STRENGTH ] > p -> attribute[ ATTR_AGILITY ] ) ? STAT_STRENGTH : STAT_AGILITY;
 
   action_callback_t* cb = new stat_proc_callback_t( item -> name(), p, stat, 1, value, 0.35, 15.0, 45.0 );
-  
+
   p -> register_tick_damage_callback( cb );
   p -> register_direct_damage_callback( cb );
 }
@@ -267,7 +267,7 @@ static void register_deathbringers_will( item_t* item )
     rng_t* rng;
 
     deathbringers_will_callback_t( player_t* p, bool h ) :
-      stat_proc_callback_t( "deathbringers_will", p, STAT_STRENGTH, 1, 600, 0.35, 30.0, 105.0 ), heroic(h) 
+        stat_proc_callback_t( "deathbringers_will", p, STAT_STRENGTH, 1, 600, 0.35, 30.0, 105.0 ), heroic( h )
     {
       rng = p -> get_rng( "deathbringers_will_stat" );
     }
@@ -286,7 +286,7 @@ static void register_deathbringers_will( item_t* item )
 
       int* stats=0;
 
-      switch( a -> player -> type )
+      switch ( a -> player -> type )
       {
       case DEATH_KNIGHT: stats = death_knight_stats; break;
       case DRUID:        stats =        druid_stats; break;
@@ -299,15 +299,15 @@ static void register_deathbringers_will( item_t* item )
 
       if ( ! stats ) return;
 
-      buff -> stat = stats[ (int) ( rng -> range( 0.0, 2.999 ) ) ];
+      buff -> stat = stats[ ( int ) ( rng -> range( 0.0, 2.999 ) ) ];
 
       if ( buff -> stat == STAT_ATTACK_POWER )
       {
-	buff -> amount = heroic ? 1400 : 1200;
+        buff -> amount = heroic ? 1400 : 1200;
       }
       else
       {
-	buff -> amount = heroic ? 700 : 600;
+        buff -> amount = heroic ? 700 : 600;
       }
 
       stat_proc_callback_t::trigger( a );
@@ -330,7 +330,7 @@ static void register_empowered_deathbringer( item_t* item )
   struct deathbringer_spell_t : public spell_t
   {
     deathbringer_spell_t( player_t* p ) :
-      spell_t( "empowered_deathbringer", p, RESOURCE_NONE, SCHOOL_SHADOW )
+        spell_t( "empowered_deathbringer", p, RESOURCE_NONE, SCHOOL_SHADOW )
     {
       trigger_gcd = 0;
       background  = true;
@@ -354,7 +354,7 @@ static void register_empowered_deathbringer( item_t* item )
     rng_t* rng;
 
     deathbringer_callback_t( player_t* p, spell_t* s ) :
-      action_callback_t( p -> sim, p ), spell( s )
+        action_callback_t( p -> sim, p ), spell( s )
     {
       rng  = p -> get_rng ( "empowered_deathbringer", RNG_DEFAULT );
     }
@@ -382,7 +382,7 @@ static void register_nibelung( item_t* item )
   struct nibelung_spell_t : public spell_t
   {
     nibelung_spell_t( player_t* p ) :
-      spell_t( "valkyr_smite", p, RESOURCE_NONE, SCHOOL_HOLY )
+        spell_t( "valkyr_smite", p, RESOURCE_NONE, SCHOOL_HOLY )
     {
       trigger_gcd = 0;
       background  = true;
@@ -400,7 +400,7 @@ static void register_nibelung( item_t* item )
     spell_t* spell;
     int remaining;
 
-    nibelung_event_t( sim_t* sim, player_t* p, spell_t* s, int r=0 ) : event_t( sim, p ), spell(s), remaining(r)
+    nibelung_event_t( sim_t* sim, player_t* p, spell_t* s, int r=0 ) : event_t( sim, p ), spell( s ), remaining( r )
     {
       name = "nibelung";
       // Valkyr gets off about 16 casts in 30sec
@@ -413,7 +413,7 @@ static void register_nibelung( item_t* item )
       if ( --remaining > 0 ) new ( sim ) nibelung_event_t( sim, player, spell, remaining );
     }
   };
-	
+
   struct nibelung_callback_t : public action_callback_t
   {
     spell_t* spell;
@@ -421,7 +421,7 @@ static void register_nibelung( item_t* item )
     rng_t* rng;
 
     nibelung_callback_t( player_t* p, spell_t* s ) :
-      action_callback_t( p -> sim, p ), spell( s )
+        action_callback_t( p -> sim, p ), spell( s )
     {
       proc = p -> get_proc( "nibelung" );
       rng  = p -> get_rng ( "nibelung" );
@@ -430,17 +430,17 @@ static void register_nibelung( item_t* item )
     virtual void trigger( action_t* a )
     {
       if (   a -> aoe      ||
-	     a -> proc     ||
-	     a -> dual     ||
-	   ! a -> harmful  ||
-	     a -> pseudo_pet)
-	return;
+             a -> proc     ||
+             a -> dual     ||
+             ! a -> harmful  ||
+             a -> pseudo_pet )
+        return;
 
       if ( rng -> roll( 0.01 ) )
       {
-	if ( sim -> log ) log_t::output( sim, "%s summons a Valkyr from the Halls of Valhalla", a -> player -> name() );
-	new ( sim ) nibelung_event_t( sim, a -> player, spell );
-	proc -> occur();
+        if ( sim -> log ) log_t::output( sim, "%s summons a Valkyr from the Halls of Valhalla", a -> player -> name() );
+        new ( sim ) nibelung_event_t( sim, a -> player, spell );
+        proc -> occur();
       }
     }
   };
@@ -481,8 +481,8 @@ static void register_shadowmourne( item_t* item )
     buff_t* buff;
     spell_t* spell;
     int slot;
-    
-    shadowmourne_trigger_t( player_t* p, buff_t* b, spell_t* sp, int s ) : action_callback_t( p -> sim, p ), buff(b), spell(sp), slot(s) {}
+
+    shadowmourne_trigger_t( player_t* p, buff_t* b, spell_t* sp, int s ) : action_callback_t( p -> sim, p ), buff( b ), spell( sp ), slot( s ) {}
     virtual void trigger( action_t* a )
     {
       // FIXME! Can specials trigger the proc?
@@ -544,18 +544,18 @@ void unique_gear_t::init( player_t* p )
 // ==========================================================================
 
 action_callback_t* unique_gear_t::register_stat_proc( int                type,
-                                                      int                mask,
-                                                      const std::string& name,
-                                                      player_t*          player,
-                                                      int                stat,
-                                                      int                max_stacks,
-                                                      double             amount,
-                                                      double             proc_chance,
-                                                      double             duration,
-                                                      double             cooldown,
-                                                      double             tick,
-						      bool               reverse,
-                                                      int                rng_type )
+    int                mask,
+    const std::string& name,
+    player_t*          player,
+    int                stat,
+    int                max_stacks,
+    double             amount,
+    double             proc_chance,
+    double             duration,
+    double             cooldown,
+    double             tick,
+    bool               reverse,
+    int                rng_type )
 {
   action_callback_t* cb = new stat_proc_callback_t( name, player, stat, max_stacks, amount, proc_chance, duration, cooldown, tick, reverse, rng_type );
 
@@ -593,16 +593,16 @@ action_callback_t* unique_gear_t::register_stat_proc( int                type,
 // ==========================================================================
 
 action_callback_t* unique_gear_t::register_discharge_proc( int                type,
-                                                           int                mask,
-                                                           const std::string& name,
-                                                           player_t*          player,
-                                                           int                max_stacks,
-                                                           int                school,
-                                                           double             min_dmg,
-                                                           double             max_dmg,
-                                                           double             proc_chance,
-                                                           double             cooldown,
-                                                           int                rng_type )
+    int                mask,
+    const std::string& name,
+    player_t*          player,
+    int                max_stacks,
+    int                school,
+    double             min_dmg,
+    double             max_dmg,
+    double             proc_chance,
+    double             cooldown,
+    int                rng_type )
 {
   action_callback_t* cb = new discharge_proc_callback_t( name, player, max_stacks, school, min_dmg, max_dmg, proc_chance, cooldown, rng_type );
 
@@ -638,8 +638,8 @@ action_callback_t* unique_gear_t::register_discharge_proc( int                ty
 // unique_gear_t::register_stat_proc
 // ==========================================================================
 
-action_callback_t* unique_gear_t::register_stat_proc( item_t& i, 
-						      item_t::special_effect_t& e )
+action_callback_t* unique_gear_t::register_stat_proc( item_t& i,
+    item_t::special_effect_t& e )
 {
   const char* name = e.name_str.empty() ? i.name() : e.name_str.c_str();
 
@@ -650,8 +650,8 @@ action_callback_t* unique_gear_t::register_stat_proc( item_t& i,
 // unique_gear_t::register_discharge_proc
 // ==========================================================================
 
-action_callback_t* unique_gear_t::register_discharge_proc( item_t& i, 
-							   item_t::special_effect_t& e )
+action_callback_t* unique_gear_t::register_discharge_proc( item_t& i,
+    item_t::special_effect_t& e )
 {
   const char* name = e.name_str.empty() ? i.name() : e.name_str.c_str();
 
@@ -729,7 +729,7 @@ bool unique_gear_t::get_equip_encoding( std::string&       encoding,
 
   // Enchants
   else if ( name == "lightweave"            ) e = "OnSpellHit_295SP_35%_15Dur_60Cd";  // temporary for backwards compatibility
-  else if ( name == "lightweave_embroidery" ) e = "OnSpellHit_295SP_35%_15Dur_60Cd";  
+  else if ( name == "lightweave_embroidery" ) e = "OnSpellHit_295SP_35%_15Dur_60Cd";
   else if ( name == "darkglow_embroidery"   ) e = "OnSpellHit_400Mana_35%_60Cd";
   else if ( name == "swordguard_embroidery" ) e = "OnAttackHit_400AP_25%_60Cd";
 
@@ -750,12 +750,12 @@ bool unique_gear_t::get_equip_encoding( std::string&       encoding,
 // ==========================================================================
 
 bool unique_gear_t::get_hidden_encoding( std::string&       encoding,
-                                         const std::string& name,
-                                         const std::string& id )
+    const std::string& name,
+    const std::string& id )
 {
   std::string e;
   if      ( name == "lightweave"                 ) e = "1spi";
-  else if ( name == "lightweave_embroidery"      ) e = "1spi";  
+  else if ( name == "lightweave_embroidery"      ) e = "1spi";
   else if ( name == "darkglow_embroidery"        ) e = "1spi";
 
   if ( e.empty() ) return false;
