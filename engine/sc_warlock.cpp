2a72a46fe2457b57ede5f853cb41c086b4fb195f
jon.valvatne@gmail.com <jon.valvatne@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-03-24 18:29:32 +0000

    Update life tap mechanics to how they actually work on live after the patch (mana=2000+0.5*SP).
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@4397 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -2947,9 +2947,9 @@ struct life_tap_t : public warlock_spell_t
     harmful = false;
 
     // FIXME!!! Need to test new base mana at levels below 80
-    base_tap = util_t::ability_rank( player -> level,  2400,80,  710,68,  500,0 );
+    base_tap = util_t::ability_rank( player -> level,  2000,80,  710,68,  500,0 );
 
-    direct_power_mod = 0.6;
+    direct_power_mod = 0.5;
   }
 
   virtual void execute()
