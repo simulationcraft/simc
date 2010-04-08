128134384d27fb4d341e77edfb7de355ee8aef58
Twigele <Twigele@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-04-08 03:05:55 +0000

    Unique Gear   EotBM: OnSpellDamage, not OnSpellCast
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@4479 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_unique_gear.cpp
+++ engine/sc_unique_gear.cpp
@@ -888,7 +888,7 @@ bool unique_gear_t::get_equip_encoding( std::string&       encoding,
   else if ( name == "elemental_focus_stone"               ) e = "OnSpellCast_522Haste_10%_10Dur_45Cd";
   else if ( name == "embrace_of_the_spider"               ) e = "OnSpellCast_505Haste_10%_10Dur_45Cd";
   else if ( name == "eye_of_magtheridon"                  ) e = "OnSpellCastMiss_170SP_10Dur";
-  else if ( name == "eye_of_the_broodmother"              ) e = "OnSpellCast_25SP_5Stack_10Dur";
+  else if ( name == "eye_of_the_broodmother"              ) e = "OnSpellDamage_25SP_5Stack_10Dur";
   else if ( name == "flare_of_the_heavens"                ) e = "OnSpellCast_850SP_10%_10Dur_45Cd";
   else if ( name == "forge_ember"                         ) e = "OnSpellCastHit_512SP_10%_10Dur_45Cd";
   else if ( name == "fury_of_the_five_flights"            ) e = "OnAttackHit_16AP_20Stack_10Dur";
