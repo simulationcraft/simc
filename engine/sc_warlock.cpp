501f502738616a08c62dfa2cce359a21b49aa767
philoptik@gmail.com <philoptik@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-10-16 10:29:48 +0000

    Cata Warlock: migrated many buffs to the new constructors
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@5146 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -4163,21 +4163,22 @@ void warlock_t::init_buffs()
 {
   player_t::init_buffs();
 
-  buffs_backdraft             = new buff_t( this, "backdraft",             3, 15.0, 0.0, talent_backdraft -> rank() );
-  buffs_decimation            = new buff_t( this, "decimation",            1,  8.0, 0.0, talent_decimation -> rank() );
+  buffs_backdraft             = new buff_t( this, talent_backdraft -> effect_trigger_spell( 1 ), "backdraft" );
+  buffs_decimation            = new buff_t( this, talent_decimation -> effect_trigger_spell( 1 ), "decimation" );
   buffs_demonic_empowerment   = new buff_t( this, "demonic_empowerment",   1 );
   buffs_empowered_imp         = new buff_t( this, 47283, "empowered_imp", talent_empowered_imp -> effect_base_value( 1 ) / 100.0 );
-  buffs_eradication           = new buff_t( this, "eradication",           1, 10.0, 0.0, talent_eradication -> rank() ? 0.06 : 0.00 );
-  buffs_haunted               = new buff_t( this, "haunted",               1, 12.0, 0.0, talent_haunt -> rank() );
-  buffs_metamorphosis         = new buff_t( this, "metamorphosis",         1, 30.0 + glyphs.metamorphosis -> value() / 1000.0, 0.0, talent_metamorphosis -> rank() );
-  buffs_molten_core           = new buff_t( this, "molten_core",           3, 15.0, 0.0, talent_molten_core -> rank() * 0.02 );
-  buffs_shadow_embrace        = new buff_t( this, "shadow_embrace",        3, 12.0, 0.0, talent_shadow_embrace -> rank() );
-  buffs_shadow_trance         = new buff_t( this, "shadow_trance",         1,  0.0, 0.0, talent_nightfall -> proc_chance() +  glyphs.corruption -> value() / 100.0 );
+  buffs_eradication           = new buff_t( this, talent_eradication -> effect_trigger_spell( 1 ), "eradication", talent_eradication -> proc_chance() );
+  buffs_haunted               = new buff_t( this, talent_haunt -> spell_id(), "haunted", talent_haunt -> rank() );
+  buffs_metamorphosis         = new buff_t( this, 47241, "metamorphosis", talent_metamorphosis -> rank() );
+  buffs_metamorphosis -> buff_duration += glyphs.metamorphosis -> value() / 1000.0;
+  buffs_molten_core           = new buff_t( this, talent_molten_core -> effect_trigger_spell( 1 ), "molten_core", talent_molten_core -> rank() * 0.02 );
+  buffs_shadow_embrace        = new buff_t( this, talent_shadow_embrace -> effect_trigger_spell( 1 ), "shadow_embrace", talent_shadow_embrace -> rank() );
+  buffs_shadow_trance         = new buff_t( this, 17941, "shadow_trance", talent_nightfall -> proc_chance() +  glyphs.corruption -> value() / 100.0 );
   buffs_hand_of_guldan        = new buff_t( this, "hand_of_guldan",        1, 15.0, 0.0, talent_hand_of_guldan -> rank() );
-  buffs_improved_soul_fire    = new buff_t( this, "improved_soul_fire",    1, 15.0, 0.0, (talent_improved_soul_fire -> rank() > 0) );
-  buffs_soulburn              = new buff_t( this, "soulburn",              1, 15.0 );
-  buffs_demon_soul            = new buff_t( this, "demon_soul",            3, 30.0 );
-  buffs_bane_of_havoc         = new buff_t( this, "bane_of_havoc",         1, 300.0 );
+  buffs_improved_soul_fire    = new buff_t( this, 85383, "improved_soul_fire", (talent_improved_soul_fire -> rank() > 0) );
+  buffs_soulburn              = new buff_t( this, 74434, "soulburn" );
+  buffs_demon_soul            = new buff_t( this, 77801, "demon_soul" );
+  buffs_bane_of_havoc         = new buff_t( this, 80240, "bane_of_havoc" );
   buffs_searing_pain_soulburn = new buff_t( this, "searing_pain_soulburn", 1, 6.0 );
 
   // using the new constructors
