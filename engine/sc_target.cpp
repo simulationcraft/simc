c544f3d3e006f02b907d2d5bbabaaa2baf2abcfc
philoptik@gmail.com <philoptik@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-10-30 07:09:02 +0000

    Target Level: 88 for P403, 83 for !P403
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@5285 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_target.cpp
+++ engine/sc_target.cpp
@@ -12,7 +12,7 @@
 // target_t::target_t =======================================================
 
 target_t::target_t( sim_t* s, const std::string& n ) :
-    sim( s ), name_str( n ), next( 0 ), race( RACE_HUMANOID ), level( 88 ),
+    sim( s ), name_str( n ), next( 0 ), race( RACE_HUMANOID ), level( s -> P403?88:83 ),
     initial_armor( -1 ), armor( 0 ), block_value( 100 ),
     attack_speed( 2.0 ), attack_damage( 2000 ), weapon_skill( 0 ),
     fixed_health( 0 ), initial_health( 0 ), current_health( 0 ), total_dmg( 0 ),
