938ebb2dc288b8a332813dc1bc0e0f99bb76431b
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-11-18 00:08:54 +0000

    compiler fix for sc_enchant.cpp related to default args
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@5519 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_enchant.cpp
+++ engine/sc_enchant.cpp
@@ -614,7 +614,7 @@ struct weapon_discharge_proc_callback_t : public action_callback_t
   proc_t* proc;
   rng_t* rng;
 
-  weapon_discharge_proc_callback_t( const std::string& n, player_t* p, weapon_t* w, int ms, const school_type school, double dmg, double fc, double ppm=0, double cd, int rng_type=RNG_DEFAULT ) :
+  weapon_discharge_proc_callback_t( const std::string& n, player_t* p, weapon_t* w, int ms, const school_type school, double dmg, double fc, double ppm=0, double cd=0, int rng_type=RNG_DEFAULT ) :
       action_callback_t( p -> sim, p ),
       name_str( n ), weapon( w ), stacks( 0 ), max_stacks( ms ), fixed_chance( fc ), PPM( ppm ), cooldown( cd ), cooldown_ready( 0 )
   {
