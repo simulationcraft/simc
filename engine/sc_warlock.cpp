c3beda6197757e180ac70e9f3346d1d10103ec1f
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-12-01 12:51:54 +0000

    commit nasty hacky fix just to get people running
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@5826 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -2681,10 +2681,11 @@ struct conflagrate_t : public warlock_spell_t
 
     double periodic_dmg = a -> base_td + a -> total_power() * a -> tick_power_mod;
 
-    base_dd_min = base_dd_max = periodic_dmg * a -> num_ticks * effect_base_value( 2 ) / 100.0 ;
+    int periodic_ticks = p -> dots_immolate -> num_ticks - p -> dots_immolate -> added_ticks;
+
+    base_dd_min = base_dd_max = periodic_dmg * periodic_ticks * effect_base_value( 2 ) / 100.0 ;
 
     warlock_spell_t::execute();
-
   }
 
   virtual void travel( int travel_result, double travel_dmg)
