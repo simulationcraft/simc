b151373560ec1e2ba04729586c2b724d08adc0fc
jon.valvatne@gmail.com <jon.valvatne@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2010-12-21 08:05:35 +0000

    Update imp firebolt coefficient to tested in-game value (it's been ninja-fixed to reflect the tooltip).
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6093 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -1237,7 +1237,7 @@ struct imp_pet_t : public warlock_main_pet_t
     {
       warlock_t*  o = player -> cast_pet() -> owner -> cast_warlock();
       base_multiplier *= 1.0 + ( o -> glyphs.imp -> base_value() );
-      direct_power_mod = 0.619; // seems to be 0.00 in DBC now?
+      direct_power_mod = 0.657; // tested in-game as of 2010/21/12
       base_execute_time += o -> talent_dark_arts -> effect_base_value( 1 ) / 1000.0;
       if ( o -> bugs ) min_gcd = 1.5;
     }
