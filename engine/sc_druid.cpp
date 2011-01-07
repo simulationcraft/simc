cc33cd65926808275056712860e14e05398f0313
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-01-07 21:50:43 +0000

    Druid:   Treants - Base Crit is 5%, still benefits from debuffs
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6210 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_druid.cpp
+++ engine/sc_druid.cpp
@@ -541,13 +541,6 @@ struct treants_pet_t : public pet_t
       // Model the three Treants as one actor hitting 3x hard
       base_multiplier *= 3.0;
     }
-    
-    virtual void player_buff()
-    {
-      attack_t::player_buff();
-      // Trees have 5% crit
-      player_crit = .05;
-    }
   };
 
   melee_t* melee;
@@ -572,6 +565,7 @@ struct treants_pet_t : public pet_t
     attribute_base[ ATTR_INTELLECT ] = 65;
     attribute_base[ ATTR_SPIRIT    ] = 109;
 
+    base_attack_crit = .05;
     base_attack_power = -20;
     initial_attack_power_per_strength = 2.0;
 
