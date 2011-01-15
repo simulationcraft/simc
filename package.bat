dc5e0f55242993d128245e65b558aec5176fb1a8
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-01-15 03:40:02 +0000

    REALLY share my packaging script
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6303 3b4652a1-8050-0410-ac47-3d40261b0f8b

---
+++ package.bat
@@ -0,0 +1,16 @@
+
+set dir=simc-403-22-win32
+set rev=r6289
+
+svn checkout https://simulationcraft.googlecode.com/svn/branches/cataclysm/ %dir% --username natehieter --revision %rev%
+
+mkdir %dir%\imageformats
+
+xcopy *.dll %dir%
+xcopy /I imageformats %dir%\imageformats
+xcopy vcredist_x86.exe %dir%
+xcopy Win32OpenSSL_Light-0_9_8m.exe %dir%
+
+xcopy simc.exe %dir%
+xcopy SimulationCraft.exe %dir%
+
