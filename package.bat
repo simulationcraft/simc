92cc2fea0850fc175a333f8258defe0a89e4f54f
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-01-18 19:14:46 +0000

    packing script updated....  should have included in minor version tick commit
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6348 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- package.bat
+++ package.bat
@@ -2,8 +2,8 @@
 REM This script is used for packaging a release built using MSVC 2008
 REM Update the "dir" and "rev" variables appropriately
 
-set dir=simc-403-22-win32
-set rev=r6289
+set dir=simc-403-24-win32
+set rev=r6347
 
 svn checkout https://simulationcraft.googlecode.com/svn/branches/cataclysm/ %dir% --username natehieter --revision %rev%
 
