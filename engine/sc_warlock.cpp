87bed1bc16fed99aed44c95a03bf0c509d9ff8a2
jon.valvatne@gmail.com <jon.valvatne@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-01-19 13:28:17 +0000

    Don't ever decrement demon soul imp under PTR mechanics.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6365 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -871,7 +871,7 @@ struct warlock_spell_t : public spell_t
 
     spell_t::player_buff();
 
-    if ( p -> buffs_demon_soul_imp -> up() && execute_time() > 0 && s_tree == WARLOCK_DESTRUCTION )
+    if ( execute_time() > 0 && s_tree == WARLOCK_DESTRUCTION && p -> buffs_demon_soul_imp -> up() )
     {
       if ( p -> ptr )
         player_crit += p -> buffs_demon_soul_imp -> effect_base_value( 1 ) / 100.0;
@@ -919,7 +919,7 @@ struct warlock_spell_t : public spell_t
   {
     warlock_t* p = player -> cast_warlock();
     spell_t::execute();
-    if ( p -> buffs_demon_soul_imp -> up() && execute_time() > 0 && s_tree == WARLOCK_DESTRUCTION )
+    if ( ! p -> ptr && p -> buffs_demon_soul_imp -> check() && execute_time() > 0 && s_tree == WARLOCK_DESTRUCTION )
     {
       p -> buffs_demon_soul_imp -> decrement();
     }
