35f16612e5783db20027e62467bd59d45379eb94
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-01-28 18:39:05 +0000

    tick version ahead of release
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/cataclysm@6581 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- package.bat
+++ package.bat
@@ -2,8 +2,8 @@
 REM This script is used for packaging a release built using MSVC 2008
 REM Update the "dir" and "rev" variables appropriately
 
-set dir=simc-403-28-win32
-set rev=r6568
+set dir=simc-403-29-win32
+set rev=r6581
 
 svn checkout https://simulationcraft.googlecode.com/svn/branches/cataclysm/ %dir% --username natehieter --revision %rev%
 
