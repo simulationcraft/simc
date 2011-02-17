afcfbcb9b04d2d00f0326a62d82f3c971880be56
natehieter <natehieter@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-02-17 19:21:03 +0000

    update package for v5
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@7366 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- package.bat
+++ package.bat
@@ -2,8 +2,8 @@
 REM This script is used for packaging a release built using MSVC 2008
 REM Update the "dir" and "rev" variables appropriately
 
-set dir=simc-406-1-win32
-set rev=r6937
+set dir=simc-406-6-win32
+set rev=r7366
 
 svn checkout https://simulationcraft.googlecode.com/svn/trunk/ %dir% --username natehieter --revision %rev%
 
