154b6fc96bc2ad320b851f0184fb0e56bb6db3da
wrd-lists@manoutoftime.org <wrd-lists@manoutoftime.org@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-02-21 19:38:07 +0000

    Generate both screen and print styles
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@7588 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- util_scripts/css-to-cpp.py
+++ util_scripts/css-to-cpp.py
@@ -6,31 +6,97 @@ import string
 # Read current styles.css file and write slimmed-down version for inclusion
 # in sc_report.cpp
 
-INFILE = open('../html/css/styles-new.css', 'r')
 OUTFILE = open('cpp-styles.txt', 'w')
 
+OUTFILE.write('  // Styles\n')
+OUTFILE.write('  // If file is being hosted on simulationcraft.org, link to the local\n')
+OUTFILE.write('  // stylesheet; otherwise, embed the styles.\n')
+OUTFILE.write('  if ( sim -> hosted_html )\n')
+OUTFILE.write('  {\n')
+OUTFILE.write('    util_t::fprintf( file,\n')
+OUTFILE.write('      "    <style type=\\"text/css\\" media=\\"screen\\">\\n"\n')
+OUTFILE.write('      "      @import url(\'http://www.simulationcraft.org/css/styles-new.css\');\\n"\n')
+OUTFILE.write('      "    </style>\\n"\n')
+OUTFILE.write('      "    <style type=\\"text/css\\" media=\\"print\\">\\n"\n')
+OUTFILE.write('      "      @import url(\'http://www.simulationcraft.org/css/styles-print.css\');\\n"\n')
+OUTFILE.write('      "    </style>\\n" );\n')
+OUTFILE.write('  }\n')
+OUTFILE.write('  else if ( sim -> print_styles )\n')
+OUTFILE.write('  {\n')
+OUTFILE.write('    util_t::fprintf( file,\n')
+OUTFILE.write('      "    <style type=\\"text/css\\" media=\\"all\\">\\n"\n')
+
+INFILE = open('../html/css/styles-print.css', 'r')
 lines = []
+comment = False
 for line in INFILE.readlines():
     line = string.strip(line)
     if not line:
+        continue
+    if line[0:2] == '/*':
+        comment = True
+        continue
+    elif line[0:2] == '*/':
+        comment = False
+        continue
+    if comment:
         continue
     line = re.sub('"', '\\"', line)
     lines.append(line)
 line_count = len(lines)
 
-OUTFILE.write('    util_t::fprintf( file,\n')
-new_line = '      "      '
-line_num = 0
+new_line = '      "     '
 
 for line in lines:
-    line_num += 1
     if line == '}':
         new_line = '{0} }}\\n"'.format(new_line)
-        if line_num == line_count:
-            new_line = '{0} );'.format(new_line)
         new_line = '{0}\n'.format(new_line)
         OUTFILE.write(new_line)
-        new_line = '      "      '
+        new_line = '      "     '
         continue
     
     new_line = '{0} {1}'.format(new_line, line)
+
+OUTFILE.write('      "    </style>\\n" );\n')
+INFILE.close()
+
+OUTFILE.write('  }\n')
+OUTFILE.write('  else\n')
+OUTFILE.write('  {\n')
+OUTFILE.write('    util_t::fprintf( file,\n')
+OUTFILE.write('      "    <style type=\\"text/css\\" media=\\"all\\">\\n"\n')
+
+INFILE = open('../html/css/styles-new.css', 'r')
+lines = []
+comment = False
+for line in INFILE.readlines():
+    line = string.strip(line)
+    if not line:
+        continue
+    if line[0:2] == '/*':
+        comment = True
+        continue
+    elif line[0:2] == '*/':
+        comment = False
+        continue
+    if comment:
+        continue
+    line = re.sub('"', '\\"', line)
+    lines.append(line)
+line_count = len(lines)
+
+new_line = '      "     '
+
+for line in lines:
+    if line == '}':
+        new_line = '{0} }}\\n"'.format(new_line)
+        new_line = '{0}\n'.format(new_line)
+        OUTFILE.write(new_line)
+        new_line = '      "     '
+        continue
+    
+    new_line = '{0} {1}'.format(new_line, line)
+
+OUTFILE.write('      "    </style>\\n" );\n')
+OUTFILE.write('  }\n')
+INFILE.close()
