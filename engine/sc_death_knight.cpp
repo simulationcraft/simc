6ecc5270066d12a9185d18c05056406ca27f1fa3
DonQuiche@gmail.com <DonQuiche@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-02-22 13:30:43 +0000

    ebon_plaguebringer_track renamed to ebon_plaguebringer (there is a debuff with the same name but no buff with that name; besides it is "quiet", not reported).
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@7605 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_death_knight.cpp
+++ engine/sc_death_knight.cpp
@@ -4326,7 +4326,7 @@ void death_knight_t::init_buffs()
   buffs_crimson_scourge     = new buff_t( this, 81141, "crimson_scourge", talents.crimson_scourge -> proc_chance() );
   buffs_dancing_rune_weapon = new buff_t( this, "dancing_rune_weapon",                                1,  12.0,  0.0, 1.0, true );
   buffs_dark_transformation = new buff_t( this, "dark_transformation",                                1,  30.0 );
-  buffs_ebon_plaguebringer  = new buff_t( this, "ebon_plaguebringer_track",                           1,  21.0 + talents.epidemic -> mod_additive( P_DURATION ), 0.0, 1.0, true );
+  buffs_ebon_plaguebringer  = new buff_t( this, "ebon_plaguebringer",								  1,  21.0 + talents.epidemic -> mod_additive( P_DURATION ), 0.0, 1.0, true );
   buffs_frost_presence      = new buff_t( this, "frost_presence" );
   buffs_killing_machine     = new buff_t( this, "killing_machine",                                    1,  30.0,  0.0, 0.0 ); // PPM based!
   buffs_pillar_of_frost     = new buff_t( this, "pillar_of_frost",                                    1,  20.0 );
