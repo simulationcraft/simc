6210ad3b8b90a9fc175f02645745685c9d4a58ab
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-03-29 13:52:22 +0000

    Death Knight:   Killing Machine procs from any MH attack, not just MH melee
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8264 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_death_knight.cpp
+++ engine/sc_death_knight.cpp
@@ -1852,6 +1852,14 @@ void death_knight_attack_t::execute()
     if ( school == SCHOOL_FROST || school == SCHOOL_SHADOW )
       if ( ! proc )
         p -> buffs_rune_of_cinderglacier -> decrement();
+
+    if ( weapon -> slot == SLOT_MAIN_HAND )
+    {
+      // TODO: Confirm PPM for ranks 1 and 2 http://elitistjerks.com/f72/t110296-frost_dps_|_cataclysm_4_0_3_nothing_lose/p9/#post1869431
+      double chance = weapon -> proc_chance_on_swing( util_t::talent_rank( p -> talents.killing_machine -> rank(), 3, 1, 3, 5 ) );
+      if ( p -> buffs_killing_machine -> trigger( 1, 1.0, chance ) && p -> ptr )
+        p -> buffs_tier11_4pc_melee -> trigger();
+    }
   }
 }
 
@@ -2083,11 +2091,6 @@ struct melee_t : public death_knight_attack_t
       if ( weapon -> slot == SLOT_MAIN_HAND )
       {
         p -> buffs_sudden_doom -> trigger( 1, -1, weapon -> proc_chance_on_swing( p -> talents.sudden_doom -> rank() ) );
-        // TODO: Confirm PPM for ranks 1/2 http://elitistjerks.com/f72/t110296-frost_dps_|_cataclysm_4_0_3_nothing_lose/p9/#post1869431
-        // FIXME: This needs to happen for every melee strike/auto attack, not just auto attacks.
-        double chance = weapon -> proc_chance_on_swing( util_t::talent_rank( p -> talents.killing_machine -> rank(), 3, 1, 3, 5 ) );
-        if ( p -> buffs_killing_machine -> trigger( 1, 1.0, chance ) && p -> ptr )
-          p -> buffs_tier11_4pc_melee -> trigger();
       }
 
       if ( p -> dots_blood_plague && p -> dots_blood_plague -> ticking )
