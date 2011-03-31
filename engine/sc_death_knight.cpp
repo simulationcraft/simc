5b3913f2971d7e0ea410b5c9f738f3dbcea6bf61
zeroxbit@gmail.com <zeroxbit@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-03-31 22:04:45 +0000

    should have used composite_attack_crit()
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8307 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_death_knight.cpp
+++ engine/sc_death_knight.cpp
@@ -2982,8 +2982,8 @@ struct howling_blast_t : public death_knight_spell_t
   // FIXME: need to verify this for other spells as well (and get definitive confirmation for HB).
   virtual double crit_chance( int delta_level ) SC_CONST
   {
-	death_knight_t* p = player -> cast_death_knight();
-	return p -> buffed_attack_crit - delta_level*0.006; // eww
+    death_knight_t* p = player -> cast_death_knight();
+    return p -> composite_attack_crit() - delta_level*0.006; // eww
   }
 
   virtual double cost() SC_CONST
