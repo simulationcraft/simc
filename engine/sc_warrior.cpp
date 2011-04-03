c2ac416f6d4043011b17bd2e786f8968a0e50714
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-04-03 23:40:33 +0000

    Warrior:   Stance: 4pc bonus doesn't drop on the PTR
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8330 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warrior.cpp
+++ engine/sc_warrior.cpp
@@ -2661,9 +2661,8 @@ struct stance_t : public warrior_spell_t
       case STANCE_DEFENSE:    p -> buffs_defensive_stance -> trigger(); break;
     }
 
-    // Switching stances causes the T11 4pc DPS buff to drop
-    // Unclear if this is intended by Blizz or not, so marked as bugs for now
-    if ( p -> bugs )
+    // Switching stances causes the T11 4pc DPS buff to drop on live
+    if ( ! p -> ptr )
       p -> buffs_tier11_4pc_melee -> expire();
 
     consume_resource();
