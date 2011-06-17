631b6b35f76888a5eede2a0d9fbb9a61ea9d601e
delmortis1@gmail.com <delmortis1@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-06-17 05:51:07 +0000

    On PTR heals crit for *2
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8700 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_heal.cpp
+++ engine/sc_heal.cpp
@@ -32,6 +32,11 @@ void heal_t::_init_heal_t()
 
   stats -> type = STATS_HEAL;
 
+  if ( player -> dbc.ptr )
+  {
+    crit_bonus = 1.0;
+  }
+
   if ( player -> meta_gem == META_REVITALIZING_SHADOWSPIRIT )
   {
     crit_multiplier *= 1.03;
