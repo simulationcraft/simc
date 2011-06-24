f09915b73abdca6e47f3cc3cb1dc20476b635325
jon@valvatne.com <jon@valvatne.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-06-24 14:23:45 +0000

    VPLC procs off periodic crits.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8818 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_unique_gear.cpp
+++ engine/sc_unique_gear.cpp
@@ -2064,7 +2064,7 @@ bool unique_gear_t::get_equip_encoding( std::string&       encoding,
   else if ( name == "bryntroll_the_bone_arbiter"          ) e = ( heroic ? "OnAttackHit_2538Drain_11%" : "OnAttackHit_2250Drain_11%" );
 
   // Variable Stack Discharge Procs
-  else if ( name == "variable_pulse_lightning_capacitor"  ) e = ( heroic ? "OnHarmfulSpellCrit_1269.5Nature_15%_10Stack_2.5Cd_chance" : "OnHarmfulSpellCrit_1125.5Nature_15%_10Stack_2.5Cd_chance" );
+  else if ( name == "variable_pulse_lightning_capacitor"  ) e = ( heroic ? "OnSpellCrit_1269.5Nature_15%_10Stack_2.5Cd_chance" : "OnSpellCrit_1125.5Nature_15%_10Stack_2.5Cd_chance" );
 
   // Some Normal/Heroic items have same name
   else if ( name == "reign_of_the_unliving"               ) e = ( heroic ? "OnSpellDirectCrit_2117Fire_3Stack_2.0Cd" : "OnSpellDirectCrit_1882Fire_3Stack_2.0Cd" );
