acd96e3994e709867222fe4d9f5bed9d15959664
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-06-26 21:07:53 +0000

    Add T11 to GUI BIS list.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@8866 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- qt/sc_window.cpp
+++ qt/sc_window.cpp
@@ -727,8 +727,8 @@ void SimulationCraftWindow::createBestInSlotTab()
   bisTree->setHeaderLabels( headerLabels );
   importTab->addTab( bisTree, "BiS" );
 
-  const int TIER_MAX=1;
-  const char* tierNames[] = { "T12" };
+  const int TIER_MAX=2;
+  const char* tierNames[] = { "T11", "T12" };
   QTreeWidgetItem* rootItems[ PLAYER_MAX ][ TIER_MAX ];
   for( int i=DEATH_KNIGHT; i <= WARRIOR; i++ )
   {
