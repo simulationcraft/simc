9de7b2910ea363d2b2e785dc6919d8bc4e4838fe
philoptik@gmail.com <philoptik@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-08-07 10:33:48 +0000

    Hunter Pet: Change weapon damage numbers, re-add 5% damage multiplier.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9275 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_hunter.cpp
+++ engine/sc_hunter.cpp
@@ -312,14 +312,17 @@ struct hunter_pet_t : public pet_t
       pet_t( sim, owner, pet_name, pt )
   {
     main_hand_weapon.type       = WEAPON_BEAST;
-    main_hand_weapon.min_dmg    = rating_t::interpolate( level, 0, 0, 51, 85 ); // FIXME needs level 60 and 70 values
-    main_hand_weapon.max_dmg    = rating_t::interpolate( level, 0, 0, 78, 134 ); // FIXME needs level 60 and 70 values
+    main_hand_weapon.min_dmg    = rating_t::interpolate( level, 0, 0, 51, 73 ); // FIXME needs level 60 and 70 values
+    main_hand_weapon.max_dmg    = rating_t::interpolate( level, 0, 0, 78, 110 ); // FIXME needs level 60 and 70 values
+    // Level 85 numbers from Rivkah from EJ, 07.08.2011
     main_hand_weapon.damage     = ( main_hand_weapon.min_dmg + main_hand_weapon.max_dmg ) / 2;
     main_hand_weapon.swing_time = 2.0;
 
     stamina_per_owner = 0.45;
 
     health_per_stamina *= 1.05; // 3.1.0 change # Cunning, Ferocity and Tenacity pets now all have +5% damage, +5% armor and +5% health bonuses
+    initial_armor_multiplier *= 1.05;
+
 
     create_talents();
     create_options();
@@ -933,6 +936,9 @@ struct hunter_pet_attack_t : public attack_t
 
     // Assume happy pet
     base_multiplier *= 1.25;
+
+    // 3.1.0 change # Cunning, Ferocity and Tenacity pets now all have +5% damage, +5% armor and +5% health bonuses
+    base_multiplier *= 1.05;
   }
 
   hunter_pet_attack_t( const char* n, hunter_pet_t* player, const char* sname, bool special=true ) :
