7be6dbaf6350002fea9e2663ba7d6edbedb2e4e4
msjogren@gmail.com <msjogren@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-08-11 06:48:05 +0000

    Fix action list to check that zealotry is actually *up*
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9306 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_paladin.cpp
+++ engine/sc_paladin.cpp
@@ -2392,8 +2392,8 @@ void paladin_t::init_actions()
       if ( race == RACE_BLOOD_ELF ) action_list_str += "/arcane_torrent";
       action_list_str += "/zealotry";
       if ( level >= 85 )
-        action_list_str += "/guardian_of_ancient_kings,if=buff.zealotry.remains<31|cooldown.zealotry.remains>60";
-      action_list_str += "/avenging_wrath,if=buff.zealotry.remains<21";
+        action_list_str += "/guardian_of_ancient_kings,if=(buff.zealotry.remains<31&buff.zealotry.up)|cooldown.zealotry.remains>60";
+      action_list_str += "/avenging_wrath,if=buff.zealotry.remains<21&buff.zealotry.up";
       if ( level >= 81 )
         action_list_str += "/inquisition,if=(buff.inquisition.down|buff.inquisition.remains<5)&(holy_power=3|buff."+hp_proc_str+".react)";
       // CS before TV if <3 power, even with HoL/DP up
