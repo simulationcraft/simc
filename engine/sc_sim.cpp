56695464d761a89563b55f342f8bd9018a925cce
cartec69@gmail.com <cartec69@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-09-02 21:53:04 +0000

    Put back the error convergence calculation, but with a linear instead of quadratic algorithm.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9477 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_sim.cpp
+++ engine/sc_sim.cpp
@@ -662,7 +662,7 @@ sim_t::sim_t( sim_t* p, int index ) :
   dtr_proc_chance( -1.0 ),
   target_death_pct( 0 ), target_level( -1 ), target_race( "" ), target_adds( 0 ),
   rng( 0 ), deterministic_rng( 0 ), rng_list( 0 ),
-  smooth_rng( 0 ), deterministic_roll( 0 ), average_range( 1 ), average_gauss( 0 ),
+  smooth_rng( 0 ), deterministic_roll( 0 ), average_range( 1 ), average_gauss( 0 ), convergence_scale( 2 ),
   timing_wheel( 0 ), wheel_seconds( 0 ), wheel_size( 0 ), wheel_mask( 0 ), timing_slice( 0 ), wheel_granularity( 0.0 ),
   fight_style( "Patchwerk" ), buff_list( 0 ), aura_delay( 0.15 ), default_aura_delay( 0.3 ), default_aura_delay_stddev( 0.05 ),
   cooldown_list( 0 ), replenishment_targets( 0 ),
@@ -1456,6 +1456,63 @@ void sim_t::analyze_player( player_t* p )
     p -> dps_std_dev += delta * delta;
   }
 
+  if ( p -> dps_min >= 1.0E+50 ) p -> dps_min = 0.0;
+  if ( p -> dps_max < 0.0      ) p -> dps_max = 0.0;
+
+  if ( iterations > 1 ) p -> dps_std_dev /= ( iterations - 1 );
+  p -> dps_std_dev = sqrt( p -> dps_std_dev );
+  p -> dps_error = 2.0 * p -> dps_std_dev / sqrt( ( float ) iterations );
+
+
+  // Error Convergence =========================================
+  {
+    int    convergence_iterations = 0;
+    double convergence_dps = 0;
+    double convergence_min = +1.0E+50;
+    double convergence_max = -1.0E+50;
+    double convergence_std_dev = 0;
+
+    if ( iterations > 1 && convergence_scale > 1 )
+    {
+      for ( int i=0; i < iterations; i += convergence_scale )
+      {
+        double i_dps = p -> iteration_dps[ i ];
+        convergence_dps += i_dps;
+        if ( convergence_min > i_dps ) convergence_min = i_dps;
+        if ( convergence_max < i_dps ) convergence_max = i_dps;
+      }
+      convergence_iterations = ( iterations + convergence_scale - 1 ) / convergence_scale;
+      convergence_dps /= convergence_iterations;
+
+      assert( p -> dps_convergence_error.empty() );
+      p -> dps_convergence_error.reserve( iterations );
+
+      double sum_of_squares = 0;
+
+      for ( int i=0; i < iterations; i++ )
+      {
+        p -> dps_convergence_error.push_back( 2.0 * sqrt( sum_of_squares / i ) / sqrt( i ) );
+
+        double delta = p -> iteration_dps[ i ] - convergence_dps;
+        double delta_squared = delta * delta;
+
+        sum_of_squares += delta_squared;
+
+        if ( ( i % convergence_scale ) == 0 )
+          convergence_std_dev += delta_squared;
+      }
+    }
+
+    if ( convergence_iterations > 1 ) convergence_std_dev /= convergence_iterations;
+    convergence_std_dev = sqrt( convergence_std_dev );
+    double convergence_error = 2.0 * convergence_std_dev;
+    if ( convergence_iterations > 1 ) convergence_error /= sqrt( convergence_iterations );
+
+    if ( convergence_error > 0 )
+      p -> dps_convergence = convergence_error / ( p -> dps_error * convergence_scale );
+  }
+
+
   // DPS Statistics ============================================
   if ( ( p -> dps_max - p -> dps_min ) > 0 )
   {
@@ -1503,6 +1560,7 @@ void sim_t::analyze_player( player_t* p )
     chart_t::timeline_resource  ( pet -> timeline_resource_chart[i],         pet, i );
     }
     chart_t::timeline_dps       ( pet -> timeline_dps_chart,              pet );
+    chart_t::timeline_dps_error ( pet -> timeline_dps_error_chart,        pet );
     chart_t::dps_error          ( pet -> dps_error_chart,                 pet );
     chart_t::distribution_dps   ( pet -> distribution_dps_chart,          pet );
   }
@@ -1515,6 +1573,7 @@ void sim_t::analyze_player( player_t* p )
   chart_t::timeline_resource  ( p -> timeline_resource_chart[i],         p, i );
   }
   chart_t::timeline_dps       ( p -> timeline_dps_chart,              p );
+  chart_t::timeline_dps_error ( p -> timeline_dps_error_chart,        p );
   chart_t::dps_error          ( p -> dps_error_chart,                 p );
   chart_t::distribution_dps   ( p -> distribution_dps_chart,          p );
 
@@ -2194,6 +2253,7 @@ void sim_t::create_options()
     { "deterministic_roll",               OPT_BOOL,   &( deterministic_roll                       ) },
     { "average_range",                    OPT_BOOL,   &( average_range                            ) },
     { "average_gauss",                    OPT_BOOL,   &( average_gauss                            ) },
+    { "convergence_scale",                OPT_INT,    &( convergence_scale                        ) },
     // Misc
     { "party",                            OPT_LIST,   &( party_encoding                           ) },
     { "active",                           OPT_FUNC,   ( void* ) ::parse_active                      },
