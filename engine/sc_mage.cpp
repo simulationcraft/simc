simulationcraft/simc

commit 61f651e3de4b1af142c90279ffa11cf873ba9e7f

Enhance html reporting about sim options/infos and small performance stuff
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9525 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_mage.cpp
@@ -1443,6 +1443,8 @@ struct arcane_brilliance_t : public mage_spell_t
     bonus      = p -> dbc.effect_average( p -> dbc.spell( 79058 ) -> effect1().id(), p -> level );
     base_cost *= 1.0 + p -> glyphs.arcane_brilliance -> effect1().percent();
     harmful = false;
+
+    background = ( sim -> overrides.arcane_brilliance != 0 );
   }
 
   virtual void execute()
@@ -1460,7 +1462,10 @@ struct arcane_brilliance_t : public mage_spell_t
 
   virtual bool ready()
   {
-    return ( player -> buffs.arcane_brilliance -> current_value < bonus ) && mage_spell_t::ready();
+    if( player -> buffs.arcane_brilliance -> current_value < bonus )
+      return false;
+
+    return mage_spell_t::ready();
   }
 };
 

engine/sc_player.cpp
@@ -681,7 +681,7 @@ bool player_t::init( sim_t* sim )
     if ( sim -> default_actions && ! p -> is_pet() ) p -> action_list_str.clear();
     p -> init();
     if ( ! p -> quiet ) too_quiet = false;
-    if ( p -> primary_role() != ROLE_HEAL && p -> primary_role() != ROLE_TANK && !p -> is_pet() ) zero_dds = false;
+    if ( p -> primary_role() != ROLE_HEAL && p -> primary_role() != ROLE_TANK && !p -> is_pet() && p -> quiet ) zero_dds = false;
   }
 
   if ( too_quiet && ! sim -> debug )

engine/sc_druid.cpp
@@ -3443,6 +3443,8 @@ struct mark_of_the_wild_t : public druid_spell_t
     id          = 1126;
     base_cost  *= 1.0 + p -> glyphs.mark_of_the_wild -> mod_additive( P_RESOURCE_COST ) / 100.0;
     harmful     = false;
+
+    background = ( sim -> overrides.mark_of_the_wild != 0 );
   }
 
   virtual void execute()

engine/sc_priest.cpp
@@ -1070,6 +1070,28 @@ struct shadow_fiend_pet_t : public pet_t
     }
   };
 
+  // Wait For ShadowCrawl Action ===================================================
+
+  struct wait_for_shadowcrawl_t : public action_t
+  {
+    cooldown_t* cd_sc;
+    wait_for_shadowcrawl_t( player_t* player ) :
+      action_t( ACTION_OTHER, "wait_for_shadowcrawl", player ), cd_sc( 0 )
+    {
+      cd_sc = player -> get_cooldown( "shadow_crawl" );
+    }
+
+    virtual double execute_time() SC_CONST
+    {
+      return cd_sc -> remains();
+    }
+
+    virtual void execute()
+    {
+      player -> total_waiting += time_to_execute;
+    }
+  };
+
   shadow_fiend_pet_t( sim_t* sim, player_t* owner ) :
     pet_t( sim, owner, "shadow_fiend" ), buffs_shadowcrawl( 0 ), shadowcrawl( 0 ), mana_leech( 0 ),
     bad_swing( false ), bad_spell_power( 0.0 ), extra_tick( false )
@@ -1083,13 +1105,14 @@ struct shadow_fiend_pet_t : public pet_t
 
     bad_spell_power = util_t::ability_rank( owner -> level,  370.0,85,  358.0,82,  352.0,80,  0.0,0 );
 
-    action_list_str             = "/snapshot_stats/shadowcrawl/wait_until_ready";
+    action_list_str             = "/snapshot_stats/shadowcrawl/wait_for_shadowcrawl";
   }
 
   virtual action_t* create_action( const std::string& name,
                                    const std::string& options_str )
   {
     if ( name == "shadowcrawl" ) return new shadowcrawl_t( this );
+    if ( name == "wait_for_shadowcrawl" ) return new wait_for_shadowcrawl_t( this );
 
     return pet_t::create_action( name, options_str );
   }
@@ -1305,17 +1328,40 @@ struct cauterizing_flame_pet_t : public pet_t
     }
   };
 
+  // Wait For Cauterizing Flame ===================================================
+
+  struct wait_for_cauterizing_flame_t : public action_t
+  {
+    cooldown_t* cd_cf;
+    wait_for_cauterizing_flame_t( player_t* player ) :
+      action_t( ACTION_OTHER, "wait_for_cauterizing_flame", player ), cd_cf( 0 )
+    {
+      cd_cf = player -> get_cooldown( "cauterizing_flame_heal" );
+    }
+
+    virtual double execute_time() SC_CONST
+    {
+      return cd_cf -> remains();
+    }
+
+    virtual void execute()
+    {
+      player -> total_waiting += time_to_execute;
+    }
+  };
+
   cauterizing_flame_pet_t( sim_t* sim, player_t* owner ) :
     pet_t( sim, owner, "cauterizing_flame", PET_NONE, true )
   {
     role = ROLE_HEAL;
-    action_list_str = "/snapshot_stats/cauterizing_flame_heal/wait_until_ready";
+    action_list_str = "/snapshot_stats/cauterizing_flame_heal/wait_for_cauterizing_flame";
   }
 
   virtual action_t* create_action( const std::string& name,
                                    const std::string& options_str )
   {
     if ( name == "cauterizing_flame_heal" ) return new cauterizing_flame_heal_t( this );
+    if ( name == "wait_for_cauterizing_flame" ) return new wait_for_cauterizing_flame_t( this );
 
     return pet_t::create_action( name, options_str );
   }

engine/sc_report.cpp
@@ -993,6 +993,8 @@ static void print_html_contents( FILE*  file, sim_t* sim )
           ab = 1;
         }
         ci++;
+        cols[i] += "\t\t\t\t\t\t<li><a href=\"#sim-info\">Simulation Information</a></li>\n";
+        ci++;
       }
       if ( sim -> report_targets && ab > 0 )
       {
@@ -1030,6 +1032,133 @@ static void print_html_contents( FILE*  file, sim_t* sim )
                    "\t\t</div>\n\n" );
 }
 
+// print_html_sim_summary ===================================================
+
+static void print_html_sim_summary( FILE*  file, sim_t* sim )
+{
+
+  util_t::fprintf( file,
+                   "\t\t\t\t<div id=\"sim-info\" class=\"section" );
+  util_t::fprintf( file, " grouped-first" );
+  util_t::fprintf ( file, "\">\n" );
+
+  util_t::fprintf ( file,
+                    "\t\t\t\t\t<h2 class=\"toggle\">Simulation Information</h2>\n"
+                    "\t\t\t\t\t\t<div class=\"toggle-content hide\">\n" );
+
+
+    util_t::fprintf( file,
+                     "\t\t\t\t\t\t<table class=\"sc mt\">\n" );
+
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Iterations:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%d</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> iterations );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Threads:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%d</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> report_threads );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Fight Length:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%.0f - %.0f</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> iteration_timeline[ 0 ],
+                       sim -> iteration_timeline [sim -> iteration_timeline.size() - 1 ] );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th><h2>Performance:</h2></th>\n"
+                       "\t\t\t\t\t\t\t\t<td></td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n" );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Total Events Processed:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%ld</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       ( long ) sim -> total_events_processed );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Max Event Queue:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%ld</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       ( long ) sim -> max_events_remaining );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Sim Seconds:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%.0f</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> iterations * sim -> total_seconds );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>CPU Seconds:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%.4f</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> elapsed_cpu_seconds );
+      util_t::fprintf( file,
+                       "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                       "\t\t\t\t\t\t\t\t<th>Speed Up:</th>\n"
+                       "\t\t\t\t\t\t\t\t<td>%.0f</td>\n"
+                       "\t\t\t\t\t\t\t</tr>\n",
+                       sim -> iterations * sim -> total_seconds / sim -> elapsed_cpu_seconds );
+
+      util_t::fprintf( file,
+                            "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                            "\t\t\t\t\t\t\t\t<th><h2>Settings:</h2></th>\n"
+                            "\t\t\t\t\t\t\t\t<td></td>\n"
+                            "\t\t\t\t\t\t\t</tr>\n" );
+      util_t::fprintf( file,
+                             "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                             "\t\t\t\t\t\t\t\t<th>World Lag:</th>\n"
+                             "\t\t\t\t\t\t\t\t<td>%.0f ms ( stddev = %.0f ms )</td>\n"
+                             "\t\t\t\t\t\t\t</tr>\n",
+                             sim -> world_lag * 1000.0, sim -> world_lag_stddev * 1000.0 );
+      util_t::fprintf( file,
+                             "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                             "\t\t\t\t\t\t\t\t<th>Queue Lag:</th>\n"
+                             "\t\t\t\t\t\t\t\t<td>%.0f ms ( stddev = %.0f ms )</td>\n"
+                             "\t\t\t\t\t\t\t</tr>\n",
+                             sim -> queue_lag * 1000.0, sim -> queue_lag_stddev * 1000.0 );
+      util_t::fprintf( file,
+                             "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                             "\t\t\t\t\t\t\t\t<th>GCD Lag:</th>\n"
+                             "\t\t\t\t\t\t\t\t<td>%.0f ms ( stddev = %.0f ms )</td>\n"
+                             "\t\t\t\t\t\t\t</tr>\n",
+                             sim -> gcd_lag * 1000.0, sim -> gcd_lag_stddev * 1000.0 );
+      util_t::fprintf( file,
+                             "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                             "\t\t\t\t\t\t\t\t<th>Channel Lag:</th>\n"
+                             "\t\t\t\t\t\t\t\t<td>%.0f ms ( stddev = %.0f ms )</td>\n"
+                             "\t\t\t\t\t\t\t</tr>\n",
+                             sim -> channel_lag * 1000.0, sim -> channel_lag_stddev * 1000.0 );
+      util_t::fprintf( file,
+                             "\t\t\t\t\t\t\t<tr class=\"left\">\n"
+                             "\t\t\t\t\t\t\t\t<th>Queue GCD Reduction:</th>\n"
+                             "\t\t\t\t\t\t\t\t<td>%.0f ms</td>\n"
+                             "\t\t\t\t\t\t\t</tr>\n",
+                             sim -> queue_gcd_reduction * 1000.0 );
+
+    util_t::fprintf( file,
+                     "\t\t\t\t\t\t</table>\n" );
+
+
+    if ( sim -> iterations > 1 )
+    {
+      util_t::fprintf( file,
+                       "\t\t\t\t\t<a href=\"#help-timeline-distribution\" class=\"help\"><img src=\"%s\" alt=\"Timeline Distribution Chart\" /></a>\n",
+                       sim -> timeline_chart.c_str() );
+    }
+
+
+  // closure
+  util_t::fprintf( file,
+                   "\t\t\t\t<div class=\"clear\"></div>\n"
+                   "\t\t\t</div>\n"
+                   "\t\t</div>\n\n" );
+}
 // print_html_raid_summary ===================================================
 
 static void print_html_raid_summary( FILE*  file, sim_t* sim )
@@ -1040,6 +1169,28 @@ static void print_html_raid_summary( FILE*  file, sim_t* sim )
                    "\t\t\t<h2 class=\"toggle open\">Raid Summary</h2>\n" );
   util_t::fprintf( file,
                    "\t\t\t<div class=\"toggle-content\">\n" );
+
+
+  util_t::fprintf( file,
+                     "\t\t\t<ul class=\"params\">\n" );
+    util_t::fprintf( file,
+                     "\t\t\t\t<li><b>Raid Damage:</b> %.0f</li>\n",
+                     sim -> total_dmg );
+    util_t::fprintf( file,
+                     "\t\t\t\t<li><b>Raid DPS:</b> %.0f</li>\n",
+                     sim -> raid_dps );
+    if ( sim -> total_heal > 0 )
+    {
+    util_t::fprintf( file,
+                     "\t\t\t\t<li><b>Raid Heal:</b> %.0f</li>\n",
+                     sim -> total_heal );
+    util_t::fprintf( file,
+                     "\t\t\t\t<li><b>Raid HPS:</b> %.0f</li>\n",
+                     sim -> raid_hps );
+    }
+  util_t::fprintf( file,
+                   "\t\t\t</ul><p>&nbsp;</p>\n" );
+
 
   assert( sim ->  dps_charts.size() ==
           sim -> gear_charts.size() );
@@ -1061,12 +1212,7 @@ static void print_html_raid_summary( FILE*  file, sim_t* sim )
                      "\t\t\t\t\t<img src=\"%s\" alt=\"Gear Chart\" />\n",
                      sim -> gear_charts[ i ].c_str() );
   }
-  if ( sim -> iterations > 1 )
-  {
-    util_t::fprintf( file,
-                     "\t\t\t\t\t<a href=\"#help-timeline-distribution\" class=\"help\"><img src=\"%s\" alt=\"Timeline Distribution Chart\" /></a>\n",
-                     sim -> timeline_chart.c_str() );
-  }
+
   if ( ! sim -> raid_events_str.empty() )
   {
     util_t::fprintf( file,
@@ -4398,6 +4544,9 @@ void report_t::print_html( sim_t* sim )
 
   // Auras
   print_html_auras_buffs( file, sim );
+
+  // Auras
+  print_html_sim_summary( file, sim );
 
   // Report Targets
   if ( sim -> report_targets )

engine/sc_sim.cpp
@@ -677,7 +677,8 @@ sim_t::sim_t( sim_t* p, int index ) :
   path_str( "." ), output_file( stdout ),
   armory_throttle( 5 ), current_throttle( 5 ), debug_exp( 0 ),
   // Report
-  report_precision( 4 ),report_pets_separately( 0 ), report_targets( 1 ), report_details( 1 ), report_rng( 0 ), hosted_html( 0 ), print_styles( false ),
+  report_precision( 4 ),report_pets_separately( 0 ), report_targets( 1 ), report_details( 1 ),
+  report_rng( 0 ), hosted_html( 0 ), print_styles( false ), report_threads( 0 ),
   // Multi-Threading
   threads( 0 ), thread_index( index ),
   spell_query( 0 )
@@ -2416,6 +2417,9 @@ bool sim_t::parse_options( int    _argc,
     iterations = 1;
     threads = 1;
   }
+
+  report_threads = threads;
+  if ( report_threads < 1 ) report_threads = 1;
 
   return true;
 }

engine/sc_warlock.cpp
@@ -1439,7 +1439,6 @@ struct felguard_pet_t : public warlock_main_pet_t
 
 struct felhunter_pet_t : public warlock_main_pet_t
 {
-  cooldown_t* cooldowns_shadow_bite;
    // TODO: Need to add fel intelligence on the warlock while felhunter is out
   struct shadow_bite_t : public warlock_pet_spell_t
   {
@@ -1475,6 +1474,28 @@ struct felhunter_pet_t : public warlock_main_pet_t
     }
   };
 
+  // Wait For ShadowCrawl Action ===================================================
+
+  struct wait_for_shadow_bite_t : public action_t
+  {
+    cooldown_t* cd_sb;
+    wait_for_shadow_bite_t( player_t* player ) :
+      action_t( ACTION_OTHER, "wait_for_shadow_bite", player ), cd_sb( 0 )
+    {
+      cd_sb = player -> get_cooldown( "shadow_bite" );
+    }
+
+    virtual double execute_time() SC_CONST
+    {
+      return cd_sb -> remains();
+    }
+
+    virtual void execute()
+    {
+      player -> total_waiting += time_to_execute;
+    }
+  };
+
   felhunter_pet_t( sim_t* sim, player_t* owner ) :
       warlock_main_pet_t( sim, owner, "felhunter", PET_FELHUNTER )
   {
@@ -1482,9 +1503,7 @@ struct felhunter_pet_t : public warlock_main_pet_t
 
     action_list_str += "/snapshot_stats";
     action_list_str += "/shadow_bite";
-    action_list_str += "/wait_until_ready";
-
-    cooldowns_shadow_bite = get_cooldown( "shadow_bite" );
+    action_list_str += "/wait_for_shadow_bite";
   }
 
   virtual void init_base()
@@ -1505,6 +1524,7 @@ struct felhunter_pet_t : public warlock_main_pet_t
                                    const std::string& options_str )
   {
     if ( name == "shadow_bite" ) return new shadow_bite_t( this );
+    if ( name == "wait_for_shadow_bite" ) return new wait_for_shadow_bite_t( this );
 
     return warlock_main_pet_t::create_action( name, options_str );
   }

engine/simulationcraft.h
@@ -2726,6 +2726,7 @@ struct sim_t : private thread_t
   int report_rng;
   int hosted_html;
   int print_styles;
+  int report_threads;
 
 private:
   // Multi-Threading

commit 54db3f00bb6d8f403404a5af4ed1a355d5acbff0

Correct logic errors introduced in r9525.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9611 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_mage.cpp
@@ -1459,7 +1459,7 @@ struct arcane_brilliance_t : public mage_spell_t
   virtual bool ready()
   {
     if ( player -> buffs.arcane_brilliance -> current_value < bonus )
-      return false;
+      return true;
 
     return mage_spell_t::ready();
   }

engine/sc_player.cpp
@@ -678,7 +678,7 @@ bool player_t::init( sim_t* sim )
     if ( sim -> default_actions && ! p -> is_pet() ) p -> action_list_str.clear();
     p -> init();
     if ( ! p -> quiet ) too_quiet = false;
-    if ( p -> primary_role() != ROLE_HEAL && p -> primary_role() != ROLE_TANK && !p -> is_pet() && p -> quiet ) zero_dds = false;
+    if ( p -> primary_role() != ROLE_HEAL && p -> primary_role() != ROLE_TANK && ! p -> is_pet() ) zero_dds = false;
   }
 
   if ( too_quiet && ! sim -> debug )
@@ -1833,6 +1833,7 @@ void player_t::init_scaling()
           double new_speed = ( ranged_weapon.swing_time + v );
 
           double mult = new_speed / ranged_weapon.swing_time;
+
 
           ranged_weapon.min_dmg *= mult;
           ranged_weapon.max_dmg *= mult;