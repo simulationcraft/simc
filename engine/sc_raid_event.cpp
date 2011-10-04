68ea15e8af4d4495fafad9b0f3963e07241711b1
philoptik@gmail.com <philoptik@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-10-04 19:51:55 +0000

    Change to Stun Event, might fix Issue829
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9745 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_raid_event.cpp
+++ engine/sc_raid_event.cpp
@@ -254,7 +254,9 @@ struct stun_event_t : public raid_event_t
       if ( p -> sleeping ) continue;
       if ( ! p -> buffs.stunned -> check() )
       {
-        p -> schedule_ready();
+        // Don't schedule_ready players who are already working, like pet's auto-summoned during the stun event ( ebon imp ).
+        if ( ! p -> readying )
+          p -> schedule_ready();
       }
     }
 
