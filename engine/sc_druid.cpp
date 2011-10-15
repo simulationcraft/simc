simulationcraft/simc

commit 558e00842591a5f869ed21391ced39192c8c29e9

Stats: Change direct&tick results to a vector, change stats_results_t constructor to take a sim pointer ( and move it to sc_stats.cpp ). Call reserve for most sample data
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9900 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_druid.cpp
@@ -1509,8 +1509,6 @@ struct mangle_cat_t : public druid_cat_attack_t
     if ( result_is_hit() )
     {
       druid_t* p = player -> cast_druid();
-      target -> debuffs.mangle -> trigger();
-      target -> debuffs.mangle -> source = p;
 
       if ( p -> dbc.ptr && p -> glyphs.shred -> enabled() &&
            p -> dots_rip -> ticking  &&
@@ -1530,6 +1528,8 @@ struct mangle_cat_t : public druid_cat_attack_t
   {
     druid_cat_attack_t::travel( t, travel_result, travel_dmg );
     trigger_tier12_2pc_melee( this, direct_dmg );
+    t -> debuffs.mangle -> trigger();
+    t -> debuffs.mangle -> source = player;
   }
 
   virtual bool ready()

engine/sc_sim.cpp
@@ -1259,6 +1259,12 @@ bool sim_t::init()
 
   if ( report_precision < 0 ) report_precision = 3;
 
+  raid_dps.reserve( iterations );
+  total_dmg.reserve( iterations );
+  raid_hps.reserve( iterations );
+  total_heal.reserve( iterations );
+  simulation_length.reserve( iterations );
+
   return canceled ? false : true;
 }
 

engine/sc_stats.cpp
@@ -22,13 +22,20 @@ stats_t::stats_t( const std::string& n, player_t* p ) :
   last_execute( -1 ),
   iteration_actual_amount( 0 ), actual_amount( p -> sim -> statistics_level < 3 ),
   total_amount( p -> sim -> statistics_level < 3 ),portion_aps( p -> sim -> statistics_level < 3 ),
-  compound_actual( 0 ), compound_amount( 0 ), opportunity_cost( 0 )
+  compound_actual( 0 ), compound_amount( 0 ), opportunity_cost( 0 ),
+  direct_results( RESULT_MAX, stats_results_t( p -> sim ) ), tick_results( RESULT_MAX, stats_results_t( p -> sim ) )
 {
   int num_buckets = ( int ) sim -> max_time;
   if ( num_buckets == 0 ) num_buckets = 600; // Default to 10 minutes
   num_buckets *= 2;
 
   timeline_amount.reserve( num_buckets );
+
+  actual_amount.reserve( sim -> iterations );
+  total_amount.reserve( sim -> iterations );
+  portion_aps.reserve( sim -> iterations );
+
+
 }
 
 // stats_t::add_child =======================================================
@@ -252,6 +259,35 @@ void stats_t::analyze()
   int max_buckets = std::min( num_buckets, ( int ) sim -> divisor_timeline.size() );
   for ( int i=0; i < max_buckets; i++ )
     timeline_amount[ i ] /= sim -> divisor_timeline[ i ];
+}
+
+// stats_results_t::merge ===========================================================
+
+
+stats_t::stats_results_t::stats_results_t( sim_t* s ) :
+  actual_amount( s -> statistics_level < 8, true ),
+  total_amount(),
+  fight_actual_amount(),
+  fight_total_amount(),
+  count( s -> statistics_level < 8 ),
+  avg_actual_amount( s -> statistics_level < 8, true ),
+  iteration_count( 0 ),
+  iteration_actual_amount( 0 ),
+  iteration_total_amount( 0 ),
+  pct( 0 ),
+  overkill_pct( 0 )
+
+{ // Keep non hidden reported numbers clean
+  count.mean = 0;
+  actual_amount.mean = 0; actual_amount.max=0;
+  avg_actual_amount.mean = 0;
+
+  actual_amount.reserve( s -> iterations );
+  total_amount.reserve( s -> iterations );
+  fight_actual_amount.reserve( s -> iterations );
+  fight_total_amount.reserve( s -> iterations );
+  count.reserve( s -> iterations );
+  avg_actual_amount.reserve( s -> iterations );
 }
 
 // stats_results_t::merge ===========================================================

engine/simulationcraft.h
@@ -4420,19 +4420,12 @@ struct stats_t
   {
     sample_data_t actual_amount, total_amount,fight_actual_amount, fight_total_amount,count,avg_actual_amount;
     double iteration_count, iteration_actual_amount, iteration_total_amount,pct, overkill_pct;
-    stats_results_t() :
-      actual_amount( true, true ), total_amount(),
-      fight_actual_amount(), fight_total_amount(), count(), avg_actual_amount( true, true ),
-      iteration_count( 0 ), iteration_actual_amount( 0 ), iteration_total_amount( 0 ), pct( 0 ), overkill_pct( 0 )
-    { // Keep non hidden reported numbers clean
-      count.mean = 0;
-      actual_amount.mean = 0; actual_amount.max=0;
-      avg_actual_amount.mean = 0; }
+    stats_results_t( sim_t* s );
     void merge( const stats_results_t& other );
     void combat_end();
   };
-  stats_results_t direct_results[ RESULT_MAX ];
-  stats_results_t   tick_results[ RESULT_MAX ];
+  std::vector<stats_results_t> direct_results;
+  std::vector<stats_results_t>   tick_results;
 
   std::vector<double> timeline_amount;
   std::vector<double> timeline_aps;

commit c1fd51d346d696e04c28aa4efe1a3c86fcf1bb8f

Druid: Fix mangle bug introduced in r9900
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9909 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_druid.cpp
@@ -1529,8 +1529,11 @@ struct mangle_cat_t : public druid_cat_attack_t
   {
     druid_cat_attack_t::travel( t, travel_result, travel_dmg );
     trigger_tier12_2pc_melee( this, direct_dmg );
-    t -> debuffs.mangle -> trigger();
-    t -> debuffs.mangle -> source = player;
+    if ( result_is_hit( travel_result ) )
+    {
+      t -> debuffs.mangle -> trigger();
+      t -> debuffs.mangle -> source = player;
+    }
   }
 
   virtual bool ready()