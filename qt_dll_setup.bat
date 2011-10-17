ead4c2790327f6376b458583fa36e066743e6ebf
cartec69 <cartec69@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-10-17 05:01:23 +0000

    Don't copy SSL DLLs from QtSDK - make user install them externally. Fixes issue 875 (sort of).
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9942 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- qt_dll_setup.bat
+++ qt_dll_setup.bat
@@ -1,12 +1,12 @@
 
-:: Necessary Qt dlls are packaged with every release. 
+:: Necessary Qt dlls are packaged with every release.
 :: These dlls are not included in the SVN.
 :: They need to be copied into the dev area from the Qt install.
 :: It is important to understand that the Qt-SDK is different from the Qt-Framework
 :: Qt-SDK is an integrated development environment that is married to the MinGW compiler
 :: Qt-Framework is simply the Qt runtime dlls built against the MSVC 2008 compiler
 :: Both can be found at: http://qt.nokia.com/downloads
-:: If you build SimC with MSVC 2008, then you need to use dlls from Qt-Framework 
+:: If you build SimC with MSVC 2008, then you need to use dlls from Qt-Framework
 :: If you build SimC with the Qt SDK, then you need to use dlls from Qt-SDK
 :: As of this writing, the default locations from which to gather the dlls are:
 :: Qt-SDK: c:\QtSDK\Desktop\Qt\4.7.4\mingw
@@ -24,32 +24,23 @@ set qt_dir=c:\QtSDK\Desktop\Qt\4.7.4\mingw
 
 :: Removing existing dlls
 del /q imageformats
-del /q phonon4.dll 
+del /q phonon4.dll
 del /q QtCore4.dll
 del /q QtGui4.dll
 del /q QtNetwork4.dll
 del /q QtWebKit4.dll
 del /q QtXmlPatterns4.dll
-del /q mingwm10.dll
-del /q libgcc_s_dw2-1.dll
-del /q libeay32.dll
-del /q libssl32.dll
-del /q ssleay32.dll
+del /q mingw*.dll
+del /q libgcc*.dll
 
 :: Copying new dlls
 xcopy /I %qt_dir%\plugins\imageformats imageformats
-xcopy %qt_dir%\bin\phonon4.dll 
+xcopy %qt_dir%\bin\phonon4.dll
 xcopy %qt_dir%\bin\QtCore4.dll
 xcopy %qt_dir%\bin\QtGui4.dll
 xcopy %qt_dir%\bin\QtNetwork4.dll
 xcopy %qt_dir%\bin\QtWebKit4.dll
-:: xcopy %qt_dir%\bin\QtXmlPatterns4.dll
 
 :: Copy MingW runtime DLLs
-xcopy %qt_dir%\bin\mingwm10.dll
-xcopy %qt_dir%\bin\libgcc_s_dw2-1.dll
-
-:: Copy OpenSSL DLLs
-xcopy %qt_dir%\bin\libeay32.dll
-xcopy %qt_dir%\bin\libssl32.dll
-xcopy %qt_dir%\bin\ssleay32.dll
+xcopy %qt_dir%\bin\mingw*.dll .
+xcopy %qt_dir%\bin\libgcc*.dll .
