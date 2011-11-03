7616d6e8dd5935c2978f876a9b85bb7c72339d9b
wowharly@gmail.com <wowharly@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-11-03 08:02:19 +0000

    reverting dnd tick change: ticks = 11 with zero_tick is actually 12. It should be 11, so ticks = 10. *boggle*
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10195 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_death_knight.cpp
+++ engine/sc_death_knight.cpp
@@ -2560,7 +2560,7 @@ struct death_and_decay_t : public death_knight_spell_t
     tick_power_mod   = 0.064;
     base_td          = p -> dbc.effect_average( effect1().id(), p -> level );
     base_tick_time   = 1.0;
-    num_ticks        = 11;
+    num_ticks        = 10; // 11 with tick_zero
     tick_may_crit    = true;
     tick_zero        = true;
     hasted_ticks     = false;
