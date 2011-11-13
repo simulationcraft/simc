simulationcraft/simc

commit 4a068aa6c39a38891487f3a5f3ba1232f562663e

Item:   Verify a player has enchanting for ring enchants
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@9291 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_player.cpp
@@ -773,6 +773,7 @@ void player_t::init()
   init_race();
   init_base();
   init_racials();
+  init_professions();
   init_items();
   init_core();
   init_spell();
@@ -781,7 +782,6 @@ void player_t::init()
   init_weapon( &main_hand_weapon );
   init_weapon( &off_hand_weapon );
   init_weapon( &ranged_weapon );
-  init_professions();
   init_unique_gear();
   init_enchant();  
   init_consumables();
@@ -1150,7 +1150,7 @@ void player_t::init_weapon( weapon_t* w )
 
   if ( w -> slot == SLOT_MAIN_HAND ) assert( w -> type >= WEAPON_NONE && w -> type < WEAPON_2H );
   if ( w -> slot == SLOT_OFF_HAND  ) assert( w -> type >= WEAPON_NONE && w -> type < WEAPON_2H );
-  if ( w -> slot == SLOT_RANGED    ) assert( w -> type == WEAPON_NONE || ( w -> type > WEAPON_2H && w -> type < WEAPON_RANGED ) );
+  if ( w -> slot == SLOT_RANGED    ) assert( w -> type > WEAPON_2H && w -> type < WEAPON_RANGED );
 }
 
 // player_t::init_unique_gear ==============================================

engine/simulationcraft.h
@@ -3871,6 +3871,8 @@ struct action_t : public spell_id_t
   const spelleffect_data_t& effect3() const { return spell -> effect3(); }
 };
 
+// Attack ===================================================================
+
 struct attack_t : public action_t
 {
   double base_expertise, player_expertise, target_expertise;

engine/sc_item.cpp
@@ -330,7 +330,18 @@ bool item_t::init()
   if ( ! option_stats_str.empty()   ) encoded_stats_str   = option_stats_str;
   if ( ! option_reforge_str.empty() ) encoded_reforge_str = option_reforge_str;
   if ( ! option_gems_str.empty()    ) encoded_gems_str    = option_gems_str;
-  if ( ! option_enchant_str.empty() ) encoded_enchant_str = option_enchant_str;
+  if ( ! option_enchant_str.empty() )
+  {
+    if ( ( slot == SLOT_FINGER_1 || slot == SLOT_FINGER_2 ) && ! ( player -> profession[ PROF_ENCHANTING ] > 0 ) )
+    {
+      sim -> errorf( "Player %s at slot %s has a ring enchant without the enchanting profession'\n",
+                     player -> name(), slot_name() );
+    }
+    else
+    {
+      encoded_enchant_str = option_enchant_str;
+    }
+  }
   if ( ! option_addon_str.empty()   ) encoded_addon_str   = option_addon_str;
   if ( ! option_weapon_str.empty()  ) encoded_weapon_str  = option_weapon_str;
   if ( ! option_random_suffix_str.empty() ) encoded_random_suffix_str = option_random_suffix_str;

engine/sc_unique_gear.cpp
@@ -114,7 +114,6 @@ struct cost_reduction_proc_callback_t : public action_callback_t
   }
 };
 
-
 // discharge_proc_callback ==================================================
 
 struct discharge_proc_callback_t : public action_callback_t
@@ -1463,7 +1462,6 @@ static void register_valanyr( item_t* item )
 
     item -> unique = true;
 
-
     struct valanyr_callback_t : public action_callback_t
     {
       proc_t* proc;
@@ -1558,9 +1556,7 @@ void unique_gear_t::init( player_t* p )
     if ( ! strcmp( item.name(), "tyrandes_favorite_doll"              ) ) register_tyrandes_favorite_doll            ( &item );
     if ( ! strcmp( item.name(), "dragonwrath_tarecgosas_rest"         ) ) register_dragonwrath_tarecgosas_rest       ( &item );
     if ( ! strcmp( item.name(), "eye_of_blazing_power"                ) ) register_blazing_power                     ( &item );
-    if ( ! strcmp( item.name(), "valanyr_hammer_of_ancient_kings"     ) ) register_valanyr                     ( &item );
-
-
+    if ( ! strcmp( item.name(), "valanyr_hammer_of_ancient_kings"     ) ) register_valanyr                           ( &item );
   }
 }
 

commit 66f2650c58eabf71401b0e2a8cedc1513f926f0d

Player:   Profession bonuses must be called after init attack() and init_core(), this was broken in r9291   Fixes issue 957   Skinning should also increase spell crit
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10302 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_player.cpp
@@ -780,6 +780,7 @@ void player_t::init()
   init_weapon( &main_hand_weapon );
   init_weapon( &off_hand_weapon );
   init_weapon( &ranged_weapon );
+  init_professions_bonus();
   init_unique_gear();
   init_enchant();
   init_scaling();
@@ -1287,6 +1288,13 @@ void player_t::init_professions()
 
     profession[ prof_type ] = prof_value;
   }
+}
+
+// player_t::init_professions_bonus =========================================
+
+void player_t::init_professions_bonus()
+{
+  // This has to be called after init_attack() and init_core()
 
   // Miners gain additional stamina
   if      ( profession[ PROF_MINING ] >= 525 ) attribute_initial[ ATTR_STAMINA ] += 120.0;
@@ -1298,13 +1306,41 @@ void player_t::init_professions()
   else if ( profession[ PROF_MINING ] >=  75 ) attribute_initial[ ATTR_STAMINA ] +=   3.0;
 
   // Skinners gain additional crit rating
-  if      ( profession[ PROF_SKINNING ] >= 525 ) initial_attack_crit += 80.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >= 450 ) initial_attack_crit += 40.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >= 375 ) initial_attack_crit += 20.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >= 300 ) initial_attack_crit += 12.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >= 225 ) initial_attack_crit +=  9.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >= 150 ) initial_attack_crit +=  6.0 / rating.attack_crit;
-  else if ( profession[ PROF_SKINNING ] >=  75 ) initial_attack_crit +=  3.0 / rating.attack_crit;
+  if      ( profession[ PROF_SKINNING ] >= 525 )
+  {
+    initial_attack_crit += 80.0 / rating.attack_crit;
+    initial_spell_crit += 80.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >= 450 )
+  {
+    initial_attack_crit += 40.0 / rating.attack_crit;
+    initial_spell_crit += 40.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >= 375 )
+  {
+    initial_attack_crit += 20.0 / rating.attack_crit;
+    initial_spell_crit += 20.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >= 300 )
+  {
+    initial_attack_crit += 12.0 / rating.attack_crit;
+    initial_spell_crit += 12.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >= 225 )
+  {
+    initial_attack_crit +=  9.0 / rating.attack_crit;
+    initial_spell_crit +=  9.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >= 150 )
+  {
+    initial_attack_crit +=  6.0 / rating.attack_crit;
+    initial_spell_crit +=  6.0 / rating.spell_crit;
+  }
+  else if ( profession[ PROF_SKINNING ] >=  75 )
+  {
+    initial_attack_crit +=  3.0 / rating.attack_crit;
+    initial_spell_crit +=  3.0 / rating.spell_crit;
+  }
 }
 
 // Execute Pet Action =======================================================

engine/simulationcraft.h
@@ -4074,6 +4074,7 @@ struct player_t : public noncopyable
   virtual void init_enchant();
   virtual void init_resources( bool force = false );
   virtual void init_professions();
+  virtual void init_professions_bonus();
   virtual std::string init_use_item_actions( const std::string& append = std::string() );
   virtual std::string init_use_profession_actions( const std::string& append = std::string() );
   virtual std::string init_use_racial_actions( const std::string& append = std::string() );