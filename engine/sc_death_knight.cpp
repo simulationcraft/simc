e668f0a8953229d44ebf26bf12cefee499a5ef41
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-11-14 18:09:30 +0000

    Death Knight:   Slight tweak to Unholy's action list   Fixes issue 970   Regenerate T12 profiles
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10320 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_death_knight.cpp
+++ engine/sc_death_knight.cpp
@@ -4551,20 +4551,19 @@ void death_knight_t::init_actions()
       if ( talents.unholy_frenzy -> rank() )
         action_list_str += "/unholy_frenzy,if=!buff.bloodlust.react|target.time_to_die<=45";
       if ( level > 81 )
-        action_list_str += "/outbreak,if=dot.frost_fever.remains<2|dot.blood_plague.remains<2";
-      action_list_str += "/icy_touch,if=dot.frost_fever.remains<2";
-      action_list_str += "/plague_strike,if=dot.blood_plague.remains<2";
+        action_list_str += "/outbreak,if=dot.frost_fever.remains<=2|dot.blood_plague.remains<=2";
+      action_list_str += "/icy_touch,if=dot.frost_fever.remains<2&cooldown.outbreak.remains>2";
+      action_list_str += "/plague_strike,if=dot.blood_plague.remains<2&cooldown.outbreak.remains>2";
       if ( talents.dark_transformation -> rank() )
         action_list_str += "/dark_transformation";
       if ( talents.summon_gargoyle -> rank() )
       {
         action_list_str += "/summon_gargoyle,time<=60";
-        action_list_str += "/summon_gargoyle,if=buff.bloodlust.react";
-        action_list_str += "/summon_gargoyle,if=buff.unholy_frenzy.react";
+        action_list_str += "/summon_gargoyle,if=buff.bloodlust.react|buff.unholy_frenzy.react";
       }
-      action_list_str += "/death_and_decay,if=unholy=2";
-      action_list_str += "/scourge_strike,if=unholy=2";
-      action_list_str += "/festering_strike,if=blood=2&frost=2";
+      action_list_str += "/death_and_decay,if=unholy=2&runic_power<110";
+      action_list_str += "/scourge_strike,if=unholy=2&runic_power<110";
+      action_list_str += "/festering_strike,if=blood=2&frost=2&runic_power<110";
       action_list_str += "/death_coil,if=runic_power>90";
       if ( talents.sudden_doom -> rank() )
         action_list_str += "/death_coil,if=buff.sudden_doom.react";
