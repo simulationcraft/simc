32b08ed0f3cf89ba946b109afc654dd2a4fb8933
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-12-04 18:52:05 +0000

    Warrior:   Fix a bug where Inner Rage was applying all the time to HS/Cleave introduced in r9753
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10570 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warrior.cpp
+++ engine/sc_warrior.cpp
@@ -1493,7 +1493,8 @@ struct cleave_t : public warrior_attack_t
   {
     warrior_t* p = player -> cast_warrior();
 
-    cooldown -> duration = spell_id_t::cooldown() * ( 1.0 + p -> buffs_inner_rage -> effect1().percent() );
+    if ( p -> buffs_inner_rage -> up() )
+      cooldown -> duration = spell_id_t::cooldown() * ( 1.0 + p -> buffs_inner_rage -> effect1().percent() );
 
     warrior_attack_t::update_ready();
   }
@@ -1769,7 +1770,8 @@ struct heroic_strike_t : public warrior_attack_t
   {
     warrior_t* p = player -> cast_warrior();
 
-    cooldown -> duration = spell_id_t::cooldown() * ( 1.0 + p -> buffs_inner_rage -> effect1().percent() );
+    if ( p -> buffs_inner_rage -> up() )
+      cooldown -> duration = spell_id_t::cooldown() * ( 1.0 + p -> buffs_inner_rage -> effect1().percent() );
 
     warrior_attack_t::update_ready();
   }
