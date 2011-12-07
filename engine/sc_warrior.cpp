e4efe499ce2944931e3d154c38de52d81d667a79
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-12-07 02:07:45 +0000

    Warrior:   Strikes of Opportunity once again triggers Deep Wounds, etc.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10608 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warrior.cpp
+++ engine/sc_warrior.cpp
@@ -628,7 +628,6 @@ struct opportunity_strike_t : public warrior_attack_t
     warrior_attack_t( "opportunity_strike", 76858, p, TREE_ARMS )
   {
     background = true;
-    proc = true; // No longer triggers deep wounds
   }
 };
 
