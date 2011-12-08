63e0971521f678848369dea782b80ad8396f85c7
jon@valvatne.com <jon@valvatne.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2011-12-08 15:15:23 +0000

    Slightly better strategy for keeping the WoU stack up as affliction.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10621 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_warlock.cpp
+++ engine/sc_warlock.cpp
@@ -4713,8 +4713,12 @@ void warlock_t::init_actions()
       if ( talent_haunt -> rank() ) action_list_str += "/haunt";
       if ( level >= 81 && set_bonus.tier11_4pc_caster() ) action_list_str += "/fel_flame,if=buff.tier11_4pc_caster.react&dot.unstable_affliction.remains<8";
       if ( level >= 50 ) action_list_str += "/summon_doomguard,if=time>10";
-      if ( talent_soul_siphon -> rank() && has_wou ) action_list_str += "/wait_until_ready,if=buff.will_of_unbinding.up&cooldown.haunt.remains<action.drain_soul.tick_time&buff.will_of_unbinding.remains<cooldown.haunt.remains+action.haunt.cast_time+action.drain_soul.tick_time";
-      if ( talent_soul_siphon -> rank() ) action_list_str += "/drain_soul,interrupt=1,if=target.health_pct<=25";
+      if ( talent_soul_siphon -> rank() ) {
+        if ( has_wou )
+          action_list_str += "/drain_soul,interrupt=1,interrupt_if=buff.will_of_unbinding.up&cooldown.haunt.remains<tick_time&buff.will_of_unbinding.remains<action.haunt.cast_time+tick_time+cooldown.haunt.remains,if=target.health_pct<=25";
+        else
+          action_list_str += "/drain_soul,interrupt=1,if=target.health_pct<=25";
+      }
       if ( level >= 75 ) action_list_str += "/shadowflame";
       if ( set_bonus.tier13_4pc_caster() ) action_list_str += "/soul_fire,if=buff.soulburn.up";
       if ( talent_bane -> rank() == 3 )
