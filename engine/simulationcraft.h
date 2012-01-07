fd2e243dc318a31573d4c3fb7dc2967192159934
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-01-07 17:08:53 +0000

    Tick minor version for release
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10803 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/simulationcraft.h
+++ engine/simulationcraft.h
@@ -109,7 +109,7 @@ namespace std {using namespace tr1; }
 #include "data_definitions.hh"
 
 #define SC_MAJOR_VERSION "430"
-#define SC_MINOR_VERSION "6"
+#define SC_MINOR_VERSION "7"
 #define SC_USE_PTR ( 1 )
 #define SC_BETA ( 0 )
 #define SC_EPSILON ( 0.000001 )
