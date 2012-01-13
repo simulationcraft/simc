simulationcraft/simc

commit 50d93bafe635490307d0ecf54245c92e4f714111

Enable PTR Druid:   Caster 4pc increases Starsurge by 10% Hunter:   Bombardment no longer has a charge   LnL no longer affects Arcane Shot
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10769 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_hunter.cpp
@@ -2236,7 +2236,7 @@ struct arcane_shot_t : public hunter_attack_t
   {
     hunter_t* p = player -> cast_hunter();
 
-    if ( p -> buffs_lock_and_load -> check() )
+    if ( ! p -> dbc.ptr && p -> buffs_lock_and_load -> check() )
       return 0;
 
     return hunter_attack_t::cost();
@@ -2263,8 +2263,11 @@ struct arcane_shot_t : public hunter_attack_t
       }
     }
 
-    p -> buffs_lock_and_load -> up();
-    p -> buffs_lock_and_load -> decrement();
+    if ( p -> dbc.ptr )
+    {
+      p -> buffs_lock_and_load -> up();
+      p -> buffs_lock_and_load -> decrement();
+    }
   }
 };
 
@@ -2843,7 +2846,7 @@ struct multi_shot_t : public hunter_attack_t
     hunter_attack_t::execute();
     hunter_t* p = player -> cast_hunter();
 
-    if ( p -> buffs_bombardment -> up() )
+    if ( p -> buffs_bombardment -> up() && ! p -> dbc.ptr )
       p -> buffs_bombardment -> expire();
   }
 
@@ -4076,7 +4079,6 @@ void hunter_t::init_actions()
 
     // MAKRMANSHIP
     case TREE_MARKSMANSHIP:
-
       action_list_str += init_use_racial_actions();
       action_list_str += "/multi_shot,if=target.adds>5";
       action_list_str += "/steady_shot,if=target.adds>5";
@@ -4112,13 +4114,11 @@ void hunter_t::init_actions()
       action_list_str += "/steady_shot";
       break;
 
-
     // SURVIVAL
     case TREE_SURVIVAL:
       action_list_str += "/multi_shot,if=target.adds>2";
       action_list_str += "/cobra_shot,if=target.adds>2";
       action_list_str += "/serpent_sting,if=!ticking";
-
       action_list_str += "/explosive_shot,if=(remains<2.0)";
       action_list_str += "/explosive_trap";
       action_list_str += "/kill_shot";
@@ -4127,13 +4127,11 @@ void hunter_t::init_actions()
 //      if ( talents.black_arrow -> rank() ) action_list_str += "/black_arrow,if=!ticking";
 
       action_list_str += "/rapid_fire";
-
       action_list_str += "/arcane_shot,if=focus>=50&!buff.lock_and_load.react";
       if ( level >=81 )
         action_list_str += "/cobra_shot";
       else
         action_list_str += "/steady_shot";
-
       if ( summon_pet_str.empty() )
         summon_pet_str = "cat";
       break;

engine/sc_druid.cpp
@@ -4119,7 +4119,11 @@ struct starsurge_t : public druid_spell_t
       crit_bonus_multiplier *= 1.0 + p -> spells.moonfury -> effect2().percent();
 
     if ( p -> set_bonus.tier13_4pc_caster() )
+    {
       cooldown -> duration -= 5.0;
+      if ( p -> dbc.ptr )
+        base_multiplier *= 1.10;
+    }
 
     starfall_cd = p -> get_cooldown( "starfall" );
 

engine/simulationcraft.h
@@ -110,7 +110,7 @@ namespace std {using namespace tr1; }
 
 #define SC_MAJOR_VERSION "430"
 #define SC_MINOR_VERSION "5"
-#define SC_USE_PTR ( 0 )
+#define SC_USE_PTR ( 1 )
 #define SC_BETA ( 0 )
 #define SC_EPSILON ( 0.000001 )
 #ifndef M_PI

commit 88740d2576d46993ad74805ce66d8cc6e80b5540

Hunter:   Arcane Shot should consume LnL on live and not on the PTR   Broken in r10769
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10880 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_hunter.cpp
@@ -2264,7 +2264,7 @@ struct arcane_shot_t : public hunter_attack_t
       }
     }
 
-    if ( p -> dbc.ptr )
+    if ( ! p -> dbc.ptr )
     {
       p -> buffs_lock_and_load -> up();
       p -> buffs_lock_and_load -> decrement();