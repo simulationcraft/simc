simulationcraft/simc

commit 42d2915019c290293e74190df49d38bbc6c35122

Applied timespan_t to spell_id_t::mod_additive().
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10885 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_talent.cpp
@@ -983,6 +983,61 @@ double spell_id_t::mod_additive( property_type_t p_type ) const
   return 0.0;
 }
 
+timespan_t spell_id_t::mod_additive_duration() const
+{
+  assert(
+    (s_single     == NULL || s_single     -> misc_value1() != P_DURATION || s_single     -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[0] == NULL || s_effects[0] -> misc_value1() != P_DURATION || s_effects[0] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_DURATION || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_DURATION || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
+  );
+  return timespan_t::from_millis(mod_additive( P_DURATION ));
+}
+
+timespan_t spell_id_t::mod_additive_cast_time() const
+{
+  assert(
+    (s_single     == NULL || s_single     -> misc_value1() != P_CAST_TIME || s_single     -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[0] == NULL || s_effects[0] -> misc_value1() != P_CAST_TIME || s_effects[0] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_CAST_TIME || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_CAST_TIME || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
+  );
+  return timespan_t::from_millis(mod_additive( P_CAST_TIME ));
+}
+
+timespan_t spell_id_t::mod_additive_cooldown() const
+{
+  assert(
+    (s_single     == NULL || s_single     -> misc_value1() != P_COOLDOWN || s_single     -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[0] == NULL || s_effects[0] -> misc_value1() != P_COOLDOWN || s_effects[0] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_COOLDOWN || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_COOLDOWN || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
+  );
+  return timespan_t::from_millis(mod_additive( P_COOLDOWN ));
+}
+
+timespan_t spell_id_t::mod_additive_tick_time() const
+{
+  assert(
+    (s_single     == NULL || s_single     -> misc_value1() != P_TICK_TIME || s_single     -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[0] == NULL || s_effects[0] -> misc_value1() != P_TICK_TIME || s_effects[0] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_TICK_TIME || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_TICK_TIME || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
+  );
+  return timespan_t::from_millis(mod_additive( P_TICK_TIME ));
+}
+
+timespan_t spell_id_t::mod_additive_gcd() const
+{
+  assert(
+    (s_single     == NULL || s_single     -> misc_value1() != P_GCD || s_single     -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[0] == NULL || s_effects[0] -> misc_value1() != P_GCD || s_effects[0] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_GCD || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
+    (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_GCD || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
+  );
+  return timespan_t::from_millis(mod_additive( P_GCD ));
+}
+
 
 // ==========================================================================
 // Active Spell ID

engine/sc_action.cpp
@@ -354,7 +354,7 @@ void action_t::parse_effect_data( int spell_id, int effect_nr )
         base_crit += 0.01 * effect -> base_value();
         break;
       case P_COOLDOWN:
-        cooldown -> duration += timespan_t::from_millis( effect -> base_value() );
+        cooldown -> duration += effect -> time_value();
         break;
       default: break;
       }

engine/sc_death_knight.cpp
@@ -2413,7 +2413,7 @@ struct blood_tap_t : public death_knight_spell_t
     base_cost = 0.0; // Cost is stored as 6 in the DBC for some odd reason
     harmful   = false;
     if ( p -> talents.improved_blood_tap -> rank() )
-      cooldown -> duration += timespan_t::from_seconds( p -> talents.improved_blood_tap -> mod_additive( P_COOLDOWN ) );
+      cooldown -> duration += p -> talents.improved_blood_tap -> mod_additive_cooldown();
   }
 
   void execute()

engine/sc_druid.cpp
@@ -2075,7 +2075,7 @@ struct tigers_fury_t : public druid_cat_attack_t
     parse_options( NULL, options_str );
 
     harmful = false;
-    cooldown -> duration += timespan_t::from_seconds( p -> glyphs.tigers_fury -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> glyphs.tigers_fury -> mod_additive_cooldown();
   }
 
   virtual void execute()
@@ -2682,7 +2682,7 @@ struct healing_touch_t : public druid_heal_t
     parse_options( NULL, options_str );
 
     base_dd_multiplier *= 1.0 + p -> talents.empowered_touch -> mod_additive( P_GENERIC );
-    base_execute_time  += timespan_t::from_seconds( p -> talents.naturalist -> mod_additive( P_CAST_TIME ) );
+    base_execute_time  += p -> talents.naturalist -> mod_additive_cast_time();
     consume_ooc         = true;
 
     ns_cd = p -> get_cooldown( "natures_swiftness" );
@@ -2815,7 +2815,7 @@ struct nourish_t : public druid_heal_t
     parse_options( NULL, options_str );
 
     base_dd_multiplier *= 1.0 + p -> talents.empowered_touch -> mod_additive( P_GENERIC );
-    base_execute_time  += timespan_t::from_seconds( p -> talents.naturalist -> mod_additive( P_CAST_TIME ) );
+    base_execute_time  += p -> talents.naturalist -> mod_additive_cast_time();
   }
 
   virtual void execute()
@@ -2905,7 +2905,7 @@ struct rejuvenation_t : public druid_heal_t
     parse_options( NULL, options_str );
 
     may_crit     = p -> talents.gift_of_the_earthmother -> rank() ? true : false;
-    trigger_gcd += timespan_t::from_seconds( p -> talents.swift_rejuvenation -> mod_additive( P_GCD ) );
+    trigger_gcd += p -> talents.swift_rejuvenation -> mod_additive_gcd();
 
     additive_factors += p -> talents.genesis -> mod_additive( P_TICK_DAMAGE ) +
                         p -> talents.blessing_of_the_grove -> mod_additive( P_TICK_DAMAGE ) +
@@ -3016,7 +3016,7 @@ struct tranquility_t : public druid_heal_t
 
     // FIXME: The hot should stack
 
-    cooldown -> duration += timespan_t::from_seconds( p -> talents.malfurions_gift -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> talents.malfurions_gift -> mod_additive_cooldown();
   }
 };
 
@@ -3616,7 +3616,7 @@ struct insect_swarm_t : public druid_spell_t
     may_crit = false;
 
     // Genesis, additional time is given in ms. Current structure requires it to be converted into ticks
-    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive( P_DURATION ) / 2.0 );
+    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive_duration().total_seconds() / 2.0 );
     dot_behavior = DOT_REFRESH;
 
     if ( p -> primary_tree() == TREE_BALANCE )
@@ -3710,7 +3710,7 @@ struct moonfire_t : public druid_spell_t
     parse_options( NULL, options_str );
 
     // Genesis, additional time is given in ms. Current structure requires it to be converted into ticks
-    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive( P_DURATION ) / 2.0 );
+    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive_duration().total_seconds() / 2.0 );
     dot_behavior = DOT_REFRESH;
 
     if ( p -> primary_tree() == TREE_BALANCE )
@@ -3915,7 +3915,7 @@ struct starfire_t : public druid_spell_t
     };
     parse_options( options, options_str );
 
-    base_execute_time += timespan_t::from_seconds( p -> talents.starlight_wrath -> mod_additive( P_CAST_TIME ) );
+    base_execute_time += p -> talents.starlight_wrath -> mod_additive_cast_time();
 
     if ( p -> primary_tree() == TREE_BALANCE )
       crit_bonus_multiplier *= 1.0 + p -> spells.moonfury -> effect2().percent();
@@ -4081,7 +4081,7 @@ struct starfall_t : public druid_spell_t
     num_ticks      = 10;
     base_tick_time = timespan_t::from_seconds( 1.0 );
     hasted_ticks   = false;
-    cooldown -> duration += timespan_t::from_seconds( p -> glyphs.starfall -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> glyphs.starfall -> mod_additive_cooldown();
 
     harmful = false;
 
@@ -4245,7 +4245,7 @@ struct sunfire_t : public druid_spell_t
     parse_options( NULL, options_str );
 
     // Genesis, additional time is given in ms. Current structure requires it to be converted into ticks
-    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive( P_DURATION ) / 2.0 );
+    num_ticks   += ( int ) ( p -> talents.genesis -> mod_additive_duration().total_seconds() / 2.0 );
     dot_behavior = DOT_REFRESH;
 
     if ( p -> primary_tree() == TREE_BALANCE )
@@ -4445,7 +4445,7 @@ struct typhoon_t : public druid_spell_t
     base_dd_max           = p -> dbc.effect_max( damage_spell -> effect_id( 2 ), p -> level );
     base_multiplier      *= 1.0 + p -> talents.gale_winds -> effect1().percent();
     direct_power_mod      = damage_spell -> effect2().coeff();
-    cooldown -> duration += timespan_t::from_seconds( p -> glyphs.monsoon -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> glyphs.monsoon -> mod_additive_cooldown();
     base_cost            *= 1.0 + p -> glyphs.typhoon -> mod_additive( P_RESOURCE_COST );
   }
 };
@@ -4529,7 +4529,7 @@ struct wrath_t : public druid_spell_t
   {
     parse_options( NULL, options_str );
 
-    base_execute_time += timespan_t::from_seconds( p -> talents.starlight_wrath -> mod_additive( P_CAST_TIME ) );
+    base_execute_time += p -> talents.starlight_wrath -> mod_additive_cast_time();
     if ( p -> primary_tree() == TREE_BALANCE )
       crit_bonus_multiplier *= 1.0 + p -> spells.moonfury -> effect2().percent();
 
@@ -4967,12 +4967,12 @@ void druid_t::init_buffs()
   buffs_shooting_stars        = new buff_t( this, talents.shooting_stars -> effect_trigger_spell( 1 ), "shooting_stars", talents.shooting_stars -> proc_chance() );
   buffs_survival_instincts    = new buff_t( this, talents.survival_instincts -> spell_id(), "survival_instincts" );
   buffs_tree_of_life          = new buff_t( this, talents.tree_of_life, NULL );
-  buffs_tree_of_life -> buff_duration += timespan_t::from_seconds( talents.natural_shapeshifter -> mod_additive( P_DURATION ) );
+  buffs_tree_of_life -> buff_duration += talents.natural_shapeshifter -> mod_additive_duration();
 
   buffs_primal_madness_cat  = new stat_buff_t( this, "primal_madness_cat", STAT_MAX_ENERGY, spells.primal_madness_cat -> effect1().base_value() );
   buffs_primal_madness_bear = new      buff_t( this, "primal_madness_bear" );
-  buffs_berserk             = new      buff_t( this, "berserk", 1, timespan_t::from_seconds( 15.0 ) + timespan_t::from_seconds( glyphs.berserk -> mod_additive( P_DURATION ) ) );
-  buffs_tigers_fury         = new      buff_t( this, "tigers_fury", 1, timespan_t::from_seconds( 6.0 ) );
+  buffs_berserk             = new      buff_t( this, "berserk", 1, timespan_t::from_seconds(15.0) + glyphs.berserk -> mod_additive_duration() );
+  buffs_tigers_fury         = new      buff_t( this, "tigers_fury", 1, timespan_t::from_seconds(6.0) );
 
   // simple
   buffs_bear_form    = new buff_t( this, 5487,  "bear_form" );

engine/sc_hunter.cpp
@@ -2443,7 +2443,7 @@ struct chimera_shot_t : public hunter_attack_t
 
     normalize_weapon_speed = true;
 
-    cooldown -> duration += timespan_t::from_seconds( p -> glyphs.chimera_shot -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> glyphs.chimera_shot -> mod_additive_cooldown();
 
     consumes_tier12_4pc = true;
   }

engine/sc_mage.cpp
@@ -1572,7 +1572,7 @@ struct arcane_missiles_t : public mage_spell_t
     num_ticks += p -> talents.improved_arcane_missiles -> rank();
     hasted_ticks = false;
 
-    base_tick_time += timespan_t::from_seconds( p -> talents.missile_barrage -> mod_additive( P_TICK_TIME ) );
+    base_tick_time += p -> talents.missile_barrage -> mod_additive_tick_time();
 
     tick_spell = new arcane_missiles_tick_t( p );
   }

engine/sc_paladin.cpp
@@ -1250,7 +1250,7 @@ struct crusader_strike_t : public paladin_attack_t
     trigger_seal = true;
 
     // JotW decreases the CD by 1.5 seconds for Prot Pallies, but it's not in the tooltip
-    cooldown -> duration += timespan_t::from_seconds( p -> passives.judgements_of_the_wise -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> passives.judgements_of_the_wise -> mod_additive_cooldown();
     base_cooldown         = cooldown -> duration;
 
     base_crit       += p -> talents.rule_of_law -> mod_additive( P_CRIT );
@@ -1357,7 +1357,7 @@ struct hammer_of_justice_t : public paladin_attack_t
   {
     parse_options( NULL, options_str );
 
-    cooldown -> duration += timespan_t::from_seconds( p -> talents.improved_hammer_of_justice -> mod_additive( P_COOLDOWN ) );
+    cooldown -> duration += p -> talents.improved_hammer_of_justice -> mod_additive_cooldown();
   }
 };
 
@@ -2011,7 +2011,7 @@ struct avenging_wrath_t : public paladin_spell_t
     parse_options( NULL, options_str );
 
     harmful = false;
-    cooldown -> duration += timespan_t::from_seconds( p -> talents.sanctified_wrath -> mod_additive( P_COOLDOWN ) )
+    cooldown -> duration +=   p -> talents.sanctified_wrath -> mod_additive_cooldown()
                             + p -> talents.paragon_of_virtue -> effect2().time_value()
                             + p -> talents.shield_of_the_templar -> effect3().time_value();
   }
@@ -2460,7 +2460,7 @@ struct zealotry_t : public paladin_spell_t
     update_ready();
     p -> buffs_zealotry -> trigger();
     if ( p -> set_bonus.tier12_4pc_melee() )
-      p -> buffs_zealotry -> extend_duration( p, timespan_t::from_seconds( p -> sets -> set( SET_T12_4PC_MELEE ) -> mod_additive( P_DURATION ) ) );
+      p -> buffs_zealotry -> extend_duration( p, p -> sets -> set( SET_T12_4PC_MELEE ) -> mod_additive_duration() );
   }
 
   virtual void consume_resource()

engine/sc_rogue.cpp
@@ -1930,7 +1930,7 @@ struct expose_armor_t : public rogue_attack_t
     {
       timespan_t duration = timespan_t::from_seconds( 10 ) * combo_points_spent;
 
-      duration += timespan_t::from_seconds( p -> glyphs.expose_armor -> mod_additive( P_DURATION ) );
+      duration += p -> glyphs.expose_armor -> mod_additive_duration();
 
       if ( p -> buffs_revealing_strike -> up() )
         duration *= 1.0 + p -> buffs_revealing_strike -> value();
@@ -2452,7 +2452,7 @@ struct rupture_t : public rogue_attack_t
   {
     rogue_t* p = player -> cast_rogue();
     if ( result_is_hit( impact_result ) )
-      num_ticks = 3 + combo_points_spent + ( int )( p -> glyphs.rupture -> mod_additive( P_DURATION ) / base_tick_time.total_seconds() );
+      num_ticks = 3 + combo_points_spent + ( int )( p -> glyphs.rupture -> mod_additive_duration() / base_tick_time );
     rogue_attack_t::impact( t, impact_result, travel_dmg );
   }
 
@@ -2694,7 +2694,7 @@ struct shadow_dance_t : public rogue_attack_t
   {
     add_trigger_buff( p -> buffs_shadow_dance );
 
-    p -> buffs_shadow_dance -> buff_duration += timespan_t::from_seconds( p -> glyphs.shadow_dance -> mod_additive( P_DURATION ) );
+    p -> buffs_shadow_dance -> buff_duration += p -> glyphs.shadow_dance -> mod_additive_duration();
     if ( p -> set_bonus.tier13_4pc_melee() )
       p -> buffs_shadow_dance -> buff_duration += p -> spells.tier13_4pc -> effect1().time_value();
 
@@ -3177,7 +3177,7 @@ struct adrenaline_rush_buff_t : public buff_t
     // we track the cooldown in the actual action
     // and because of restless blades have to remove it here
     cooldown -> duration = timespan_t::zero;
-    buff_duration += timespan_t::from_seconds( p -> glyphs.adrenaline_rush -> mod_additive( P_DURATION ) );
+    buff_duration += p -> glyphs.adrenaline_rush -> mod_additive_duration();
     if ( p -> set_bonus.tier13_4pc_melee() )
       buff_duration += p -> spells.tier13_4pc -> effect2().time_value();
   }
@@ -3302,8 +3302,8 @@ struct slice_and_dice_buff_t : public buff_t
     rogue_t* p = player -> cast_rogue();
 
     timespan_t new_duration = p -> dbc.spell( id ) -> duration();
-    new_duration += timespan_t::from_seconds( 3.0 * cp );
-    new_duration += timespan_t::from_seconds( p -> glyphs.slice_and_dice -> mod_additive( P_DURATION ) );
+    new_duration += timespan_t::from_seconds( 3.0 ) * cp;
+    new_duration += p -> glyphs.slice_and_dice -> mod_additive_duration();
     new_duration *= 1.0 + p -> talents.improved_slice_and_dice -> mod_additive( P_DURATION );
 
     if ( remains_lt( new_duration ) )

commit 02d6da7db33d5bc639fd284ef25650761919d8ef

Fix broken mod_additive / timespan stuff caused by r10885.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@10922 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_talent.cpp
@@ -991,7 +991,7 @@ timespan_t spell_id_t::mod_additive_duration() const
     (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_DURATION || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
     (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_DURATION || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
   );
-  return timespan_t::from_millis(mod_additive( P_DURATION ));
+  return timespan_t::from_seconds(mod_additive( P_DURATION ));
 }
 
 timespan_t spell_id_t::mod_additive_cast_time() const
@@ -1002,7 +1002,7 @@ timespan_t spell_id_t::mod_additive_cast_time() const
     (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_CAST_TIME || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
     (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_CAST_TIME || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
   );
-  return timespan_t::from_millis(mod_additive( P_CAST_TIME ));
+  return timespan_t::from_seconds(mod_additive( P_CAST_TIME ));
 }
 
 timespan_t spell_id_t::mod_additive_cooldown() const
@@ -1013,7 +1013,7 @@ timespan_t spell_id_t::mod_additive_cooldown() const
     (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_COOLDOWN || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
     (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_COOLDOWN || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
   );
-  return timespan_t::from_millis(mod_additive( P_COOLDOWN ));
+  return timespan_t::from_seconds(mod_additive( P_COOLDOWN ));
 }
 
 timespan_t spell_id_t::mod_additive_tick_time() const
@@ -1024,7 +1024,7 @@ timespan_t spell_id_t::mod_additive_tick_time() const
     (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_TICK_TIME || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
     (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_TICK_TIME || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
   );
-  return timespan_t::from_millis(mod_additive( P_TICK_TIME ));
+  return timespan_t::from_seconds(mod_additive( P_TICK_TIME ));
 }
 
 timespan_t spell_id_t::mod_additive_gcd() const
@@ -1035,7 +1035,7 @@ timespan_t spell_id_t::mod_additive_gcd() const
     (s_effects[1] == NULL || s_effects[1] -> misc_value1() != P_GCD || s_effects[1] -> subtype() != (int) A_ADD_PCT_MODIFIER) &&
     (s_effects[2] == NULL || s_effects[2] -> misc_value1() != P_GCD || s_effects[2] -> subtype() != (int) A_ADD_PCT_MODIFIER)
   );
-  return timespan_t::from_millis(mod_additive( P_GCD ));
+  return timespan_t::from_seconds(mod_additive( P_GCD ));
 }
 
 