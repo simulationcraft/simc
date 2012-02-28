ead38f102a207ee943fa83d43d44a13b1a4bf8f9
delmortis1@gmail.com <delmortis1@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-02-28 15:05:44 +0000

    Update SC_MAJOR_VERSION and SC_MINOR_VERSION for 4.3.3 build.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@11008 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/simulationcraft.h
+++ engine/simulationcraft.h
@@ -112,8 +112,8 @@ namespace std {using namespace tr1; }
 
 #include "data_definitions.hh"
 
-#define SC_MAJOR_VERSION "432"
-#define SC_MINOR_VERSION "3"
+#define SC_MAJOR_VERSION "433"
+#define SC_MINOR_VERSION "1"
 #define SC_USE_PTR ( 0 )
 #define SC_BETA ( 0 )
 #define SC_EPSILON ( 0.000001 )
