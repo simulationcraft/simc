8e73d5ccb7d0e6f1beaabcf3bec75c9ee757780f
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-03-31 09:43:59 +0000

    Another try at fixing msvc2008
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@11243 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/simulationcraft.hpp
+++ engine/simulationcraft.hpp
@@ -53,16 +53,20 @@
 #include <typeinfo>
 #include <vector>
 
-#if _MSC_VER >= 1600 || __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+#if _MSC_VER || __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
 // Use C++11
 #include <type_traits>
 #include <unordered_map>
-#define smart_ptr unique_ptr
 #else
 // Use TR1
 #include <tr1/type_traits>
 #include <tr1/unordered_map>
 namespace std {using namespace tr1; }
+#endif
+
+#if _MSC_VER >= 1600 || __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+#define smart_ptr unique_ptr
+#else
 #define smart_ptr auto_ptr
 #define static_assert( condition, message )
 #endif
