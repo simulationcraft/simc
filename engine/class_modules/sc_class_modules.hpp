d8f6cf3e98d566ca7684dc2402cc4756c5a1c2f6
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-05-24 15:55:51 +0000

    Tick minor version ( to 501-4 ). Re-enable some class modules.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@12167 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_class_modules.hpp
+++ engine/class_modules/sc_class_modules.hpp
@@ -9,11 +9,11 @@
 #define SC_CLASS_MODULES
 
 // Enabled classes
-#define SC_DEATH_KNIGHT 0
+#define SC_DEATH_KNIGHT 1
 #define SC_DRUID        1
-#define SC_HUNTER       0
-#define SC_MAGE         0
-#define SC_MONK         0
+#define SC_HUNTER       1
+#define SC_MAGE         1
+#define SC_MONK         1
 #define SC_PALADIN      1
 #define SC_PRIEST       1
 #define SC_ROGUE        0
