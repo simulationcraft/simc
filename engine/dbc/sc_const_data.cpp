86faa6c4f6f85d3b9839364e3c1e12d891e4fe51
delmortis1@gmail.com <delmortis1@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-06-30 03:48:42 +0000

    Build number needs bumping too.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@12575 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -131,7 +131,7 @@ dbc_index_t<spellpower_data_t> idx_pd;
 } // ANONYMOUS namespace ====================================================
 
 const char* dbc_t::build_level( bool ptr )
-{ return ( SC_USE_PTR && ptr ) ? "15781" : "15781"; }
+{ return ( SC_USE_PTR && ptr ) ? "15799" : "15799"; }
 
 const char* dbc_t::wow_version( bool ptr )
 { return ( SC_USE_PTR && ptr ) ? "5.0.1" : "5.0.1"; }
