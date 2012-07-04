6525496c0a5ea9c6e680a398f563d27aff84f4ac
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-07-04 09:55:16 +0000

    DBC extractor: Don't use tabs.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@12661 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- dbc_extract/dbc/generator.py
+++ dbc_extract/dbc/generator.py
@@ -956,46 +956,46 @@ class SpellDataGenerator(DataGenerator):
         (),     
         
         # Paladin:
-        ( 	( 86700, 5 ), # Ancient Power
+        ( ( 86700, 5 ), # Ancient Power
         ),  
         
         # Hunter:
-        ( 	( 83381, 0 ), #  Kill Command base dmg
-          	( 82928, 0 ), # Aimed Shot Master Marksman
+        ( ( 83381, 0 ), # Kill Command base dmg
+          ( 82928, 0 ), # Aimed Shot Master Marksman
         ),
           
         # Rogue:
         ( ),  
         
         # Priest:
-        ( 	( 63619, 5 ), # shadowfiend "Shadowcrawl"
-        	( 94472, 0 ), # Atonement Crit
+        ( (  63619, 5 ), # Shadowfiend "Shadowcrawl"
+        	(  94472, 0 ), # Atonement Crit
         	( 124464, 3 ), ( 124465, 3 ), ( 124467, 3 ), ( 124468, 3 ), ( 124469, 3 ), # Shadow Mastery "duplicate" ticks
         	( 127627, 3 ), ( 127628, 3 ), # Shadow Cascade
         ), 
         
         # Death Knight:
-        ( 	( 70890, 0 ), # weird Scourge Strike secondary effect
+        ( ( 70890, 0 ), # weird Scourge Strike secondary effect
         	( 51963, 5 ), # gargoyle strike
         ),  
         
         # Shaman:
-        (   ( 77451, 0 ), ( 45284, 0 ), ( 45297, 0 ), 	#  Overloads
-            ( 115356, 0 ), ( 114093, 0 ), 				# Ascendance: Stormblast, offhand melee swing,
-            ( 114074, 0 ), ( 114738, 0 ),               # Ascendance: Lava Beam, Lava Beam overload
-            ( 120687, 0 ), ( 120588, 0 ),               # Stormlash, Elemental Blast overload
+        ( (  77451, 0 ), (  45284, 0 ), (  45297, 0 ), 	#  Overloads
+          ( 115356, 0 ), ( 114093, 0 ), 				        # Ascendance: Stormblast, offhand melee swing,
+          ( 114074, 0 ), ( 114738, 0 ),                 # Ascendance: Lava Beam, Lava Beam overload
+          ( 120687, 0 ), ( 120588, 0 ),                 # Stormlash, Elemental Blast overload
         ),
         
         # Mage:
         ( 
-            ( 48107, 0, False ), ( 48108, 0, False ), # Heating Up and Pyroblast! buffs
-            ( 79058, 0 ), # Mage: Mana Gem, Frostfire Orb x2, Arcane Brilliance
-            ( 88084, 5 ), ( 59637, 5 ), ( 88082, 5 ), ( 59638, 5 ), # Mirror Image spells.
-            ( 80354, 0 ),                                           # Temporal Displacement
+          ( 48107, 0, False ), ( 48108, 0, False ), # Heating Up and Pyroblast! buffs
+          ( 79058, 0 ), # Mage: Mana Gem, Frostfire Orb x2, Arcane Brilliance
+          ( 88084, 5 ), ( 59637, 5 ), ( 88082, 5 ), ( 59638, 5 ), # Mirror Image spells.
+          ( 80354, 0 ),                                           # Temporal Displacement
         ), 
         
         # Warlock:
-        ( 	( 85692, 5 ), #  doomguard doom bolt
+        ( (  85692, 5 ),       # doomguard doom bolt
         	( 115422, 2, True ), # void ray
         	( 104025, 2, True ), # immolation aura
         	( 104232, 3, True ), # destruction rain of fire
@@ -1003,14 +1003,16 @@ class SpellDataGenerator(DataGenerator):
         ),
         
         # Monk:
-        ( 	( 118864, 3 ), # Combo Breaker: Tiger Palm
+        ( ( 118864, 3 ), # Combo Breaker: Tiger Palm
         	( 116768, 3 ), # Combo Breaker: Blackout Kick 
         ),
         
         # Druid:
-        ( 	( 93402, 1, True ), ( 106996, 1, True ), ( 112071, 1, True ), 
-          	( 113769, 5 ),
-        ), # Whitelist Sunfire
+        ( (  93402, 1, True ), # Sunfire
+          ( 106996, 1, True ), # Astral Storm
+          ( 112071, 1, True ), # Celestial Alignment
+          ( 113769, 5 ),       # Wrath
+        ), 
     ]
 
     # Class specific item sets, T13, T14
