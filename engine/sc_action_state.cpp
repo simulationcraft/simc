9f1cf00eb642b0bc469ee4e463a7156293570cd1
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-07-05 09:20:03 +0000

    action_state_t::copy_state: fix rtt comparison, add some debug info.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@12697 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_action_state.cpp
+++ engine/sc_action_state.cpp
@@ -39,7 +39,11 @@ void action_t::release_state( action_state_t* s )
 void action_state_t::copy_state( const action_state_t* o )
 {
   if ( this == o || o == 0 ) return;
-  assert( typeid( this ) == typeid( o ) );
+  if( typeid( this ) != typeid( const_cast<action_state_t*>( o ) ) )
+  {
+    std::cout << "action_state_t::copy_state: state runtime types not equal! this= " << typeid( this ).name() << " o= " << typeid( const_cast<action_state_t*>( o ) ).name() << "\n";
+    assert( 0 );
+  }
 
   action = o -> action; target = o -> target;
   result = o -> result; result_amount = o -> result_amount;
