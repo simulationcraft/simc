7e56a5c3a2bceb6b0ba89951301d666e1193082a
jon@valvatne.com <jon@valvatne.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-07-08 12:37:08 +0000

    Use spell data when creating the blood fury buffs.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@12753 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_player.cpp
+++ engine/sc_player.cpp
@@ -2056,19 +2056,15 @@ void player_t::init_buffs()
     buffs.stoneform                 = buff_creator_t( this, "stoneform", find_spell( 65116 ) );
 
     // stat_buff_t( sim, name, stat, amount, max_stack, duration, cooldown, proc_chance, quiet )
-    buffs.blood_fury_ap = stat_buff_creator_t( this, "blood_fury_ap" )
-                          .max_stack( 1 )
-                          .duration( timespan_t::from_seconds( 15.0 ) )
+    buffs.blood_fury_ap = stat_buff_creator_t( this, "blood_fury_ap", find_spell( 20572 ) )
                           .stat( STAT_ATTACK_POWER )
                           .amount( is_enemy() ? 0 : floor( sim -> dbc.effect_average( sim -> dbc.spell( 33697 ) -> effectN( 1 ).id(), sim -> max_player_level ) ) );
 
-    buffs.blood_fury_sp = stat_buff_creator_t( this, "blood_fury_sp" )
-                          .max_stack( 1 )
-                          .duration( timespan_t::from_seconds( 15.0 ) )
+    buffs.blood_fury_sp = stat_buff_creator_t( this, "blood_fury_sp", find_spell( 33702 ) )
                           .stat( STAT_SPELL_POWER )
                           .amount( is_enemy() ? 0 : floor( sim -> dbc.effect_average( sim -> dbc.spell( 33697 ) -> effectN( 2 ).id(), sim -> max_player_level ) ) );
 
-      buffs.stormlash = new stormlash_buff_t( this, find_spell( 120668 ) );
+    buffs.stormlash = new stormlash_buff_t( this, find_spell( 120668 ) );
 
     double lb_amount = 0.0;
     if      ( profession[ PROF_HERBALISM ] >= 600 )
