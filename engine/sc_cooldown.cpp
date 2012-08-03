16e2c8d97e063c314413e30618d7972a79834b3d
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-08-03 23:45:26 +0000

    Oops.
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@13317 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_cooldown.cpp
+++ engine/sc_cooldown.cpp
@@ -12,7 +12,7 @@ struct recharge_event_t : event_t
   cooldown_t* cooldown;
 
   recharge_event_t( player_t* p, cooldown_t* cd, timespan_t delay = timespan_t::zero() ) :
-    event_t( p -> sim, p, "recharge_event" ).c_str() ), cooldown( cd )
+    event_t( p -> sim, p, "recharge_event" ), cooldown( cd )
   {
     sim -> add_event( this, cd -> duration + delay );
   }
