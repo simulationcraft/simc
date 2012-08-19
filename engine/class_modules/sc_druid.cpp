5ae4ee2e2e8ba50b6d16c3d1b289efb14a3b204f
Twigele <Twigele@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-08-19 16:38:25 +0000

    Druid   Balance actions: If we are one cast away form eclipse, don't bother with anything else like dot uptime.   Druid Balance T14H: Better gear/reforge choices (thanks to Tecton on EJ)   Regenerate balance profile
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@13775 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -5247,11 +5247,13 @@ void druid_t::init_actions()
       action_list_str += "/incarnation,if=talent.incarnation.enabled&(buff.lunar_eclipse.up|buff.solar_eclipse.up)";
       action_list_str += "/celestial_alignment,if=((eclipse_dir=-1&eclipse<=0)|(eclipse_dir=1&eclipse>=0))&(buff.chosen_of_elune.up|!talent.incarnation.enabled)";
       action_list_str += "/natures_vigil,if=((talent.incarnation.enabled&buff.chosen_of_elune.up)|(!talent.incarnation.enabled&buff.celestial_alignment.up))&talent.natures_vigil.enabled";
+      action_list_str += "/wrath,if=eclipse<=-70&eclipse_dir<=0";
+      action_list_str += "/starfire,if=eclipse>=60&eclipse_dir>=0";
       action_list_str += "/moonfire,if=buff.lunar_eclipse.up&(dot.moonfire.remains<(buff.natures_grace.remains-2+2*set_bonus.tier14_4pc_caster))";
       action_list_str += "/sunfire,if=buff.solar_eclipse.up&!buff.celestial_alignment.up&(dot.sunfire.remains<(buff.natures_grace.remains-2+2*set_bonus.tier14_4pc_caster))";
       action_list_str += "/moonfire,if=!dot.moonfire.ticking&!buff.celestial_alignment.up&(buff.dream_of_cenarius_damage.up|!talent.dream_of_cenarius.enabled)";
       action_list_str += "/sunfire,if=!dot.sunfire.ticking&!buff.celestial_alignment.up&(buff.dream_of_cenarius_damage.up|!talent.dream_of_cenarius.enabled)";
-      action_list_str += "/starsurge,if=(eclipse<60&eclipse_dir>0)|(eclipse>-60&eclipse_dir<0)|(eclipse>-60&eclipse<60&eclipse_dir=0)|buff.shooting_stars.up";
+      action_list_str += "/starsurge";
       action_list_str += "/starfire,if=buff.celestial_alignment.up&cast_time<buff.celestial_alignment.remains";
       action_list_str += "/wrath,if=buff.celestial_alignment.up&cast_time<buff.celestial_alignment.remains";
       action_list_str += "/starfire,if=eclipse_dir=1|(eclipse_dir=0&eclipse>0)";
