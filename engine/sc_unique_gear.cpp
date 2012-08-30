8674ed07c1875b07435d1385ebd8ced5a97033ca
delmortis1@gmail.com <delmortis1@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-08-30 14:52:58 +0000

    Priest: Tweak Tier14H gear choices.
    Profiles:
    Regenerate all to pick up changed Armory talent strings.
    
    git-svn-id: https://simulationcraft.googlecode.com/svn/branches/mop@14015 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_unique_gear.cpp
+++ engine/sc_unique_gear.cpp
@@ -2489,10 +2489,10 @@ bool unique_gear::get_equip_encoding( std::string&       encoding,
   else if ( name == "rosary_of_light"                     ) e = "OnAttackHit_1149Crit_15%_20Dur_45Cd";   // TO-DO: Confirm ICD. Min seen 50.934sec.
   else if ( name == "varothens_brooch"                    ) e = "OnAttackHit_1149Mastery_20%_20Dur_45Cd";   // TO-DO: Confirm ICD. Min seen 50.192sec.
 
-  // PvP Trinkets (FIXME: Confirm proc data.. tooltips are broken and spells are not really finalized)
+  // Cata PvP Trinkets 
   //390
-  if      ( name == "cataclysmic_gladiators_insignia_of_victory"   ) e = "OnAttackHit_1452Str_15%_20Dur_55Cd"; // TO-DO: Confirm ICD - using ICD of Dominance
-  else if ( name == "cataclysmic_gladiators_insignia_of_conquest"  ) e = "OnAttackHit_1452Agi_15%_20Dur_55Cd"; // TO-DO: Confirm ICD - using ICD of Dominance
+  if      ( name == "cataclysmic_gladiators_insignia_of_victory"   ) e = "OnAttackHit_1452Str_15%_20Dur_55Cd";
+  else if ( name == "cataclysmic_gladiators_insignia_of_conquest"  ) e = "OnAttackHit_1452Agi_15%_20Dur_55Cd";
   else if ( name == "cataclysmic_gladiators_insignia_of_dominance" ) e = "OnSpellDamage_1452SP_25%_20Dur_55Cd"; 
 
   // MoP
