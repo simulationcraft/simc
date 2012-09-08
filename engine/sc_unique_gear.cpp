c88ca4ed6fb0dfce7e0f6f3efcdf69ae8d19bc61
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-09-08 17:12:17 +0000

    Fix Darkmoon Card: Hurricane on-equip string to actually work in the sim.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14143 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_unique_gear.cpp
+++ engine/sc_unique_gear.cpp
@@ -2533,7 +2533,7 @@ bool unique_gear::get_equip_encoding( std::string&       encoding,
 
   // Discharge Procs
   else if ( name == "bandits_insignia"                    ) e = "OnAttackHit_1880Arcane_15%_45Cd";
-  else if ( name == "darkmoon_card_hurricane"             ) e = "OnAttackHit_-7000Nature_1PPM_nocrit_nobuffs";
+  else if ( name == "darkmoon_card_hurricane"             ) e = "OnAttackHit_-7000Nature_1PPM_nocrit";
   else if ( name == "darkmoon_card_volcano"               ) e = "OnSpellDamage_1200+10Fire_1600Int_30%_12Dur_45Cd";
   else if ( name == "extract_of_necromantic_power"        ) e = "OnSpellTickDamage_1050Shadow_10%_15Cd";
   else if ( name == "lightning_capacitor"                 ) e = "OnSpellCrit_750Nature_3Stack_2.5Cd";
