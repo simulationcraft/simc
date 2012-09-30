7b367009aec2854aeb713fab5a568e80c6d1c142
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-09-30 10:35:18 +0000

    Item: Don't error on back/cloak addon 'goblin_glider'.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14362 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_item.cpp
+++ engine/sc_item.cpp
@@ -871,7 +871,8 @@ bool item_t::decode_addon()
        encoded_addon_str == "spinal_healing_injector" ||
        encoded_addon_str == "z50_mana_gulper"         ||
        encoded_addon_str == "reticulated_armor_webbing" ||
-       encoded_addon_str == "mind_amplification_dish" )
+       encoded_addon_str == "mind_amplification_dish" ||
+       encoded_addon_str == "goblin_glider" )
   {
     unique_addon = true;
     return true;
