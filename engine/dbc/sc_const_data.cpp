fe3f5d3436bd43e07f3d897f4e806f0ac2e6c552
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-10-15 12:03:37 +0000

    Shaman: Hotfix Lightning Bolt.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14426 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -221,6 +221,14 @@ void dbc_t::apply_hotfixes()
   const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._base_value = 7;
   s = spell_data_t::find( 7302, true ); // Frost Armor
   const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._base_value = 7;
+  
+  // Shaman
+  s = spell_data_t::find( 403 ); // Lightning Bolt
+  const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._coeff = 0.739;
+  const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._m_avg = 1.14;
+  s = spell_data_t::find( 45284 ); // Lightning Bolt overload
+  const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._coeff = 0.554;
+  const_cast<spelleffect_data_t&>( s -> effectN( 1 ) )._m_avg = 0.856;
 
   // Misc
   // Zen Alchemist Stone
