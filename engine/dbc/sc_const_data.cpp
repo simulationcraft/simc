5d9c7bfc597aec46215e1c703fdf5249973074e5
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-10-24 19:46:41 +0000

    PTR spell data. Regenerated live spell data with a fresh cache.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14479 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -141,10 +141,10 @@ dbc_index_t<spellpower_data_t> idx_pd;
 } // ANONYMOUS namespace ====================================================
 
 const char* dbc_t::build_level( bool ptr )
-{ return ( SC_USE_PTR && ptr ) ? "16048" : "16057"; }
+{ return ( SC_USE_PTR && ptr ) ? "16173" : "16057"; }
 
 const char* dbc_t::wow_version( bool ptr )
-{ return ( SC_USE_PTR && ptr ) ? "5.0.5" : "5.0.5"; }
+{ return ( SC_USE_PTR && ptr ) ? "5.1.0" : "5.0.5"; }
 
 void dbc_t::apply_hotfixes()
 {
