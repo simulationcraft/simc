e65131a2182f3769fba7930d47900bfb8a111d45
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-10-25 19:28:59 +0000

    GUI: Change default item source order.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14481 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- qt/sc_window.cpp
+++ qt/sc_window.cpp
@@ -52,13 +52,13 @@ static OptionEntry* getItemSourceOptions()
 {
   static OptionEntry options[] =
   {
+    { "Wowhead.com (MoP)",   "mophead", "Remote Wowhead.com Mists of Pandaria item data source" },
     { "Local Item Database", "local",   "Use Simulationcraft item database" },
     { "Blizzard API",        "bcpapi",  "Remote Blizzard Community Platform API source" },
     { "Wowhead.com",         "wowhead", "Remote Wowhead.com item data source" },
     { "Mmo-champion.com",    "mmoc",    "Remote Mmo-champion.com item data source" },
     { "Blizzard Armory",     "armory",  "Remote item database from Blizzard (DEPRECATED, SHOULD NOT BE USED)" },
     { "Wowhead.com (PTR)",   "ptrhead", "Remote Wowhead.com PTR item data source" },
-    { "Wowhead.com (MoP)",   "mophead", "Remote Wowhead.com Mists of Pandaria item data source" },
     { NULL, NULL, NULL }
   };
 
