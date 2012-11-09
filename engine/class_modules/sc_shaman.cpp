9eb1fe5c0b6f13334da0b493b156985254b88bf1
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-11-09 09:25:04 +0000

    Shaman: Make Unleash Wind proc Maelstrom Weapon on PTR.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14586 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -1810,7 +1810,7 @@ struct unleash_wind_t : public shaman_melee_attack_t
     shaman_melee_attack_t( "unleash_wind", player, player -> dbc.spell( 73681 ) )
   {
     background            = true;
-    windfury              = false;
+    windfury              = player -> dbc.ptr;
     maelstrom             = false;
     primal_wisdom         = false;
     may_dodge = may_parry = false;
