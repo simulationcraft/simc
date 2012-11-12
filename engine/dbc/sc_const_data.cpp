b6363f996f81d70f46f3241df4a3e47d6aebcd01
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-11-12 17:46:05 +0000

    PTR 16281 data.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@14628 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -141,7 +141,7 @@ dbc_index_t<spellpower_data_t> idx_pd;
 } // ANONYMOUS namespace ====================================================
 
 int dbc_t::build_level( bool ptr )
-{ return ( SC_USE_PTR && ptr ) ? 16257 : 16057; }
+{ return ( SC_USE_PTR && ptr ) ? 16281 : 16057; }
 
 const char* dbc_t::wow_version( bool ptr )
 { return ( SC_USE_PTR && ptr ) ? "5.1.0" : "5.0.5"; }
