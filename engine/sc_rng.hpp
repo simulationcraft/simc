simulationcraft/simc

commit 63f496c8b400c42f45021a12db8fa88fe5066db8

Add new module sc_io.cpp to interpose a layer between SimC and various OS APIs so we have an opportunity to encode/decode between our internal UTF-8 strings and what the OS expects. (**NOTE: Did not add to the XCode project, I don't grok XCode.) Basically dealing with the fact that Windows wants UTF-16 and every other OS expects UTF-8. For now, handles translation of command line arguments (io::utf8_args) and filenames passed to fopen (io::fopen) and ofstream::open (io::ofstream).
Add option_db_t::parse_text() to break a chunk of text up into lines to be parsed. The GUI uses it to interface with the simulator. (Fixes bug I introduced in r15056.)

On Windows, use API functions to retrieve Unicode command line args instead of the non-standard "int wmain( int argc, wchar_t** argv )" entry point. This solution works with MSVC and a much wider selection of MinGW compilers. (Fixes build issue I caused with r15060.)

report::print_html() now returns early upon failing to open the output file.

git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15065 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/simulationcraft.hpp
@@ -12,7 +12,8 @@
 
 // Platform Initialization ==================================================
 
-#if defined( _MSC_VER ) || defined( __MINGW__ ) || defined( __MINGW32__ ) || defined( _WINDOWS ) || defined( WIN32 )
+#if defined( WIN32 ) || defined( _WIN32 ) || defined( __WIN32 )
+#  define SC_WINDOWS
 #  define WIN32_LEAN_AND_MEAN
 #  define VC_EXTRALEAN
 #  ifndef _CRT_SECURE_NO_WARNINGS
@@ -22,12 +23,17 @@
 #  ifndef UNICODE
 #    define UNICODE
 #  endif
+#  if defined(_MSC_VER)
+#    define SC_MSC
+#  elif defined( __MINGW__ ) || defined( __MINGW32__ )
+#    define SC_MINGW
+#  endif
 #else
 #  define DIRECTORY_DELIMITER "/"
 #  define SC_SIGACTION
 #endif
 
-#if ( _MSC_VER && _MSC_VER < 1600 )
+#if defined( _MSC_VER ) && _MSC_VER < 1600
 #  include "../vs/stdint.h"
 #else
 #  include <stdint.h>
@@ -43,6 +49,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
+#include <fstream>
 #include <functional>
 #include <iomanip>
 #include <iostream>
@@ -1173,9 +1180,6 @@ int fprintf( FILE *stream, const char *format, ... ) PRINTF_ATTRIBUTE( 2,3 );
 int vfprintf( FILE *stream, const char *format, va_list fmtargs ) PRINTF_ATTRIBUTE( 2,0 );
 int vprintf( const char *format, va_list fmtargs ) PRINTF_ATTRIBUTE( 1,0 );
 
-std::string& str_to_utf8( std::string& str );
-// std::string& str_to_latin1( std::string& str );
-
 std::string encode_html( const std::string& );
 std::string decode_html( const std::string& str );
 std::string& urlencode( std::string& str );
@@ -1203,6 +1207,65 @@ void fuzzy_stats( std::string& encoding, const std::string& description );
 template <class T>
 int numDigits( T number );
 } // namespace util
+
+namespace io {
+
+// Converts a wide (UTF-16 or UTF-32) string to narrow (UTF-8)
+std::string narrow( const wchar_t* wstr );
+inline std::string narrow( const std::wstring& wstr )
+{ return narrow( wstr.c_str() ); }
+
+// Converts a narrow (UTF-8) string to wide (UTF-16 or UTF-32)
+std::wstring widen( const char* str );
+std::wstring widen( const std::string& str );
+
+std::string latin1_to_utf8( const std::string& str );
+std::string utf8_to_latin1( const std::string& str );
+std::string maybe_latin1_to_utf8( const std::string& str );
+
+// Like std::fopen, but works with UTF-8 filenames on windows.
+FILE* fopen( const std::string& filename, const char* mode );
+
+// RAII wrapper for FILE*.
+class cfile : public noncopyable
+{
+  FILE* file;
+public:
+  cfile( const std::string& filename, const char* mode ) : file( fopen( filename, mode ) ) {}
+  explicit cfile( FILE* f ) : file( f ) {}
+  ~cfile() { if ( file ) fclose( file ); }
+
+  operator FILE* () const { return file; }
+
+  void reset( FILE* f )
+  { if ( file ) fclose( file ); file = f; }
+
+  friend int fclose( cfile& file )
+  { int rval = std::fclose( file.file ); file.file = NULL; return rval; }
+};
+
+class ofstream : public std::ofstream
+{
+public:
+  ofstream& printf( const char* format, ... );
+  void open( sim_t* sim, const std::string& filename, openmode mode = out | trunc );
+  void open( const char* filename, openmode mode = out | trunc );
+  void open( const std::string& filename, openmode mode = out | trunc )
+  { return open( filename.c_str(), mode ); }
+};
+
+#ifndef SC_WINDOWS
+FILE* fopen( const std::string& filename, const char* mode )
+{ return std::fopen( filename.c_str(), mode ); }
+#endif
+
+class utf8_args : public std::vector<std::string>
+{
+public:
+  utf8_args( int argc, char** argv );
+};
+
+} // namespace io
 
 // Spell information struct, holding static functions to output spell data in a human readable form
 
@@ -2034,6 +2097,7 @@ struct option_db_t : public std::vector<option_tuple_t>
   bool parse_file( FILE* file );
   bool parse_token( const std::string& token );
   bool parse_line( const std::string& line );
+  bool parse_text( const std::string& text );
   bool parse_args( const std::vector<std::string>& args );
 };
 

CMakeLists.txt
@@ -40,6 +40,7 @@ SET(SIMC_ENGINE_SOURCE
   "engine/sc_expressions.cpp"
   "engine/sc_gear_stats.cpp"
   "engine/sc_heal.cpp"
+  "engine/sc_io.cpp"
   "engine/sc_item.cpp"
   "engine/sc_option.cpp"
   "engine/sc_pet.cpp"

engine/Makefile
@@ -65,7 +65,6 @@ ifeq (WINDOWS,${OS})
 MODULE_EXT = .exe
 CPP_FLAGS += -DUNICODE
 LINK_LIBS  = -lws2_32 -lwininet
-LINK_FLAGS += -municode
 ifeq (${NO_STATIC},)
 LINK_FLAGS += -static
 endif
@@ -105,6 +104,7 @@ SRC = \
 	sc_event.cpp \
 	sc_gear_stats.cpp \
 	sc_heal.cpp \
+	sc_io.cpp \
 	sc_item.cpp \
 	sc_main.cpp \
 	sc_option.cpp \

engine/interfaces/sc_http.cpp
@@ -79,7 +79,7 @@ static bool download( url_cache_entry_t&,
   return false;
 }
 
-#elif defined( _MSC_VER ) || defined( __MINGW32__ )
+#elif defined( SC_WINDOWS )
 
 // ==========================================================================
 // HTTP-DOWNLOAD FOR WINDOWS
@@ -92,6 +92,8 @@ static bool download( url_cache_entry_t&,
 static bool download( url_cache_entry_t& entry,
                       const std::string& url )
 {
+  // Requires cache_mutex to be held.
+
   class InetWrapper : public noncopyable
   {
   public:
@@ -106,55 +108,54 @@ static bool download( url_cache_entry_t& entry,
   if ( !hINet )
   {
     // hINet = InternetOpen( L"simulationcraft", INTERNET_OPEN_TYPE_PROXY, "proxy-server", NULL, 0 );
-    hINet = InternetOpen( TEXT( "simulationcraft" ), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0 );
+    hINet = InternetOpenW( L"simulationcraft", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0 );
     if ( ! hINet )
       return false;
   }
 
-  std::string headers = cookies;
+  std::wstring headers = io::widen( cookies );
 
   if ( ! entry.last_modified_header.empty() )
   {
-    headers += "If-Modified-Since: ";
-    headers += entry.last_modified_header;
-    headers += "\r\n";
+    headers += L"If-Modified-Since: ";
+    headers += io::widen( entry.last_modified_header );
+    headers += L"\r\n";
   }
 
-  std::string URL = url;
-  util::urlencode( URL );
-
-  InetWrapper hFile( InternetOpenUrlA( hINet, URL.c_str(), headers.data(), static_cast<DWORD>( headers.length() ),
+  InetWrapper hFile( InternetOpenUrlW( hINet, io::widen( url ).c_str(), headers.data(), static_cast<DWORD>( headers.length() ),
                                        INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE, 0 ) );
   if ( ! hFile )
     return false;
 
-  char buffer[ NETBUFSIZE ];
+  union {
+    char chars[ NETBUFSIZE ];
+    wchar_t wchars[ ( NETBUFSIZE + 1 ) / 2 ];
+  } buffer;
   DWORD amount = sizeof( buffer );
-  if ( ! HttpQueryInfoA( hFile, HTTP_QUERY_STATUS_CODE, buffer, &amount, 0 ) )
+  if ( ! HttpQueryInfoA( hFile, HTTP_QUERY_STATUS_CODE, buffer.chars, &amount, 0 ) )
     return false;
 
-  if ( ! std::strcmp( buffer, "304" ) )
+  if ( ! std::strcmp( buffer.chars, "304" ) )
   {
     entry.validated = cache::era();
     return true;
   }
 
-  std::string result;
-  while ( InternetReadFile( hFile, buffer, sizeof( buffer ), &amount ) )
+  entry.result.clear();
+  while ( InternetReadFile( hFile, buffer.chars, sizeof( buffer ), &amount ) )
   {
     if ( amount == 0 )
       break;
-    result.append( &buffer[ 0 ], &buffer[ amount ] );
+    entry.result.append( buffer.chars, buffer.chars + amount );
   }
 
-  entry.result = result;
   entry.modified = entry.validated = cache::era();
 
   entry.last_modified_header.clear();
   amount = sizeof( buffer );
   DWORD index = 0;
-  if ( HttpQueryInfoA( hFile, HTTP_QUERY_LAST_MODIFIED, buffer, &amount, &index ) )
-    entry.last_modified_header.assign( &buffer[ 0 ], &buffer[ amount ] );
+  if ( HttpQueryInfoW( hFile, HTTP_QUERY_LAST_MODIFIED, buffer.wchars, &amount, &index ) )
+    entry.last_modified_header = io::narrow( buffer.wchars );
 
   return true;
 }
@@ -162,11 +163,11 @@ static bool download( url_cache_entry_t& entry,
 #else
 
 // ==========================================================================
-// HTTP-DOWNLOAD FOR WINDOWS (MinGW Only) and FOR POSIX COMPLIANT PLATFORMS
+// HTTP-DOWNLOAD FOR POSIX COMPLIANT PLATFORMS
 // ==========================================================================
 
-#if defined( __MINGW32__ )
-
+#if defined( SC_MINGW )
+// Keep this code compiling on MinGW just for the hell of it.
 #include <windows.h>
 #include <wininet.h>
 #include <Winsock2.h>
@@ -361,7 +362,7 @@ static std::string build_request( const url_t&       url,
 static bool download( url_cache_entry_t& entry,
                       const std::string& url )
 {
-#if defined( __MINGW32__ )
+#if defined( SC_MINGW )
 
   static bool initialized = false;
   if ( ! initialized )

engine/interfaces/sc_rawr.cpp
@@ -735,7 +735,7 @@ player_t* load_player_xml( sim_t* sim,
 player_t* rawr::load_player( sim_t* sim,
                              const std::string& character_file )
 {
-  FILE* f = fopen( character_file.c_str(), "r" );
+  io::cfile f( character_file, "r" );
   if ( ! f )
   {
     sim -> errorf( "Unable to open Rawr Character Save file '%s'\n", character_file.c_str() );

engine/report/sc_report.cpp
@@ -348,7 +348,7 @@ void report::print_profiles( sim_t* sim )
 
     if ( !p -> report_information.save_gear_str.empty() ) // Save gear
     {
-      file = fopen( p -> report_information.save_gear_str.c_str(), "w" );
+      file = io::fopen( p -> report_information.save_gear_str, "w" );
       if ( ! file )
       {
         sim -> errorf( "Unable to save gear profile %s for player %s\n", p -> report_information.save_gear_str.c_str(), p -> name() );
@@ -364,7 +364,7 @@ void report::print_profiles( sim_t* sim )
 
     if ( !p -> report_information.save_talents_str.empty() ) // Save talents
     {
-      file = fopen( p -> report_information.save_talents_str.c_str(), "w" );
+      file = io::fopen( p -> report_information.save_talents_str, "w" );
       if ( ! file )
       {
         sim -> errorf( "Unable to save talents profile %s for player %s\n", p -> report_information.save_talents_str.c_str(), p -> name() );
@@ -380,7 +380,7 @@ void report::print_profiles( sim_t* sim )
 
     if ( !p -> report_information.save_actions_str.empty() ) // Save actions
     {
-      file = fopen( p -> report_information.save_actions_str.c_str(), "w" );
+      file = io::fopen( p -> report_information.save_actions_str, "w" );
       if ( ! file )
       {
         sim -> errorf( "Unable to save actions profile %s for player %s\n", p -> report_information.save_actions_str.c_str(), p -> name() );
@@ -411,7 +411,7 @@ void report::print_profiles( sim_t* sim )
 
     if ( file_name.empty() ) continue;
 
-    file = fopen( file_name.c_str(), "w" );
+    file = io::fopen( file_name, "w" );
     if ( ! file )
     {
       sim -> errorf( "Unable to save profile %s for player %s\n", file_name.c_str(), p -> name() );
@@ -429,7 +429,7 @@ void report::print_profiles( sim_t* sim )
   if ( sim -> save_raid_summary )
   {
     static const char* const filename = "Raid_Summary.simc";
-    FILE* file = fopen( filename, "w" );
+    FILE* file = io::fopen( filename, "w" );
     if ( ! file )
     {
       sim -> errorf( "Unable to save overview profile %s\n", filename );
@@ -478,7 +478,7 @@ void report::print_spell_query( sim_t* sim, unsigned level )
   xml_node_t* root = NULL;
   if ( ! sim -> spell_query_xml_output_file_str.empty() )
   {
-    file = fopen( sim -> spell_query_xml_output_file_str.c_str(), "w" );
+    file = io::fopen( sim -> spell_query_xml_output_file_str.c_str(), "w" );
     if ( ! file )
     {
       sim -> errorf( "Unable to open spell query xml output file '%s', using stdout instead\n", sim -> spell_query_xml_output_file_str.c_str() );

engine/report/sc_report.hpp
@@ -85,71 +85,35 @@ std::string dps_error( player_t& );
 namespace report
 {
 
-struct sc_ofstream : public std::ofstream
+class indented_stream : public io::ofstream
 {
-  int level;
-
-  sc_ofstream() :
-    std::ofstream(), level( 0 ) {}
-
-  void set_level( int l )
-  { level = l; }
-
-  sc_ofstream& operator+=( int c ) { assert( level + c >= 0 ); level += c; return *this; }
-  sc_ofstream& operator-=( int c ) { assert( level - c >= 0 ); level -= c; return *this; }
-
-  sc_ofstream& operator++() { ++level; return *this; }
-  sc_ofstream& operator--() { assert( level > 0 ); --level; return *this; }
-
+  int level_;
   virtual const char* _tabs() const = 0;
 
-  sc_ofstream& printf( const char* format, ... )
-  {
-    char buffer[ 16384 ];
+public:
+  indented_stream() : level_( 0 ) {}
 
-    va_list fmtargs;
-    va_start( fmtargs, format );
-    int rval = ::vsnprintf( buffer, sizeof( buffer ), format, fmtargs );
-    va_end( fmtargs );
+  int level() const { return level_; }
+  void set_level( int l ) { level_ = l; }
 
-    if ( rval >= 0 )
-      assert( static_cast<size_t>( rval ) < sizeof( buffer ) );
-
-    *this << buffer;
-    return *this;
-  }
-
-  sc_ofstream& tabs()
+  indented_stream& tabs()
   {
     *this << _tabs();
     return *this;
   }
 
-  // Opens the file specified by filename
-  void open_file( sim_t* sim, const char* filename )
-  {
-    exceptions( std::ofstream::failbit | std::ofstream::badbit );
-    try
-    {
-      open( filename, std::ios_base::in|std::ios_base::out|std::ios_base::trunc );
-    }
-    catch ( std::ofstream::failure& )
-    {
-      sim -> errorf( "Unable to open html file '%s'.\n", filename );
-      return;
-    }
-  }
+  ofstream& operator+=( int c ) { assert( level_ + c >= 0 ); level_ += c; return *this; }
+  ofstream& operator-=( int c ) { assert( level_ - c >= 0 ); level_ -= c; return *this; }
+
+  ofstream& operator++() { ++level_; return *this; }
+  ofstream& operator--() { assert( level_ > 0 ); --level_; return *this; }
 };
 
-struct sc_html_stream : public sc_ofstream
+struct sc_html_stream : public indented_stream
 {
-
-  sc_html_stream() :
-    sc_ofstream() {}
-
   virtual const char* _tabs() const
   {
-    switch ( level )
+    switch ( level() )
     {
     case  0: return "";
     case  1: return "\t";
@@ -168,8 +132,6 @@ struct sc_html_stream : public sc_ofstream
     }
   }
 };
-
-
 
 
 void generate_player_charts         ( player_t*, player_t::report_information_t& );

engine/report/sc_report_html_sim.cpp
@@ -4273,7 +4273,9 @@ void print_html( sim_t* sim )
 
   // Setup file stream and open file
   report::sc_html_stream s;
-  s.open_file( sim, sim -> html_file_str.c_str() );
+  s.open( sim, sim -> html_file_str );
+  if ( ! s )
+    return;
 
   report::generate_sim_report_information( sim, sim -> report_information );
 

commit 4709bc7d094683a2edd2f64f2da7254e18b62481

Fix compilation errors introduced in r15065 and r15066.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15070 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_rng.hpp
@@ -98,7 +98,12 @@ public:
 
 #if defined(__SSE2__) || defined(_M_X64)
 #define SC_USE_SSE2
+#if defined(_M_X64)
 #include <intrin.h>
+#else
+#include <emmintrin.h>
+#include <mm_malloc.h>
+#endif
 
 /** mask data for sse2 */
 static const __m128i sse2_param_mask = _mm_set_epi32( DSFMT_MSK32_3, DSFMT_MSK32_4,

engine/simulationcraft.hpp
@@ -1255,7 +1255,7 @@ public:
 };
 
 #ifndef SC_WINDOWS
-FILE* fopen( const std::string& filename, const char* mode )
+inline FILE* fopen( const std::string& filename, const char* mode )
 { return std::fopen( filename.c_str(), mode ); }
 #endif
 