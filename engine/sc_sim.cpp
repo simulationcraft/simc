fd1f383588c527b93b7520556c3b738f04e5232c
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2012-12-18 22:25:09 +0000

    rng: whooops, let's be random again!
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15118 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_sim.cpp
+++ engine/sc_sim.cpp
@@ -1396,7 +1396,7 @@ bool sim_t::init()
 
   // Seed RNG
   if ( seed == 0 )
-    seed = deterministic_rng ? (int) time( NULL ) : 31459 + thread_index;
+    seed = deterministic_rng ? ( 31459 + thread_index ) : (int) time( NULL );
   rng.seed( seed );
 
   // Timing wheel depth defaults to about 17 minutes with a granularity of 32 buckets per second.
