5755a4f929603e677ddb0620f459a4f33f4dbfee
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-01-03 11:28:22 +0000

    Flip versioning information.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15239 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -240,10 +240,10 @@ tokenized_map_t<talent_data_t> tokenized_talent_map;
 } // ANONYMOUS namespace ====================================================
 
 int dbc::build_level( bool ptr )
-{ return maybe_ptr( ptr ) ? 16309 : 16408; }
+{ return maybe_ptr( ptr ) ? 16408 : 16309; }
 
 const char* dbc::wow_version( bool ptr )
-{ return maybe_ptr( ptr ) ? "5.1.0" : "5.2.0"; }
+{ return maybe_ptr( ptr ) ? "5.2.0" : "5.1.0"; }
 
 
 const item_data_t* dbc::items( bool ptr )
