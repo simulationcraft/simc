69119db801b3d4cacff3f6b5d0b7bbb8465ffe40
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-01-10 21:09:55 +0000

    Update generate_profiles.bat to be dynamic. Remove profiles/generate_profiles.simc as it's outdated.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15349 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -1,22 +1,5 @@
+del /s simc_cache.dat
 
-setlocal
-
-set SIMC=%~dp0\simc.exe
-set BASEDIR=%~dp0
-
-
-cd %BASEDIR%\profiles\mop_test
-%SIMC% generate_priests.simc
-%SIMC% generate_priests.simc
-cd %BASEDIR%\profiles\PreRaid
-%SIMC% generate_PreRaid.simc
-cd %BASEDIR%\profiles\RaidDummy
-%SIMC% generate_dummy.simc
-cd %BASEDIR%\profiles\Tier13H
-%SIMC% generate_profiles_T13H.simc
-cd %BASEDIR%\profiles\Tier14N
-%SIMC% generate_T14N.simc
-cd %BASEDIR%\profiles\Tier14H
-%SIMC% generate_T14H.simc
+forfiles -s -m generate*.simc -c "cmd /c %~dp0\simc.exe @file"
 
 pause
