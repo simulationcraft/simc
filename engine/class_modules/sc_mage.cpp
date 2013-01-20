5de36ff5e285a1031e97b511654d681e56cd26b9
damien.thivolle@gmail.com <damien.thivolle@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-01-20 13:23:19 +0000

    Mage:   Improve Arcane rotation slightly when Rune of Power or Incanter's Ward are chosen as Tier 6 talents.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15412 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -3825,8 +3825,9 @@ void mage_t::init_actions()
       }
       if ( talents.rune_of_power -> ok() || talents.incanters_ward -> ok() )
       {
-        add_action( "Arcane Blast", "if=mana.pct>92|buff.arcane_charge.remains<3|(cooldown.mana_gem.remains<=9&!mana.pct<=80)" );
+        add_action( "Arcane Blast", "if=mana.pct>92|buff.alter_time.up|buff.arcane_charge.remains<3" );
         add_action( "Arcane Missiles", "if=buff.arcane_missiles.react&(cooldown.alter_time_activate.remains>4|target.time_to_die<10)" );
+        add_action( "Arcane Blast", "if=cooldown.mana_gem.remains<=9&!mana.pct<=80" );
         if ( talents.scorch -> ok() )
           add_action( "Scorch", "if=buff.alter_time.down" );
       }
