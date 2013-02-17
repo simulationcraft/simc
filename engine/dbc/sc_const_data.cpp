151a4db111bc60d58960a31514e0b16b9091e0a6
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-02-17 00:16:43 +0000

    PTR build 16577.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15553 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -240,7 +240,7 @@ tokenized_map_t<talent_data_t> tokenized_talent_map;
 } // ANONYMOUS namespace ====================================================
 
 int dbc::build_level( bool ptr )
-{ return maybe_ptr( ptr ) ? 16562 : 16357; }
+{ return maybe_ptr( ptr ) ? 16577 : 16357; }
 
 const char* dbc::wow_version( bool ptr )
 { return maybe_ptr( ptr ) ? "5.2.0" : "5.1.0"; }
