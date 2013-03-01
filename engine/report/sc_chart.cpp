26f6d9309a447e02519afe8de229d2250bfbb26f
swbusche@gmail.com <swbusche@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-03-01 02:53:59 +0000

    Chart: remove unused variable
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15720 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/report/sc_chart.cpp
+++ engine/report/sc_chart.cpp
@@ -1460,7 +1460,6 @@ std::string chart::reforge_dps( player_t* p )
   if ( num_stats == 2 )
   {
     int num_points = ( int ) pd.size();
-    int range = static_cast< int >( std::fabs( pd[ 0 ][ 0 ].value ) );
     std::vector<stat_e> stat_indices = p -> sim -> reforge_plot -> reforge_plot_stat_indices;
     plot_data_t& baseline = pd[ num_points / 2 ][ 2 ];
     double min_delta = baseline.value - ( min_dps - baseline.error / 2 );
