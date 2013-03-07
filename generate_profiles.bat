736ccda4c74e2684370b1c57677fac059f60b78f
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-03-07 14:30:00 +0000

    Regenerate all the profiles:   Remove invalid glyphs from the PreRaid profiles   Warlock_Destruction_T14H.simc had hardcoded profile overwritten   generate_Mage_T14N.simc had the wrong name for the neck slot   generate_Mage_T15H.simc had paths in save=   generate_Priest_T15N.simc still had ptr=1   The Death_Knight_Unholy_T15N.simc profile was converting to a frost profile
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15823 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -3,6 +3,6 @@ del /s simc_cache.dat
 :: Preraid doesn't match the typical pattern
 %~dp0\simc.exe generate_PreRaid.simc
 
-forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0\simc.exe @file"
+forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
 
 pause
