cbf42e10558692328a6175f8582d1e1c673972d7
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-03-08 15:50:09 +0000

    Regenerate spell data.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15860 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- dbc_extract/generate.sh
+++ dbc_extract/generate.sh
@@ -30,7 +30,7 @@ fi
 
 
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t talent                  > $OUTPATH/sc_talent_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT --itemcache=$CACHEDIR -b $BUILD$PTR -t spell> $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR -b $BUILD$PTR -t spell> $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t scale                   > $OUTPATH/sc_scale_data${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t class_list              > $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t spec_spell_list        >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
@@ -38,7 +38,7 @@ fi
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t racial_list            >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t glyph_list             >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t set_list               >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT --itemcache=$CACHEDIR -b $BUILD$PTR -t item > $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR -b $BUILD$PTR -t item > $OUTPATH/sc_item_data${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t random_property_points >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t random_suffix          >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT -b $BUILD$PTR -t item_ench              >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
