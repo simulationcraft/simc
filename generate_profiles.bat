3269cab927b89fed31aa2f36a2c2b1f6f35081a2
swbusche <swbusche@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-03-13 14:33:49 +0000

    generate_profiles.bat fix Preraid to save in the Preraid directory Update all the profiles
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@15926 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -1,7 +1,9 @@
 del /s simc_cache.dat
 
 :: Preraid doesn't match the typical pattern
+cd profiles/PreRaid
 %~dp0\simc.exe generate_PreRaid.simc
+cd ../..
 
 forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
 
