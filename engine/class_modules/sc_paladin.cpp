simulationcraft/simc

commit a9ed0324813ffcd0259e8c57b4819fc1e4411057

Cache: Add player_multiplier and player_heal_multiplier cache.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16031 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/class_modules/sc_paladin.cpp
@@ -248,7 +248,7 @@ public:
   virtual double    composite_attribute_multiplier( attribute_e attr );
   virtual double    composite_attack_crit();
   virtual double    composite_spell_crit();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = NULL );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    composite_spell_power( school_e school );
   virtual double    composite_spell_power_multiplier();
   virtual double    composite_spell_speed();
@@ -523,6 +523,18 @@ struct paladin_melee_attack_t : public paladin_action_t< melee_attack_t >
         p() -> active_seal_of_justice_aoe_proc -> execute();
 
     }
+  }
+
+  virtual double action_multiplier()
+  {
+    double am = base_t::action_multiplier();
+
+    if ( class_flag1 && ( p() -> passives.sword_of_light -> ok() ) && ( p() -> main_hand_weapon.group() == WEAPON_2H ) )
+    {
+      am *= 1.0 + p() -> passives.sword_of_light_value -> effectN( 1 ).percent();
+    }
+
+    return am;
   }
 };
 
@@ -2917,7 +2929,7 @@ void paladin_t::create_buffs()
                                        .duration( find_spell( glyphs.templars_verdict -> effectN( 1 ).trigger_spell_id() ) -> duration() )
                                        .default_value( find_spell( glyphs.templars_verdict -> effectN( 1 ).trigger_spell_id() ) -> effectN( 1 ).percent() );
 
-  buffs.glyph_of_word_of_glory = buff_creator_t( this, "glyph_word_of_glory", spells.glyph_of_word_of_glory_damage );
+  buffs.glyph_of_word_of_glory = buff_creator_t( this, "glyph_word_of_glory", spells.glyph_of_word_of_glory_damage ).add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   // Talents
   buffs.divine_purpose         = buff_creator_t( this, "divine_purpose", find_talent_spell( "Divine Purpose" ) )
@@ -2925,14 +2937,18 @@ void paladin_t::create_buffs()
   buffs.holy_avenger           = buff_creator_t( this, "holy_avenger", find_talent_spell( "Holy Avenger" ) ).cd( timespan_t::zero() ); // Let the ability handle the CD
 
   // General
-  buffs.avenging_wrath         = buff_creator_t( this, "avenging_wrath", find_class_spell( "Avenging Wrath" ) ).cd( timespan_t::zero() ); // Let the ability handle the CD
+  buffs.avenging_wrath         = buff_creator_t( this, "avenging_wrath", find_class_spell( "Avenging Wrath" ) )
+                                 .cd( timespan_t::zero() ) // Let the ability handle the CD
+                                 .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   if ( find_talent_spell( "Sanctified Wrath" ) -> ok() )
   {
     buffs.avenging_wrath -> buff_duration *= 1.0 + find_talent_spell( "Sanctified Wrath" ) -> effectN( 2 ).percent();
   }
 
   buffs.divine_protection      = buff_creator_t( this, "divine_protection", find_class_spell( "Divine Protection" ) ).cd( timespan_t::zero() ); // Let the ability handle the CD
-  buffs.divine_shield          = buff_creator_t( this, "divine_shield", find_class_spell( "Divine Shield" ) ).cd( timespan_t::zero() ); // Let the ability handle the CD
+  buffs.divine_shield          = buff_creator_t( this, "divine_shield", find_class_spell( "Divine Shield" ) )
+                                 .cd( timespan_t::zero() ) // Let the ability handle the CD
+                                 .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   // Holy
   buffs.daybreak               = buff_creator_t( this, "daybreak", find_class_spell( "Daybreak" ) );
@@ -2944,7 +2960,7 @@ void paladin_t::create_buffs()
 
   // Ret
   buffs.ancient_power          = buff_creator_t( this, "ancient_power", passives.ancient_power ).add_invalidate( CACHE_STRENGTH );
-  buffs.inquisition            = buff_creator_t( this, "inquisition", find_class_spell( "Inquisition" ) ).add_invalidate( CACHE_CRIT );
+  buffs.inquisition            = buff_creator_t( this, "inquisition", find_class_spell( "Inquisition" ) ).add_invalidate( CACHE_CRIT ).add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buffs.judgments_of_the_wise  = buff_creator_t( this, "judgments_of_the_wise", find_specialization_spell( "Judgments of the Wise" ) );
   buffs.tier15_2pc_melee       = buff_creator_t( this, "tier15_2pc_melee", find_spell( 138162 ) )
                                  .default_value( find_spell( 138162 ) -> effectN( 1 ).percent() );
@@ -3489,9 +3505,9 @@ double paladin_t::composite_spell_crit()
 
 // paladin_t::composite_player_multiplier ===================================
 
-double paladin_t::composite_player_multiplier( school_e school, action_t* a )
+double paladin_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   // These affect all damage done by the paladin
   m *= 1.0 + buffs.avenging_wrath -> value();
@@ -3504,11 +3520,6 @@ double paladin_t::composite_player_multiplier( school_e school, action_t* a )
     }
 
     m *= 1.0 + buffs.tier15_2pc_melee -> value();
-  }
-
-  if ( a && a -> type == ACTION_ATTACK && ! a -> class_flag1 && ( passives.sword_of_light -> ok() ) && ( main_hand_weapon.group() == WEAPON_2H ) )
-  {
-    m *= 1.0 + passives.sword_of_light_value -> effectN( 1 ).percent();
   }
 
   if ( buffs.divine_shield -> up() )

engine/class_modules/sc_death_knight.cpp
@@ -373,7 +373,7 @@ public:
   virtual double    composite_attribute_multiplier( attribute_e attr );
   virtual double    matching_gear_multiplier( attribute_e attr );
   virtual double    composite_tank_parry();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = NULL );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    composite_tank_crit( school_e school );
   virtual void      regen( timespan_t periodicity );
   virtual void      reset();
@@ -1078,9 +1078,9 @@ struct death_knight_pet_t : public pet_t
   death_knight_t* o()
   { return debug_cast<death_knight_t*>( owner ); }
 
-  double composite_player_multiplier( school_e school, action_t* a = 0 )
+  double composite_player_multiplier( school_e school )
   {
-    double m = pet_t::composite_player_multiplier( school, a );
+    double m = pet_t::composite_player_multiplier( school );
 
     if ( owner -> race == RACE_ORC )
       m *= 1.0 + command -> effectN( 1 ).percent();
@@ -5165,9 +5165,9 @@ double death_knight_t::composite_tank_parry()
 
 // death_knight_t::composite_player_multiplier ==============================
 
-double death_knight_t::composite_player_multiplier( school_e school, action_t* a )
+double death_knight_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   if ( dbc::is_school( school, SCHOOL_SHADOW ) )
     m *= 1.0 + mastery.dreadblade -> effectN( 1 ).mastery_value() * cache.mastery();

engine/class_modules/sc_druid.cpp
@@ -399,7 +399,7 @@ public:
   virtual double    composite_attack_expertise( weapon_t* );
   virtual double    composite_attack_crit();
   virtual double    composite_attack_power();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = NULL );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    composite_player_td_multiplier( school_e, action_t* );
   virtual double    composite_player_heal_multiplier( school_e school );
   virtual double    composite_spell_haste();
@@ -779,6 +779,8 @@ public:
   {
     invalidate_list.push_back( CACHE_HIT );
     invalidate_list.push_back( CACHE_EXP );
+    invalidate_list.push_back( CACHE_PLAYER_HEAL_MULTIPLIER );
+    requires_invalidation = true;
   }
 
   bool heals_are_free()
@@ -4799,7 +4801,8 @@ struct cat_form_t : public druid_buff_t< buff_t >
 struct moonkin_form_t : public druid_buff_t< buff_t >
 {
   moonkin_form_t( druid_t& p ) :
-    base_t( p, buff_creator_t( &p, "moonkin_form", p.find_class_spell( "Moonkin Form" ) ) )
+    base_t( p, buff_creator_t( &p, "moonkin_form", p.find_class_spell( "Moonkin Form" ) )
+               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER ) )
   { }
 
   virtual void expire_override()
@@ -5201,7 +5204,8 @@ void druid_t::create_buffs()
   // http://mop.wowhead.com/spell=122114 Chosen of Elune
   buff.chosen_of_elune    = buff_creator_t( this, "chosen_of_elune"   , talent.incarnation -> ok() ? find_spell( 122114 ) : spell_data_t::not_found() )
                             .duration( talent.incarnation -> duration() )
-                            .chance( talent.incarnation -> ok() ? ( specialization() == DRUID_BALANCE ) : 0.0 );
+                            .chance( talent.incarnation -> ok() ? ( specialization() == DRUID_BALANCE ) : 0.0 )
+                            .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   // http://mop.wowhead.com/spell=102548 Incarnation: King of the Jungle
   buff.king_of_the_jungle = buff_creator_t( this, "king_of_the_jungle", talent.incarnation -> ok() ? find_spell( 102543 ) : spell_data_t::not_found() )
@@ -5223,15 +5227,19 @@ void druid_t::create_buffs()
   buff.dream_of_cenarius_heal   = buff_creator_t( this, "dream_of_cenarius_heal",   talent.dream_of_cenarius -> ok() ? find_spell( 108382 ) : spell_data_t::not_found() )
                                   .max_stack( 2 );
 
-  buff.natures_vigil      = buff_creator_t( this, "natures_vigil", talent.natures_vigil -> ok() ? find_spell( 124974 ) : spell_data_t::not_found() );
+  buff.natures_vigil      = buff_creator_t( this, "natures_vigil", talent.natures_vigil -> ok() ? find_spell( 124974 ) : spell_data_t::not_found() )
+                            .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER )
+                            .add_invalidate( CACHE_PLAYER_HEAL_MULTIPLIER );
 
   buff.heart_of_the_wild  = new heart_of_the_wild_buff_t( *this );
 
   // Balance
 
   buff.celestial_alignment   = new celestial_alignment_t( *this );
-  buff.eclipse_lunar         = buff_creator_t( this, "lunar_eclipse",  find_specialization_spell( "Eclipse" ) -> ok() ? find_spell( 48518 ) : spell_data_t::not_found() );
-  buff.eclipse_solar         = buff_creator_t( this, "solar_eclipse",  find_specialization_spell( "Eclipse" ) -> ok() ? find_spell( 48517 ) : spell_data_t::not_found() );
+  buff.eclipse_lunar         = buff_creator_t( this, "lunar_eclipse",  find_specialization_spell( "Eclipse" ) -> ok() ? find_spell( 48518 ) : spell_data_t::not_found() )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
+  buff.eclipse_solar         = buff_creator_t( this, "solar_eclipse",  find_specialization_spell( "Eclipse" ) -> ok() ? find_spell( 48517 ) : spell_data_t::not_found() )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buff.lunar_shower          = buff_creator_t( this, "lunar_shower",   spec.lunar_shower -> effectN( 1 ).trigger() );
   buff.shooting_stars        = buff_creator_t( this, "shooting_stars", spec.shooting_stars -> effectN( 1 ).trigger() )
                                .chance( spec.shooting_stars -> proc_chance() );
@@ -5248,9 +5256,11 @@ void druid_t::create_buffs()
 
   // Feral
   buff.tigers_fury           = buff_creator_t( this, "tigers_fury", find_specialization_spell( "Tiger's Fury" ) )
-                               .default_value( find_specialization_spell( "Tiger's Fury" ) -> effectN( 1 ).percent() );
+                               .default_value( find_specialization_spell( "Tiger's Fury" ) -> effectN( 1 ).percent() )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buff.savage_roar           = buff_creator_t( this, "savage_roar", find_specialization_spell( "Savage Roar" ) )
-                               .default_value( find_specialization_spell( "Savage Roar" ) -> effectN( 2 ).percent() );
+                               .default_value( find_specialization_spell( "Savage Roar" ) -> effectN( 2 ).percent() )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buff.predatory_swiftness   = buff_creator_t( this, "predatory_swiftness", spec.predatory_swiftness -> ok() ? find_spell( 69369 ) : spell_data_t::not_found() );
   buff.tier15_4pc_melee      = buff_creator_t( this, "tier15_4pc_melee", spell_data_t::nil() )
                                .max_stack( 3 );
@@ -5994,9 +6004,9 @@ double druid_t::composite_attack_crit()
 
 // druid_t::composite_player_multiplier =====================================
 
-double druid_t::composite_player_multiplier( school_e school, action_t* a )
+double druid_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   if ( buff.natures_vigil -> up() )
   {

engine/class_modules/sc_hunter.cpp
@@ -308,7 +308,7 @@ public:
   virtual double    composite_attack_speed();
   virtual double    ranged_haste_multiplier();
   virtual double    ranged_speed_multiplier();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = NULL );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    matching_gear_multiplier( attribute_e attr );
   virtual void      create_options();
   virtual action_t* create_action( const std::string& name, const std::string& options );
@@ -444,9 +444,9 @@ public:
   hunter_t* o() const
   { return static_cast< hunter_t* >( owner ); }
 
-  virtual double composite_player_multiplier( school_e school, action_t* a )
+  virtual double composite_player_multiplier( school_e school )
   {
-    double m = base_t::composite_player_multiplier( school, a );
+    double m = base_t::composite_player_multiplier( school );
     m *= o() -> beast_multiplier();
     return m;
   }
@@ -799,9 +799,9 @@ public:
       o() -> active.pet = nullptr;
   }
 
-  virtual double composite_player_multiplier( school_e school, action_t* a )
+  virtual double composite_player_multiplier( school_e school )
   {
-    double m = base_t::composite_player_multiplier( school, a );
+    double m = base_t::composite_player_multiplier( school );
 
     if ( ! buffs.stampede -> check() && buffs.bestial_wrath -> up() )
       m *= 1.0 + buffs.bestial_wrath -> data().effectN( 2 ).percent();
@@ -2511,6 +2511,15 @@ struct serpent_sting_spread_t : public serpent_sting_t
     return 0;
   }
 
+  virtual double action_multiplier()
+  {
+    double am = hunter_ranged_attack_t::action_multiplier();
+
+    am /= 1.0 + p() -> mastery.essence_of_the_viper -> effectN( 1 ).mastery_value() * p() -> cache.mastery();
+
+    return am;
+  }
+
   virtual void impact( action_state_t* s )
   {
     hunter_ranged_attack_t::impact( s );
@@ -3743,7 +3752,9 @@ void hunter_t::create_buffs()
 
   buffs.aspect_of_the_hawk          = buff_creator_t( this, 13165, "aspect_of_the_hawk" ).add_invalidate( CACHE_ATTACK_POWER );
 
-  buffs.beast_within                = buff_creator_t( this, 34471, "beast_within" ).chance( specs.the_beast_within -> ok() );
+  buffs.beast_within                = buff_creator_t( this, 34471, "beast_within" )
+                                      .chance( specs.the_beast_within -> ok() )
+                                      .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buffs.beast_within -> buff_duration += sets -> set( SET_T14_4PC_MELEE ) -> effectN( 1 ).time_value();
 
   buffs.bombardment                 = buff_creator_t( this, "bombardment", specs.bombardment -> effectN( 1 ).trigger() );
@@ -4147,17 +4158,16 @@ double hunter_t::ranged_speed_multiplier()
 
 // hunter_t::composite_player_multiplier ====================================
 
-double hunter_t::composite_player_multiplier( school_e school, action_t* a )
+double hunter_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   if ( dbc::is_school( school, SCHOOL_NATURE ) ||
        dbc::is_school( school, SCHOOL_ARCANE ) ||
        dbc::is_school( school, SCHOOL_SHADOW ) ||
        dbc::is_school( school, SCHOOL_FIRE   ) )
   {
-    if ( likely( a -> id != 82834 ) )
-      m *= 1.0 + mastery.essence_of_the_viper -> effectN( 1 ).mastery_value() * cache.mastery();
+    m *= 1.0 + mastery.essence_of_the_viper -> effectN( 1 ).mastery_value() * cache.mastery();
   }
 
   if ( buffs.beast_within -> up() )

engine/class_modules/sc_mage.cpp
@@ -276,7 +276,7 @@ public:
   virtual resource_e primary_resource() { return RESOURCE_MANA; }
   virtual role_e primary_role() { return ROLE_SPELL; }
   virtual double    mana_regen_per_second();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = NULL );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    composite_spell_crit();
   virtual double    composite_spell_haste();
   virtual double    matching_gear_multiplier( attribute_e attr );
@@ -436,10 +436,11 @@ struct water_elemental_pet_t : public pet_t
     return pet_t::create_action( name, options_str );
   }
 
-  virtual double composite_player_multiplier( school_e school, action_t* a )
+  virtual double composite_player_multiplier( school_e school )
   {
-    double m = pet_t::composite_player_multiplier( school, a );
-    m *= 1.0 + o() -> spec.frostburn -> effectN( 3 ).mastery_value() * o() -> composite_mastery();
+    double m = pet_t::composite_player_multiplier( school );
+
+    m *= 1.0 + o() -> spec.frostburn -> effectN( 3 ).mastery_value() * o() -> cache.mastery();
 
     if ( o() -> buffs.invokers_energy -> up() )
     {
@@ -598,9 +599,9 @@ struct mirror_image_pet_t : public pet_t
   }
 
 
-  virtual double composite_player_multiplier( school_e school, action_t* a )
+  virtual double composite_player_multiplier( school_e school )
   {
-    double m = pet_t::composite_player_multiplier( school, a );
+    double m = pet_t::composite_player_multiplier( school );
 
     // Orc racial
     if ( owner -> race == RACE_ORC )
@@ -766,7 +767,8 @@ struct alter_time_t : public buff_t
 struct arcane_power_t : public buff_t
 {
   arcane_power_t( mage_t* p ) :
-    buff_t( buff_creator_t( p, "arcane_power", p -> find_class_spell( "Arcane Power" ) ) )
+    buff_t( buff_creator_t( p, "arcane_power", p -> find_class_spell( "Arcane Power" ) )
+            .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER ) )
   {
     cooldown -> duration = timespan_t::zero(); // CD is managed by the spell
 
@@ -3603,11 +3605,15 @@ void mage_t::create_buffs()
   buffs.frost_armor          = buff_creator_t( this, "frost_armor", find_spell( 7302 ) ).add_invalidate( CACHE_SPELL_HASTE );
   buffs.icy_veins            = new buffs::icy_veins_t( this );
   buffs.ice_floes            = buff_creator_t( this, "ice_floes", talents.ice_floes );
-  buffs.invokers_energy           = buff_creator_t( this, "invokers_energy", find_spell( 116257 ) ).chance( talents.invocation -> ok() ? 1.0 : 0 );
+  buffs.invokers_energy      = buff_creator_t( this, "invokers_energy", find_spell( 116257 ) )
+                               .chance( talents.invocation -> ok() ? 1.0 : 0 )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buffs.mage_armor           = stat_buff_creator_t( this, "mage_armor" ).spell( find_spell( 6117 ) );
   buffs.molten_armor         = buff_creator_t( this, "molten_armor", find_spell( 30482 ) ).add_invalidate( CACHE_SPELL_CRIT );
   buffs.presence_of_mind     = buff_creator_t( this, "presence_of_mind", talents.presence_of_mind ).duration( timespan_t::zero() ).activated( true );
-  buffs.rune_of_power        = buff_creator_t( this, "rune_of_power", find_spell( 116014 ) ).duration( timespan_t::from_seconds( 60 ) );
+  buffs.rune_of_power        = buff_creator_t( this, "rune_of_power", find_spell( 116014 ) )
+                               .duration( timespan_t::from_seconds( 60 ) )
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   buffs.heating_up           = buff_creator_t( this, "heating_up", find_class_spell( "Pyroblast" ) -> ok() ? find_spell( 48107 ) : spell_data_t::not_found() );
   buffs.pyroblast            = buff_creator_t( this, "pyroblast",  find_class_spell( "Pyroblast" ) -> ok() ? find_spell( 48108 ) : spell_data_t::not_found() );
@@ -3618,7 +3624,8 @@ void mage_t::create_buffs()
   buffs.alter_time           = new buffs::alter_time_t( this );
   buffs.incanters_ward       = new buffs::incanters_ward_t( this );
   buffs.incanters_absorption  = buff_creator_t( this, "incanters_absorption" )
-                                .spell( find_spell( 116267 ) );
+                                .spell( find_spell( 116267 ) )
+                                .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   buffs.tier15_2pc_crit      = stat_buff_creator_t( this, "tier15_2pc_crit", find_spell( 138317 ) )
                                .add_stat( STAT_CRIT_RATING, find_spell( 138317 ) -> effectN( 1 ).base_value() );
@@ -4040,9 +4047,9 @@ double mage_t::mana_regen_per_second()
 
 // mage_t::composite_player_multipler =======================================
 
-double mage_t::composite_player_multiplier( school_e school, action_t* a )
+double mage_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   if ( buffs.arcane_power -> check() )
   {

engine/class_modules/sc_monk.cpp
@@ -207,7 +207,7 @@ public:
   virtual action_t* create_action( const std::string& name, const std::string& options );
   virtual double    composite_attack_speed();
   virtual double    energy_regen_per_second();
-  virtual double    composite_player_multiplier( school_e school, action_t* a );
+  virtual double    composite_player_multiplier( school_e school );
   virtual pet_t*    create_pet   ( const std::string& name, const std::string& type = std::string() );
   virtual void      create_pets();
   virtual void      init_spells();
@@ -1216,6 +1216,8 @@ struct stance_t : public monk_spell_t
 
     p() -> active_stance = switch_to_stance;
 
+    p() -> invalidate_cache( CACHE_PLAYER_DAMAGE_MULTIPLIER );
+
     //TODO: Add stances once implemented
     if ( switch_to_stance == STANCE_FIERCE_TIGER )
     {
@@ -1827,7 +1829,7 @@ void monk_t::create_buffs()
   buff.tiger_stance      = buff_creator_t( this, "tiger_stance"        ).spell( find_spell( 103985 ) ).add_invalidate( CACHE_ATTACK_SPEED );
   buff.serpent_stance    = buff_creator_t( this, "serpent_stance"      ).spell( find_spell( 115070 ) );
   buff.tigereye_brew     = buff_creator_t( this, "tigereye_brew"       ).spell( find_spell( 125195 ) );
-  buff.tigereye_brew_use = buff_creator_t( this, "tigereye_brew_use"   ).spell( find_spell( 116740 ) );
+  buff.tigereye_brew_use = buff_creator_t( this, "tigereye_brew_use"   ).spell( find_spell( 116740 ) ).add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buff.power_strikes     = buff_creator_t( this, "power_strikes"       ).spell( find_spell( 129914 ) );
   buff.tiger_strikes     = haste_buff_creator_t( this, "tiger_strikes" ).spell( find_spell( 120273 ) )
                            .chance( find_spell( 120272 ) -> proc_chance() );
@@ -2152,9 +2154,9 @@ double monk_t::composite_attack_speed()
 
 // monk_t::composite_player_multiplier
 
-double monk_t::composite_player_multiplier( school_e school, action_t* a )
+double monk_t::composite_player_multiplier( school_e school )
 {
-  double m = player_t::composite_player_multiplier( school, a );
+  double m = player_t::composite_player_multiplier( school );
 
   if ( active_stance == STANCE_FIERCE_TIGER )
   {

engine/class_modules/sc_priest.cpp
@@ -338,7 +338,7 @@ public:
   virtual double    composite_spell_power_multiplier();
   virtual double    composite_spell_hit();
   virtual double    composite_attack_hit();
-  virtual double    composite_player_multiplier( school_e school, action_t* a = nullptr );
+  virtual double    composite_player_multiplier( school_e school );
   virtual double    composite_player_heal_multiplier( school_e school );
   virtual double    composite_movement_speed();
   virtual double    composite_attribute_multiplier( attribute_e attr );
@@ -430,9 +430,9 @@ struct priest_pet_t : public pet_t
     pet_t::schedule_ready( delta_time, waiting );
   }
 
-  virtual double composite_player_multiplier( school_e school, action_t* a )
+  virtual double composite_player_multiplier( school_e school )
   {
-    double m = pet_t::composite_player_multiplier( school, a );
+    double m = pet_t::composite_player_multiplier( school );
 
     // Orc racial
     if ( owner -> race == RACE_ORC )
@@ -4450,7 +4450,8 @@ struct shadowform_t : public priest_buff_t<buff_t>
 {
   shadowform_t( priest_t& p ) :
     base_t( p, buff_creator_t( &p, "shadowform" )
-               .spell( p.find_class_spell( "Shadowform" ) ) )
+               .spell( p.find_class_spell( "Shadowform" ) )
+               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER ) )
   {
 
   }
@@ -4847,9 +4848,9 @@ double priest_t::composite_attack_hit()
 
 // priest_t::composite_player_multiplier ====================================
 
-double priest_t::composite_player_multiplier( school_e school, action_t* a )
+double priest_t::composite_player_multiplier( school_e school )
 {
-  double m = base_t::composite_player_multiplier( school, a );
+  double m = base_t::composite_player_multiplier( school );
 
   if ( dbc::is_school( SCHOOL_SHADOW, school ) )
   {
@@ -4887,8 +4888,6 @@ double priest_t::composite_player_heal_multiplier( school_e s )
   {
     m *= 1.0 + buffs.twist_of_fate -> current_value;
   }
-
-  // FIXME: Nothing in {heal_t|absorb_t} is actually using composite_player_{heal|absorb}_multiplier().
 
   return m;
 }
@@ -5207,12 +5206,15 @@ void priest_t::create_buffs()
   // Talents
   buffs.power_infusion = buff_creator_t( this, "power_infusion" )
                          .spell( talents.power_infusion )
-                         .add_invalidate( CACHE_SPELL_HASTE );
+                         .add_invalidate( CACHE_SPELL_HASTE )
+                         .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   buffs.twist_of_fate = buff_creator_t( this, "twist_of_fate" )
                         .spell( talents.twist_of_fate )
                         .duration( talents.twist_of_fate -> effectN( 1 ).trigger() -> duration() )
-                        .default_value( talents.twist_of_fate -> effectN( 1 ).trigger() -> effectN( 2 ).percent() );
+                        .default_value( talents.twist_of_fate -> effectN( 1 ).trigger() -> effectN( 2 ).percent() )
+                        .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER )
+                        .add_invalidate( CACHE_PLAYER_HEAL_MULTIPLIER );
 
   buffs.surge_of_light = buff_creator_t( this, "surge_of_light" )
                          .spell( find_spell( 114255 ) )
@@ -5252,7 +5254,8 @@ void priest_t::create_buffs()
   buffs.chakra_chastise = buff_creator_t( this, "chakra_chastise" )
                           .spell( find_spell( 81209 ) )
                           .chance( specialization() == PRIEST_HOLY ? 1.0 : 0.0 )
-                          .cd( timespan_t::zero() );
+                          .cd( timespan_t::zero() )
+                          .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
   buffs.chakra_sanctuary = buff_creator_t( this, "chakra_sanctuary" )
                            .spell( find_spell( 81206 ) )

commit 74030993dcb70bab4988c77fe62db14481edf63b

Paladin: Fix bug introduced in r16031.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16093 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/class_modules/sc_paladin.cpp
@@ -527,7 +527,7 @@ struct paladin_melee_attack_t : public paladin_action_t< melee_attack_t >
   {
     double am = base_t::action_multiplier();
 
-    if ( class_flag1 && ( p() -> passives.sword_of_light -> ok() ) && ( p() -> main_hand_weapon.group() == WEAPON_2H ) )
+    if ( ! class_flag1 && ( p() -> passives.sword_of_light -> ok() ) && ( p() -> main_hand_weapon.group() == WEAPON_2H ) )
     {
       am *= 1.0 + p() -> passives.sword_of_light_value -> effectN( 1 ).percent();
     }