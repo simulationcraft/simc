bae98a315bc85f12ce1504abffab85a73da94f35
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-03-25 07:13:35 +0000

    Apparently GCC workaround with C-arrays and range::fill is needed for 4.6 onwards.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16098 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_generic.hpp
+++ engine/sc_generic.hpp
@@ -243,8 +243,8 @@ template <typename Range>
 inline Range& fill( Range& r, typename range::value_type<Range>::type const& t )
 { std::fill( range::begin( r ), range::end( r ), t ); return r; }
 
-#if SC_GCC == 406
-// Workaround for GCC 4.6 optimization ( -o3 ) issue with filling C-arrays
+#if SC_GCC >= 406
+// Workaround for GCC 4.6+ optimization ( -O3 ) issue with filling C-arrays
 template <typename T, size_t N>
 inline T ( &fill( T ( &r )[N], const T& t ) )[N]
 {
