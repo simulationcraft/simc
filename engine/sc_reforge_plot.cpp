b9c0e501ab6a9820c3277cd0793168ef87b474f2
xythian@gmail.com <xythian@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-04-11 16:50:39 +0000

    Typo fix in variable declaration.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16229 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/sc_reforge_plot.cpp
+++ engine/sc_reforge_plot.cpp
@@ -213,7 +213,7 @@ void reforge_plot_t::analyze_stats()
     if ( sim -> report_progress )
     {
       tmp += ":";
-      ssize_t n_spaces = 23 - tmp.length();
+      size_t n_spaces = 23 - tmp.length();
       if ( n_spaces > 0 )
         tmp.append( n_spaces, ' ' );
     }
