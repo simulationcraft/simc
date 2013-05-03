726d85ca06d0ed37c7a3801bada04ef02db62b22
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-05-03 14:35:09 +0000

    Windows Installer: batch file requires version number ( eg. 520-9 ) as a argument, and uses it for various things: - Application is named with Version - Installdir is now [ProgramFiles]/SimulationCraft/<Version>
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16358 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- windows_installer/create_installer.bat
+++ windows_installer/create_installer.bat
@@ -3,13 +3,15 @@
 :: Wix Toolset: http://wixtoolset.org/
 :: Documentation: http://wix.tramontana.co.hu/
 
+IF (%1) == () exit /b
+
+set version=%1
 
 :: create automated file list for profiles
 call heat dir ./profiles PROFILES -generate container -template fragment -projectname SimulationCraft -gg -cg Profiles -var var.profiles -dr INSTALLDIR -out profiles.wxs
 
-
 :: compile Installer configuration & profiles
-call candle -dprofiles=profiles SimulationCraft.wxs profiles.wxs
+call candle -dprofiles=profiles -dprod_ver=%version% SimulationCraft.wxs profiles.wxs
 
 :: link everything together into SimulationCraft.msi
-call light -ext WixUIExtension -out SimulationCraft.msi SimulationCraft.wixobj profiles.wixobj
+call light -ext WixUIExtension -out SimulationCraft-%version%.msi SimulationCraft.wixobj profiles.wixobj
