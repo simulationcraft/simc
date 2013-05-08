083df10127f3f1dd2d56fd147dcb0da091d71f95
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-05-08 13:01:31 +0000

    Add Blood death knight bloodworms spells to spell data.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16371 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- dbc_extract/dbc/generator.py
+++ dbc_extract/dbc/generator.py
@@ -1106,6 +1106,7 @@ class SpellDataGenerator(DataGenerator):
           ( 66188, 0 ), # Death Strike off-hand
           ( 113516, 0, True ), # Symbiosis Wild Mushroom: Plague
           ( 52212, 0, False ), # Death and Decay false positive for activatable
+          ( 81277, 5 ), ( 81280, 5 ) # Bloodworms heal / burst
         ),  
         
         # Shaman:
