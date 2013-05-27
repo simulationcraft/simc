4f6793b0684f3d9958fda7181f65be83af5d86b6
philoptik <philoptik@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-05-27 10:56:19 +0000

    timeline_dmg_taken: change color to yellow and add average.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@16553 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/report/sc_report_html_player.cpp
+++ engine/report/sc_report_html_player.cpp
@@ -1761,7 +1761,7 @@ void print_html_player_charts( report::sc_html_stream& os, sim_t* sim, player_t*
     os.printf( fmt, ri.scaling_dps_chart.c_str() );
   }
 
-  std::string timeline_dmg_takenchart = chart::timeline( p, p -> timeline_dmg_taken.data(), "dmg_taken", 0, "ff0000", static_cast<size_t>( p -> fight_length.max ) );
+  std::string timeline_dmg_takenchart = chart::timeline( p, p -> timeline_dmg_taken.data(), "dmg_taken", p -> timeline_dmg_taken.average( 0, static_cast<size_t>( p -> fight_length.max ) ), "FDD017", static_cast<size_t>( p -> fight_length.max ) );
   if ( ! timeline_dmg_takenchart.empty() )
   {
     os << "<img src=\"" << timeline_dmg_takenchart << "\" alt=\"Damage Taken Timeline Chart\" />\n";
