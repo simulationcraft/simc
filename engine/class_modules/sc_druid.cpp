simulationcraft/simc

commit e82eb737fa68ebda001f12fac34804ab3500154d

Druid:  - Remove the cooldown on tigers_fury buff (fixes case w/ AoC).  - Use spell data for tier16_4p_melee buff.  - Change pre-TF opener to ticking_ebon_detonator only.  - flurry_of_xuen is no longer affected by Razor Claws. sc_unique_gear.cpp:  - Added a note about buff cooldowns to cooldown_reduction_trinket.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@17541 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/class_modules/sc_druid.cpp
@@ -5952,16 +5952,15 @@ void druid_t::create_buffs()
   // Feral
   buff.tigers_fury           = buff_creator_t( this, "tigers_fury", find_specialization_spell( "Tiger's Fury" ) )
                                .default_value( find_specialization_spell( "Tiger's Fury" ) -> effectN( 1 ).percent() )
-                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
+                               .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER )
+                               .cd( timespan_t::zero() );
   buff.savage_roar           = buff_creator_t( this, "savage_roar", find_specialization_spell( "Savage Roar" ) )
                                .default_value( find_spell( 62071 ) -> effectN( 1 ).percent() )
                                .add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
   buff.predatory_swiftness   = buff_creator_t( this, "predatory_swiftness", spec.predatory_swiftness -> ok() ? find_spell( 69369 ) : spell_data_t::not_found() );
   buff.tier15_4pc_melee      = buff_creator_t( this, "tier15_4pc_melee", find_spell( 138358 ) );
   buff.feral_fury            = buff_creator_t( this, "feral_fury", find_spell( 144865 ) ); // tier16_2pc_melee
-  buff.feral_rage            = buff_creator_t( this, "feral_rage", spell_data_t::nil() ) // tier16_4pc_melee find_spell( 146874 )
-                               .chance( 1 )
-                               .duration( timespan_t::from_seconds( 12.0 ) );
+  buff.feral_rage            = buff_creator_t( this, "feral_rage", find_spell( 146874 ) ); // tier16_4pc_melee
 
   // Guardian
   buff.enrage                = buff_creator_t( this, "enrage" , find_specialization_spell( "Enrage" ) );
@@ -6216,7 +6215,7 @@ void druid_t::apl_feral()
 
   // APL: ADVANCED
 
-  if ( talent.dream_of_cenarius -> ok() && find_item( "rune_of_reorigination" ) && ( find_item( "haromms_talisman" ) || find_item( "ticking_ebon_detonator" ) ) )
+  if ( talent.dream_of_cenarius -> ok() && find_item( "rune_of_reorigination" ) && find_item( "ticking_ebon_detonator" ) )
   {
     advanced -> add_action( this, "Tiger's Fury", "if=time=0&set_bonus.tier16_4pc_melee" );
     advanced -> add_action( this, "Savage Roar", "if=time=0&set_bonus.tier16_4pc_melee" );
@@ -6858,7 +6857,7 @@ double druid_t::composite_player_td_multiplier( school_e school, action_t* a )
 {
   double m = player_t::composite_player_td_multiplier( school, a );
 
-  if ( school == SCHOOL_PHYSICAL && mastery.razor_claws -> ok() && buff.cat_form -> up() )
+  if ( school == SCHOOL_PHYSICAL && mastery.razor_claws -> ok() && buff.cat_form -> up() & a -> name_str != "flurry_of_xuen" )
     m *= 1.0 + cache.mastery_value();
 
   return m;

engine/player/sc_unique_gear.cpp
@@ -2955,6 +2955,7 @@ void cooldown_reduction_trinket( item_t* item )
 
   static const cooldowns_t __cd[] =
   {
+    // NOTE: Spells that trigger buffs must have the cooldown of their buffs removed if they have one, or this trinket may cause undesirable results.
     { ROGUE_ASSASSINATION, { "evasion", "vanish", "cloak_of_shadows", "vendetta", "shadow_blades", 0, 0 } },
     { ROGUE_COMBAT,        { "evasion", "adrenaline_rush", "cloak_of_shadows", "killing_spree", "shadow_blades", 0, 0 } },
     { ROGUE_SUBTLETY,      { "evasion", "vanish", "cloak_of_shadows", "shadow_dance", "shadow_blades", 0, 0 } },

commit 33788c838e73ee20008aa416d09e2c0a3322d4b3

Druid:  - Silence warning caused by r17541.
git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@17544 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/class_modules/sc_druid.cpp
@@ -6857,7 +6857,7 @@ double druid_t::composite_player_td_multiplier( school_e school, action_t* a )
 {
   double m = player_t::composite_player_td_multiplier( school, a );
 
-  if ( school == SCHOOL_PHYSICAL && mastery.razor_claws -> ok() && buff.cat_form -> up() & a -> name_str != "flurry_of_xuen" )
+  if ( school == SCHOOL_PHYSICAL && mastery.razor_claws -> ok() && buff.cat_form -> up() && a -> name_str != "flurry_of_xuen" )
     m *= 1.0 + cache.mastery_value();
 
   return m;