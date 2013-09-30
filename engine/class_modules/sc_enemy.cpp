9ea5e086ecf2405df4e39107f7682dcbbcbb7cc5
theckhd@gmail.com <theckhd@gmail.com@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-09-30 01:15:16 +0000

    Enemy: TMI bosses tweaked, should be more consistent with log data now
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@17790 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_enemy.cpp
+++ engine/class_modules/sc_enemy.cpp
@@ -794,8 +794,8 @@ void enemy_t::init_actions()
             break;
           default:
             // boss damage information ( could move outside this function and make a constant )
-            int aa_damage [ 8 ] = { 0, 550000, 750000, 900000, 1200000, 1350000, 1500000, 1650000 };
-            int dot_damage [ 8 ] = { 0, 20000, 25000, 30000, 40000, 50000, 55000, 600000 };
+            int aa_damage [ 8 ] = { 0, 550000, 750000, 900000, 1250000, 1550000, 1900000, 2300000 };
+            int dot_damage [ 8 ] = { 0, 20000, 25000, 30000, 50000, 65000, 85000, 100000 };
             action_list_str += "/auto_attack,damage=" + util::to_string( aa_damage[ tmi_boss_enum ] ) + ",attack_speed=1.5";
             action_list_str += "/spell_dot,damage=" + util::to_string( dot_damage[ tmi_boss_enum ] ) + ",tick_time=2,num_ticks=15,aoe_tanks=1,if=!ticking";
         }
