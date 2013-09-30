0b38f075af6c7575eeeb807694ff5386199b531d
max@cs.rwth-aachen.de <max@cs.rwth-aachen.de@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-09-30 13:45:41 +0000

    Regen Profiles Warlock: Fix SS inhale bug
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@17795 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/class_modules/sc_warlock.cpp
+++ engine/class_modules/sc_warlock.cpp
@@ -233,7 +233,7 @@ public:
     {
         soul_swap_buffer_target -> cancel(); //clear any previous versions in the buffer;
 
-        if (soul_swap_buffer_target -> state ) 
+        if (soul_swap_buffer_target -> state == NULL )
 			soul_swap_buffer_target -> state = to_inhale -> current_action -> get_state(); //make sure we have memory allocated for the state
 
         soul_swap_buffer_target -> state -> copy_state(to_inhale -> state); //copy state and the other stuff
