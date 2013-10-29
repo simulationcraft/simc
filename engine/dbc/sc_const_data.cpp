b4f1658daf3fa11e9970258f43a3658b9a9ff651
navv1234 <navv1234@3b4652a1-8050-0410-ac47-3d40261b0f8b>
2013-10-29 22:26:18 +0000

    WoW 5.4.1 17538.
    git-svn-id: https://simulationcraft.googlecode.com/svn/trunk@17939 3b4652a1-8050-0410-ac47-3d40261b0f8b

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -240,10 +240,10 @@ tokenized_map_t<talent_data_t> tokenized_talent_map;
 } // ANONYMOUS namespace ====================================================
 
 int dbc::build_level( bool ptr )
-{ return maybe_ptr( ptr ) ? 17345 : 17345; }
+{ return maybe_ptr( ptr ) ? 17345 : 17538; }
 
 const char* dbc::wow_version( bool ptr )
-{ return maybe_ptr( ptr ) ? "5.4.0" : "5.4.0"; }
+{ return maybe_ptr( ptr ) ? "5.4.0" : "5.4.1"; }
 
 const char* dbc::wow_ptr_status( bool ptr )
 { return ( maybe_ptr( ptr ) ?
