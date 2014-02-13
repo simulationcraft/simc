0270792ac01cec3a07f0b46a575ab2f4cc771353
Navv <navv1234>
2014-02-13 12:28:47 +0200

    Oops, restore timeouted version of the run.

--- test_helper.bash
+++ test_helper.bash
@@ -1,7 +1,6 @@
 function sim() {
   cd "${SIMC_PROFILES_PATH}"
-#  run timeout 5m "${SIMC_CLI_PATH}" "${SIMC_PROFILE}" iterations="${SIMC_ITERATIONS}" $@
-  run "${SIMC_CLI_PATH}" "${SIMC_PROFILE}" iterations="${SIMC_ITERATIONS}" $@
+  run timeout 5m "${SIMC_CLI_PATH}" "${SIMC_PROFILE}" iterations="${SIMC_ITERATIONS}" $@
   cd -
   BATS_TEST_DESCRIPTION+=" ($(basename ${SIMC_PROFILE}) iterations=${SIMC_ITERATIONS} $@)"
 }
