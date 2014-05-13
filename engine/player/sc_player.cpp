c64e2864a9c877686ebe38ad850d920ddafda4bd
Theck <theckhd@gmail.com>
2014-05-13 14:34:44 -0400

    Arcane Torrent grants holy power

--- engine/player/sc_player.cpp
+++ engine/player/sc_player.cpp
@@ -5292,6 +5292,7 @@ struct arcane_torrent_t : public racial_spell_t
       case RESOURCE_FOCUS:
       case RESOURCE_RAGE:
       case RESOURCE_RUNIC_POWER:
+      case RESOURCE_HOLY_POWER:
         gain = data().effectN( 2 ).resource( resource );
         break;
       default:
