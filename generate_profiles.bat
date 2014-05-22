2105d907b159f7c93cf5f805d33e6d6e0758be1b
Alex <s.alexlowery@gmail.com>
2014-05-21 22:36:10 -0400

    No more generating profiles for every copy of simc on my hard drive.

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -3,7 +3,7 @@ del /s simc_cache.dat
 :: Preraid doesn't match the typical pattern
 cd profiles/PreRaid
 %~dp0\simc.exe generate_PreRaid.simc
-cd ../..
+cd ../
 
 forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
 
--- generate_profiles_x64.bat
+++ generate_profiles_x64.bat
@@ -3,7 +3,7 @@ del /s simc_cache.dat
 :: Preraid doesn't match the typical pattern
 cd profiles/PreRaid
 %~dp0\simc64.exe generate_PreRaid.simc
-cd ../..
+cd ../
 
 forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc64.exe @file"
 
