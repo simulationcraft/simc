85e362d4091b21c8f6f5f975a4ce3c9634a4b979
Alex <s.alexlowery@gmail.com>
2014-06-16 07:27:50 -0400

    Fix compilation error from merging

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -262,7 +262,6 @@ public:
     spec( specializations_t() ),
     talents( talents_list_t() ),
     pyro_switch( pyro_switch_t() ),
-    mana_gem_charges( 0 ),
     current_arcane_charges()
   {
     // Cooldowns
