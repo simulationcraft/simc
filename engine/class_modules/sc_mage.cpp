319393e5d190a72117baeef0c7c53c4608191c05
kent <kenthklui@gmail.com>
2014-06-17 00:49:41 -0400

    [Mage] Sanity check for invocation in Fire mage APL

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -4423,7 +4423,7 @@ void mage_t::apl_fire()
   default_list -> add_talent( this, "Rune of Power",
                               "if=buff.rune_of_power.remains<cast_time" );
   default_list -> add_action( this, "Evocation",
-                              "if=buff.invokers_energy.remains=0|mana.pct<5" );
+                              "if=(talent.invocation.enabled&buff.invokers_energy.remains=0)|mana.pct<5" );
   default_list -> add_action( "cancel_buff,name=alter_time,if=buff.amplified.up&buff.alter_time.up&(trinket.stat.intellect.cooldown_remains-buff.alter_time.remains>109)",
                               "Cancelaura AT if PBoI procs" );
 
@@ -4439,7 +4439,7 @@ void mage_t::apl_fire()
                               "if=buff.alter_time.down&buff.rune_of_power.remains<4*action.fireball.execute_time&(buff.heating_up.down|buff.pyroblast.down|!action.fireball.in_flight)",
                               "Cast RoP/Evoc/MI only when player does not have both HU, Pyro proc and fireball mid flight - this causes Proc munching" );
   default_list -> add_action( this, "Evocation",
-                              "if=buff.alter_time.down&buff.invokers_energy.remains<4*action.fireball.execute_time&(buff.heating_up.down|buff.pyroblast.down|!action.fireball.in_flight)" );
+                              "if=talent.invocation.enabled&buff.alter_time.down&buff.invokers_energy.remains<4*action.fireball.execute_time&(buff.heating_up.down|buff.pyroblast.down|!action.fireball.in_flight)" );
   default_list -> add_action( this, "Mirror Image",
                               "if=buff.alter_time.down&(buff.heating_up.down|buff.pyroblast.down|!action.fireball.in_flight)" );
 
