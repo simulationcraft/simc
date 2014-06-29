8e640868f9e22ebe2998325904c47025b30cba6c
kent <kenthklui@gmail.com>
2014-06-29 13:34:41 -0400

    Include preraid profiles in profile generation script

--- generate_profiles.sh
+++ generate_profiles.sh
@@ -3,6 +3,9 @@ for tier in 16H 16N 15H 15N
 do
   PROFDIR='profiles/Tier'$tier'/'
   cd $PROFDIR
-	../../simc 'generate_T'$tier".simc"
+	../../engine/simc 'generate_T'$tier".simc"
   cd ../..
 done
+cd profiles/PreRaid/
+../../engine/simc 'generate_PreRaid.simc'
+cd ../..
