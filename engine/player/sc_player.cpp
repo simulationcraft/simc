5a3cb38237c752c3de13e3a2e9f2265daee76ee7
jorgster247123@gmail.com <jorgster247123@gmail.com>
2014-07-01 02:13:24 -0500

    Racials: End shadowmeld on movement.

--- engine/player/sc_player.cpp
+++ engine/player/sc_player.cpp
@@ -3771,7 +3771,7 @@ void player_t::moving()
   // FIXME! In the future, some movement events may not cause auto-attack to stop.
 
   halt();
-  p() -> buffs.shadowmeld -> expire();
+  buffs.shadowmeld -> expire();
 }
 
 // player_t::clear_debuffs===================================================
