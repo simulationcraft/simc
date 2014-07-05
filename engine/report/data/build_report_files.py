d17eb20ef393333a6c3ff575f00af513e4ebcda3
philoptik <philoptik@gmail.com>
2014-07-05 22:29:59 +0200

    Extract stylesheet into separate file as well. Kill old styles.

--- engine/report/data/build_report_files.py
+++ engine/report/data/build_report_files.py
@@ -6,8 +6,9 @@ def get_file_as_lines( input, out_line_length ):
     out = []
     with open( input ) as f:
         str = f.read()
-        str = re.sub( " ", "", str )
         str = re.sub( "\n", "", str )
+        str = re.sub( "\t", "", str )
+        str = re.sub( "\s+", " ", str )
         str = json.dumps(str).strip('"') # Misuse to build a C string with escaped characters
         out = [str[x:x+out_line_length] for x in range(0,len(str),out_line_length)]
     return out
@@ -25,6 +26,8 @@ def main():
         f.write( print_as_char_array( "__logo", get_file_as_lines( "simc_logo.txt", 80 ) ) )
         f.write( "\n\n" )
         f.write( print_as_char_array( "__image_load_script", get_file_as_lines( "image_load_script.js", 80 ) ) )
+        f.write( "\n\n" )
+        f.write( print_as_char_array( "__mop_stylesheet", get_file_as_lines( "mop_style.css", 80 ) ) )
     print( "done")
     
 if __name__ == "__main__":
