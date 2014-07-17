a47efcef425fbf6f74ded5fe070a45b1a4ff81df
Alex <s.alexlowery@gmail.com>
2014-07-17 03:06:06 -0400

    Merge windows generate profile files.

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -1,10 +1,24 @@
-del /s simc_cache.dat
-
+@echo off
+if exist simc_cache.dat del /s simc_cache.dat
 :: Preraid doesn't match the typical pattern
 ::cd profiles/PreRaid
 ::%~dp0\simc64.exe generate_PreRaid.simc
 ::cd ../
 
-forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
-
+if exist simc64.exe goto 64
+if exist simc.exe goto 32
+goto exit
+:64
+@echo on
+forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc64.exe @file"
 pause
+exit
+:32
+@echo on
+forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
+pause
+exit
+:exit
+echo "This file requires the CLI to be located in the same folder"
+pause
+exit
