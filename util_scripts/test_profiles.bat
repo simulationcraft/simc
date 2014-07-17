3c74ca806333842cc7d51e441e453707f20512f4
Alex <s.alexlowery@gmail.com>
2014-07-17 03:17:00 -0400

    Delete VS2010 files as they are very out of date.

--- util_scripts/test_profiles.bat
+++
@@ -1,9 +0,0 @@
-call simc.exe Raid_T15H.simc threads=4 iterations=10 > log.txt
-call simc.exe Raid_T15N.simc threads=4 iterations=10 > log.txt
-call simc.exe Raid_T15H.simc optimal_raid=0 threads=4 iterations=10 > log.txt
-call simc.exe Raid_T15N.simc optimal_raid=0 threads=4 iterations=10 > log.txt
-
-call simc.exe ptr=1 Raid_T15H.simc optimal_raid=0 threads=4 iterations=10 > log.txt
-call simc.exe ptr=1 Raid_T15N.simc optimal_raid=0 threads=4 iterations=10 > log.txt
-call simc.exe ptr=1 Raid_T15H.simc optimal_raid=0 threads=4 iterations=10 > log.txt
-call simc.exe ptr=1 Raid_T15N.simc optimal_raid=0 threads=4 iterations=10 > log.txt
