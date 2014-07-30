fbb0af3ed7e4b7658ebefde52bf3d1e02e9a4177
Alex <s.alexlowery@gmail.com>
2014-07-30 07:41:05 -0400

    Add glyph to whitelist, generate spell data.

--- dbc_extract/dbc/generator.py
+++ dbc_extract/dbc/generator.py
@@ -1263,6 +1263,7 @@ class SpellDataGenerator(DataGenerator):
         # Warrior:
         (
             ( 118340, 0 ),          # Impending Victory Heal
+            ( 58385,  0 ),          # Glyph of Hamstring
             ( 118779, 0, False ),   # Victory Rush heal is not directly activatable
             ( 144500, 0 ),          # T16 tank 4pc proc
             ( 156321, 0 ),          # Shield Charge for Gladiator Stance
