fb0476b9178a910f291f7055ced946d0936ba81d
Alex <s.alexlowery@gmail.com>
2014-08-19 02:58:57 -0400

    [Warrior] Fix damage on arms whirlwind.

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -2694,7 +2694,7 @@ struct whirlwind_t: public warrior_attack_t
       add_child( oh_attack );
     }
     else
-      weapon_multiplier *= 2;
+      weapon_multiplier = p -> find_spell( 168695 ) -> effectN( 2 ).percent();
 
     weapon = &( p -> main_hand_weapon );
   }
