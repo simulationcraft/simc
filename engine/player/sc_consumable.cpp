5802f2fd251f0a0ce9043deef100de4b6263c2b6
Alex <s.alexlowery@gmail.com>
2014-08-28 07:18:23 -0400

    Panda food stat bonus is now 75%

--- engine/player/sc_consumable.cpp
+++ engine/player/sc_consumable.cpp
@@ -514,7 +514,7 @@ struct food_t : public action_t
 
     double food_stat_multiplier = 1.0;
     if ( is_pandaren( p -> race ) )
-      food_stat_multiplier = 2.0;
+      food_stat_multiplier = 1.75;
 
     for ( size_t i = 0; i < sizeof_array( food_data ); ++i )
     {
