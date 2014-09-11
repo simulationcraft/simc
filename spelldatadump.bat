e2887fef60f10ceaab21326b7303cb60436b5aa0
Alex <s.alexlowery@gmail.com>
2014-09-10 21:48:40 -0400

    Add Spell Data Dump batch file that does a complete spell query on each class, put into text files in the /SpellDataDump/ Folder. I will update this with each major patch, so all you will need to do is check the diff of the previous version to see changes that may not show up on mmo-c.

---
+++ spelldatadump.bat
@@ -0,0 +1,13 @@
+del /q /spelldatadump/
+simc64 spell_query="spell.class=warrior">spelldatadump/warrior.txt
+simc64 spell_query="spell.class=hunter">spelldatadump/hunter.txt
+simc64 spell_query="spell.class=monk">spelldatadump/monk.txt
+simc64 spell_query="spell.class=paladin">spelldatadump/paladin.txt
+simc64 spell_query="spell.class=rogue">spelldatadump/rogue.txt
+simc64 spell_query="spell.class=shaman">spelldatadump/shaman.txt
+simc64 spell_query="spell.class=mage">spelldatadump/mage.txt
+simc64 spell_query="spell.class=warlock">spelldatadump/warlock.txt
+simc64 spell_query="spell.class=druid">spelldatadump/druid.txt
+simc64 spell_query="spell.class=deathknight">spelldatadump/deathknight.txt
+simc64 spell_query="spell.class=priest">spelldatadump/priest.txt
+pause
