26df93f65f482877e940a8a89ddef6060db6ebf8
Navv <navv1234@gmail.com>
2014-09-23 21:03:16 +0300

    Revert stupidity.

--- engine/player/sc_player.cpp
+++ engine/player/sc_player.cpp
@@ -1353,9 +1353,6 @@ struct execute_pet_action_t : public action_t
     }
   }
 
-  virtual timespan_t execute_time() const
-  { return pet_action -> execute_time(); }
-
   virtual void execute()
   {
     pet_action -> execute();
