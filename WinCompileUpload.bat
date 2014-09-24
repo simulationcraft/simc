6a33237b5a97e118a4c603b607330dcf47a7aa4c
Alex <s.alexlowery@gmail.com>
2014-09-24 07:53:42 -0400

    Update ftp upload.

--- WinCompileUpload.bat
+++ WinCompileUpload.bat
@@ -1,13 +1,16 @@
 :: Used to automate everything for Alex so he can be lazy.
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b)
+git log --no-merges -1 --pretty="%%h">bla.txt
+set /p revision=<bla.txt
+del bla.txt
 
 
 call win32_release_msvc12.bat
-set filename=E:\simulationcraft\simc-602-alpha-win32-%mydate%.zip
+set filename=E:\simulationcraft\simc-602-alpha-win32-%mydate%-%revision%.zip
 call start winscp /command "open downloads" "put %filename% -nopreservetime -nopermissions -transfer=binary" "exit"
 
 call win64_release_msvc12.bat
-set filename=E:\simulationcraft\simc-602-alpha-win64-%mydate%.zip
+set filename=E:\simulationcraft\simc-602-alpha-win64-%mydate%-%revision%.zip
 winscp /command "open downloads" "put %filename% -nopreservetime -nopermissions -transfer=binary" "exit"
 pause
