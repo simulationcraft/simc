5a1be43f0012bf15c921ad34818e674758e4303a
Alex <s.alexlowery@gmail.com>
2014-09-30 17:01:25 -0400

    There is no more 'PR' or 'Pre-raid' folder, it is now called T17P.

--- engine/sim/sc_option.cpp
+++ engine/sim/sc_option.cpp
@@ -517,8 +517,6 @@ option_db_t::option_db_t()
 
     prefix += "/";
 
-    auto_path.push_back( prefix + "PreRaid" );
-
     // Add profiles for each tier, except pvp
     for ( int i = 0; i < ( N_TIER - 1 ); i++ )
     {
@@ -526,7 +524,7 @@ option_db_t::option_db_t()
       auto_path.push_back( prefix + "Tier" + util::to_string( MIN_TIER + i ) + "H" );
       auto_path.push_back( prefix + "Tier" + util::to_string( MIN_TIER + i ) + "N" );
       auto_path.push_back( prefix + "Tier" + util::to_string( MIN_TIER + i ) + "M" );
-      auto_path.push_back( prefix + "Tier" + util::to_string( MIN_TIER + i ) + "PR" );
+      auto_path.push_back( prefix + "Tier" + util::to_string( MIN_TIER + i ) + "P" );
     }
   }
 
