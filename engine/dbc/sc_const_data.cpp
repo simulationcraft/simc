6b4878809e4e858042446d930b68f42520770794
Alex <s.alexlowery@gmail.com>
2014-10-07 19:40:12 -0400

    18996 spell data + differences

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -185,7 +185,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 18990 : 18990;
+  return maybe_ptr( ptr ) ? 18996 : 18996;
 }
 
 const char* dbc::wow_version( bool ptr )
