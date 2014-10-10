146122b5d0e8cbd5a79b852c7359ab2c2d5f5ad0
Theck <theckhd@gmail.com>
2014-10-10 09:57:00 -0400

    Scaling: Change default delta to rating equivalent of 3.5% haste

--- engine/sim/sc_scaling.cpp
+++ engine/sim/sc_scaling.cpp
@@ -159,10 +159,8 @@ void scaling_t::init_deltas()
 {
   assert ( scale_delta_multiplier != 0 );
 
-  // Use block rating coefficient * 3.39 because it happens to result in nice round numbers both at level 85 and at level 90
-  double default_delta = util::round( sim -> dbc.combat_rating( RATING_BLOCK, sim -> max_player_level ) * 0.0339 ) * scale_delta_multiplier;
-  if ( default_delta < 60 ) // Default delta at level 90 is too low, error margin is gigantic.
-    default_delta = 60;
+  // Use haste rating coefficient * 3.5 to determine default delta
+  double default_delta = util::round( sim -> dbc.combat_rating( RATING_MELEE_HASTE, sim -> max_player_level ) * 0.035 ) * scale_delta_multiplier;
 
   if ( stats.attribute[ ATTR_SPIRIT ] == 0 ) stats.attribute[ ATTR_SPIRIT ] = default_delta;
 
