473847588a977324f477a5779e01434c3ef21ffb
Navv <navv1234@gmail.com>
2014-10-12 20:44:42 +0300

    Add cooldown recharge modifier to html report base cooldown information.

--- engine/report/sc_report_html_player.cpp
+++ engine/report/sc_report_html_player.cpp
@@ -816,7 +816,7 @@ void print_html_action_info( report::sc_html_stream& os, unsigned stats_mask, st
         "<li><span class=\"label\">trigger_gcd:</span>%.4f</li>\n"
         "<li><span class=\"label\">min_gcd:</span>%.4f</li>\n"
         "<li><span class=\"label\">base_cost:</span>%.1f</li>\n"
-        "<li><span class=\"label\">cooldown:</span>%.2f</li>\n"
+        "<li><span class=\"label\">cooldown:</span>%.3f</li>\n"
         "<li><span class=\"label\">base_execute_time:</span>%.2f</li>\n"
         "<li><span class=\"label\">base_crit:</span>%.2f</li>\n"
         "<li><span class=\"label\">target:</span>%s</li>\n"
@@ -832,7 +832,7 @@ void print_html_action_info( report::sc_html_stream& os, unsigned stats_mask, st
         a -> trigger_gcd.total_seconds(),
         a -> min_gcd.total_seconds(),
         a -> base_costs[ a -> current_resource() ],
-        a -> cooldown -> duration.total_seconds(),
+        a -> cooldown -> duration.total_seconds() * a -> cooldown -> get_recharge_multiplier(),
         a -> base_execute_time.total_seconds(),
         a -> base_crit,
         a -> target ? a -> target -> name() : "",
