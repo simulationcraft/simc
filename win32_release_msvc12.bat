26564aa11cc7ca40123f657c3f94e6457a28d1da
Alex <s.alexlowery@gmail.com>
2014-10-15 09:16:02 -0400

    Bump minor version again for next time.

--- win32_release_msvc12.bat
+++ win32_release_msvc12.bat
@@ -11,7 +11,7 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b)
 
 :: Update the qt_dir as necessary
 set qt_dir=C:\Qt\Qt5.3.2\5.3\msvc2013
-set install=simc-602-1-win32
+set install=simc-602-3-win32
 set redist="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT"
 
 :: IMPORTANT NOTE FOR DEBUGGING
