45df1ced6f8b8fa00a576127947df33ba5bbda5a
Navv <navv1234@gmail.com>
2014-10-27 21:26:20 +0200

    [Warlock] Fix Fire and Brimstone some.

--- engine/class_modules/sc_warlock.cpp
+++ engine/class_modules/sc_warlock.cpp
@@ -2805,7 +2805,7 @@ struct immolate_t: public warlock_spell_t
 
   immolate_t( warlock_t* p ):
     warlock_spell_t( p, "Immolate" ),
-    fnb( new immolate_t( "immolate", p, p -> find_spell( 108686 ) ) )
+    fnb( new immolate_t( "immolate_fnb", p, p -> find_spell( 108686 ) ) )
   {
     havoc_consume = 1;
     base_costs[RESOURCE_MANA] *= 1.0 + p -> spec.chaotic_energy -> effectN( 2 ).percent();
@@ -2821,9 +2821,12 @@ struct immolate_t: public warlock_spell_t
     warlock_spell_t( n, p, spell ),
     fnb( 0 )
   {
+    base_tick_time = p -> find_spell( 157736 ) -> effectN( 1 ).period();
+    dot_duration = p -> find_spell( 157736 ) -> duration();
+    hasted_ticks = true;
+    tick_may_crit = true;
+    spell_power_mod.tick = data().effectN( 1 ).sp_coeff();
     aoe = -1;
-
-    stats = p -> get_stats( "immolate_fnb", this );
   }
 
   void schedule_travel( action_state_t* s )
@@ -2932,7 +2935,7 @@ struct conflagrate_t: public warlock_spell_t
 
   conflagrate_t( warlock_t* p ):
     warlock_spell_t( p, "Conflagrate" ),
-    fnb( new conflagrate_t( "conflagrate", p, p -> find_spell( 108685 ) ) )
+    fnb( new conflagrate_t( "conflagrate_fnb", p, p -> find_spell( 108685 ) ) )
   {
     if ( p -> talents.charred_remains -> ok() ){
       base_multiplier *= 1.0 + p -> talents.charred_remains -> effectN( 1 ).percent();
@@ -2946,7 +2949,6 @@ struct conflagrate_t: public warlock_spell_t
     fnb( 0 )
   {
     aoe = -1;
-    stats = p -> get_stats( "conflagrate_fnb", this );
   }
 
   void schedule_execute( action_state_t* state )
@@ -3041,7 +3043,7 @@ struct incinerate_t: public warlock_spell_t
   // Normal incinerate
   incinerate_t( warlock_t* p ):
     warlock_spell_t( p, "Incinerate" ),
-    fnb( new incinerate_t( "incinerate", p, p -> find_spell( 114654 ) ) )
+    fnb( new incinerate_t( "incinerate_fnb", p, p -> find_spell( 114654 ) ) )
   {
     if ( p -> talents.charred_remains -> ok() )
       base_multiplier *= 1.0 + p -> talents.charred_remains -> effectN( 1 ).percent();
@@ -3055,7 +3057,6 @@ struct incinerate_t: public warlock_spell_t
     fnb( 0 )
   {
     aoe = -1;
-    stats = p -> get_stats( "incinerate_fnb", this );
   }
 
   void init()
@@ -3272,7 +3273,7 @@ struct chaos_bolt_t: public warlock_spell_t
   chaos_bolt_t* fnb;
   chaos_bolt_t( warlock_t* p ):
     warlock_spell_t( p, "Chaos Bolt" ),
-    fnb( new chaos_bolt_t( "chaos_bolt", p, p -> find_spell( 116858 ) ) )
+    fnb( new chaos_bolt_t( "chaos_bolt_fnb", p, p -> find_spell( 116858 ) ) )
   {
     if ( !p -> talents.charred_remains -> ok() )
       fnb = 0;
@@ -3289,7 +3290,6 @@ struct chaos_bolt_t: public warlock_spell_t
     aoe = -1;
     backdraft_consume = 3;
     base_execute_time += p -> perk.enhanced_chaos_bolt -> effectN( 1 ).time_value();
-    stats = p -> get_stats( "chaos_bolt_fnb", this );
   }
 
   void schedule_execute( action_state_t* state )
