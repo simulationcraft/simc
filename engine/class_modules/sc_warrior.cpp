bcac78b1a7077d0122c88928f5c4b067b94137d7
Alex <s.alexlowery@gmail.com>
2014-11-03 05:14:26 -0500

    [Warrior] Fix another crash.

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -2632,7 +2632,7 @@ struct slam_t: public warrior_attack_t
   {
     double am = warrior_attack_t::action_multiplier();
 
-    if ( td( execute_state -> target ) -> debuffs_slam-> up() )
+    if ( td( target ) -> debuffs_slam -> up() )
       am *= 1.0 + ( ( (double)td( execute_state -> target ) -> debuffs_slam -> current_stack ) / 2 );
 
     return am;
