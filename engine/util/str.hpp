67e8dec3be765e7ba3dbd81931e831cff5e6c993
Nathaniel Hieter <natehieter@gmail.com>
2014-11-05 13:27:13 -0500

    add str::format; we can move other str utils here later

---
+++ engine/util/str.hpp
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Dedmonwakeen's Raid DPS/TPS Simulator.
+// Send questions to natehieter@gmail.com
+// ==========================================================================
+
+#ifndef STR_HPP
+#define STR_HPP
+
+namespace str {
+
+// These functions will concatenate to buffer.
+std::string& format( std::string& buffer, const char *fmt, va_list args );
+std::string& format( std::string& buffer, const char *fmt, ... );
+
+// Please use the above two if at all convenient.
+std::string format( const char *fmt, va_list args );
+std::string format( const char *fmt, ... );
+
+}; // NAMESPACE STR
+
+#endif // STR_HPP
