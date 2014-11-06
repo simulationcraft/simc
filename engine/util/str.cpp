f172b441a13465346684ebff75ef1d4b045bd004
philoptik <philoptik@gmail.com>
2014-11-06 16:06:34 +0100

    Add using std::isnan; . Don't know if that's how it was intended or if it's a good fix, but it does help here ;)

--- engine/util/str.cpp
+++ engine/util/str.cpp
@@ -142,6 +142,7 @@ static void format_double( std::string& buffer, double d, int min_width, int pre
 
   str.reserve( 32 );
 
+  using std::isnan;
   if( isnan( d ) )
   {
     str         = "NaN";  // reverse
