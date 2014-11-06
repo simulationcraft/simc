d6c737b2b5bbb9d3b528e4bda5659b9ef26568d5
Alex <s.alexlowery@gmail.com>
2014-11-06 13:54:11 -0500

    Fix visual studio 2012

--- engine/config.hpp
+++ engine/config.hpp
@@ -163,7 +163,7 @@ public:
 template<class T>
 inline bool sc_isfinite( T x )
 {
-#if defined( SC_VS )
+#if defined ( SC_VS ) && SC_VS < 12 // std::isfinite was added in vs2013
   return _finite( x ) != 0;
 #else
   return std::isfinite( x ) != 0;
@@ -173,8 +173,8 @@ inline bool sc_isfinite( T x )
 template<class T>
 inline bool sc_isnan( T x )
 {
-#if defined( SC_VS )
-  return isnan( x ) != 0;
+#if defined ( SC_VS ) && SC_VS < 12 // std::isnan was added in vs2013
+  return _isnan( x ) != 0;
 #else
   return std::isnan( x ) != 0;
 #endif
