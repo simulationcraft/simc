e6395b6624e80034c7e63fec1a0251780e87d9cf
Theck <theckhd@gmail.com>
2014-11-19 09:47:02 -0500

    [GUI] target level selector bugfix.

--- engine/class_modules/sc_enemy.cpp
+++ engine/class_modules/sc_enemy.cpp
@@ -844,7 +844,7 @@ void enemy_t::init_base_stats()
     // target_level override
     if ( sim -> target_level >= 0 )
       level = sim -> target_level;
-    else if ( sim -> rel_target_level > 0 && ( sim -> max_player_level + sim -> rel_target_level ) >= 0 )
+    else if ( sim -> rel_target_level >= 0 && ( sim -> max_player_level + sim -> rel_target_level ) >= 0 )
       level = sim -> max_player_level + sim -> rel_target_level;
     else
       level = sim -> max_player_level + 3;
