9fcd3166fc87c413b63b6ec9ab04aed15c570e16
Navv <navv1234@gmail.com>
2014-11-23 18:33:47 +0200

    Bah.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -450,8 +450,6 @@ void dbc::apply_hotfixes()
   item = item_data_t::find( 118876, false );
   assert( item -> stat_alloc[ 0 ] == 6666 && "Lucky Double-Sided Coin static stat has been fixed" );
   item -> stat_alloc[ 0 ] = 4455;
-  assert( item -> stat_type_e[ 0 ] == ITEM_MOD_AGILITY && "Lucky Double-Sided Coin static stat has been fixed" );
-  item -> stat_type_e[ 0 ] = ITEM_MOD_CRIT_RATING;
 
   item = item_data_t::find( 118882, false );
   assert( item -> stat_alloc[ 0 ] == 6666 && "Scabbard of Kyanos static stat has been fixed" );
