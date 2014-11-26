93917e5496aa9a159559a68abb7e192ebc758f5c
Alex <s.alexlowery@gmail.com>
2014-11-26 03:55:31 -0500

    Only add the adb files that we need.

---
+++ dbc_extract/cache/live/ADBFiles.bat
@@ -0,0 +1,2 @@
+robocopy "C:\Program Files (x86)\World of Warcraft\Cache\ADB\enUS" "E:\Simulationcraft\dbc_extract\cache\live" item.adb itembonus.adb itembonustreenode.adb itemeffect.adb itemxbonustree.adb spellclassoptions.adb spellpower.adb spellrunecost.adb item-sparse.adb spellmisc.adb
+pause
