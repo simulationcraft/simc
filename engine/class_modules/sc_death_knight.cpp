ada2d3f570701f50d09259539a30ede0e3534cb0
Navv <navv1234@gmail.com>
2014-12-10 00:20:59 +0200

    [Death Knight] Enable haste scaling for Rune of the Fallen Crusader RPPM. Fixes Issue 2266. Potentially. Gulp.

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -6788,6 +6788,7 @@ void runeforge::fallen_crusader( special_effect_t& effect,
   const death_knight_t* dk = debug_cast<const death_knight_t*>( item.player );
 
   effect.ppm_ = -1.0 * dk -> fallen_crusader_rppm;
+  effect.rppm_scale = RPPM_HASTE;
   effect.custom_buff = b;
   effect.execute_action = heal;
 
