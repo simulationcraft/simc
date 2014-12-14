e053ce0c706d38a9d14af488054366dc9320fbf4
Alex <s.alexlowery@gmail.com>
2014-12-14 04:28:55 -0500

    Revert windows script changes, it doesn't appear that qtwindeploy can properly differentiate 32 bit and 64 bit qt dlls.

--- win32_release_msvc12.bat
+++ win32_release_msvc12.bat
@@ -9,6 +9,8 @@
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b)
 
+:: Update the qt_dir as necessary
+set qt_dir=C:\Qt\Qt5.4.0\5.4\msvc2013
 set install=simc-603-18-win32
 set redist="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT"
 
@@ -19,44 +21,49 @@ set redist="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Mi
 :: The debug Qt dlls are named: Qt___d5.dll
 
 :: Delete old folder/files
+
 rd %install% /s /q
-rd Plugins /s /q 
+:: Copying new dlls
 
 for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
 
 "%MSBUILDDIR%msbuild.exe" E:\simulationcraft\simc_vs2013.sln /p:configuration=release /p:platform=win32 /nr:true /m:8
-forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
-windeployqt --release --no-translations --no-compiler-runtime --dir Plugins simulationcraft.exe
 
+del /s simc_cache.dat
+forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
+
+xcopy %qt_dir%\plugins\imageformats %install%\imageformats\
 xcopy %redist%\msvcp120.dll %install%\
 xcopy %redist%\msvcr120.dll %install%\
 xcopy %redist%\vccorlib120.dll %install%\
+xcopy %qt_dir%\bin\Qt5Core.dll %install%\
+xcopy %qt_dir%\bin\Qt5OpenGL.dll %install%\
+xcopy %qt_dir%\bin\Qt5PrintSupport.dll %install%\
+xcopy %qt_dir%\bin\Qt5Quick.dll %install%\
+xcopy %qt_dir%\bin\Qt5Qml.dll %install%\
+xcopy %qt_dir%\bin\Qt5Positioning.dll %install%\
+xcopy %qt_dir%\bin\Qt5Sql.dll %install%\
+xcopy %qt_dir%\bin\Qt5Gui.dll %install%\
+xcopy %qt_dir%\bin\Qt5Widgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Network.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebKit.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebKitWidgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Multimedia.dll %install%\
+xcopy %qt_dir%\bin\Qt5MultimediaWidgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Sensors.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebChannel.dll %install%\
 
-move Plugins\Qt5Core.dll %install%\
-move Plugins\Qt5OpenGL.dll %install%\
-move Plugins\Qt5PrintSupport.dll %install%\
-move Plugins\Qt5Quick.dll %install%\
-move Plugins\Qt5Qml.dll %install%\
-move Plugins\Qt5Positioning.dll %install%\
-move Plugins\Qt5Sql.dll %install%\
-move Plugins\Qt5Gui.dll %install%\
-move Plugins\Qt5Widgets.dll %install%\
-move Plugins\Qt5Network.dll %install%\
-move Plugins\Qt5WebKit.dll %install%\
-move Plugins\Qt5WebKitWidgets.dll %install%\
-move Plugins\Qt5Multimedia.dll %install%\
-move Plugins\Qt5MultimediaWidgets.dll %install%\
-move Plugins\Qt5Sensors.dll %install%\
-move Plugins\Qt5WebChannel.dll %install%\
-move Plugins\Qt5Svg.dll %install%\
+xcopy %qt_dir%\bin\libGLESv2.dll %install%\
+xcopy %qt_dir%\bin\icudt53.dll %install%\
+xcopy %qt_dir%\bin\icuin53.dll %install%\
+xcopy %qt_dir%\bin\icuuc53.dll %install%\
+xcopy %qt_dir%\bin\libEGL.dll %install%\
+xcopy %qt_dir%\bin\D3DCompiler_47.dll %install%\
 
-move Plugins\libGLESv2.dll %install%\
-move Plugins\icudt53.dll %install%\
-move Plugins\icuin53.dll %install%\
-move Plugins\icuuc53.dll %install%\
-move Plugins\libEGL.dll %install%\
-move Plugins\D3DCompiler_47.dll %install%\
+xcopy %qt_dir%\plugins\platforms\qminimal.dll %install%\platforms\
+xcopy %qt_dir%\plugins\platforms\qwindows.dll %install%\platforms\
 
+:: Copy other relevant files for windows release
 xcopy Welcome.html %install%\
 xcopy Welcome.png %install%\
 xcopy Simulationcraft.exe %install%\
@@ -65,6 +72,5 @@ xcopy readme.txt %install%\
 xcopy Error.html %install%\
 xcopy COPYING %install%\
 xcopy Profiles %install%\profiles\ /s /e
-xcopy Plugins %install%\Plugins\ /s /e
 xcopy C:\OpenSSL-Win32\bin\libeay32.dll %install%\
 xcopy C:\OpenSSL-Win32\bin\ssleay32.dll %install%\
--- win64_release_msvc12.bat
+++ win64_release_msvc12.bat
@@ -9,6 +9,8 @@
 
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b)
 
+:: Update the qt_dir as necessary
+set qt_dir=C:\Qt\Qt5.4.0\5.4\msvc2013_64
 set install=simc-603-18-win64
 set redist="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x64\Microsoft.VC120.CRT"
 
@@ -19,43 +21,50 @@ set redist="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x64\Mi
 :: The debug Qt dlls are named: Qt___d5.dll
 
 :: Delete old folder/files
+
 rd %install% /s /q
-rd Plugins /s /q 
 
 for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\12.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
-"%MSBUILDDIR%msbuild.exe" E:\simulationcraft\simc_vs2013.sln /p:configuration=pgo /p:platform=x64 /nr:true
-forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc64.exe @file"
-windeployqt64 --release --no-translations --no-compiler-runtime --dir Plugins simulationcraft64.exe
 
+"%MSBUILDDIR%msbuild.exe" E:\simulationcraft\simc_vs2013.sln /p:configuration=pgo /p:platform=x64 /nr:true
+
+del /s simc_cache.dat
+forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc64.exe @file"
+
+:: Copying new dlls
+
+xcopy %qt_dir%\plugins\imageformats %install%\imageformats\
 xcopy %redist%\msvcp120.dll %install%\
 xcopy %redist%\msvcr120.dll %install%\
 xcopy %redist%\vccorlib120.dll %install%\
+xcopy %qt_dir%\bin\Qt5Core.dll %install%\
+xcopy %qt_dir%\bin\Qt5OpenGL.dll %install%\
+xcopy %qt_dir%\bin\Qt5Quick.dll %install%\
+xcopy %qt_dir%\bin\Qt5PrintSupport.dll %install%\
+xcopy %qt_dir%\bin\Qt5Qml.dll %install%\
+xcopy %qt_dir%\bin\Qt5Sql.dll %install%\
+xcopy %qt_dir%\bin\Qt5Positioning.dll %install%\
+xcopy %qt_dir%\bin\Qt5Gui.dll %install%\
+xcopy %qt_dir%\bin\Qt5Widgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Network.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebKit.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebKitWidgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Multimedia.dll %install%\
+xcopy %qt_dir%\bin\Qt5MultimediaWidgets.dll %install%\
+xcopy %qt_dir%\bin\Qt5Sensors.dll %install%\
+xcopy %qt_dir%\bin\Qt5WebChannel.dll %install%\
 
-move Plugins\Qt5Core.dll %install%\
-move Plugins\Qt5OpenGL.dll %install%\
-move Plugins\Qt5PrintSupport.dll %install%\
-move Plugins\Qt5Quick.dll %install%\
-move Plugins\Qt5Qml.dll %install%\
-move Plugins\Qt5Positioning.dll %install%\
-move Plugins\Qt5Sql.dll %install%\
-move Plugins\Qt5Gui.dll %install%\
-move Plugins\Qt5Widgets.dll %install%\
-move Plugins\Qt5Network.dll %install%\
-move Plugins\Qt5WebKit.dll %install%\
-move Plugins\Qt5WebKitWidgets.dll %install%\
-move Plugins\Qt5Multimedia.dll %install%\
-move Plugins\Qt5MultimediaWidgets.dll %install%\
-move Plugins\Qt5Sensors.dll %install%\
-move Plugins\Qt5WebChannel.dll %install%\
-move Plugins\Qt5Svg.dll %install%\
+xcopy %qt_dir%\bin\libGLESv2.dll %install%\
+xcopy %qt_dir%\bin\icudt53.dll %install%\
+xcopy %qt_dir%\bin\icuin53.dll %install%\
+xcopy %qt_dir%\bin\icuuc53.dll %install%\
+xcopy %qt_dir%\bin\libEGL.dll %install%\
+xcopy %qt_dir%\bin\D3DCompiler_47.dll %install%\
 
-move Plugins\libGLESv2.dll %install%\
-move Plugins\icudt53.dll %install%\
-move Plugins\icuin53.dll %install%\
-move Plugins\icuuc53.dll %install%\
-move Plugins\libEGL.dll %install%\
-move Plugins\D3DCompiler_47.dll %install%\
+xcopy %qt_dir%\plugins\platforms\qminimal.dll %install%\platforms\
+xcopy %qt_dir%\plugins\platforms\qwindows.dll %install%\platforms\
 
+:: Copy other relevant files for windows release
 xcopy Welcome.html %install%\
 xcopy Welcome.png %install%\
 xcopy Simulationcraft64.exe %install%\
@@ -64,6 +73,5 @@ xcopy readme.txt %install%\
 xcopy Error.html %install%\
 xcopy COPYING %install%\
 xcopy Profiles %install%\profiles\ /s /e
-xcopy Plugins %install%\Plugins\ /s /e
 xcopy C:\OpenSSL-Win64\bin\libeay32.dll %install%\
 xcopy C:\OpenSSL-Win64\bin\ssleay32.dll %install%\
