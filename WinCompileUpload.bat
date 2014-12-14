dd99bb0a4a457b9c59bd719d51535fd098f3c690
Alex <s.alexlowery@gmail.com>
2014-12-14 13:39:57 -0500

    Update windows build script

--- WinCompileUpload.bat
+++ WinCompileUpload.bat
@@ -1,19 +1,26 @@
 :: Used to automate everything for Alex so he can be lazy.
 
+git clean -f -x
+
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b)
 git log --no-merges -1 --pretty="%%h">bla.txt
 set /p revision=<bla.txt
 del bla.txt
 
+set install=simc-603-18-
+
+robocopy . %install%source /s *.* /xd .git %install%source /xf *.pgd /xn
+set filename=%install%source.zip
+7z a -r -mx9 %filename% %install%source
+call start winscp /command "open downloads" "put E:\Simulationcraft\%filename% -nopreservetime -nopermissions -transfer=binary" "exit"
+
 call win32_release_msvc12.bat
-set install=simc-603-18-win32
-set filename=%install%-%mydate%-%revision%.zip
+set filename=%install%win32-%mydate%-%revision%.zip
 7z a -r -mx9 %filename% %install%
 call start winscp /command "open downloads" "put E:\Simulationcraft\%filename% -nopreservetime -nopermissions -transfer=binary" "exit"
 
 call win64_release_msvc12.bat
-set install=simc-603-18-win64
-set filename=%install%-%mydate%-%revision%.zip
+set filename=%install%win64-%mydate%-%revision%.zip
 7z a -r -mx9 %filename% %install%
 winscp /command "open downloads" "put E:\Simulationcraft\%filename% -nopreservetime -nopermissions -transfer=binary" "exit"
 pause
