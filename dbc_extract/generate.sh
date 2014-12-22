283285152818dc4b0c417e5f5df692d7c96bc0c7
Navv <navv1234@gmail.com>
2014-12-22 03:02:18 +0200

    Fix DBC export scripts. Regenerate exported data.

--- dbc_extract/generate.sh
+++ dbc_extract/generate.sh
@@ -35,31 +35,31 @@ if [ ! -d $INPUT ]; then
 fi
 
 
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t talent                  > $OUTPATH/sc_talent_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t spec_list               > $OUTPATH/sc_spec_list${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t talent                  > $OUTPATH/sc_talent_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t spec_list               > $OUTPATH/sc_spec_list${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t spell> $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
 if [ x"$WOWVERSION" != x"-v 5.3.0" ]; then
-  ./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t rppm_coeff             >> $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
+  ./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t rppm_coeff             >> $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
 fi
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t scale                   > $OUTPATH/sc_scale_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t class_list              > $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t spec_spell_list        >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t mastery_list           >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t racial_list            >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t glyph_list             >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t set_list               >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t set_list2              >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t perk_list              >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t glyph_property_list    >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t scale                   > $OUTPATH/sc_scale_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t class_list              > $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t spec_spell_list        >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t mastery_list           >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t racial_list            >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t glyph_list             >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t set_list               >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t set_list2              >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t perk_list              >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t glyph_property_list    >> $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t item > $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t random_property_points >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t random_suffix          >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t item_ench              >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t item_armor             >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t weapon_damage          >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t gem_properties         >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t item_upgrade           >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
-./dbc_extract.py -p $INPUT $WOWVERSION -b $BUILD$PTR -t item_bonus             >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t random_property_points >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t random_suffix          >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t item_ench              >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t item_armor             >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t weapon_damage          >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t gem_properties         >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t item_upgrade           >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
+./dbc_extract.py -p $INPUT --cache=$CACHEDIR $WOWVERSION -b $BUILD$PTR -t item_bonus             >> $OUTPATH/sc_item_data${PTR:+_ptr}.inc
 
 (
 cat << zz1234
