0e7b344f7ee78eb0f8570829506a67c5ad0416b8
Navv <navv1234@gmail.com>
2015-01-05 15:58:14 +0200

    [Druid] Fix Balance of Power Moonfire duration extension (was giving 6 milliseconds, now 6 seconds).

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -5356,7 +5356,7 @@ struct starfire_t : public druid_spell_t
     druid_spell_t::impact( s );
 
     if ( p() -> talent.balance_of_power && result_is_hit( s -> result ) )
-      td( s -> target ) -> dots.moonfire -> extend_duration( p() -> talent.balance_of_power -> effectN( 1 ).time_value(), 0 );
+      td( s -> target ) -> dots.moonfire -> extend_duration( timespan_t::from_seconds( p() -> talent.balance_of_power -> effectN( 1 ).base_value() ), 0 );
   }
 };
 
