97087a00d6dfc6783e976569ce3c4f775b33b076
Alex <s.alexlowery@gmail.com>
2015-01-07 15:21:13 -0500

    [WebEngine] Fix webengine path issues by removing library path.

--- win32_release_msvc12.bat
+++ win32_release_msvc12.bat
@@ -25,21 +25,22 @@ for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBui
 forfiles -s -m generate_????.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
 
 robocopy %redist%\ %install%\ msvcp120.dll msvcr120.dll vccorlib120.dll
-robocoby .\locale\ %install%\locale sc_de.qm sc_zh.qm
+robocoby locale\ %install%\locale sc_de.qm sc_zh.qm
 robocopy %qt_dir%\bin\ %install%\ Qt5Core.dll Qt5Quick.dll Qt5PrintSupport.dll Qt5Qml.dll Qt5Svg.dll Qt5Gui.dll Qt5Widgets.dll Qt5Network.dll Qt5WebEngineCore.dll Qt5WebEngine.dll Qt5WebEngineWidgets.dll libGLESv2.dll icudt53.dll icuin53.dll icuuc53.dll libEGL.dll D3DCompiler_47.dll opengl32sw.dll QtWebEngineProcess.exe
+robocoby vs\ %install%\ qt.conf
 robocopy %qt_dir%\ %install%\ icudtl.dat qtwebengine_resources.pak
-robocopy %qt_dir%\plugins\platforms %install%\plugins\platforms\ qwindows.dll
-robocopy %qt_dir%\plugins\audio %install%\plugins\audio qtaudio_windows.dll
-robocopy %qt_dir%\plugins\bearer %install%\plugins\bearer qgenericbearer.dll qnativewifibearer.dll
-robocopy %qt_dir%\plugins\iconengines %install%\plugins\iconengines qsvgicon.dll
-robocopy %qt_dir%\plugins\imageformats %install%\plugins\imageformats qdds.dll qgif.dll qicns.dll qico.dll qjp2.dll qjpeg.dll qmng.dll qsvg.dll qtga.dll qtiff.dll qwbmp.dll qwebp.dll
-robocopy %qt_dir%\plugins\mediaserver %install%\plugins\mediaserver dsengine.dll qtmedia_audioengine.dll wmfengine.dll 
-robocopy %qt_dir%\plugins\playlistformats %install%\plugins\playlistformats qtmultimedia_m3u.dll
-robocopy %qt_dir%\plugins\position %install%\plugins\position qtposition_positionpoll.dll
-robocopy %qt_dir%\plugins\printsupport %install%\plugins\printsupport windowsprintersupport.dll
-robocopy %qt_dir%\plugins\sensorgestures %install%\plugins\sensorgestures qtsensorgestures_plugin.dll qtsensorgestures_shakeplugin.dll
-robocopy %qt_dir%\plugins\sensors %install%\plugins\sensors qtsensors_dummy.dll qtsensors_generic.dll
-robocopy %qt_dir%\plugins\sqldrivers %isntall%\plugins\sqldrivers qsqlite.dll qsqlmysql.dll qsqlodbc.dll qsqlpsql.dll
+robocopy %qt_dir%\plugins\platforms %install%\platforms\ qwindows.dll
+robocopy %qt_dir%\plugins\audio %install%\audio qtaudio_windows.dll
+robocopy %qt_dir%\plugins\bearer %install%\bearer qgenericbearer.dll qnativewifibearer.dll
+robocopy %qt_dir%\plugins\iconengines %install%\iconengines qsvgicon.dll
+robocopy %qt_dir%\plugins\imageformats %install%\imageformats qdds.dll qgif.dll qicns.dll qico.dll qjp2.dll qjpeg.dll qmng.dll qsvg.dll qtga.dll qtiff.dll qwbmp.dll qwebp.dll
+robocopy %qt_dir%\plugins\mediaserver %install%\mediaserver dsengine.dll qtmedia_audioengine.dll wmfengine.dll 
+robocopy %qt_dir%\plugins\playlistformats %install%\playlistformats qtmultimedia_m3u.dll
+robocopy %qt_dir%\plugins\position %install%\position qtposition_positionpoll.dll
+robocopy %qt_dir%\plugins\printsupport %install%\printsupport windowsprintersupport.dll
+robocopy %qt_dir%\plugins\sensorgestures %install%\sensorgestures qtsensorgestures_plugin.dll qtsensorgestures_shakeplugin.dll
+robocopy %qt_dir%\plugins\sensors %install%\sensors qtsensors_dummy.dll qtsensors_generic.dll
+robocopy %qt_dir%\plugins\sqldrivers %install%\sqldrivers qsqlite.dll qsqlmysql.dll qsqlodbc.dll qsqlpsql.dll
 robocopy . %install%\ Welcome.html Welcome.png Simulationcraft.exe simc.exe readme.txt Error.html COPYING
 robocopy C:\OpenSSL-Win32\bin %install%\ libeay32.dll ssleay32.dll 
 robocopy Profiles\ %install%\profiles\ *.* /S
