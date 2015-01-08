600b162429ffd97f85a73853b68ef3dd94c9d5d9
Alex <s.alexlowery@gmail.com>
2015-01-08 15:55:08 -0500

    bump minor

--- WinCompileUpload.bat
+++ WinCompileUpload.bat
@@ -10,7 +10,7 @@ set /p revision=<bla.txt
 :: Hacky hack because windows command prompt is annoying.
 del bla.txt
 
-set simcversion=603-22
+set simcversion=603-23
 set install=simc-%simcversion%-source
 cd>bla.txt
 set /p download=<bla.txt
