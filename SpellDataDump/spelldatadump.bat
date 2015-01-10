fb5c21b53547d98f25320f04d72270e2cc6e7b7f
Alex <s.alexlowery@gmail.com>
2015-01-10 14:15:46 -0500

    Spelldata differences between live and ptr.

--- SpellDataDump/spelldatadump.bat
+++ SpellDataDump/spelldatadump.bat
@@ -1,14 +1,14 @@
 cd ..
-simc64 spell_query="spell.class=warrior">spelldatadump/warrior.txt
-simc64 spell_query="spell.class=hunter">spelldatadump/hunter.txt
-simc64 spell_query="spell.class=monk">spelldatadump/monk.txt
-simc64 spell_query="spell.class=paladin">spelldatadump/paladin.txt
-simc64 spell_query="spell.class=rogue">spelldatadump/rogue.txt
-simc64 spell_query="spell.class=shaman">spelldatadump/shaman.txt
-simc64 spell_query="spell.class=mage">spelldatadump/mage.txt
-simc64 spell_query="spell.class=warlock">spelldatadump/warlock.txt
-simc64 spell_query="spell.class=druid">spelldatadump/druid.txt
-simc64 spell_query="spell.class=deathknight">spelldatadump/deathknight.txt
-simc64 spell_query="spell.class=priest">spelldatadump/priest.txt
-simc64 spell_query="spell">spelldatadump/allspells.txt
+simc64 ptr=1 spell_query="spell.class=warrior">spelldatadump/warrior.txt
+simc64 ptr=1 spell_query="spell.class=hunter">spelldatadump/hunter.txt
+simc64 ptr=1 spell_query="spell.class=monk">spelldatadump/monk.txt
+simc64 ptr=1 spell_query="spell.class=paladin">spelldatadump/paladin.txt
+simc64 ptr=1 spell_query="spell.class=rogue">spelldatadump/rogue.txt
+simc64 ptr=1 spell_query="spell.class=shaman">spelldatadump/shaman.txt
+simc64 ptr=1 spell_query="spell.class=mage">spelldatadump/mage.txt
+simc64 ptr=1 spell_query="spell.class=warlock">spelldatadump/warlock.txt
+simc64 ptr=1 spell_query="spell.class=druid">spelldatadump/druid.txt
+simc64 ptr=1 spell_query="spell.class=deathknight">spelldatadump/deathknight.txt
+simc64 ptr=1 spell_query="spell.class=priest">spelldatadump/priest.txt
+simc64 ptr=1 spell_query="spell">spelldatadump/allspells.txt
 pause
