b155467b9dc2c98fac75bd972810b0e0b01375c4
Navv <navv1234@gmail.com>
2015-01-27 23:20:25 +0200

    19533.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -89,7 +89,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19508 : 19342;
+  return maybe_ptr( ptr ) ? 19533 : 19342;
 }
 
 const char* dbc::wow_version( bool ptr )
