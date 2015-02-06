71623531126b502bdd4952c281947ed2bcf76aab
philoptik <philoptik@gmail.com>
2015-02-06 22:52:54 +0100

    oh and catch the other printf warnings as well.

--- engine/report/sc_report_text.cpp
+++ engine/report/sc_report_text.cpp
@@ -600,7 +600,7 @@ void print_text_iteration_data( FILE* file, sim_t* sim )
 
     for ( size_t i = 0; i < sim -> iteration_data.size(); i++ )
     {
-      fprintf( file, "| %9.1f | %20llu | %10llu |\n",
+      util::fprintf( file, "| %9.1f | %20" PRId64 " | %10" PRId64 " |\n",
           sim -> iteration_data[ i ].metric,
           sim -> iteration_data[ i ].seed,
           sim -> iteration_data[ i ].target_health );
