20965fd2f71fbfd7b96214241ad030f74813610f
Navv <navv1234@gmail.com>
2015-02-19 15:21:47 +0200

    [Mage] Fix an issue with Prismatic Crystal and non-mage specific damage spells.

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -779,7 +779,12 @@ struct prismatic_crystal_t : public pet_t
       // execute with a state
       const prismatic_crystal_aoe_state_t* st = debug_cast<const prismatic_crystal_aoe_state_t*>( pre_execute_state );
 
-      assert( p() -> proxy_stats[ st -> owner_action -> internal_id ] != 0 );
+      if ( p() -> proxy_stats.size() < st -> owner_action -> internal_id ||
+           ! p() -> proxy_stats[ st -> owner_action -> internal_id ] )
+      {
+        p() -> add_proxy_stats( st -> owner_action );
+      }
+
       stats = p() -> proxy_stats[ st -> owner_action -> internal_id ];
 
       spell_t::execute();
