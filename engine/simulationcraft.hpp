9db4fac2f3265b7ebe5a3509875dec34e69439aa
Alex <s.alexlowery@gmail.com>
2015-02-20 22:16:43 -0500

    Bump version to 610-01, copy ptr spell data to live.

--- engine/simulationcraft.hpp
+++ engine/simulationcraft.hpp
@@ -5,9 +5,9 @@
 #ifndef SIMULATIONCRAFT_H
 #define SIMULATIONCRAFT_H
 
-#define SC_MAJOR_VERSION "603"
-#define SC_MINOR_VERSION "28"
-#define SC_USE_PTR ( 1 )
+#define SC_MAJOR_VERSION "610"
+#define SC_MINOR_VERSION "01"
+#define SC_USE_PTR ( 0 )
 #define SC_BETA ( 0 )
 #define SC_BETA_STR "wod"
 #define SC_VERSION ( SC_MAJOR_VERSION "-" SC_MINOR_VERSION )
