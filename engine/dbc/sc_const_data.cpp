f3c8d24c8bfe286c578ff1310d4f9dfa07591039
Alex <s.alexlowery@gmail.com>
2015-02-24 14:01:03 -0500

    19678 spell data, small changes.. mostly items.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,7 +86,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19658 : 19658;
+  return maybe_ptr( ptr ) ? 19678 : 19678;
 }
 
 const char* dbc::wow_version( bool ptr )
