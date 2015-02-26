1db55b3fa72e0f8aa5187d093edc30c012877389
jorgster247123@gmail.com <jorgster247123@gmail.com>
2015-02-26 03:09:35 -0600

    [Druid] FB multistrike bug fixed.

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -2423,17 +2423,6 @@ struct ferocious_bite_t : public cat_attack_t
           td( state -> target ) -> dots.rip -> refresh_duration( 0 );
       }
     }
-    // 02/24/2015: Ferocious Bite multistrikes trigger the extra energy consumption.
-    else if ( p() -> bugs && result_is_multistrike( state -> result ) )
-    {
-      double max_amount = data().effectN( 2 ).base_value();
-      if ( p() -> buff.berserk -> check() )
-        max_amount *= 1.0 + p() -> spell.berserk_cat -> effectN( 1 ).percent();
-
-      double amount = std::min( p() -> resources.current[ RESOURCE_ENERGY ], max_amount );
-      p() -> resource_loss( current_resource(), amount );
-      stats -> consume_resource( current_resource(), amount );
-    }
   }
 
   virtual void consume_resource()
