3a63939830f502f53353b8e9ba309fe42d247e77
Alex <s.alexlowery@gmail.com>
2015-02-27 18:27:11 -0500

    spelldata differences

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,7 +86,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19678 : 19678;
+  return maybe_ptr( ptr ) ? 19702 : 19702;
 }
 
 const char* dbc::wow_version( bool ptr )
