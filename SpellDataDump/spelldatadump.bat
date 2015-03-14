deaed34d7eeaac4653fc81b80acf55597fb7cf32
Alex <s.alexlowery@gmail.com>
2015-03-14 02:23:51 -0400

    Now, do spell differences from live to ptr.
    Also improve the script used for generating these.

--- SpellDataDump/spelldatadump.bat
+++ SpellDataDump/spelldatadump.bat
@@ -1,14 +1,10 @@
 cd ..
-simc64 spell_query="spell.class=warrior">spelldatadump/warrior.txt
-simc64 spell_query="spell.class=hunter">spelldatadump/hunter.txt
-simc64 spell_query="spell.class=monk">spelldatadump/monk.txt
-simc64 spell_query="spell.class=paladin">spelldatadump/paladin.txt
-simc64 spell_query="spell.class=rogue">spelldatadump/rogue.txt
-simc64 spell_query="spell.class=shaman">spelldatadump/shaman.txt
-simc64 spell_query="spell.class=mage">spelldatadump/mage.txt
-simc64 spell_query="spell.class=warlock">spelldatadump/warlock.txt
-simc64 spell_query="spell.class=druid">spelldatadump/druid.txt
-simc64 spell_query="spell.class=deathknight">spelldatadump/deathknight.txt
-simc64 spell_query="spell.class=priest">spelldatadump/priest.txt
-simc64 spell_query="spell">spelldatadump/allspells.txt
+set ptr="ptr=1"
+set classes=warrior,hunter,monk,paladin,rogue,shaman,mage,warlock,druid,deathknight,priest
+
+for %%i in (%classes%) do (
+simc64 %ptr% spell_query="spell.class=%%i">spelldatadump/%%i.txt
+)
+
+simc64 %ptr% spell_query="spell">spelldatadump/allspells.txt
 pause
