9bb7ce8e23b12ae8e1f0f549a43125d97f7ea999
Alex <s.alexlowery@gmail.com>
2015-03-19 05:16:35 -0400

    update ptr to 19783

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,7 +86,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19769 : 19702;
+  return maybe_ptr( ptr ) ? 19783 : 19702;
 }
 
 const char* dbc::wow_version( bool ptr )
