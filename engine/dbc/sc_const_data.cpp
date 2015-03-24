42eb5f4d0b160da73826db8f4231eae76210d915
Alex <s.alexlowery@gmail.com>
2015-03-23 21:28:20 -0400

    6.1.2 is being released tomorrow. Bump the release version of simc up, disable ptr, import 19802 spell data.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,11 +86,11 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19783 : 19702;
+  return maybe_ptr( ptr ) ? 19802 : 19802;
 }
 
 const char* dbc::wow_version( bool ptr )
-{ return maybe_ptr( ptr ) ? "6.1.2" : "6.1.0"; }
+{ return maybe_ptr( ptr ) ? "6.1.2" : "6.1.2"; }
 
 const char* dbc::wow_ptr_status( bool ptr )
 { return ( maybe_ptr( ptr ) ?
