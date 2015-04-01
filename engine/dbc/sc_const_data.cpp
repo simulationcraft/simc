983d3a30d11a5010db70d4a1bbd000e55da6bee4
Alex <s.alexlowery@gmail.com>
2015-04-01 18:32:14 -0400

    import 19831 spell data, no changes.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,7 +86,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19802 : 19802;
+  return maybe_ptr( ptr ) ? 19831 : 19831;
 }
 
 const char* dbc::wow_version( bool ptr )
