9cf29a7c83a64bb81b522ad8c603b67146cfa5d2
Alex <s.alexlowery@gmail.com>
2015-04-07 17:54:14 -0400

    bump version to 18965

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -86,7 +86,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 19831 : 19831;
+  return maybe_ptr( ptr ) ? 19865 : 19865;
 }
 
 const char* dbc::wow_version( bool ptr )
