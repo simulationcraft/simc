552685849350d94fafabecce3fa766b6825c5b72
Alex <s.alexlowery@gmail.com>
2015-04-27 01:14:56 -0400

    Change spell generation script files so that they don't need to be modified on other computers.

--- casc_extract/WinGenerateSpellDataPTR.bat
+++ casc_extract/WinGenerateSpellDataPTR.bat
@@ -2,12 +2,11 @@ call python casc_extract.py -m batch --cdn --ptr -o wow
 cd wow
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
-set /p oldname=<hi.txt
 set wowdir=%wowdir:~4,-14%
-ren %oldname% %wowdir%
 del hi.txt
 cd ..
 cd ..
+set curr=%cd%
 cd dbc_extract
-call generate.bat ptr %wowdir% E:\simulationcraft\casc_extract\wow\
+call generate.bat ptr %wowdir% %curr%\casc_extract\wow
 pause
