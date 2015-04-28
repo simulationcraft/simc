61d0d6d328f09e503fbea2e2873f8cbd22e51c97
Alex <s.alexlowery@gmail.com>
2015-04-28 19:44:52 -0400

    whoops, screwed up my spelldata scripts

--- casc_extract/WinGenerateSpellData.bat
+++ casc_extract/WinGenerateSpellData.bat
@@ -2,7 +2,9 @@ call python casc_extract.py -m batch --cdn -o wow
 cd wow
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
+set /p oldname=<hi.txt
 set wowdir=%wowdir:~4,-17%
+ren %oldname% %wowdir%
 del hi.txt
 cd ..
 cd ..
