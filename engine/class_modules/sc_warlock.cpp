d4039f4ec1890fb816e10814b7120ccac6141c9b
gahddo <tjc12821@gmail.com>
2015-06-14 20:26:40 -0400

    [Warlock] Affliction apl update.

--- engine/class_modules/sc_warlock.cpp
+++ engine/class_modules/sc_warlock.cpp
@@ -6055,7 +6055,7 @@ void warlock_t::apl_affliction()
   else
     add_action( "Haunt", "if=shard_react&!talent.soulburn_haunt.enabled&!in_flight_to_target&(dot.haunt.remains<duration*0.3+cast_time+travel_time|soul_shard=4)&(trinket.proc.any.react|trinket.stacking_proc.any.react>6|buff.dark_soul.up|soul_shard>2|soul_shard*14<=target.time_to_die)&(buff.dark_soul.down|set_bonus.tier18_4pc=0)" );
   
-  add_action( "Soulburn", "if=shard_react&talent.soulburn_haunt.enabled&buff.soulburn.down&(buff.haunting_spirits.remains-action.haunt.cast_time<=buff.haunting_spirits.duration*0.3)" );
+  add_action( "Soulburn", "if=shard_react&soul_shard>=2&talent.soulburn_haunt.enabled&buff.soulburn.down&(buff.haunting_spirits.remains-action.haunt.cast_time<=buff.haunting_spirits.duration*0.3)" );
   add_action( "Haunt", "if=shard_react&talent.soulburn_haunt.enabled&!in_flight_to_target&((buff.soulburn.up&((buff.haunting_spirits.remains-cast_time<=buff.haunting_spirits.duration*0.3&dot.haunt.remains-cast_time<=dot.haunt.duration*0.3)|buff.haunting_spirits.down)))" );
   
   if ( find_item( "nithramus_the_allseer" ) )
