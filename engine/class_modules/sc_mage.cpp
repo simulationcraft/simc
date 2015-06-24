3c5182c368efa16017f3e4a30eec784852e72d34
Kent <kenthklui@gmail.com>
2015-06-24 16:54:03 -0500

    [Mage] Disable Frozen Orb from triggering Doom Nova

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -3393,6 +3393,12 @@ struct frozen_orb_bolt_t : public mage_spell_t
           -> trigger( 1, buff_t::DEFAULT_VALUE(), fof_proc_chance );
     }
   }
+
+  // Override damage type because Frozen Orb is considered a DOT
+  dmg_e amount_type( const action_state_t* /* state */, bool /* periodic */ ) const
+  {
+    return DMG_OVER_TIME;
+  }
 };
 
 struct frozen_orb_t : public mage_spell_t
