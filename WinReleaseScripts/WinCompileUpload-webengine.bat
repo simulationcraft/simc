a5496971a7f1d25188b58d9dd1f259d3c473a521
Alex <s.alexlowery@gmail.com>
2015-07-06 22:36:38 -0400

    Update release scripts.

--- WinReleaseScripts/WinCompileUpload-webengine.bat
+++ WinReleaseScripts/WinCompileUpload-webengine.bat
@@ -16,7 +16,7 @@ set /p ask=Build with PGO data? Only applies to 64-bit installation. (y/n)
 
 set simcversion=620-02
 :: For bumping the minor version, just change the above line.  Make sure to also change setup32.iss and setup64.iss as well. 
-set simcfiles=E:\Simulationcraft\
+set simcfiles=C:\Simulationcraft\
 :: Location of source files
 set ssllocation32=C:\OpenSSL-Win32\bin
 set ssllocation64=C:\OpenSSL-Win64\bin
