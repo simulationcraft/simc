2ed682b820b5555ed86c2000ddf802dae7d92ab8
Alex <s.alexlowery@gmail.com>
2015-07-12 12:55:00 -0400

    update spelldata/item cache

--- dbc_extract/cache/live/ADBFiles.bat
+++ dbc_extract/cache/live/ADBFiles.bat
@@ -1,2 +1,2 @@
-robocopy "H:\World of Warcraft\Cache\ADB\enUS" "E:\Simulationcraft\dbc_extract\cache\live" item.adb itembonus.adb itembonustreenode.adb itemeffect.adb itemxbonustree.adb spellclassoptions.adb spellpower.adb spellrunecost.adb item-sparse.adb spellmisc.adb
+robocopy "C:\World of Warcraft\Cache\ADB\enUS" "C:\Simulationcraft\dbc_extract\cache\live" item.adb itembonus.adb itembonustreenode.adb itemeffect.adb itemxbonustree.adb spellclassoptions.adb spellpower.adb spellrunecost.adb item-sparse.adb spellmisc.adb
 pause
