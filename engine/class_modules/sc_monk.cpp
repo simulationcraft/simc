d1ae8c32ab14844e98c1c9afbf47fa0ff9848646
Hinalover <tseltman@gmail.com>
2015-07-21 11:37:40 -0400

    [Monk] One final hotfix info adjustment

--- engine/class_modules/sc_monk.cpp
+++ engine/class_modules/sc_monk.cpp
@@ -7270,17 +7270,17 @@ struct monk_module_t: public module_t
   {
     hotfix::register_effect( "Monk", "2015-06-23", "Tier-18 2-piece set bonus for Windwalker Monks now gives Rising Sun Kick "
                                                    "a 30% chance (down from 40%) to generate Combo Breaker.", 269423 )
-    .field( "base_value" )
-    .operation( hotfix::HOTFIX_SET )
-    .modifier( 30 )
-    .verification_value( 40 );
+      .field( "base_value" )
+      .operation( hotfix::HOTFIX_SET )
+      .modifier( 30 )
+      .verification_value( 40 );
 
     hotfix::register_effect( "Monk", "2015-07-20", "Stance of the Fierce Tiger now increases all damage dealt by 10% (up from "
-                                                   "5%", 138797 )
+                                                   "5%(.", 138797 )
       .field( "base_value" )
       .operation( hotfix::HOTFIX_SET )
       .modifier( 10 )
-     .verification_value( 5 );
+      .verification_value( 5 );
   }
 
   virtual void init( player_t* p ) const
