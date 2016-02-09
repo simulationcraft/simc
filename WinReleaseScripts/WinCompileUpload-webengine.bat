e249e7719b9b329fbd5042759b50b5e671ad9a45
Collision <s.alexlowery@gmail.com>
2016-02-08 20:55:50 -0500

    update release versions

--- WinReleaseScripts/WinCompileUpload-webengine.bat
+++ WinReleaseScripts/WinCompileUpload-webengine.bat
@@ -13,7 +13,7 @@
 set /p ask=Build with PGO data? Only applies to 64-bit installation. (y/n)
 @echo on
 
-set simcversion=623-02
+set simcversion=623-03
 :: For bumping the minor version, just change the above line.  Make sure to also change setup32.iss and setup64.iss as well. 
 set simcfiles=C:\Simulationcraft\
 :: Location of source files
@@ -21,6 +21,8 @@ set qt_dir=C:\Qt\Qt5.5.1\5.5\
 :: Location of QT
 set redist=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\
 :: This is a really standard location for VS2013, but change it if you installed it somewhere else.
+set ssllocation64=C:\OpenSSL-Win64\bin
+:: Location of openssl32/64
 
 cd ..
 git clean -f -x -d
@@ -41,6 +43,7 @@ robocopy "%redist%x64\Microsoft.VC120.CRT" %install%\ msvcp120.dll msvcr120.dll 
 robocopy locale\ %install%\locale sc_de.qm sc_zh.qm sc_it.qm
 robocopy winreleasescripts\ %install%\ qt.conf
 robocopy . %install%\ Welcome.html Welcome.png Simulationcraft64.exe simc64.exe readme.txt Error.html COPYING
+robocopy %ssllocation64% %install%\ libeay32.dll ssleay32.dll 
 robocopy Profiles\ %install%\profiles\ *.* /S
 cd %install%
 %qt_dir%\msvc2013_64\bin\windeployqt.exe --no-translations simulationcraft64.exe
@@ -52,23 +55,4 @@ cd ..
 call start winscp /command "open downloads" "put %download%\SimcSetup-%simcversion%-win64.exe -nopreservetime -nopermissions -transfer=binary" "exit"
 7z a -r %install% %install% -mx9 -md=32m
 call start winscp /command "open downloads" "put %download%\%install%.7z -nopreservetime -nopermissions -transfer=binary" "exit"
-
-set install=simc-%simcversion%-win32
-"%MSBUILDDIR%msbuild.exe" %simcfiles%\simc_vs2013.sln /p:configuration=WebEngine /p:platform=win32 /nr:true /m:8
-
-robocopy "%redist%x86\Microsoft.VC120.CRT" %install%\ msvcp120.dll msvcr120.dll vccorlib120.dll
-robocopy locale\ %install%\locale sc_de.qm sc_zh.qm sc_it.qm
-robocopy winreleasescripts\ %install%\ qt.conf
-robocopy . %install%\ Welcome.html Welcome.png Simulationcraft.exe simc.exe readme.txt Error.html COPYING
-robocopy Profiles\ %install%\profiles\ *.* /S
-cd %install%
-C:\Qt\Qt5.5.1.32\5.5\msvc2013\bin\windeployqt.exe --no-translations simulationcraft.exe
-cd ..
-
-cd winreleasescripts
-iscc.exe "setup32.iss"
-cd ..
-call start winscp /command "open downloads" "put %download%\SimcSetup-%simcversion%-win32.exe -nopreservetime -nopermissions -transfer=binary" "exit"
-7z a -r %install% %install% -mx9 -md=32m
-winscp /command "open downloads" "put %download%\%install%.7z -nopreservetime -nopermissions -transfer=binary" "exit"
 pause
