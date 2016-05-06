ba2a602efd66a623a16f284b7f05a61266247cde
Navv <navv1234@gmail.com>
2016-05-06 11:08:05 +0300

    Remove unused dbc extractor.

--- dbc_extract2/Cache/Live/ADBFiles.bat
+++
@@ -1,2 +0,0 @@
-robocopy "C:\Program Files (x86)\World of Warcraft Beta\Cache\ADB\enUS" "C:\Simulationcraft\dbc_extract2\cache\live" item.adb itembonus.adb itembonustreenode.adb itemeffect.adb itemxbonustree.adb spellclassoptions.adb spellpower.adb spellrunecost.adb item-sparse.adb spellmisc.adb
-pause
