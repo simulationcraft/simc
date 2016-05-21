b973ef35ad26ed22f8146af05a4f02db1201cd6c
aggixx <jorgster247123@yahoo.com>
2016-05-20 23:00:20 -0500

    Bump build number.

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -87,7 +87,7 @@ std::vector< std::vector< const spell_data_t* > > ptr_class_family_index;
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 20726 : 21570;
+  return maybe_ptr( ptr ) ? 20726 : 21737;
 }
 
 const char* dbc::wow_version( bool ptr )
