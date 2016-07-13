3eb46c7f677d06bad7284f34b7bed0453afc68d9
Kennyloggins <billeberlein@gmail.com>
2016-07-13 14:41:51 -0400

    [Hunter] BM T18 4pc.

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -93,6 +93,7 @@ public:
 
   pets::hati_t* hati;
   std::array< pets::hunter_secondary_pet_t*, 2 > dark_minion;
+  std::array< pets::hunter_secondary_pet_t*, 10 > felboars;
 
   // Tier 15 4-piece bonus
   attack_t* action_lightning_arrow_aimed_shot;
@@ -206,6 +207,7 @@ public:
     proc_t* hunters_mark;
     proc_t* wild_call;
     proc_t* tier17_2pc_bm;
+    proc_t* tier18_4pc_bm;
     proc_t* black_arrow_trinket_reset;
     proc_t* tier18_2pc_mm_wasted_proc;
     proc_t* tier18_2pc_mm_wasted_overwrite;
@@ -433,6 +435,7 @@ public:
     thunderhawk(),
     hati(),
     dark_minion(),
+    felboars(),
     action_lightning_arrow_aimed_shot(),
     action_lightning_arrow_arcane_shot(),
     action_lightning_arrow_multi_shot(),
@@ -2214,6 +2217,10 @@ struct dire_critter_t: public hunter_secondary_pet_t
   }
 };
 
+// ==========================================================================
+// Hati
+// ==========================================================================
+
 struct hati_t: public hunter_secondary_pet_t
 {
   static void trigger_beast_cleave( action_state_t* s )
@@ -2326,6 +2333,41 @@ struct hati_t: public hunter_secondary_pet_t
       m *= 1.0 + buffs.bestial_wrath -> current_value;
 
     return m;
+  }
+};
+
+// ==========================================================================
+// BM T18 4P Fel Boar
+// ==========================================================================
+struct bm_t18_4pc_felboar: public hunter_secondary_pet_t
+{  
+  struct felboar_melee_t: public secondary_pet_melee_t
+  {
+    felboar_melee_t( bm_t18_4pc_felboar& p ):
+      secondary_pet_melee_t( "felboar_melee", p )
+    {
+    }
+
+    bool init_finished() override
+    {
+      if ( p() -> o() -> felboars[ 0 ] )
+        stats = p() -> o() -> felboars[ 0 ] -> get_stats( "felboar_melee" );
+
+      return secondary_pet_melee_t::init_finished();
+    }
+  };
+
+  bm_t18_4pc_felboar( hunter_t& owner ):
+    hunter_secondary_pet_t( owner, std::string( "felboar" ) )
+  {
+    owner_coeff.ap_from_ap = 1.38;
+  }
+
+  virtual void init_base_stats() override
+  {
+    hunter_secondary_pet_t::init_base_stats();
+
+    main_hand_attack = new felboar_melee_t( *this );
   }
 };
 
@@ -4625,6 +4667,7 @@ struct dire_beast_t: public hunter_spell_t
     for (auto pet : p() -> pet_list)
     {
       stats -> add_child( pet -> get_stats( "dire_beast_melee" ) );
+      stats -> add_child( pet -> get_stats( "felboar_melee" ) );
       stats -> add_child( pet -> get_stats( "stomp" ) );
     }
 
@@ -4682,6 +4725,20 @@ struct dire_beast_t: public hunter_spell_t
     timespan_t summon_duration = base_attacks_per_summon * swing_time;
     assert( beast );
     beast -> summon( summon_duration );
+
+    if ( p() -> sets.has_set_bonus( HUNTER_BEAST_MASTERY, T18, B4 ) && rng().roll( 0.20 ) )
+    {
+      p() -> procs.tier18_4pc_bm -> occur();
+
+      for( int i = 0; i < p() -> felboars.size(); i++ )
+      {
+        if ( p() -> felboars[ i ] -> is_sleeping() )
+        {
+          p() -> felboars[ i ] -> summon( p() -> find_spell( 188507 ) -> duration() );
+          break;
+        }
+      }
+    }
   }
 
   virtual bool ready() override
@@ -5358,7 +5415,7 @@ void hunter_t::create_pets()
   {
     for ( size_t i = 0; i < pet_dire_beasts.size(); ++i )
     {
-      pet_dire_beasts[i] = new pets::dire_critter_t( *this, i + 1 );
+      pet_dire_beasts[ i ] = new pets::dire_critter_t( *this, i + 1 );
     }
   }
 
@@ -5367,8 +5424,14 @@ void hunter_t::create_pets()
 
   if ( talents.black_arrow -> ok() )
   {
-    dark_minion[0] = new pets::hunter_secondary_pet_t( *this, std::string( "dark_minion" ) );
-    dark_minion[1] = new pets::hunter_secondary_pet_t( *this, std::string( "dark_minion_2" ) );
+    dark_minion[ 0 ] = new pets::hunter_secondary_pet_t( *this, std::string( "dark_minion" ) );
+    dark_minion[ 1 ] = new pets::hunter_secondary_pet_t( *this, std::string( "dark_minion_2" ) );
+  }
+
+  if ( sets.has_set_bonus( HUNTER_BEAST_MASTERY, T18, B4 ) )
+  {
+    for ( size_t i = 0; i < felboars.size(); i++ )
+      felboars[ i ] = new pets::bm_t18_4pc_felboar( *this );
   }
 }
 
@@ -5778,6 +5841,7 @@ void hunter_t::init_procs()
   procs.hunters_mark                 = get_proc( "hunters_mark" );
   procs.wild_call                    = get_proc( "wild_call" );
   procs.tier17_2pc_bm                = get_proc( "tier17_2pc_bm" );
+  procs.tier18_4pc_bm                = get_proc( "tier18_4pc_bm" );
   procs.black_arrow_trinket_reset    = get_proc( "black_arrow_trinket_reset" );
   procs.tier18_2pc_mm_wasted_proc    = get_proc( "tier18_2pc_mm_wasted_proc" );
   procs.tier18_2pc_mm_wasted_overwrite     = get_proc ( "tier18_2pc_mm_wasted_overwrite" );
