62ad07f8785b5524059882d079508f4e2c583176
Collision <s.alexlowery@gmail.com>
2016-07-13 10:09:23 -0400

    Remove beta tags from uploaded .7z/exe files.

--- WinReleaseScripts/WinCompileUpload-webengine - QT 5.6.bat
+++ WinReleaseScripts/WinCompileUpload-webengine - QT 5.6.bat
@@ -13,7 +13,7 @@
 set /p ask=Build with PGO data? Only applies to 64-bit installation. (y/n)
 @echo on
 
-set simcversion=703-01-BETA
+set simcversion=703-01
 :: For bumping the minor version, just change the above line.  Make sure to also change setup32.iss and setup64.iss as well. 
 set simcfiles=C:\Simulationcraft\
 :: Location of source files
