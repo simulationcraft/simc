a90c83fe3aece6dff29295ef39748c89374fa76a
Navv <navv1234@gmail.com>
2016-07-13 22:00:07 +0300

    Use target list for cost-on-tick dot canceling.

--- engine/action/sc_action.cpp
+++ engine/action/sc_action.cpp
@@ -3284,7 +3284,7 @@ bool action_t::consume_cost_per_tick( const dot_t& /* dot */ )
   if ( cancel_action )
   {
     cancel();
-    range::for_each( sim -> target_non_sleeping_list, [ this ]( player_t* target ) {
+    range::for_each( target_list(), [ this ]( player_t* target ) {
       if ( dot_t* d = find_dot( target ) )
       {
         d -> cancel();
