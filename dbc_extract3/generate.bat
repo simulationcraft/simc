e56b1d5e7d2d7a0079fb9fbe37d90e7797b1b60a
Navv <navv1234@gmail.com>
2016-07-19 22:20:57 +0300

    Update generate.bat.

--- dbc_extract3/generate.bat
+++ dbc_extract3/generate.bat
@@ -26,32 +26,32 @@ goto usage
 :okay
 
 
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% -t spec_list                  > %OUTPATH%\sc_spec_list.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t talent                  > %OUTPATH%\sc_talent_data%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t spell > %OUTPATH%\sc_spell_data%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t rppm_coeff              >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t spec_list                  > %OUTPATH%\sc_spec_list.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t talent                  > %OUTPATH%\sc_talent_data%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t spell > %OUTPATH%\sc_spell_data%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t rppm_coeff              >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
 py -3  %RUNFILE% -p %GTINPATH% -b %BUILD% %PTR%  -t scale                   > %OUTPATH%\sc_scale_data%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t class_list              > %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t spec_spell_list        >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t mastery_list           >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t racial_list            >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t set_list2              >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t rppm_coeff             >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t artifact               >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item                    > %OUTPATH%/sc_item_data%PTREXT%.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t random_property_points  > %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t random_suffix          >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_ench              >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_armor             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t weapon_damage          >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t gem_properties         >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_upgrade           >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_bonus             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_scaling           >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_name_desc         >> %OUTPATH%/sc_item_data%PTREXT%2.inc
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% %PTR%  -t item_child             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t class_list              > %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t spec_spell_list        >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t mastery_list           >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t racial_list            >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t set_list2              >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t rppm_coeff             >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t artifact               >> %OUTPATH%\sc_spell_lists%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item                    > %OUTPATH%/sc_item_data%PTREXT%.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t random_property_points  > %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t random_suffix          >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_ench              >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_armor             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t weapon_damage          >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t gem_properties         >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_upgrade           >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_bonus             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_scaling           >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_name_desc         >> %OUTPATH%/sc_item_data%PTREXT%2.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% %PTR%  -t item_child             >> %OUTPATH%/sc_item_data%PTREXT%2.inc
 
-py -3  %RUNFILE% -p %INPATH% -b %BUILD% -t spec_enum > %OUTPATH%\sc_specialization_data.inc
+py -3  %RUNFILE% -p %INPATH% -b %BUILD% --cache %CACHEDIR% -t spec_enum > %OUTPATH%\sc_specialization_data.inc
 
 echo Done!
 
