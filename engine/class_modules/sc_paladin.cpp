827d871ac5f8b5fabe7f45de78a2bafb9d6455bb
Theck <theckhd@gmail.com>
2016-07-20 12:30:09 -0400

    [Paladin] Cap base (before modifiers) LotP heal size at 25% max health

--- engine/class_modules/sc_paladin.cpp
+++ engine/class_modules/sc_paladin.cpp
@@ -2307,7 +2307,7 @@ struct light_of_the_protector_t : public paladin_heal_t
   virtual void execute() override
   {
     // heals for 25% of missing health.
-    base_dd_min = base_dd_max = health_diff_pct * ( p() -> resources.max[ RESOURCE_HEALTH ] - p() -> resources.current[ RESOURCE_HEALTH ] );
+    base_dd_min = base_dd_max = health_diff_pct * ( p() -> resources.max[ RESOURCE_HEALTH ] - std::max( p() -> resources.current[ RESOURCE_HEALTH ], 0.0 ) );
 
     paladin_heal_t::execute();
 
