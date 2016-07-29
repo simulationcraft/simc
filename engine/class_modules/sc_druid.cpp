866a1e80dde52df8f3042110314a5388d77750d1
aggixx <jorgster247123@yahoo.com>
2016-07-28 20:18:41 -0500

    [Druid] Feral
     - Make Balance Affinity negate Feral Overrides' Moonfire damage penalty.

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -1786,10 +1786,16 @@ struct moonfire_t : public druid_spell_t
       dual = background = true;
       dot_duration       += p -> spec.balance_overrides -> effectN( 4 ).time_value();
       base_dd_multiplier *= 1.0 + p -> spec.guardian -> effectN( 4 ).percent();
-      base_dd_multiplier *= 1.0 + p -> spec.feral_overrides -> effectN( 1 ).percent();
-      base_td_multiplier *= 1.0 + p -> spec.feral_overrides -> effectN( 2 ).percent();
       base_dd_multiplier *= 1.0 + p -> spec.guardian -> effectN( 6 ).percent();
       base_td_multiplier *= 1.0 + p -> spec.guardian -> effectN( 7 ).percent();
+
+      /* June 2016: This hotfix is negated if you shift into Moonkin Form (ever),
+        so only apply it if the druid does not have balance affinity. */
+      if ( ! p -> talent.balance_affinity -> ok() )
+      {
+        base_dd_multiplier *= 1.0 + p -> spec.feral_overrides -> effectN( 1 ).percent();
+        base_td_multiplier *= 1.0 + p -> spec.feral_overrides -> effectN( 2 ).percent();
+      }
 
       base_multiplier    *= 1.0 + p -> artifact.twilight_glow.percent();
     }
