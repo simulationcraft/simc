a7c5b59cc3bf5e5ec3c30d656bc6a7caf6128695
Collision <s.alexlowery@gmail.com>
2016-08-05 21:27:49 -0400

    Update release scripts to use qmake.

--- WinReleaseScripts/WinCompileUpload.bat
+++ WinReleaseScripts/WinCompileUpload.bat
@@ -1,4 +1,4 @@
-:: Used to automate everything for Alex so he can be lazy.
+:: Used to automate everything for Collision so he can be lazy.
 :: Requirements - Everything needs to be located in your windows path, except MSVC2015
 :: 7Zip
 :: WINSCP - Optional, it will just not upload the file automatically if you don't have it.
@@ -8,10 +8,13 @@
 :: QT 5.7.0, or whatever version we are currently using
 :: Inno Setup - http://www.jrsoftware.org/isinfo.php - Used to make the installer, optional if you just want a compressed file.
 
-@echo off
-:: Building with PGO data will add 10-15 minutes to compile.
-set /p ask=Build with PGO data? Only applies to 64-bit installation. (y/n)
-@echo on
+git clean -f -x -d
+cd ..
+set currdir=%cd%
+call C:\Qt\Qt5.7.0\5.7\msvc2015_64\bin\qtenv2.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 10.0.10586.0
+cd %currdir%
+qmake -r -tp vc -spec win32-msvc2015 simulationcraft.pro PGO=1
 
 set simcversion=703-01
 set SIMCAPPFULLVERSION=7.0.3.01
@@ -23,8 +26,6 @@ set qt_dir=C:\Qt\Qt5.7.0\5.7\
 set redist=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\
 :: This is a really standard location for VS2015, but change it if you installed it somewhere else.
 
-cd ..
-git clean -f -x -d
 for /F "delims=" %%i IN ('git --git-dir=%simcfiles%\.git\ rev-parse --short HEAD') do set GITREV=-%%i
 
 cd>bla.txt
@@ -36,16 +37,15 @@ set install=simc-%simcversion%-win64
 
 for /f "skip=2 tokens=2,*" %%A in ('reg.exe query "HKLM\SOFTWARE\Microsoft\MSBuild\ToolsVersions\14.0" /v MSBuildToolsPath') do SET MSBUILDDIR=%%B
 
-if %ask%==y "%MSBUILDDIR%msbuild.exe" %simcfiles%\simc_vs2015.sln /p:configuration=WebEngine-PGO /p:platform=x64 /nr:true
-if %ask%==n "%MSBUILDDIR%msbuild.exe" %simcfiles%\simc_vs2015.sln /p:configuration=WebEngine /p:platform=x64 /nr:true
+"%MSBUILDDIR%msbuild.exe" %simcfiles%\simulationcraft.sln /p:configuration=Release /nr:true /m
 
 robocopy "%redist%x64\Microsoft.VC140.CRT" %install%\ msvcp140.dll vccorlib140.dll vcruntime140.dll
 robocopy locale\ %install%\locale sc_de.qm sc_zh.qm sc_it.qm
 robocopy winreleasescripts\ %install%\ qt.conf
-robocopy . %install%\ Welcome.html Welcome.png Simulationcraft64.exe simc64.exe readme.txt Error.html COPYING
+robocopy . %install%\ Welcome.html Welcome.png Simulationcraft.exe simc.exe readme.txt Error.html COPYING
 robocopy Profiles\ %install%\profiles\ *.* /S
 cd %install%
-%qt_dir%\msvc2015_64\bin\windeployqt.exe --no-translations simulationcraft64.exe
+%qt_dir%\msvc2015_64\bin\windeployqt.exe --no-translations simulationcraft.exe
 cd ..
 
 cd winreleasescripts 
