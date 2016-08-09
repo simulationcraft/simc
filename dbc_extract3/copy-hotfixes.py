31007883a48bd87b6b69c5e7b673c480231242ff
Navv <navv1234@gmail.com>
2016-08-09 16:24:44 +0300

    Update hotfixes to pick up slam. Add copy-hotfixes.py
    Copy-hotfixes.py will check (with md5) and copy over any changed hotfix
    files.
    
    [ci skip]

---
+++ dbc_extract3/copy-hotfixes.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys, os, pathlib, hashlib, shutil
+
+if len(sys.argv) < 3:
+    print('copy-hotfixes.py input_dir output_dir')
+    sys.exit(1)
+
+if not os.access(sys.argv[1], os.R_OK):
+    print('copy-hotfixes.py: Unable to access %s' % sys.argv[1])
+    sys.exit(1)
+
+if not os.access(sys.argv[2], os.R_OK):
+    print('copy-hotfixes.py: Unable to access %s' % sys.argv[2])
+    sys.exit(1)
+
+TIMESTAMP_OFFSET = 32
+
+_input_base = pathlib.Path(sys.argv[1])
+_output_base = pathlib.Path(sys.argv[2])
+_input = {}
+_output = {}
+
+print('Reading output files from %s' % sys.argv[2])
+for fn in _output_base.glob('*.adb'):
+    data = bytearray(fn.read_bytes())
+    data[TIMESTAMP_OFFSET:TIMESTAMP_OFFSET + 4] = b'\x00\x00\x00\x00'
+
+    _output[fn.stem] = { 'name': fn, 'md5': hashlib.md5(data).digest() }
+
+print('Reading input files from %s' % sys.argv[1])
+for fn in _input_base.glob('*.adb'):
+    if fn.stem not in _output.keys():
+        continue
+
+    data = bytearray(fn.read_bytes())
+    data[TIMESTAMP_OFFSET:TIMESTAMP_OFFSET + 4] = b'\x00\x00\x00\x00'
+
+    _input[fn.stem] = { 'name': fn, 'md5': hashlib.md5(data).digest() }
+
+# Copy over any modified files
+for _fn, _data in _output.items():
+    if _data['md5'] != _input[_fn]['md5']:
+        print('Copying %s -> %s ...' % (_input[_fn]['name'], _data['name']))
+        try:
+            shutil.copyfile(str(_input[_fn]['name']), str(_data['name']))
+        except Exception as e:
+            print('copy-hotfixes.py: Copy error on %s: %s' % (_fn, e))
+            sys.exit(1)
