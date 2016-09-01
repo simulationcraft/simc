0614f357d989c6bac548fda6d8142d9d91c88c16
Navv <navv1234@gmail.com>
2016-09-01 12:34:21 +0300

    Fix OS X (and probably Linux) SC_DEFAULT_APIKEY compilation

--- engine/simulationcraft.hpp
+++ engine/simulationcraft.hpp
@@ -54,6 +54,11 @@
 #include <Availability.h>
 #endif
 
+// Needed for usleep in engine/interface/sc_bcp_api.cpp when default apikey builds are done
+#if defined( SC_DEFAULT_APIKEY ) && ! defined( SC_WINDOWS )
+#include <unistd.h>
+#endif
+
 // Forward Declarations =====================================================
 
 struct absorb_buff_t;
