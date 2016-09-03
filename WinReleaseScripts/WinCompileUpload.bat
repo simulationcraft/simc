989c3d8740bd9e0225b507d6ee59ab5b5846752d
Collision <s.alexlowery@gmail.com>
2016-09-03 14:35:17 -0400

    Bump release script version.

--- WinReleaseScripts/WinCompileUpload.bat
+++ WinReleaseScripts/WinCompileUpload.bat
@@ -16,8 +16,8 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 
 cd %currdir%
 qmake -r -tp vc -spec win32-msvc2015 simulationcraft.pro PGO=1
 
-set simcversion=703-01
-set SIMCAPPFULLVERSION=7.0.3.01
+set simcversion=703-02
+set SIMCAPPFULLVERSION=7.0.3.02
 :: For bumping the minor version, just change the above line.  Make sure to also change setup32.iss and setup64.iss as well. 
 set simcfiles=C:\Simulationcraft\
 :: Location of source files
