71ab0269f2db3480765c87871a5976d2d08be0dd
Collision <s.alexlowery@gmail.com>
2016-09-14 19:41:57 -0400

    Import hotfix data + spelldatadump.

--- casc_extract/WinGenerateSpellDataPTR.bat
+++ casc_extract/WinGenerateSpellDataPTR.bat
@@ -1,4 +1,4 @@
-call py -3 casc_extract.py -m batch --cdn --beta -o wow
+call py -3 casc_extract.py -m batch --cdn --ptr -o wow
 cd wow
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
@@ -10,5 +10,5 @@ cd ..
 cd ..
 set curr=%cd%
 cd dbc_extract3
-call generate.bat %wowdir% %curr%\casc_extract\wow
+call generate.bat ptr=1 %wowdir% %curr%\casc_extract\wow
 pause
