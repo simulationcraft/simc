5135e9645e02d5433d238a66a448a3d209912cf5
Marok <adamhl44@gmail.com>
2016-09-19 05:43:01 -0400

    [Warrior] Remove Dragon Scales from Ignore Pain cap calculation

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -4034,7 +4034,6 @@ struct ignore_pain_t: public warrior_spell_t
   {
     double ip_cap = 0;
     ip_cap = ip_cap_ratio * ( data().effectN( 1 ).ap_coeff() * p() -> composite_melee_attack_power() * p() -> composite_attack_power_multiplier() ) * p() -> cache.damage_versatility();
-    ip_cap *= 1.0 + p() -> buff.dragon_scales -> check_value();
     return ip_cap;
   }
 
