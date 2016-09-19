1276b5fea81274868ee34f491e2a36420205e759
Collision <s.alexlowery@gmail.com>
2016-09-19 18:14:17 -0400

    Fix typo.

--- WinReleaseScripts/WinCompileUpload.bat
+++ WinReleaseScripts/WinCompileUpload.bat
@@ -5,14 +5,14 @@
 ::  - "open downloads" is the command that selects the downloads alias in winscp, which for me is the simulationcraft server. Change downloads to whatever suits you.
 :: MSVC 2015 - Fully updated
 :: Git
-:: QT 5.7.0, or whatever version we are currently using
+:: QT 5.6.1, or whatever version we are currently using
 :: Inno Setup - http://www.jrsoftware.org/isinfo.php - Used to make the installer, optional if you just want a compressed file.
 
 cd ..
 git clean -f -x -d
 set currdir=%cd%
 call C:\Qt\Qt5.6.1\5.6\msvc2015_64\bin\qtenv2.bat
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 10.0.14393.0
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 cd %currdir%
 qmake -r -tp vc -spec win32-msvc2015 simulationcraft.pro PGO=1
 
@@ -21,7 +21,7 @@ set SIMCAPPFULLVERSION=7.0.3.03
 :: For bumping the minor version, just change the above line.  Make sure to also change setup32.iss and setup64.iss as well. 
 set simcfiles=C:\Simulationcraft\
 :: Location of source files
-set qt_dir=C:\Qt\Qt5.6.1\5.5\
+set qt_dir=C:\Qt\Qt5.6.1\5.6\
 :: Location of QT
 set redist=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\
 :: This is a really standard location for VS2015, but change it if you installed it somewhere else.
