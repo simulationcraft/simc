159074a63afcf047617abfa8e5e5a250b890cb32
aggixx <jorgster247123@yahoo.com>
2016-09-27 15:20:11 -0500

    Pull ADB hotfixes and regenerate spell data dumps.

--- dbc_extract3/cache/live/copy.bat
+++ dbc_extract3/cache/live/copy.bat
@@ -1,4 +1,4 @@
 cd ..
 cd ..
-py -3 copy-hotfixes.py "C:\Program Files (x86)\World of Warcraft\Cache\ADB\enUS" %cd%\cache\live\
+py -3 copy-hotfixes.py "F:\Program Files\World of Warcraft\Cache\ADB\enUS" %cd%\cache\live\
 py -3 copy-hotfixes.py "C:\World of Warcraft\Cache\ADB\enUS" %cd%\cache\live\
--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -7379,33 +7379,6 @@ public:
 
   void register_hotfixes() const override
   {
-    /*
-    hotfix::register_effect( "Demon Hunter", "2016-09-24", "Fel Mastery (Talent) damage bonus to Fel Rush reduced to 30%.", 297759 )
-      .field( "base_value" )
-      .operation( hotfix::HOTFIX_SET )
-      .modifier( 30 )
-      .verification_value( 50 );
-    hotfix::register_effect( "Demon Hunter", "2016-09-24", "Bloodlet (Talent) now deals 150% of initial Throw Glaive damage.", 305358 )
-      .field( "base_value" )
-      .operation( hotfix::HOTFIX_SET )
-      .modifier( 150 )
-      .verification_value( 200 );
-    hotfix::register_effect( "Demon Hunter", "2016-09-24", "Fury of the Illidari (Artifact Ability) damage reduced by 20%.", 297083 )
-      .field( "base_value" )
-      .operation( hotfix::HOTFIX_MUL )
-      .modifier( 0.80 )
-      .verification_value( 140 );
-    hotfix::register_effect( "Demon Hunter", "2016-09-24", "Fury of the Illidari (Artifact Ability) damage reduced by 20%. #2", 297378 )
-      .field( "base_value" )
-      .operation( hotfix::HOTFIX_MUL )
-      .modifier( 0.80 )
-      .verification_value( 140 );
-    hotfix::register_effect( "Demon Hunter", "2016-09-24", "Balanced Blades (Artifact Trait) damage bonus to Blade Dance reduced to 3% per target.", 296823 )
-      .field( "base_value" )
-      .operation( hotfix::HOTFIX_SET )
-      .modifier( 3 )
-      .verification_value( 10 );
-      */
   }
 
   void combat_begin( sim_t* ) const override
