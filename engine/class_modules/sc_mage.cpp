2c290d2e13c9fa669fac82c65655d10f42c74935
Darrin <dzahorn1@gmail.com>
2016-10-24 10:58:50 -0400

    [Mage] Frost APL fix

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -8767,17 +8767,15 @@ void mage_t::apl_frost()
   action_priority_list_t* cooldowns         = get_action_priority_list( "cooldowns"         );
 
   default_list -> add_action( this, "Counterspell", "if=target.debuff.casting.react" );
-  default_list -> add_action( this, "Ice Lance", "if=prev_gcd.flurry" );
-  default_list -> add_action( this, "Time Warp", "if=(time=0&buff.bloodlust.down)|(buff.bloodlust.down&equipped.132410&!talent.ray_of_frost.enabled)" );
+  default_list -> add_action( this, "Ice Lance", "if=buff.fingers_of_frost.react=0&prev_gcd.flurry" );
+  default_list -> add_action( this, "Time Warp", "if=(time=0&buff.bloodlust.down)|(buff.bloodlust.down&equipped.132410)" );
   default_list -> add_action( "call_action_list,name=cooldowns" );
   default_list -> add_action( this, "Blizzard", "if=buff.potion_of_deadly_grace.up&!prev_off_gcd.water_jet" );
-  default_list -> add_talent( this, "Rune of Power", "if=buff.icy_veins.up|cooldown.ray_of_frost.ready|(buff.fingers_of_frost.stack>=2&(cooldown.water_jet.ready|cooldown.frozen_touch.ready|cooldown.ebonbolt.ready)&debuff.frost_bomb.remains>8&cooldown.ray_of_frost.remains>50)" );
-  default_list -> add_action( this, "Time Warp", "if=cooldown.ray_of_frost.ready&buff.bloodlust.down&equipped.132410&talent.ray_of_frost.enabled" );
   default_list -> add_talent( this, "Ice Nova", "if=debuff.winters_chill.up" );
   default_list -> add_action( this, "Frostbolt", "if=prev_off_gcd.water_jet" );
   default_list -> add_action( "water_jet,if=prev_gcd.frostbolt&buff.fingers_of_frost.stack<(2+artifact.icy_hand.enabled)&buff.brain_freeze.react=0" );
   default_list -> add_talent( this, "Ray of Frost", "if=buff.icy_veins.up|(cooldown.icy_veins.remains>action.ray_of_frost.cooldown&buff.rune_of_power.down)" );
-  default_list -> add_action( this, "Flurry", "if=buff.brain_freeze.react&buff.fingers_of_frost.react=0" );
+  default_list -> add_action( this, "Flurry", "if=buff.brain_freeze.react&buff.fingers_of_frost.react=0&prev_gcd.frostbolt" );
   default_list -> add_talent( this, "Glacial Spike" );
   default_list -> add_talent( this, "Frozen Touch", "if=buff.fingers_of_frost.stack<=(0+artifact.icy_hand.enabled)" );
   default_list -> add_talent( this, "Frost Bomb", "if=debuff.frost_bomb.remains<action.ice_lance.travel_time&buff.fingers_of_frost.react>0" );
