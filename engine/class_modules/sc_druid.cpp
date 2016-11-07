d4ffc13b957cfb5c8210ede61aa16713f9c0336b
Alpheus <denis.cahuk@gmail.com>
2016-11-07 01:24:17 +0100

    [Druid] Fix Galactic Guardian moonfire damage (buff introduced by 7.1) #3109

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -1813,7 +1813,13 @@ struct moonfire_t : public druid_spell_t
       dual = background = true;
       dot_duration       += p -> spec.balance_overrides -> effectN( 4 ).time_value();
       base_dd_multiplier *= 1.0 + p -> spec.guardian -> effectN( 6 ).percent();
-
+      
+      if (p->active.galactic_guardian)
+      {
+        // Galactic Guardian 7.1 Damage Buff
+        base_dd_multiplier *= 1.0 + p->buff.galactic_guardian->data().effectN(3).percent();
+      }
+      
       /* June 2016: This hotfix is negated if you shift into Moonkin Form (ever),
         so only apply it if the druid does not have balance affinity. */
       if ( ! p -> talent.balance_affinity -> ok() )
