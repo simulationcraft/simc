577dd2554f09d2b55568ed632f1e677ac0085276
Navv <navv1234@gmail.com>
2016-12-02 11:41:24 +0200

    Fix Mark of the Hidden Satyr for 7.1.5.

--- dbc_extract3/generate.sh
+++ dbc_extract3/generate.sh
@@ -34,16 +34,22 @@ if [ ! -d ${INPUT} ]; then
   exit 1
 fi
 
+echo "Generating ${OUTPATH}/sc_specialization_data.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD                         -t spec_enum              -o $OUTPATH/sc_specialization_data.inc
 
+echo "Generating ${OUTPATH}/sc_talent_data${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t talent                 -o $OUTPATH/sc_talent_data${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_spec_list${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t spec_list              -o $OUTPATH/sc_spec_list${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_spell_data${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t spell                  -o $OUTPATH/sc_spell_data${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_scale_data${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $GTINPUT -b $BUILD $PTR --cache "${CACHE}" -t scale                  -o $OUTPATH/sc_scale_data${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_spell_lists${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t class_list             -o $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t spec_spell_list        -a $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t mastery_list           -a $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
@@ -52,8 +58,10 @@ fi
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t rppm_coeff             -a $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t artifact               -a $OUTPATH/sc_spell_lists${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_item_data${PTR:+_ptr}.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t item                   -o $OUTPATH/sc_item_data${PTR:+_ptr}.inc
 
+echo "Generating ${OUTPATH}/sc_item_data${PTR:+_ptr}2.inc"
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t random_property_points -o $OUTPATH/sc_item_data${PTR:+_ptr}2.inc
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t random_suffix          -a $OUTPATH/sc_item_data${PTR:+_ptr}2.inc
 ./dbc_extract.py -p $DBCINPUT -b $BUILD $PTR --cache "${CACHE}" -t item_ench              -a $OUTPATH/sc_item_data${PTR:+_ptr}2.inc
