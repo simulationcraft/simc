simulationcraft/simc

commit 3c4f48063920797dadea78db7cac0df631b160f2

Fix previous commit by checking the back of the vector (most recent gcds) instead of the start of the vector.

engine/action/sc_action.cpp
@@ -2937,7 +2937,7 @@ expr_t* action_t::create_expression( const std::string& name_str )
       virtual double evaluate() override
       {
         if ( action.player -> prev_gcd_actions.size() >= gcd )
-          return action.player -> prev_gcd_actions[gcd - 1] -> internal_id == previously_used -> internal_id;
+          return action.player -> prev_gcd_actions[action.player -> prev_gcd_actions.end() - gcd] -> internal_id == previously_used -> internal_id;
         return false;
       }
     };

commit 8f16a35d5cc520f75d034e6a8e6f63f8d58a69ca

Fix prev_gcd expression evaluation
Make it compile.

Fixes: 3c4f4806

engine/action/sc_action.cpp
@@ -2937,7 +2937,7 @@ expr_t* action_t::create_expression( const std::string& name_str )
       virtual double evaluate() override
       {
         if ( action.player -> prev_gcd_actions.size() >= gcd )
-          return action.player -> prev_gcd_actions[action.player -> prev_gcd_actions.end() - gcd] -> internal_id == previously_used -> internal_id;
+          return ( *( action.player -> prev_gcd_actions.end() - gcd ) ) -> internal_id == previously_used -> internal_id;
         return false;
       }
     };