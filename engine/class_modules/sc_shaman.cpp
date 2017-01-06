7d185d4f1bcabe0a77fe8bd865422ebe94241591
pkaskaround <kruse.peter.1990@gmail.com>
2017-01-06 12:48:28 +0100

    [SHAMAN] FrS doesn't profit from Earthen Attunement artifact trait

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -4962,7 +4962,7 @@ struct frost_shock_t : public shaman_spell_t
     shaman_spell_t( "frost_shock", player, player -> find_specialization_spell( "Frost Shock" ), options_str ),
     damage_coefficient( data().effectN( 3 ).percent() / secondary_costs[ RESOURCE_MAELSTROM ] )
   {
-    base_multiplier *= 1.0 + player -> artifact.earthen_attunement.percent();
+
   }
 
   double action_multiplier() const override
