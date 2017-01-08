simulationcraft/simc

commit 597f8b9cc9dca5630ea62afff362c0f89e821b33

[Hunter] fixup BM T19 2p bonus
2 fixes here really:

- use buff_t::remains() instead of buff_t.buff_duration as the latter is
  the maximum buff duration while Dire Beasts inherit only the remaining
  time of Bestial Wrath

- move buff trigger to Dire Beast summon before Stomp execute() as it
  [Stomp] benefits from the buff

Fixes: #3331

engine/class_modules/sc_hunter.cpp
@@ -1604,6 +1604,12 @@ struct dire_critter_t: public hunter_secondary_pet_t
 
   virtual void summon( timespan_t duration = timespan_t::zero() ) override
   {
+    if ( o() -> sets.has_set_bonus( HUNTER_BEAST_MASTERY, T19, B2 ) && o() -> buffs.bestial_wrath -> check() )
+    {
+      const timespan_t bw_duration = o() -> buffs.bestial_wrath -> remains();
+      buffs.bestial_wrath -> trigger( 1, buff_t::DEFAULT_VALUE(), -1.0, bw_duration );
+    }
+
     hunter_secondary_pet_t::summon( duration );
 
     if ( o() -> talents.stomp -> ok() )
@@ -4663,12 +4669,6 @@ struct dire_beast_t: public hunter_spell_t
           break;
         }
       }
-    }
-
-    if ( p() -> sets.has_set_bonus( HUNTER_BEAST_MASTERY, T19, B2 ) )
-    {
-      const timespan_t player_duration = p() -> buffs.bestial_wrath -> buff_duration;
-      static_cast<pets::dire_critter_t *>( beast ) -> buffs.bestial_wrath -> trigger(1, buff_t::DEFAULT_VALUE(), -1.0, player_duration );
     }
   }
 

commit 9935a227965b795915a9f99a2a5ac6b0833f5055

[Hunter] really ix Dire Beast T19 2p interaction
Apparently players can't gain buffs before they are summoned
and last-minute untested changes are bad.

This sadly makes it so the first Dire Beast attack does not benefit from
the buff. Going to fix it later.

Fixes: 597f8b9cc9

engine/class_modules/sc_hunter.cpp
@@ -1604,13 +1604,14 @@ struct dire_critter_t: public hunter_secondary_pet_t
 
   virtual void summon( timespan_t duration = timespan_t::zero() ) override
   {
+    hunter_secondary_pet_t::summon( duration );
+
+    // FIXME: First melee attack should also benefit from it
     if ( o() -> sets.has_set_bonus( HUNTER_BEAST_MASTERY, T19, B2 ) && o() -> buffs.bestial_wrath -> check() )
     {
       const timespan_t bw_duration = o() -> buffs.bestial_wrath -> remains();
       buffs.bestial_wrath -> trigger( 1, buff_t::DEFAULT_VALUE(), -1.0, bw_duration );
     }
-
-    hunter_secondary_pet_t::summon( duration );
 
     if ( o() -> talents.stomp -> ok() )
       active.stomp -> execute();