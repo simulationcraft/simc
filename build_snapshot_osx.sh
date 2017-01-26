3787b85ef0211a69c2cf388f08e3891264cec531
Navv <navv1234@gmail.com>
2017-01-26 14:39:36 +0200

    Adjust MacOS snapshot build script
    [ci skip]

--- build_snapshot_osx.sh
+++ build_snapshot_osx.sh
@@ -22,7 +22,7 @@ MAJOR_VERSION=$(grep -E -e "^#define SC_MAJOR_VERSION" engine/simulationcraft.hp
 MINOR_VERSION=$(grep -E -e "^#define SC_MINOR_VERSION" engine/simulationcraft.hpp|sed -E -e "s/#define SC_MINOR_VERSION \"([0-9]+)\"/\1/g")
 REVISION=$(git log --no-merges -1 --pretty="%h")
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-QT_DIR=${QTDIR:-${HOME}/Qt5.6/5.6/clang_64/bin}
+QT_DIR=${QTDIR:-${HOME}/Qt5.6/5.6}
 INPUT_NAME=simc-${MAJOR_VERSION}-${MINOR_VERSION}-osx-x86.dmg
 OUTPUT_NAME=simc-${MAJOR_VERSION}-${MINOR_VERSION}-${SUFFIX:-${BRANCH}}-osx-x86-${REVISION}.dmg
 
@@ -30,7 +30,7 @@ if [ -f .qmake.stash ]; then
   make distclean
 fi
 
-( ${QT_DIR}/qmake LTO=1 SC_DEFAULT_APIKEY=${APIKEY} simulationcraft.pro && make -j 4 create_release ) || exit 1
+( ${QT_DIR}/clang_64/bin/qmake LTO=1 SC_DEFAULT_APIKEY=${APIKEY} simulationcraft.pro && make -j 4 create_release ) || exit 1
 
 if [ -f ${INPUT_NAME} ]; then
   mv ${INPUT_NAME} ${OUTPUT_NAME}
