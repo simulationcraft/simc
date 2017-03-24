ad5c66f3b7788be981cbcd2568048e8e43d98392
Hinalover <tseltman@verizon.net>
2017-03-24 10:19:02 -0400

    [Monk] Small fix

--- engine/class_modules/sc_monk.cpp
+++ engine/class_modules/sc_monk.cpp
@@ -2437,11 +2437,7 @@ public:
             if ( !p() -> pet.sef[SEF_FIRE] -> is_sleeping() )
               p() -> pet.sef[SEF_FIRE] -> expiration -> reschedule( p() -> pet.sef[SEF_FIRE] -> expiration -> remains() + timespan_t::from_millis( extension ) );
           }
-<<<<<<< Updated upstream
-          else if ( p() -> buff.serenity -> up() && maybe_ptr( p() -> dbc.ptr ) ) // FIXME
-=======
           else if ( p() -> buff.serenity -> up() && maybe_ptr( p() -> dbc.ptr ) )
->>>>>>> Stashed changes
           {
             // Since this is extended based on chi spender instead of chi spent, extention is the duration
             // Effect is saved as 3; extension is saved as 300 milliseconds
