bdbab420c9169ee54f7002c2f8da301bd92a1fd6
Jayezi <Jayezi@users.noreply.github.com>
2017-04-06 19:56:09 -0500

    [Hunter] Track vulnerable primarily on action->target

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -730,9 +730,9 @@ struct vulnerability_stats_t
     }
   }
 
-  void update( hunter_t* p, const action_state_t* state )
+  void update( hunter_t* p, const action_t* a )
   {
-    if ( ! p -> get_target_data( state -> target ) -> debuffs.vulnerable -> check() )
+    if ( ! p -> get_target_data( a -> target ) -> debuffs.vulnerable -> check() )
     {
       no_vuln -> occur();
     }
@@ -741,15 +741,15 @@ struct vulnerability_stats_t
       if ( has_patient_sniper )
       {
         // it looks like we can get called with current_tick == 6 (last tick) which is oor
-        size_t current_tick = std::min<size_t>( p -> get_target_data( state -> target ) -> debuffs.vulnerable -> current_tick, patient_sniper.size() - 1 );
+        size_t current_tick = std::min<size_t>( p -> get_target_data( a -> target ) -> debuffs.vulnerable -> current_tick, patient_sniper.size() - 1 );
         patient_sniper[ current_tick ] -> occur();
       }
 
       if ( check_secondary )
       {
-        for ( player_t* tar : state -> action -> target_list() )
+        for ( player_t* tar : a -> target_list() )
         {
-          if ( tar != state -> target && !p -> get_target_data( tar ) -> debuffs.vulnerable -> check() )
+          if ( tar != a -> target && !p -> get_target_data( tar ) -> debuffs.vulnerable -> check() )
           {
             no_vuln_secondary -> occur();
             return;
@@ -3263,7 +3263,7 @@ struct aimed_shot_t: public aimed_shot_base_t
     else if ( p() -> legendary.mm_gloves )
       p() -> buffs.gyroscopic_stabilization -> trigger();
 
-    vulnerability_stats.update( p(), execute_state );
+    vulnerability_stats.update( p(), this );
   }
 
   virtual timespan_t execute_time() const override
@@ -3540,7 +3540,7 @@ struct piercing_shot_t: public hunter_ranged_attack_t
       execute_state -> target = p() -> target;
     }
 
-    vulnerability_stats.update( p(), execute_state );
+    vulnerability_stats.update( p(), this );
 
     if ( original_target )
       execute_state -> target = original_target;
