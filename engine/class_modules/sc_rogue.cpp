08eba014dd7f61632262e2e018afb607ab0e25b9
Florian Meißner <developer@mystler.eu>
2017-05-13 18:12:12 +0200

    [Rogue] Implement Soul of the Shadowblade legendary

--- engine/class_modules/sc_rogue.cpp
+++ engine/class_modules/sc_rogue.cpp
@@ -7105,6 +7105,9 @@ void rogue_t::init_action_list()
 
   precombat -> add_talent( this, "Marked for Death", "if=raid_event.adds.in>40" );
 
+  // Vigor variable to check for Vigor effect from talent or legendary ring
+  precombat -> add_action( "variable,name=vigor_active,value=talent.vigor.enabled|equipped.soul_of_the_shadowblade" );
+
   if ( specialization() == ROGUE_ASSASSINATION )
   {
     def -> add_action( "variable,name=energy_regen_combined,value=energy.regen+poisoned_bleeds*(7+talent.venom_rush.enabled*3)%2" );
@@ -7249,7 +7252,7 @@ void rogue_t::init_action_list()
   {
     // Pre-Combat
     precombat -> add_action( "variable,name=ssw_refund,value=equipped.shadow_satyrs_walk*(6+ssw_refund_offset)", "Defined variables that doesn't change during the fight" );
-    precombat -> add_action( "variable,name=stealth_threshold,value=(15+talent.vigor.enabled*35+talent.master_of_shadows.enabled*(25+ptr*15)+variable.ssw_refund)" );
+    precombat -> add_action( "variable,name=stealth_threshold,value=(15+variable.vigor_active*35+talent.master_of_shadows.enabled*(25+ptr*15)+variable.ssw_refund)" );
     precombat -> add_action( "variable,name=shd_fractionnal,value=ptr*(1.725+0.6*talent.enveloping_shadows.enabled)+(1-ptr)*2.45" );
     precombat -> add_talent( this, "Enveloping Shadows", "if=combo_points>=5&ptr=0" );
     precombat -> add_action( this, "Shadow Dance", "if=talent.subterfuge.enabled&bugs", "Since 7.1.5, casting Shadow Dance before going in combat let you extends the stealth buff, so it's worth to use with Subterfuge talent. Has not been fixed in 7.2.5!" ); // Before SoD because we do it while not in stealth in-game
@@ -7321,7 +7324,7 @@ void rogue_t::init_action_list()
     stealth_cds -> add_action( this, "Shadow Dance", "if=charges_fractional>=variable.shd_fractionnal" );
     stealth_cds -> add_action( "pool_resource,for_next=1,extra_amount=40" );
     stealth_cds -> add_action( "shadowmeld,if=energy>=40&energy.deficit>=10+variable.ssw_refund" );
-    stealth_cds -> add_action( this, "Shadow Dance", "if=combo_points.deficit>=5-talent.vigor.enabled" );
+    stealth_cds -> add_action( this, "Shadow Dance", "if=combo_points.deficit>=5-variable.vigor_active" );
 
     // Stealthed Rotation
     action_priority_list_t* stealthed = get_action_priority_list( "stealthed", "Stealthed Rotation" );
@@ -7350,7 +7353,7 @@ void rogue_t::init_action_list()
     // Cooldowns
     action_priority_list_t* ptr_cds = get_action_priority_list( "ptr_cds", "Cooldowns" );
     ptr_cds -> add_action( "potion,name=old_war,if=buff.bloodlust.react|target.time_to_die<=25|buff.shadow_blades.up" );
-    ptr_cds -> add_action( "use_item,name=draught_of_souls,if=!stealthed.rogue&energy.deficit>30+talent.vigor.enabled*10" );
+    ptr_cds -> add_action( "use_item,name=draught_of_souls,if=!stealthed.rogue&energy.deficit>30+variable.vigor_active*10" );
     for ( size_t i = 0; i < items.size(); i++ )
     {
       if ( items[i].has_special_effect( SPECIAL_EFFECT_SOURCE_ITEM, SPECIAL_EFFECT_USE ) && items[i].name_str != "draught_of_souls" )
@@ -7365,7 +7368,7 @@ void rogue_t::init_action_list()
     }
     ptr_cds -> add_action( this, "Symbols of Death", "if=!stealthed.all" );
     ptr_cds -> add_action( this, "Shadow Blades", "if=combo_points.deficit>=2+stealthed.all-equipped.mantle_of_the_master_assassin" );
-    ptr_cds -> add_action( this, "Goremaw's Bite", "if=!stealthed.all&cooldown.shadow_dance.charges_fractional<=variable.shd_fractionnal&((combo_points.deficit>=4-(time<10)*2&energy.deficit>50+talent.vigor.enabled*25-(time>=10)*15)|(combo_points.deficit>=1&target.time_to_die<8))" );
+    ptr_cds -> add_action( this, "Goremaw's Bite", "if=!stealthed.all&cooldown.shadow_dance.charges_fractional<=variable.shd_fractionnal&((combo_points.deficit>=4-(time<10)*2&energy.deficit>50+variable.vigor_active*25-(time>=10)*15)|(combo_points.deficit>=1&target.time_to_die<8))" );
     ptr_cds -> add_talent( this, "Marked for Death", "target_if=min:target.time_to_die,if=target.time_to_die<combo_points.deficit|(raid_event.adds.in>40&combo_points.deficit>=cp_max_spend)" );
 
     // Finishers
@@ -8946,6 +8949,24 @@ struct mantle_of_the_master_assassin_t : public unique_gear::scoped_actor_callba
   { rogue -> legendary.mantle_of_the_master_assassin = e.driver(); }
 };
 
+struct soul_of_the_shadowblade_t : public unique_gear::scoped_actor_callback_t<rogue_t>
+{
+  soul_of_the_shadowblade_t() : super( ROGUE )
+  { }
+
+  void manipulate( rogue_t* rogue, const special_effect_t& e ) override
+  {
+    if ( ! rogue -> talent.vigor -> ok() )
+    {
+      rogue -> talent.vigor = rogue -> find_talent_spell( "Vigor", "", rogue -> specialization(), false, false );
+
+      // Apply Vigor here because init_base_stats was already called earlier
+      rogue -> resources.base[ RESOURCE_ENERGY ] += rogue -> talent.vigor -> effectN( 1 ).base_value();
+      rogue -> base_energy_regen_per_second *= 1.0 + rogue -> talent.vigor -> effectN( 2 ).percent();
+    }
+  }
+};
+
 struct rogue_module_t : public module_t
 {
   rogue_module_t() : module_t( ROGUE ) {}
@@ -8975,6 +8996,7 @@ struct rogue_module_t : public module_t
     unique_gear::register_special_effect( 208692, the_dreadlords_deceit_t()             );
     unique_gear::register_special_effect( 209041, insignia_of_ravenholdt_t()            );
     unique_gear::register_special_effect( 235022, mantle_of_the_master_assassin_t()     );
+    unique_gear::register_special_effect( 247509, soul_of_the_shadowblade_t()           );
   }
 
   void register_hotfixes() const override
