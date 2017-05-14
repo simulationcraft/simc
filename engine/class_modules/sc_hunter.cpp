simulationcraft/simc

commit 107ae699d9067fca164e43e1585059f4725e2fe8

[Hunter] update Augmentation rune setup

engine/class_modules/sc_hunter.cpp
@@ -517,6 +517,7 @@ public:
   std::string default_potion() const override;
   std::string default_flask() const override;
   std::string default_food() const override;
+  std::string default_rune() const override;
 
   void              add_item_actions( action_priority_list_t* list );
 
@@ -6114,6 +6115,15 @@ std::string hunter_t::default_food() const
          "disabled";
 }
 
+// hunter_t::default_rune ===================================================
+
+std::string hunter_t::default_rune() const
+{
+  return ( true_level >=  100 ) ? "defiled" :
+         ( true_level >=  90  ) ? "focus" :
+         "disabled";
+}
+
 // hunter_t::init_actions ===================================================
 
 void hunter_t::init_action_list()
@@ -6129,13 +6139,9 @@ void hunter_t::init_action_list()
 
     action_priority_list_t* precombat = get_action_priority_list( "precombat" );
 
-    // Flask
+    // Flask, Rune, Food
     precombat -> add_action( "flask" );
-    // Added Rune if Flask are allowed since there is no "allow_runes" bool.
-    if ( sim -> allow_flasks && true_level >= 110 )
-      precombat -> add_action( "augmentation,type=defiled" );
-
-    // Food
+    precombat -> add_action( "augmentation" );
     precombat -> add_action( "food" );
 
     precombat -> add_action( "summon_pet" );

commit c2f0fb5c14f89aa26612515654e167dc6aae843a

[Hunter] fix up default Augment runes
Check for appropriate levels and use a Hyper rune instead of the Focus
one as we need agility instead of intellect.

Fixes: 107ae699d906

engine/class_modules/sc_hunter.cpp
@@ -6066,8 +6066,8 @@ std::string hunter_t::default_food() const
 
 std::string hunter_t::default_rune() const
 {
-  return ( true_level >=  100 ) ? "defiled" :
-         ( true_level >=  90  ) ? "focus" :
+  return ( true_level >= 110 ) ? "defiled" :
+         ( true_level >= 100 ) ? "hyper" :
          "disabled";
 }
 