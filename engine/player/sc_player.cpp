d7fdf668dded3d1f5a4e0cef8280c52e529f4f74
Navv <navv1234@gmail.com>
2017-05-26 22:52:26 +0300

    Fix wowhead artifact= option parsing some

--- engine/player/sc_player.cpp
+++ engine/player/sc_player.cpp
@@ -8297,6 +8297,17 @@ bool player_t::parse_artifact_wowhead( const std::string& artifact_string )
   }
 
   unsigned artifact_id = util::to_unsigned( splits[ 0 ] );
+  if ( artifact_id == 0 )
+  {
+    return false;
+  }
+
+  auto spec_artifact_id = dbc.artifact_by_spec( specialization() );
+  if ( spec_artifact_id != artifact_id )
+  {
+    return false;
+  }
+
   size_t n_relics = 0, n_excess_points = 0, relic_idx = 0;
   for ( size_t i = 1; i < 5; ++i )
   {
@@ -8322,31 +8333,47 @@ bool player_t::parse_artifact_wowhead( const std::string& artifact_string )
     if ( pos != artifact_powers.end() )
     {
       artifact.points[ pos - artifact_powers.begin() ].first = rank;
-      // Sanitize the input on any ranks > 3 so that we can get accurate purchased ranks for the
-      // user, even if the string contains zero relic ids. Note the power type check, power type 5
-      // is the 20 rank "final" power.
-      if ( n_relics == 0 && rank > 3 && ( *pos ) -> power_type != 5 )
-      {
-        n_excess_points += rank - 3;
-      }
-
       artifact.n_points += rank;
+      artifact.n_purchased_points += rank;
     }
   }
 
-  if ( artifact.n_points < ( n_relics == 0 ? n_excess_points : n_relics ) )
-  {
-    artifact.n_purchased_points = 0;
-  }
-  else
-  {
-    artifact.n_purchased_points = artifact.n_points - static_cast<unsigned int>(( n_relics == 0 ? n_excess_points : n_relics ));
-  }
+  // Adjust artifact points based on relic traits
+  range::for_each( artifact.relics, [ this, artifact_id, &artifact_powers ]( unsigned item_id ) {
+    if ( item_id == 0 )
+    {
+      return;
+    }
+
+    auto relic_data = dbc.artifact_relic_rank_index( artifact_id, item_id );
+    if ( relic_data.first == 0 || relic_data.second == 0 )
+    {
+      return;
+    }
+
+    // Trait internal index
+    auto pos = range::find_if( artifact_powers,
+                               [ &relic_data ]( const artifact_power_data_t* power_data ) {
+                                 return power_data -> id == relic_data.first;
+                               } );
+
+    if ( pos != artifact_powers.end() )
+    {
+      auto index = pos - artifact_powers.begin();
+      auto actual = std::min( as<uint8_t>( relic_data.second ), artifact.points[ index ].first );
+
+      artifact.points[ index ].first -= actual;
+      artifact.points[ index ].second += actual;
+
+      artifact.n_purchased_points -= actual;
+    }
+  } );
 
   // The initial power does not count towards the purchased points
   if ( artifact.n_purchased_points > 0 )
   {
     artifact.n_purchased_points--;
+    artifact.n_points--;
   }
 
   return true;
