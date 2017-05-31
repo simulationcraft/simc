8daa01175aea41ba87d09fa0f4332801bf8a97a5
Hinalover <tseltman@verizon.net>
2017-05-31 09:05:40 -0400

    [Monk] Small bug fix with last commit.

--- engine/class_modules/sc_monk.cpp
+++ engine/class_modules/sc_monk.cpp
@@ -10008,7 +10008,7 @@ void monk_t::apl_combat_windwalker()
   serenity -> add_action( this, "Rising Sun Kick", "cycle_targets=1,if=active_enemies<3" );
   serenity -> add_action( this, "Strike of the Windlord" );
   serenity -> add_action( this, "Fists of Fury", "if=((equipped.drinking_horn_cover&buff.pressure_point.remains<=1&set_bonus.tier20_2pc)&(cooldown.rising_sun_kick.remains>1|active_enemies>1))" );
-  serenity -> add_action( this, "Fists of Fury", "((!equipped.drinking_horn_cover|buff.bloodlust.up|buff.serenity.remains<1)&(cooldown.rising_sun_kick.remains>1|active_enemies>1))" );
+  serenity -> add_action( this, "Fists of Fury", "if=((!equipped.drinking_horn_cover|buff.bloodlust.up|buff.serenity.remains<1)&(cooldown.rising_sun_kick.remains>1|active_enemies>1))" );
   serenity -> add_action( this, "Spinning Crane Kick", "if=active_enemies>=3&!prev_gcd.1.spinning_crane_kick" );
   serenity -> add_action( this, "Rising Sun Kick", "cycle_targets=1,if=active_enemies>=3" );
   serenity -> add_action( this, "Spinning Crane Kick", "if=!prev_gcd.1.spinning_crane_kick" );
