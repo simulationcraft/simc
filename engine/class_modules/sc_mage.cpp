fd5821d9da640b749ea4b4e5c419e1edb1f1c879
Vít Šefl <vituscze@gmail.com>
2017-06-08 00:10:51 +0200

    [Mage] Bug update for 24287.

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -1722,7 +1722,7 @@ struct arcane_mage_spell_t : public mage_spell_t
       // The damage bonus given by mastery seems to be snapshot at the moment
       // Arcane Charge is gained. As long as the stack number remains the same,
       // any future changes to mastery will have no effect.
-      // As of PTR build 24271, 2017-06-06.
+      // As of PTR build 24287, 2017-06-08.
       if ( ac -> check() < ac -> max_stack() )
       {
         ac -> trigger( stacks, savant_damage_bonus() );
@@ -1967,7 +1967,7 @@ struct fire_mage_spell_t : public mage_spell_t
   // Helper methods for Contained Infernal Core.
   void trigger_infernal_core( player_t* target )
   {
-    // As of PTR build 24271, 2017-06-06, casting Fireball and instant Pyroblast
+    // As of PTR build 24287, 2017-06-08, casting Fireball and instant Pyroblast
     // while at 28 stacks removes tracking buff, then triggers primed buff and
     // one stack of tracking buff (instead of triggering the Meteor). Next cast
     // then expires the primed buff, triggers the Meteor and triggers a second
@@ -2237,7 +2237,7 @@ struct frost_mage_spell_t : public mage_spell_t
     // It seems that casting Flurry while Frostbolt is mid-air does not
     // trigger the buff if Frostbolt hits before Flurry does (thankfully,
     // it usually happens the other way around).
-    // Last checked: PTR build 24271, 2017-06-06
+    // Last checked: PTR build 24287, 2017-06-08.
     // TODO: Check this
     trigger_legendary_effect( p() -> buffs.shattered_fragments_of_sindragosa,
                               p() -> buffs.rage_of_the_frost_wyrm,
@@ -2747,7 +2747,7 @@ struct time_and_space_t : public arcane_mage_spell_t
     background = true;
 
     // All other background actions trigger Erosion.
-    // As of PTR build 24271, 2017-06-06.
+    // As of PTR build 24287, 2017-06-08.
     if ( p -> bugs )
     {
       triggers_erosion = false;
@@ -3436,13 +3436,6 @@ struct comet_storm_projectile_t : public frost_mage_spell_t
   {
     aoe = -1;
     background = true;
-
-    // Should be 6 yd according to the tooltip, but it's 4 yd
-    // in game. As of PTR build 24218, 2017-06-03.
-    if ( ! p -> bugs )
-    {
-      radius = 6.0;
-    }
   }
 };
 
@@ -4304,7 +4297,7 @@ struct ice_time_nova_t : public frost_mage_spell_t
     aoe = -1;
 
     // According to the spell data.
-    // As of PTR build 24271, 2017-06-06.
+    // As of PTR build 24287, 2017-06-08.
     if ( p -> bugs )
     {
       affected_by.frost_mage = false;
@@ -4337,7 +4330,7 @@ struct frozen_orb_bolt_t : public frost_mage_spell_t
     crit_bonus_multiplier *= 1.0 + p -> artifact.orbital_strike.percent();
     chills = true;
 
-    // As of PTR build 24271, 2017-06-06.
+    // As of PTR build 24287, 2017-06-08.
     if ( p -> bugs )
     {
       affected_by.shatter = false;
@@ -4947,7 +4940,7 @@ struct mark_of_aluneth_explosion_t : public arcane_mage_spell_t
     background = true;
     aoe = -1;
 
-    // As of PTR build 24271, 2017-06-06.
+    // As of PTR build 24287, 2017-06-08.
     if ( p -> bugs )
     {
       affected_by.arcane_mage = false;
@@ -5134,7 +5127,7 @@ struct meteor_impact_t: public fire_mage_spell_t
     timespan_t ground_aoe_duration = p() -> find_spell( 175396 ) -> duration();
 
     // It seems that the 8th tick happens only very rarely in game.
-    // As of PTR build 24271, 2017-06-06.
+    // As of PTR build 24287, 2017-06-08.
     if ( p() -> bugs )
     {
       ground_aoe_duration -= pulse_time;
@@ -5369,7 +5362,7 @@ struct phoenixs_flames_splash_t : public fire_mage_spell_t
     double am = fire_mage_spell_t::action_multiplier();
 
     // Phoenix's Flames splash deal 25% less damage compared to the
-    // spell data/tooltip values. As of PTR build 24271, 2017-06-06.
+    // spell data/tooltip values. As of PTR build 24287, 2017-06-08.
     am *= std::pow( strafing_run_multiplier, p() -> bugs ? chain_number + 1 : chain_number );
 
     return am;
