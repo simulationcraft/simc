9f2c3c718e2b3a71b77a1505b57d68d849ce62bd
Collision <s.alexlowery@gmail.com>
2017-06-16 22:48:01 -0400

    [Warrior] Cleanup trauma since it doesn't do that fancy crit thing anymore.

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -3639,48 +3639,11 @@ struct slam_t: public warrior_attack_t
 
 struct trauma_dot_t: public residual_action::residual_periodic_action_t < warrior_spell_t >
 {
-  double crit_chance_of_last_ability;
   trauma_dot_t( warrior_t* p ):
-    base_t( "trauma", p, p -> find_spell( 215537 ) ),
-    crit_chance_of_last_ability( 0 )
+    base_t( "trauma", p, p -> find_spell( 215537 ) )
   {
     dual = true;
     tick_may_crit = false;
-  }
-
-  double calculate_tick_amount( action_state_t* state, double dmg_multiplier ) const override
-  {
-    double amount = 0.0;
-
-    if ( dot_t* d = find_dot( state -> target ) )
-    {
-      residual_action::residual_periodic_state_t* dot_state = debug_cast<residual_action::residual_periodic_state_t*>(d -> state);
-      amount += dot_state -> tick_amount;
-    }
-
-    state -> result_raw = amount;
-    state -> result = RESULT_HIT; // Reset result to hit, as it has already been rolled inside tick().
-    if ( tick_may_crit && rng().roll( composite_crit_chance() ) )
-      state -> result = RESULT_CRIT;
-
-    if ( state -> result == RESULT_CRIT )
-      amount *= 1.0 + total_crit_bonus( state );
-
-    amount *= dmg_multiplier;
-    state -> result_total = amount;
-
-    return amount;
-  }
-
-  double composite_crit_chance() const override
-  {
-    return crit_chance_of_last_ability;
-  }
-
-  void impact( action_state_t* s ) override
-  {
-    residual_periodic_action_t::impact( s );
-    crit_chance_of_last_ability = p() -> composite_melee_crit_chance();
   }
 };
 
