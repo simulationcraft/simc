3466f0fb675cc10b226664b6f33bd3e67eda0ec8
Maxime Serrano <mserrano@cmu.edu>
2017-07-01 01:36:06 -0400

    [Paladin] Oops, this should have stayed at 20

--- engine/class_modules/sc_paladin.cpp
+++ engine/class_modules/sc_paladin.cpp
@@ -1725,7 +1725,7 @@ struct shield_of_vengeance_t : public paladin_absorb_t
 
 
     may_crit = true;
-    attack_power_mod.direct = 10;
+    attack_power_mod.direct = 20;
     if ( p -> artifact.deflection.rank() )
     {
       cooldown -> duration += timespan_t::from_millis( p -> artifact.deflection.value() );
