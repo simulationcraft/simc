ddb60946450291cf2bef36e3b5127c890cb86662
Melekus <antoine.colin1@hotmail.fr>
2017-07-17 05:02:04 +0200

    [Death Knight] More fixes
    Fix typos in the APL, removed more mentions to unholy's obsolete T20 2P
    Fixed a typo in blood 7.2 traits

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -753,7 +753,7 @@ public:
     artifact_power_t the_hungering_maw;
     // 7.2
     artifact_power_t fortitude_of_the_ebon_blade;
-    artifact_power_t carrion_fest;
+    artifact_power_t carrion_feast;
     artifact_power_t vampiric_aura;
     artifact_power_t souldrinker;
   } artifact;
@@ -7059,11 +7059,6 @@ bool death_knight_t::create_actions()
         this, find_spell( 243122 ) );
   }
 
-  if ( sets -> has_set_bonus( DEATH_KNIGHT_UNHOLY, T20, B2 ) )
-  {
-    active_spells.t20_2pc_unholy = new explosive_army_t( this );
-  }
-
   return player_t::create_actions();
 }
 
@@ -7722,9 +7717,9 @@ void death_knight_t::default_apl_frost()
   machinegun -> add_action( this, "Howling Blast", "if=buff.rime.react&!(buff.obliteration.up&spell_targets.howling_blast<2)&!(equipped.perseverance_of_the_ebon_martyr&talent.gathering_storm.enabled" );
   machinegun -> add_action( this, "Howling Blast", "buff.rime.react&!(buff.obliteration.up&spell_targets.howling_blast<2)&equipped.perseverance_of_the_ebon_martyr&talent.gathering_storm.enabled&(debuff.perseverance_of_the_ebon_martyr.up|cooldown.remorseless_winter.remains>3)" );
   machinegun -> add_action( this, "Obliterate", "if=!buff.obliteration.up&(equipped.koltiras_newfound_will&talent.frozen_pulse.enabled&(set_bonus.tier19_2pc=1|set_bonus.tier19_4pc=1))|rune.time_to_5<gcd" );
-  machinegun -> add_talent( this, "Obliteration", "if=(!talent.frozen_pulse.enabled|(rune<2&runic_power<28))&talent.gathering_storm.enabled&buff.remorseless_winter.up" )
+  machinegun -> add_talent( this, "Obliteration", "if=(!talent.frozen_pulse.enabled|(rune<2&runic_power<28))&talent.gathering_storm.enabled&buff.remorseless_winter.up" );
   machinegun -> add_action( this, "Sindragosa's Fury", "if=(equipped.consorts_cold_core|buff.pillar_of_frost.up)&buff.unholy_strength.up&debuff.razorice.stack=5&!buff.obliteration.up" );
-  machinegun -> add_action( this, "Frost Strike", "if=(!buff.obliteration.up&runic_power.deficit<=10)|(buff.obliteration.up&!buff.killing_machine.react" )
+  machinegun -> add_action( this, "Frost Strike", "if=(!buff.obliteration.up&runic_power.deficit<=10)|(buff.obliteration.up&!buff.killing_machine.react" );
   machinegun -> add_action( this, "Remorseless Winter", "if=spell_targets.remorseless_winter>=2&!(talent.frostscythe.enabled&buff.killing_machine.react&spell_targets.frostscythe>=2)" );
   machinegun -> add_talent( this, "Frostscythe", "if=buff.killing_machine.up&(!equipped.koltiras_newfound_will|spell_targets.frostscythe>=2)" );
   machinegun -> add_talent( this, "Glacial Advance", "if=spell_targets.glacial_advance>=2" );
