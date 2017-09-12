c31a50fb5f6cf10bb7bae6702c94971f6deb93d9
Lucas Kern <lucasgilbertokern@gmail.com>
2017-09-12 11:01:32 -0300

    [Report] Adds spec to the Pawn string name (#3985)
    Fixes https://github.com/simulationcraft/simc/issues/3977

--- engine/report/sc_gear_weights.cpp
+++ engine/report/sc_gear_weights.cpp
@@ -197,6 +197,8 @@ std::array<std::string, SCALE_METRIC_MAX> gear_weights::pawn(
     bool first    = true;
     std::string s = "( Pawn: v1: \"";
     s += p.name();
+    s += "-";
+    s += util::spec_string_no_class(p);
     s += "\": ";
     s += "Class=";
     std::string name = util::player_type_string( p.type );
