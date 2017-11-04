2873ead9d85b30098512b602c654273f20541c07
Aethys256 <aethys256@users.noreply.github.com>
2017-11-04 08:36:35 +0100

    [GUI] Fix default profiles display to show PreRaids

--- qt/sc_SampleProfilesTab.cpp
+++ qt/sc_SampleProfilesTab.cpp
@@ -41,7 +41,7 @@ void SC_SampleProfilesTab::fillTree( QDir baseDir )
     baseDir.setFilter( QDir::Dirs );
 
     static const char* tierNames[] = { "T21", "T20", "T19", "PR" };
-    static const int TIER_MAX = 3; // = sizeof_array( tierNames );
+    static const int TIER_MAX = 4; // = sizeof_array( tierNames );
 
     QTreeWidgetItem* playerItems[PLAYER_MAX];
     range::fill( playerItems, 0 );
