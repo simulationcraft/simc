880c631c74710a8a3fa43b42272e843ad91312ef
Aethys256 <aethys256@users.noreply.github.com>
2017-11-04 08:19:30 +0100

    [Profiles] Update generate_profiles scripts

--- generate_profiles.bat
+++ generate_profiles.bat
@@ -1,17 +1,27 @@
 @echo off
 if exist simc_cache.dat del /s simc_cache.dat
-:: Preraid doesn't match the typical pattern
-::cd profiles/PreRaid
-::%~dp0\simc.exe generate_PreRaid.simc
-::cd ../
 
 if exist simc.exe goto 32
 goto exit
+
 :32
-@echo on
-forfiles -s -m generate_T*.simc -c "cmd /c echo Running @path && %~dp0simc.exe @file"
+cd "profiles"
+:: PreRaids doesn't match the typical pattern
+cd "PreRaids"
+echo Running PR_Generate.simc in %cd%
+%~dp0simc.exe PR_Generate.simc
+cd ..\
+:: TierXX profiles generation
+for %%g in (19, 20, 21) do (
+  cd Tier%%g
+  echo Running T%%g_Generate.simc in %cd%
+  %~dp0simc.exe T%%g_Generate.simc
+  cd ..\
+)
 goto pause
+
 :exit
-echo "This file requires the CLI to be located in the same folder"
+echo "This file requires the CLI (simc.exe) to be located in the same folder"
+
 :pause
 pause
