d2f75eb66adfa665c6430311fb47e1415614ab89
Vít Šefl <vituscze@gmail.com>
2017-11-16 18:42:06 +0100

    [Mage] Heating Up duration scales with casting speed.

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -1895,7 +1895,9 @@ struct fire_mage_spell_t : public mage_spell_t
         else
         {
           p -> procs.heating_up_generated -> occur();
-          p -> buffs.heating_up -> trigger();
+          p -> buffs.heating_up -> trigger(
+            1, buff_t::DEFAULT_VALUE(), -1.0,
+            p -> buffs.heating_up -> data().duration() * p -> cache.spell_speed() );
 
           // Controlled Burn HU -> HS conversion
           if ( p -> talents.controlled_burn -> ok() &&
