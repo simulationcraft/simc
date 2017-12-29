aef3ba79be847b4fdb1ff09304d4be288634eab0
Aethys256 <aethys256@users.noreply.github.com>
2017-12-29 19:40:13 +0100

    [Rogue] DP Bug & Stealth expires delay

--- engine/class_modules/sc_rogue.cpp
+++ engine/class_modules/sc_rogue.cpp
@@ -858,12 +858,12 @@ namespace actions { // namespace actions
 
 static void break_stealth( rogue_t* p )
 {
-  
+  // As of 12/29/2017: Stealth effects remains for 100-200ms after we break it
   if ( p -> buffs.stealth -> check() )
-    p -> buffs.stealth -> expire();
+    p -> buffs.stealth -> expire( timespan_t::from_millis( 150 ) );
 
   if ( p -> buffs.vanish -> check() )
-    p -> buffs.vanish -> expire();
+    p -> buffs.vanish -> expire( timespan_t::from_millis( 150 ) );
 }
 
 // ==========================================================================
@@ -2049,7 +2049,22 @@ struct deadly_poison_t : public rogue_poison_t
       }
 
       return m;
-    };
+    }
+
+    // As of 12/29/2017 Nighstalker multiplier remains until the DoT fade out
+    // See: https://github.com/Ravenholdt-TC/Rogue/issues/81
+    void snapshot_internal( action_state_t* state, unsigned flags, dmg_e rt ) override
+    {
+      // Disable persistent multiplier update if the dot is already on
+      if ( p() -> bugs && flags & STATE_MUL_PERSISTENT )
+      {
+        if ( td( state -> target ) -> dots.deadly_poison -> is_ticking() != 0 ) {
+          flags &= ~STATE_MUL_PERSISTENT;
+        }
+      }
+
+      rogue_poison_t::snapshot_internal( state, flags, rt );
+    }
   };
 
   deadly_poison_dd_t*  proc_instant;
@@ -7266,6 +7281,7 @@ void rogue_t::init_action_list()
     def -> add_action( "variable,name=energy_regen_combined,value=energy.regen+poisoned_bleeds*(7+talent.venom_rush.enabled*3)%2" );
     def -> add_action( "variable,name=energy_time_to_max_combined,value=energy.deficit%variable.energy_regen_combined" );
     def -> add_action( "variable,name=use_fok_rotation,value=fok_rotation&(artifact.poison_knives.rank>=5|equipped.zoldyck_family_training_shackles&target.health.pct<30)" );
+    def -> add_action( this, "Poisoned Knife", "if=bugs&talent.nightstalker.enabled&stealthed.rogue&!dot.deadly_poison.ticking" );
     def -> add_action( "call_action_list,name=cds" );
     def -> add_action( "run_action_list,name=aoe,if=spell_targets.fan_of_knives>(3-variable.use_fok_rotation)" );
     def -> add_action( "call_action_list,name=maintain" );
