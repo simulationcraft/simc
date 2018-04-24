05a4625ffb400346c14881d85786d15110943bbe
Julien Rechaussat <Kutikuti@users.noreply.github.com>
2018-04-24 14:57:33 +0200

    Make Spell generator space compatible

--- dbc_extract3/generate.bat
+++ dbc_extract3/generate.bat
@@ -2,24 +2,24 @@
 chcp 65001
 setlocal
 
-set OUTPATH=%~dp0..\engine\dbc\generated
-set RUNFILE=%~dp0\dbc_extract.py
-set CACHEFILE=%~dp0\cache\live\DBCache.bin
-set BATCHFILE=%~dp0\live.conf
+set OUTPATH="%~dp0..\engine\dbc\generated"
+set RUNFILE="%~dp0\dbc_extract.py"
+set CACHEFILE="%~dp0\cache\live\DBCache.bin"
+set BATCHFILE="%~dp0\live.conf"
 
 set PTR=
 set PTREXT=
 if not %1 == ptr goto next
 set PTR= --prefix=ptr
 set PTREXT=_ptr
-set CACHEFILE=%~dp0\cache\ptr\DBCache.bin
-set BATCHFILE=%~dp0\ptr.conf
+set CACHEFILE="%~dp0\cache\ptr\DBCache.bin"
+set BATCHFILE="%~dp0\ptr.conf"
 shift
 
 :next
 set BUILD=%1
-set INPATH=%~f2\%BUILD%\DBFilesClient
-set GTINPATH=%~f2\%BUILD%\GameTables
+set INPATH="%~f2\%BUILD%\DBFilesClient"
+set GTINPATH="%~f2\%BUILD%\GameTables"
 
 if exist %INPATH% goto okay
 echo Error: Unable to find input files! %INPATH%
