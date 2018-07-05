cb3099dccb2ec50225df3420541003d00361c8de
Navv <navv1234@gmail.com>
2018-07-05 17:07:40 +0300

    Fix dot.<action-name>.<expression> expressions

--- engine/action/sc_action.cpp
+++ engine/action/sc_action.cpp
@@ -3137,7 +3137,23 @@ expr_t* action_t::create_expression( const std::string& name_str )
 
   if ( splits.size() == 3 && splits[ 0 ] == "dot" )
   {
-    auto expr = dot_t::create_expression(nullptr, this, splits[ 2 ], true );
+    auto action = player->find_action( splits[ 1 ] );
+    if ( !action )
+    {
+      action = player->create_action( splits[ 1 ], "" );
+      // Can't make an action for the given dot, and no action found. Just implicitly always return
+      // "false" (0) for the evaluated expression
+      if ( !action )
+      {
+        return expr_t::create_constant( splits[ 2 ], 0 );
+      }
+      else
+      {
+        action->quiet = action->background = true;
+      }
+    }
+
+    auto expr = dot_t::create_expression(nullptr, action, splits[ 2 ], true );
     if ( expr )
     {
       return expr;
