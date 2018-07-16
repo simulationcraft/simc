d11e8f444dfc674977bb6858c1c67d5002e59259
Quentin Giraud <dev@aethys.io>
2018-07-16 20:42:23 +0200

    [Profiles] Make a better generate_profiles.sh with more feedback & error handling

--- generate_profiles.sh
+++ generate_profiles.sh
@@ -1,16 +1,28 @@
+if [ ! -d "profiles" ]; then
+  echo 'You must run this script with simc root as current working directory.'
+  exit 0
+fi
+cd 'profiles'
 # PreRaids doesn't match the typical pattern
-cd 'profiles/PreRaids/'
-pwd
-rm -rf ./*
-../../engine/simc '../generators/PreRaids/PR_Generate.simc'
-cd ../
+PROFDIR='PreRaids'
+echo "---$PROFDIR---"
+if [ -d $PROFDIR ]; then
+  cd $PROFDIR/
+  ../../engine/simc "../generators/$PROFDIR/PR_Generate.simc"
+  cd ../
+else
+  echo 'Skipped PreRaids, directory not found.'
+fi
 # TierXX profiles generation
-for tier in 21
+for tier in 21 22
 do
-  PROFDIR='Tier'$tier'/'
-  cd $PROFDIR
-  pwd
-  rm -rf ./*
+  PROFDIR="Tier$tier"
+  echo "---$PROFDIR---"
+  if [ ! -d $PROFDIR ]; then
+    echo "Skipped $PROFDIR, directory not found."
+    continue
+  fi
+  cd $PROFDIR/
   ../../engine/simc '../generators/Tier'$tier'/T'$tier'_Generate.simc'
   cd ../
 done
