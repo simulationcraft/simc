9558d194397fb6393d7bd339abe97e34253405c2
Quentin Giraud <dev@aethys.io>
2018-07-20 19:52:15 +0200

    [Profilesets] Save global statistics back to parent sim

--- engine/sim/sc_profileset.cpp
+++ engine/sim/sc_profileset.cpp
@@ -133,6 +133,13 @@ void simulate_profileset( sim_t* parent, profileset::profile_set_t& set, sim_t*&
     } );
   }
 
+  // Save global statistics back to parent sim
+  parent -> elapsed_cpu  += profile_sim -> elapsed_cpu;
+  parent -> elapsed_time += profile_sim -> elapsed_time;
+  parent -> init_time    += profile_sim -> init_time;
+  parent -> merge_time   += profile_sim -> merge_time;
+  parent -> analyze_time += profile_sim -> analyze_time;
+
   set.cleanup_options();
 }
 
