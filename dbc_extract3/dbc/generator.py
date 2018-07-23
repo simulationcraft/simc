5ce45c151164c77be7dc907fa6e46b9186309ac3
Hinalover <tseltman@verizon.net>
2018-07-22 22:24:50 -0400

    [Monk] Add Swift Roundhouse Azerite Buff to DBC extract

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1510,6 +1510,8 @@ class SpellDataGenerator(DataGenerator):
           ( 261682, 3 ), # Chi Burst Chi generation cap
           # Legendary
           ( 213114, 3 ), # Hidden Master's Forbidden Touch buff
+          # Azerite Traits
+          ( 278710, 3 ), # Swift Roundhouse
         ),
 
         # Druid:
