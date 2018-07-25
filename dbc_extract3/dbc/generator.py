656b3bbe5eaa9854ff89622915a8f22ec22bb01e
Antoine Colin <antoine.colin1@hotmail.fr>
2018-07-25 11:56:28 +0200

    [Death Knight] Whitelist Festermight azerite buff

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1330,6 +1330,7 @@ class SpellDataGenerator(DataGenerator):
           ( 205165, 0 ),    # More crystalline swords stuff
           ( 191730, 0 ), ( 191727, 0 ), ( 191728, 0 ), ( 191729, 0 ), # Armies of the Damned debuffs
           ( 253590, 0 ),    # T21 4P frost damage component
+          ( 274373, 0 ),    # Festermight azerite trait
         ),
 
         # Shaman:
