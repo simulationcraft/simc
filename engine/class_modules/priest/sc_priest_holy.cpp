993297b373173027e2e568da2180a2ece3fbb71b
Juliacare <rik.van.hoogstraten@gmail.com>
2018-08-27 19:33:28 +0200

    [Priest] Holy: Different racial use

--- engine/class_modules/priest/sc_priest_holy.cpp
+++ engine/class_modules/priest/sc_priest_holy.cpp
@@ -247,14 +247,18 @@ void priest_t::generate_apl_holy_d()
          "potion,if=buff.bloodlust.react|target.time_to_die<=80");
 
     // Racials
-    if (race == RACE_DARK_IRON_DWARF)
-        default_list->add_action("fireblood");
-    if (race == RACE_TROLL)
-        default_list->add_action("berserking");
-    if (race == RACE_LIGHTFORGED_DRAENEI)
-        default_list->add_action("lights_judgment");
-    if (race == RACE_MAGHAR_ORC)
-        default_list->add_action("ancestral_call");
+    if (race != RACE_BLOOD_ELF)
+    {
+        for (const std::string& racial_action : get_racial_actions())
+        {
+            default_list->add_action(racial_action);
+        }
+    }
+// Belf is a DPS decrease to use until like 20 targets for now
+//  if (race == RACE_BLOOD_ELF)
+//   {
+//           default_list->add_action("arcane_torrent,if=mana.pct<=95");
+//   }
 
     // Default APL
     default_list->add_action( this, "Holy Fire", "if=refreshable&dot.holy_fire.ticking&dot.holy_fire.stack>1|dot.holy_fire.stack<2");
