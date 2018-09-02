0be9d55d61cd6a38f6fe38f406556e91559e9763
EvanMichaels <evan.michaels@gmail.com>
2018-09-01 21:26:12 -0400

    [Outlaw] Adjust Azerite Reroll Conditions
    * With 2+ ranks of Deadshot, only avoid rerolling for Ruthless Precision, not Grand Melee
    * With any number of ranks of Snake Eyes, always consider rerolling unless you have 2+ buffs (as long as you won't waste buff stacks)

--- engine/class_modules/sc_rogue.cpp
+++ engine/class_modules/sc_rogue.cpp
@@ -6305,6 +6305,8 @@ void rogue_t::init_action_list()
 
     // Main Rotation
     def -> add_action( "variable,name=rtb_reroll,value=rtb_buffs<2&(buff.loaded_dice.up|!buff.grand_melee.up&!buff.ruthless_precision.up)", "Reroll for 2+ buffs with Loaded Dice up. Otherwise reroll for 2+ or Grand Melee or Ruthless Precision." );
+    def -> add_action( "variable,name=rtb_reroll,op=set,if=azerite.deadshot.rank>=2,value=rtb_buffs<2&(buff.loaded_dice.up|!buff.ruthless_precision.up)", "Reroll for 2+ buffs or Ruthless Precision with Deadshot Rank 2+." );
+    def -> add_action( "variable,name=rtb_reroll,op=set,if=azerite.snake_eyes.enabled,value=rtb_buffs<2", "Always reroll for 2+ buffs with Snake Eyes." );
     def -> add_action( "variable,name=rtb_reroll,op=reset,if=azerite.snake_eyes.rank>=2&buff.snake_eyes.stack>=2-buff.broadside.up", "Do not reroll if Snake Eyes is at 2+ Ranks and 2+ stacks of the buff (1+ stack with Broadside up)" );
     def -> add_action( "variable,name=ambush_condition,value=combo_points.deficit>=2+2*(talent.ghostly_strike.enabled&cooldown.ghostly_strike.remains<1)+buff.broadside.up&energy>60&!buff.skull_and_crossbones.up" );
     def -> add_action( "variable,name=blade_flurry_sync,value=spell_targets.blade_flurry<2&raid_event.adds.in>20|buff.blade_flurry.up", "With multiple targets, this variable is checked to decide whether some CDs should be synced with Blade Flurry" );
