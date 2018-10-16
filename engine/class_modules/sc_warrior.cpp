85cf5f2d7769316e55c8e71feac830cdf8f65838
Antoine Colin <antoine.colin1@hotmail.fr>
2018-10-16 12:35:49 +0200

    [Warrior] Typo

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -4021,7 +4021,7 @@ struct last_stand_t : public warrior_spell_t
     range              = -1;
     cooldown->duration = data().cooldown();
     cooldown->duration *= 1.0 + p->sets->set( WARRIOR_PROTECTION, T18, B2 )->effectN( 1 ).percent();
-    cooldown -> duration += timespan_t::from_millis( p -> talents.bolster -> effectN( 1 ).base_value() )
+    cooldown -> duration += timespan_t::from_millis( p -> talents.bolster -> effectN( 1 ).base_value() );
   }
 
   void execute() override
