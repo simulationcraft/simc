222b8b73e0b83a56c02ddca573df2c5b215f6ffa
Rusah <dbaird337@gmail.com>
2018-11-16 01:43:29 -0600

    [Shaman] Enhancement - Update TF trait to reflect buggy state on PTR.

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -3003,7 +3003,9 @@ struct stormstrike_attack_t : public shaman_attack_t
 
     if ( p()->azerite.thunderaans_fury.ok() )
     {
-      double tf_bonus = 0.5 * p()->azerite.thunderaans_fury.value( 2 );
+	  //currently buggy on ptr, is applying 2/3 to each hit instead of 1/3 on oh
+      //double tf_bonus = 0.5 * p()->azerite.thunderaans_fury.value( 2 );
+      double tf_bonus = (2/3) * p()->azerite.thunderaans_fury.value( 2 );
       b += tf_bonus;
     }
 
@@ -3048,7 +3050,9 @@ struct windstrike_attack_t : public stormstrike_attack_t
 
     if ( p()->azerite.thunderaans_fury.ok() )
     {
-      double tf_bonus = 0.5 * p()->azerite.thunderaans_fury.value( 2 ); 
+      // currently buggy on ptr, is applying 2/3 to each hit instead of 1/3 on oh
+      // double tf_bonus = 0.5 * p()->azerite.thunderaans_fury.value( 2 );
+      double tf_bonus = ( 2 / 3 ) * p()->azerite.thunderaans_fury.value( 2 );
       b += tf_bonus;
     }
 
