0c09d2bc8fcf72459ab2e4f61f4a6bf9ff4f692b
Laust <lafa16ac@student.cbs.dk>
2018-11-17 20:08:57 +0100

    Update BM APL Changed Potion usage, moved crows down in the opener, tweaked stampede to be used with aotw, save Bestial Wrath for AotW and dump Barbed Shot charges before AotW - credit to Cerbi

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -5417,17 +5417,16 @@ void hunter_t::apl_bm()
   default_list -> add_action( "lights_judgment" );
 
   // In-combat potion
-  default_list -> add_action( "potion,if=buff.bestial_wrath.up&buff.aspect_of_the_wild.up" );
+  default_list -> add_action( "potion,if=buff.bestial_wrath.up&buff.aspect_of_the_wild.up&(target.health.pct<35|!talent.killer_instinct.enabled)|target.time_to_die<25" );
 
   // Generic APL
-  default_list -> add_action( this, "Barbed Shot", "if=pet.cat.buff.frenzy.up&pet.cat.buff.frenzy.remains<=gcd.max" );
-  default_list -> add_talent( this, "A Murder of Crows", "if=active_enemies=1" );
-  default_list -> add_action( this, "Barbed Shot", "if=full_recharge_time<gcd.max&cooldown.bestial_wrath.remains" );
+  default_list -> add_action( this, "Barbed Shot", "if=pet.cat.buff.frenzy.up&pet.cat.buff.frenzy.remains<=gcd.max|full_recharge_time<gcd.max&cooldown.bestial_wrath.remains" );
   default_list -> add_talent( this, "Spitting Cobra" );
-  default_list -> add_talent( this, "Stampede", "if=buff.bestial_wrath.up|cooldown.bestial_wrath.remains<gcd|target.time_to_die<15" );
   default_list -> add_action( this, "Aspect of the Wild" );
+  default_list -> add_talent( this, "A Murder of Crows", "if=active_enemies=1" );
+  default_list -> add_talent( this, "Stampede", "if=buff.aspect_of_the_wild.up&buff.bestial_wrath.up|target.time_to_die<15" );
   default_list -> add_action( this, "Multi-Shot", "if=spell_targets>2&gcd.max-pet.cat.buff.beast_cleave.remains>0.25" );
-  default_list -> add_action( this, "Bestial Wrath", "if=!buff.bestial_wrath.up" );
+  default_list -> add_action( this, "Bestial Wrath", "cooldown.aspect_of_the_wild.remains>20|target.time_to_die<15" );
   default_list -> add_talent( this, "Barrage", "if=active_enemies>1" );
   default_list -> add_talent( this, "Chimaera Shot", "if=spell_targets>1");
   default_list -> add_action( this, "Multi-Shot", "if=spell_targets>1&gcd.max-pet.cat.buff.beast_cleave.remains>0.25" );
@@ -5435,7 +5434,7 @@ void hunter_t::apl_bm()
   default_list -> add_talent( this, "Chimaera Shot" );
   default_list -> add_talent( this, "A Murder of Crows" );
   default_list -> add_talent( this, "Dire Beast" );
-  default_list -> add_action( this, "Barbed Shot", "if=pet.cat.buff.frenzy.down&charges_fractional>1.8|target.time_to_die<9" );
+  default_list -> add_action( this, "Barbed Shot", "if=pet.cat.buff.frenzy.down&(charges_fractional>1.8|buff.bestial_wrath.up)|cooldown.aspect_of_the_wild.remains<6&azerite.primal_instincts.enabled|target.time_to_die<9" );
   default_list -> add_talent( this, "Barrage" );
   default_list -> add_action( this, "Cobra Shot", "if=(active_enemies<2|cooldown.kill_command.remains>focus.time_to_max)&(focus-cost+focus.regen*(cooldown.kill_command.remains-1)>action.kill_command.cost|cooldown.kill_command.remains>1+gcd)&cooldown.kill_command.remains>1" );
   // Arcane torrent if nothing else is available
