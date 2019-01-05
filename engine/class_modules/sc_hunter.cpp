3a876c84a09b09f02f3fe73c436e6503a2fe984f
Putro <isondkwamm@gmail.com>
2019-01-05 23:51:11 +0100

    [Hunter] Lower Dire Consequence proc chance by 45% to accurately model in-game behaviour

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -3664,6 +3664,11 @@ struct kill_command_t: public hunter_spell_t
     {
       auto driver = p -> find_spell( 287097 );
       dire_consequences.rppm = p -> get_rppm( "dire_consequences", driver );
+      //Dire Consequences is currently bugged and has reduced procrate compared to what the spell data indicates.
+      //This is still the case as of 05/01/2019
+      //TODO: Follow up on this prior to raid release on 22/01/2019
+      dire_consequences.rppm -> set_modifier(
+        dire_consequences.rppm -> get_modifier() * 0.55); 
       dire_consequences.icd = p -> get_cooldown( "dire_consequences" );
       dire_consequences.icd -> duration = driver -> internal_cooldown();
       dire_consequences.proc = p -> get_proc( "Dire Consequences" );
