30f9024630c215696f202cfc5fbe22bb3f9a6605
Quirin Teunis <jundarer@gmail.com>
2019-01-17 01:32:28 +0100

    [Resto Druid] Update APL

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -8433,7 +8433,7 @@ void druid_t::apl_restoration()
   default_list->add_action("sunfire,if=refreshable|(prev_gcd.1.moonfire&remains<duration*0.5)");
   default_list->add_action("innervate,if=!buff.cat_form.up&energy<40");
   default_list->add_action("cat_form,if=!buff.cat_form.up&energy>50");
-  default_list->add_action("ferocious_bite,if=(combo_points>3&target.time_to_die<3)|(combo_points=5&energy>=50&dot.rip.remains>14)|(dot.rip.ticking&target.health.pct<25&combo_points=5&energy>=50)|(dot.rip.ticking&dot.rip.remains<3&target.health.pct<25)");
+  default_list->add_action("ferocious_bite,if=(combo_points>3&target.time_to_die<3)|(combo_points=5&energy>=50&dot.rip.remains>14)");
   default_list->add_action("swipe_cat,if=spell_targets.swipe_cat>=6");
   default_list->add_action("rip,if=refreshable&combo_points=5");
   default_list->add_action("ferocious_bite,max_energy=1,if=combo_points=5&energy.time_to_max<2");
