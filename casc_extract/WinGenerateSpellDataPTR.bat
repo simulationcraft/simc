17089bcf660e33a4388bb5df068a57e927981c93
Vít Šefl <vituscze@gmail.com>
2019-01-30 01:23:16 +0100

    Use full build number in WinGenerate* scripts.

--- casc_extract/WinGenerateSpellDataPTR.bat
+++ casc_extract/WinGenerateSpellDataPTR.bat
@@ -2,9 +2,6 @@ call py -3 casc_extract.py -m batch --cdn --ptr -o wow
 cd wow
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
-set /p oldname=<hi.txt
-set wowdir=%wowdir:~6%
-ren %oldname% %wowdir%
 del hi.txt
 cd ..
 cd ..
