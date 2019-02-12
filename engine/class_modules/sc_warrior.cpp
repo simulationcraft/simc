56eb7ae0e7ff2021faf9a684cc93bbf85b0135a9
Antoine Colin <antoine.colin1@hotmail.fr>
2019-02-12 13:25:57 +0100

    [Warrior] Demoralizing shout is now correctly affected by Anger Management

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -5399,6 +5399,7 @@ struct debuff_demo_shout_t : public warrior_buff_t<buff_t>
     : base_t( p, "demoralizing_shout_debuff", w -> find_specialization_spell( "Demoralizing Shout" ) ),
       extended( 0 ), deafening_crash_cap( as<int>( w -> azerite.deafening_crash.spell() -> effectN( 3 ).base_value() ) )
   {
+    cooldown -> duration = timespan_t::zero(); // Cooldown handled by the action
     default_value = data().effectN( 1 ).percent();
   }
 
