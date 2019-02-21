cc3d8f5887b9c9035d2a3611669917e22ac5996d
Florian Meißner <developer@mystler.eu>
2019-02-21 14:57:05 +0100

    [Assassination] Update default usage for PvP and Plumage trinkets

--- engine/class_modules/sc_rogue.cpp
+++ engine/class_modules/sc_rogue.cpp
@@ -5590,8 +5590,11 @@ void rogue_t::init_action_list()
     {
       if ( items[i].has_special_effect( SPECIAL_EFFECT_SOURCE_ITEM, SPECIAL_EFFECT_USE ) )
       {
+        auto use_effect_id = items[i].special_effect( SPECIAL_EFFECT_SOURCE_ITEM, SPECIAL_EFFECT_USE ) -> spell_id;
         if ( items[i].name_str == "galecallers_boon" )
           cds -> add_action( "use_item,name=" + items[i].name_str + ",if=cooldown.vendetta.remains<=1&(!talent.subterfuge.enabled|dot.garrote.pmultiplier>1)|cooldown.vendetta.remains>45" );
+        else if ( use_effect_id == 271107 || use_effect_id == 277179 || use_effect_id == 277185 ) // Golden Luster, Gladiator's Medallion Gladiator's Badge
+          cds -> add_action( "use_item,name=" + items[i].name_str + ",if=debuff.vendetta.up" );
         else // Default: Use on CD
           cds -> add_action( "use_item,name=" + items[i].name_str, "Default Trinket usage: Use on cooldown." );
       }
