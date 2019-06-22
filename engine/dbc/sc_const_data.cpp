aca369609c429912f32510dceee04cb3b5caa9e9
Navv <navv1234@gmail.com>
2019-06-22 17:48:42 +0300

    Fix item-based value scaling for scaling class -1

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -1728,7 +1728,8 @@ double spelleffect_data_t::average( const item_t* item ) const
     const auto& props = item -> player -> dbc.random_property( item -> item_level() );
     budget = props.damage_replace_stat;
   }
-  else if ( item->player->dbc.ptr && _spell->flags( spell_attribute::SX_SCALE_ILEVEL ) )
+  else if ( item->player->dbc.ptr && _spell->scaling_class() == PLAYER_NONE &&
+            _spell->flags( spell_attribute::SX_SCALE_ILEVEL ) )
   {
     const auto& props = item -> player -> dbc.random_property( item -> item_level() );
     budget = props.damage_secondary;
