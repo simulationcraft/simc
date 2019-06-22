3fdce3c773028c6ec5d7cd8088a0eab334ff272e
Dave Hendler <sollaires@gmail.com>
2019-06-21 23:24:06 -0700

    Add new gems to whitelist

--- dbc_extract3/dbc/constants.py
+++ dbc_extract3/dbc/constants.py
@@ -13,6 +13,12 @@ CONSUMABLE_ITEM_WHITELIST = {
    # "Other"
    8: [
      # Battle for Azeroth
+     168489, 168498, 168500, 168499,    # Superior Battle potions (8.2)
+     168506, 168529, 169299,            # focused resolve, empowered proximity, unbridled fury (8.2)
+     # 8.2 gems are class/subclass of JC, not gems
+     168637, 168638, 168636,            # Epic main stat gems (8.2)
+     168639, 168641, 168640, 168642,    # Epic secondary gems (8.2)
+
      163222, 163223, 163224, 163225,    # Battle potions
      152560, 152559, 152557,            # Potions of Bursting Blood, Rising Death, Steelskin
      160053,                            # Battle-Scarred Augment Rune
