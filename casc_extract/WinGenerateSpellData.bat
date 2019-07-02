ea025aaca28b8e5e3a9324813a1d5f56423c1e87
gastank <42421688+gastank@users.noreply.github.com>
2019-07-02 11:44:00 -0700

    Update WinGenerateSpellData.bat with new folder structure

--- casc_extract/WinGenerateSpellData.bat
+++ casc_extract/WinGenerateSpellData.bat
@@ -7,7 +7,8 @@ cd ..
 cd ..
 set curr=%cd%
 cd dbc_extract3
-robocopy "C:\Program Files (x86)\Warcraft\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
-robocopy "C:\World of Warcraft\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
+robocopy "C:\Program Files (x86)\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
+robocopy "C:\Program Files (x86)\Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
+robocopy "C:\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
 call generate.bat %wowdir% "%curr%\casc_extract\wow"
 pause
