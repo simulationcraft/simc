af1babdc7e3170efc6dc2cf877593ff35e8adde5
Vít Šefl <vituscze@gmail.com>
2019-07-07 14:41:08 +0200

    [Mage] Essences + non-standard Frost talents.

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -5978,24 +5978,27 @@ void mage_t::apl_frost()
   {
     case ROTATION_STANDARD:
     case ROTATION_NO_ICE_LANCE:
-      essences->add_action( "focused_azerite_beam" );
-      essences->add_action( "memory_of_lucid_dreams,if=buff.icicles.stack<2&cooldown.frozen_orb.remains>execute_time" + std::string( options.rotation == ROTATION_STANDARD ? "&!action.frozen_orb.in_flight&ground_aoe.frozen_orb.remains=0" : "" ) );
-      essences->add_action( "blood_of_the_enemy,if=buff.icicles.stack=5&buff.brain_freeze.react|!talent.glacial_spike.enabled|active_enemies>4" );
-      essences->add_action( "purifying_blast" );
-      essences->add_action( "ripple_in_space" );
-      essences->add_action( "concentrated_flame,line_cd=6" );
+      essences->add_action( "focused_azerite_beam,if=buff.rune_of_power.down|active_enemies>3" );
+      essences->add_action( "memory_of_lucid_dreams,if=active_enemies<5&(buff.icicles.stack<=1|!talent.glacial_spike.enabled)&cooldown.frozen_orb.remains>10"
+        + std::string( options.rotation == ROTATION_STANDARD ? "&!action.frozen_orb.in_flight&ground_aoe.frozen_orb.remains=0" : "" ) );
+      essences->add_action( "blood_of_the_enemy,if=(talent.glacial_spike.enabled&buff.icicles.stack=5&(buff.brain_freeze.react|prev_gcd.1.ebonbolt))"
+        "|((active_enemies>3|!talent.glacial_spike.enabled)&(prev_gcd.1.frozen_orb|ground_aoe.frozen_orb.remains>5))" );
+      essences->add_action( "purifying_blast,if=buff.rune_of_power.down|active_enemies>3" );
+      essences->add_action( "ripple_in_space,if=buff.rune_of_power.down|active_enemies>3" );
+      essences->add_action( "concentrated_flame,line_cd=6,if=buff.rune_of_power.down" );
       essences->add_action( "the_unbound_force,if=buff.reckless_force.up" );
-      essences->add_action( "worldvein_resonance" );
+      essences->add_action( "worldvein_resonance,if=buff.rune_of_power.down|active_enemies>3" );
       break;
     case ROTATION_FROZEN_ORB:
-      essences->add_action( "focused_azerite_beam,if=debuff.packed_ice.down|active_enemies>3" );
-      essences->add_action( "memory_of_lucid_dreams,if=debuff.packed_ice.down&cooldown.frozen_orb.remains>execute_time&!action.frozen_orb.in_flight&ground_aoe.frozen_orb.remains=0" );
-      essences->add_action( "blood_of_the_enemy,if=prev_gcd.1.rune_of_power&prev_gcd.2.frozen_orb|active_enemies>3" );
-      essences->add_action( "purifying_blast,if=debuff.packed_ice.down|active_enemies>3" );
-      essences->add_action( "ripple_in_space,if=debuff.packed_ice.down|active_enemies>3" );
-      essences->add_action( "concentrated_flame,if=debuff.packed_ice.down,line_cd=6" );
+      essences->add_action( "focused_azerite_beam,if=buff.rune_of_power.down&debuff.packed_ice.down|active_enemies>3" );
+      essences->add_action( "memory_of_lucid_dreams,if=active_enemies<5&debuff.packed_ice.down&cooldown.frozen_orb.remains>5"
+        "&!action.frozen_orb.in_flight&ground_aoe.frozen_orb.remains=0" );
+      essences->add_action( "blood_of_the_enemy,if=prev_gcd.1.frozen_orb|ground_aoe.frozen_orb.remains>5" );
+      essences->add_action( "purifying_blast,if=buff.rune_of_power.down&debuff.packed_ice.down|active_enemies>3" );
+      essences->add_action( "ripple_in_space,if=buff.rune_of_power.down&debuff.packed_ice.down|active_enemies>3" );
+      essences->add_action( "concentrated_flame,line_cd=6,if=buff.rune_of_power.down&debuff.packed_ice.down" );
       essences->add_action( "the_unbound_force,if=buff.reckless_force.up" );
-      essences->add_action( "worldvein_resonance,if=debuff.packed_ice.down|active_enemies>4" );
+      essences->add_action( "worldvein_resonance,if=buff.rune_of_power.down&debuff.packed_ice.down|active_enemies>3" );
       break;
     default:
       break;
