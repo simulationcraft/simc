7fa099167d6d764fe7fa3ff24ef7d804b21fb784
gastank <42421688+gastank@users.noreply.github.com>
2019-07-09 19:54:51 -0700

    Potion of Focused Resolve hotfix, grows by 1% per stack Potion of Focused Resolve changed to increase critical chane by 1% per stack, instead of 4% per stack. Stack still grows at 1/s. Also debuff no longer lingers after the 25s duration and drops from your target immediately.

--- engine/player/sc_unique_gear_x7.cpp
+++ engine/player/sc_unique_gear_x7.cpp
@@ -466,8 +466,6 @@ void consumables::potion_of_focused_resolve( special_effect_t& effect )
         auto td = listener->get_target_data( current_target );
         auto remaining_time = td->debuff.focused_resolve->remains();
         td->debuff.focused_resolve->expire();
-        // Leave a single stack debuff on the target for 2
-        td->debuff.focused_resolve->trigger( 1, buff_t::DEFAULT_VALUE(), -1.0, remaining_time );
       }
     }
 
