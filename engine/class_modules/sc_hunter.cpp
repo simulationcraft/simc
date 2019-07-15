908b31de071d343d58929be86c7b6f6a71586dc8
Jayezi <4423285+Jayezi@users.noreply.github.com>
2019-07-15 04:01:22 -0500

    [Hunter] Temporary dungeonslice precast-channel crash bandaid

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -5361,7 +5361,7 @@ void hunter_t::apl_bm()
   precombat -> add_action( "worldvein_resonance" );
   precombat -> add_action( "guardian_of_azeroth" );
   precombat -> add_action( "memory_of_lucid_dreams" );
-  precombat -> add_action( "focused_azerite_beam" );
+  precombat -> add_action( "focused_azerite_beam,if=!raid_event.invulnerable.exists" );
   precombat -> add_action( this, "Aspect of the Wild", "precast_time=1.1,if=!azerite.primal_instincts.enabled",
           "Adjusts the duration and cooldown of Aspect of the Wild and Primal Instincts by the duration of an unhasted GCD when they're used precombat. As AotW has a 1.3s GCD and affects itself this is 1.1s." );
   precombat -> add_action( this, "Bestial Wrath", "precast_time=1.5,if=azerite.primal_instincts.enabled&(!equipped.pocketsized_computation_device|!cooldown.cyclotronic_blast.duration)",
