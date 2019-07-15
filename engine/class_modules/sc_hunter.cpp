dc8c3dc2659f6efd5f1e432f630dd18156dc0b0f
Thyminde <51829317+Thyminde@users.noreply.github.com>
2019-07-15 02:31:28 +0200

    [SV] Razor coral trinket optimization

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -5549,12 +5549,12 @@ void hunter_t::apl_surv()
   default_list -> add_action( "arcane_torrent" );
 
   for ( std::string racial : { "blood_fury", "ancestral_call", "fireblood" } )
-    cds -> add_action( racial + ",if=cooldown.coordinated_assault.remains>30" );
+  cds -> add_action( racial + ",if=cooldown.coordinated_assault.remains>30" );
   cds -> add_action( "lights_judgment" );
   cds -> add_action( "berserking,if=cooldown.coordinated_assault.remains>60|time_to_die<13");
-
   cds -> add_action( "potion,if=buff.coordinated_assault.up&(buff.berserking.up|buff.blood_fury.up|!race.troll&!race.orc)|time_to_die<26" );
   cds -> add_action( this, "Aspect of the Eagle", "if=target.distance>=6" );
+  cds -> add_action( "use_item,name=ashvanes_razor_coral,if=buff.memory_of_lucid_dreams.up|buff.guardian_of_azeroth.up|debuff.razor_coral_debuff.down|target.time_to_die<20" );
 	
   // Essences
   cds->add_action( "focused_azerite_beam" );
