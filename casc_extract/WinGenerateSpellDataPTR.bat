60efcfe4fc34f748a8c7ca9795f2d2ea81ccd55b
gastank <42421688+gastank@users.noreply.github.com>
2019-08-02 19:16:09 -0700

    Output WinGenerateSpellDataPTR.bat to separate wow_ptr folder Prevent passing incorrect build directory to generate.bat, since windows directory timestamps are not updated when files are updated in subdirectories.

--- casc_extract/WinGenerateSpellDataPTR.bat
+++ casc_extract/WinGenerateSpellDataPTR.bat
@@ -1,5 +1,5 @@
-call py -3 casc_extract.py -m batch --cdn --ptr -o wow
-cd wow
+call py -3 casc_extract.py -m batch --cdn --ptr -o wow_ptr
+cd wow_ptr
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
 del hi.txt
@@ -7,5 +7,5 @@ cd ..
 cd ..
 set curr=%cd%
 cd dbc_extract3
-call generate.bat ptr %wowdir% %curr%\casc_extract\wow
+call generate.bat ptr %wowdir% %curr%\casc_extract\wow_ptr
 pause
