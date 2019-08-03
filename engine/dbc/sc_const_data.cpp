1bd13238c3da1765f7d484491f5e21a1172afa55
gastank <42421688+gastank@users.noreply.github.com>
2019-08-02 18:07:11 -0700

    Spell data update Aug 2 hotfix (VoP R3 minor fix) Also update live build #

--- engine/dbc/sc_const_data.cpp
+++ engine/dbc/sc_const_data.cpp
@@ -221,7 +221,7 @@ const std::vector<class_passives_entry_t> _class_passives {
 
 int dbc::build_level( bool ptr )
 {
-  return maybe_ptr( ptr ) ? 31337 : 30918;
+  return maybe_ptr( ptr ) ? 31337 : 31229;
 }
 
 const char* dbc::wow_version( bool ptr )
