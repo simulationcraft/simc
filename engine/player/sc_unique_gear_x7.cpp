9ef2ebe25e3fecb29c427a85b13163147a1486ab
Navv <navv1234@gmail.com>
2019-08-05 21:53:13 +0300

    Make Bile-Stained Crawg Tusks use a single dot

--- engine/player/sc_unique_gear_x7.cpp
+++ engine/player/sc_unique_gear_x7.cpp
@@ -188,6 +188,7 @@ namespace items
   void remote_guidance_device( special_effect_t& );
   void gladiators_maledict( special_effect_t& );
   void getiikku_cut_of_death( special_effect_t& );
+  void bilestained_crawg_tusks( special_effect_t& );
   // 8.2.0 - Rise of Azshara Punchcards
   void yellow_punchcard( special_effect_t& );
   void subroutine_overclock( special_effect_t& );
@@ -4675,6 +4676,18 @@ void items::getiikku_cut_of_death( special_effect_t& effect )
   new dbc_proc_callback_t( effect.player, effect );
 }
 
+// Bile-Stained Crawg Tusks ===============================================
+
+void items::bilestained_crawg_tusks( special_effect_t& effect )
+{
+  // Note, no create_proc_action here, since there is the possibility of dual-wielding them and
+  // special_effect_t does not have enough support for defining "shared spells" on initialization
+  effect.execute_action = new proc_spell_t( "vile_bile", effect.player,
+    effect.player->find_spell( 281721 ), effect.item );
+
+  new dbc_proc_callback_t( effect.player, effect );
+}
+
 // Punchcard stuff ========================================================
 
 item_t init_punchcard( const special_effect_t& effect )
@@ -5639,6 +5652,7 @@ void unique_gear::register_special_effects_bfa()
   register_special_effect( 302307, items::remote_guidance_device );
   register_special_effect( 305252, items::gladiators_maledict );
   register_special_effect( 281712, items::getiikku_cut_of_death );
+  register_special_effect( 281720, items::bilestained_crawg_tusks );
   // 8.2 Mechagon combo rings
   register_special_effect( 300124, items::logic_loop_of_division );
   register_special_effect( 300125, items::logic_loop_of_recursion );
