5b4cbf571d3d11585d669367d66278b780708ba7
Kendrick Walls <43417173+kendrick-walls-work@users.noreply.github.com>
2019-09-11 14:34:03 -0700

    #4949 - DEATH KNIGHT: updated T24 feast

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -7534,8 +7534,8 @@ std::string death_knight_t::default_food() const
                             ( true_level >= 80 ) ? "seafood_magnifique_feast" :
                             "disabled";
 
-  std::string blood_food =  ( true_level > 110 ) ? "bountiful_captains_feast" :
-	                          ( true_level > 100 ) ? "lavish_suramar_feast" :
+  std::string blood_food =  ( true_level > 110 ) ? "famine_evaluator_and_snack_table" :
+	                    ( true_level > 100 ) ? "lavish_suramar_feast" :
                             ( true_level >  90 ) ? "pickled_eel" :
                             ( true_level >= 85 ) ? "sea_mist_rice_noodles" :
                             ( true_level >= 80 ) ? "seafood_magnifique_feast" :
