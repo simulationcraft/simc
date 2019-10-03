b33e7f3b64972d3959588961c4130445088dd339
Dorovon <50294688+Dorovon@users.noreply.github.com>
2019-10-03 16:11:24 -0600

    [Mage] Arcane APL double on use support

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -5591,6 +5591,7 @@ void mage_t::apl_precombat()
       precombat->add_talent( this, "Arcane Familiar" );
       precombat->add_action( "variable,name=conserve_mana,op=set,value=60+20*azerite.equipoise.enabled",
         "conserve_mana is the mana percentage we want to go down to during conserve. It needs to leave enough room to worst case scenario spam AB only during AP." );
+      precombat->add_action( "variable,name=font_double_on_use,op=set,value=equipped.azsharas_font_of_power&(equipped.gladiators_badge|equipped.gladiators_medallion|equipped.ignition_mages_fuse|equipped.tzanes_barkspines|equipped.azurethos_singed_plumage|equipped.ancient_knot_of_wisdom|equipped.shockbiters_fang|equipped.neural_synapse_enhancer|equipped.balefire_branch)" );
       break;
     case MAGE_FIRE:
       precombat->add_action( "variable,name=combustion_rop_cutoff,op=set,value=60",
@@ -5719,7 +5720,7 @@ void mage_t::apl_arcane()
 
   default_list->add_action( this, "Counterspell" );
   default_list->add_action( "call_action_list,name=essences" );
-  default_list->add_action( "use_item,name=azsharas_font_of_power,if=buff.rune_of_power.down&buff.arcane_power.down&(cooldown.arcane_power.remains<=5&cooldown.evocation.remains<=variable.average_burn_length+5|time_to_die<cooldown.arcane_power.remains)" );
+  default_list->add_action( "use_item,name=azsharas_font_of_power,if=buff.rune_of_power.down&buff.arcane_power.down&(cooldown.arcane_power.remains<=4+10*variable.font_double_on_use&cooldown.evocation.remains<=variable.average_burn_length+4+10*variable.font_double_on_use|time_to_die<cooldown.arcane_power.remains)" );
   default_list->add_action( "call_action_list,name=burn,if=burn_phase|target.time_to_die<variable.average_burn_length", "Go to Burn Phase when already burning, or when boss will die soon." );
   default_list->add_action( "call_action_list,name=burn,if=(cooldown.arcane_power.remains=0&cooldown.evocation.remains<=variable.average_burn_length&(buff.arcane_charge.stack=buff.arcane_charge.max_stack|(talent.charged_up.enabled&cooldown.charged_up.remains=0&buff.arcane_charge.stack<=1)))", "Start Burn Phase when Arcane Power is ready and Evocation will be ready (on average) before the burn phase is over. Also make sure we got 4 Arcane Charges, or can get 4 Arcane Charges with Charged Up." );
   default_list->add_action( "call_action_list,name=conserve,if=!burn_phase" );
