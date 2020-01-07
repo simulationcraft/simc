f7faef3e0562d03eaec8c7a6b6d0f72a7d1e1766
Dorovon <50294688+Dorovon@users.noreply.github.com>
2020-01-07 15:09:42 -0700

    Add bonus ids with just item effects to show_bonus_ids=1

--- engine/dbc/sc_item_data.cpp
+++ engine/dbc/sc_item_data.cpp
@@ -1659,7 +1659,8 @@ std::string dbc::bonus_ids_str( dbc_t& dbc)
     if ( e -> type != ITEM_BONUS_ILEVEL && e -> type != ITEM_BONUS_MOD &&
          e -> type != ITEM_BONUS_SOCKET && e -> type != ITEM_BONUS_SCALING &&
          e -> type != ITEM_BONUS_SCALING_2 && e -> type != ITEM_BONUS_SET_ILEVEL &&
-         e -> type != ITEM_BONUS_ADD_RANK && e -> type != ITEM_BONUS_QUALITY )
+         e -> type != ITEM_BONUS_ADD_RANK && e -> type != ITEM_BONUS_QUALITY &&
+         e -> type != ITEM_BONUS_ADD_ITEM_EFFECT )
     {
       e++;
       continue;
