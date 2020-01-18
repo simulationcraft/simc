a976999c46dcc831206f604c96875b91a49c04c2
Dorovon <50294688+Dorovon@users.noreply.github.com>
2020-01-18 03:52:51 -0700

    [Gear] Keep Current Echoing Void Proc Flags

--- engine/player/sc_unique_gear_x7.cpp
+++ engine/player/sc_unique_gear_x7.cpp
@@ -6535,6 +6535,7 @@ void corruption::echoing_void( special_effect_t& effect )
 
     effect.spell_id    = 317014;
     effect.custom_buff = buff;
+    effect.proc_flags_ = PF_MELEE_ABILITY | PF_RANGED_ABILITY | PF_NONE_SPELL | PF_MAGIC_SPELL;
 
     new echoing_void_cb_t( effect, echoing_void_damage );
   }
