f1079a022993c47769275057a4b30c83f852a49f
Joscha Rentz <rentz@hotmail.de>
2020-01-20 15:20:15 +0100

    resto shaman APL updates

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -8102,7 +8102,19 @@ std::string shaman_t::default_food() const
                                                    ? "sea_mist_rice_noodles"
                                                    : ( true_level >= 80 ) ? "seafood_magnifique_feast" : "disabled";
 
-  return specialization() == SHAMAN_ENHANCEMENT ? enhance_food : elemental_food;
+  std::string restoration_food = ( true_level > 110 )
+                                     ? "baked_port_tato"
+                                     : ( true_level > 100 )
+                                           ? "lemon_herb_filet"
+                                           : ( true_level > 90 )
+                                                 ? "pickled_eel"
+                                                 : ( true_level >= 90 )
+                                                       ? "mogu_fish_stew"
+                                                       : ( true_level >= 80 ) ? "seafood_magnifique_feast" : "disabled";
+
+  return specialization() == SHAMAN_ENHANCEMENT
+             ? enhance_food
+             : specialization() == SHAMAN_ELEMENTAL ? elemental_food : restoration_food;
 }
 
 // shaman_t::default_rune ===================================================
@@ -8806,6 +8818,7 @@ void shaman_t::init_action_list_restoration_dps()
   precombat->add_action( "snapshot_stats", "Snapshot raid buffed stats before combat begins and pre-potting is done." );
   // Actual precombat
   precombat->add_action( "potion" );
+  precombat->add_action( "use_item,name=azsharas_font_of_power" );
   precombat->add_action( this, "Lava Burst" );
 
   // In-combat potion
@@ -8815,22 +8828,20 @@ void shaman_t::init_action_list_restoration_dps()
   def->add_action( this, "Wind Shear" );
   def->add_action( this, "Spiritwalker's Grace", "moving=1,if=movement.distance>6" );
   // On-use items
+  def->add_action( this, "Flame Shock", "target_if=(!ticking|dot.flame_shock.remains<=gcd)|refreshable" );
   def->add_action( "use_items" );
-  // Racials
   def->add_action( "blood_fury" );
   def->add_action( "berserking" );
   def->add_action( "fireblood" );
   def->add_action( "ancestral_call" );
-  // Azerite Essences
-  def->add_action( "concentrated_flame" );
-  def->add_action( "ripple_in_space" );
   def->add_action( "worldvein_resonance" );
-
-  def->add_action( this, "Flame Shock", "target_if=(!ticking|dot.flame_shock.remains<=gcd)|refreshable" );
   def->add_action( this, "Lava Burst", "if=dot.flame_shock.remains>cast_time&cooldown_react" );
+  def->add_action( "concentrated_flame,if=dot.concentrated_flame_burn.remains=0" );
+  def->add_action( "ripple_in_space" );
   def->add_action( this, "Earth Elemental" );
+  def->add_action( "bag_of_tricks" );
   def->add_action( this, "Lightning Bolt", "if=spell_targets.chain_lightning<2" );
-  def->add_action( this, "Chain Lightning", "if=active_enemies>1&spell_targets.chain_lightning>1" );
+  def->add_action( this, "Chain Lightning", "if=spell_targets.chain_lightning>1" );
   def->add_action( this, "Flame Shock", "moving=1" );
 }
 
