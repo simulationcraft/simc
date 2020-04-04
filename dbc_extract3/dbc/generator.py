1bd2a63fdf191c36dcdd895e34a1c4bb69e8bc5a
EvanMichaels <evan.michaels@gmail.com>
2020-04-04 03:42:08 -0400

    [Subtlety] Whitelist Shot in the Dark (Talent) Buff

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1433,6 +1433,7 @@ class SpellDataGenerator(DataGenerator):
             ( 278962, 0 ),          # Paradise Lost, Azerite trait buff
             ( 286131, 0 ),          # Replicating Shadows, Azerite trait dmg effect
             ( 193315, 0 ),          # Sinister Strike extra attack damage spell
+            ( 257506, 0 ),          # Shot in the Dark buff for free Cheap Shot casts
         ),
 
         # Priest:
