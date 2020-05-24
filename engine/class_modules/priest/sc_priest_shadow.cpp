14b8a34cd08415cc16e1f3d7d00bee774845cb67
seanpeters86 <sean.peters86@icloud.com>
2020-05-24 11:12:42 -0500

    [Priest] adjust lucid timing to be uncapped

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -2036,7 +2036,6 @@ void priest_t::generate_apl_shadow()
 
   // CDs
   cds->add_action( "memory_of_lucid_dreams,if=(buff.voidform.stack>20&insanity<=50)|"
-                   "buff.voidform.stack>(26+7*buff.bloodlust.up)|"
                    "(current_insanity_drain*((gcd.max*2)+action.mind_blast.cast_time))>insanity",
                    "Use Memory of Lucid Dreams right before you are about to fall out of Voidform" );
   cds->add_action( "blood_of_the_enemy" );
