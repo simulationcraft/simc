65568215a5512bf3baadfb6c4df21e0c33af2898
Navv <navv1234@gmail.com>
2020-06-01 10:38:17 +0300

    [DBC] Fix unnecessary crash if things go wrong in string parsing
    [ci skip]

--- dbc_extract3/dbc/data.py
+++ dbc_extract3/dbc/data.py
@@ -391,8 +391,8 @@ class DBCRecord(RawDBCRecord):
             if type_ == 'S' and self._d[i] > 0:
                 str_ = self._dbcp.get_string(self._d[i], self._id, i)
                 s.append('%s=%s%s' % (field,
-                    len(str_) and repr(str_) or '0',
-                    len(str_) and ' ({})'.format(self._d[i]) or ''
+                    (str_ and len(str_)) and repr(str_) or '0',
+                    (str_ and len(str_)) and ' ({})'.format(self._d[i]) or ''
                 ))
             elif type_ == 'f':
                 s.append('%s=%f' % (field, self._d[i]))
