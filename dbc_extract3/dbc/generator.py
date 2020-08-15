e72fe8d47356fc14d999d9a88c717e7131d21e78
EvanMichaels <evan.michaels@gmail.com>
2020-08-15 17:46:32 -0400

    [Rogue] Whitelist Echoing Reprimand buffs

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1543,6 +1543,7 @@ class SpellDataGenerator(DataGenerator):
             ( 341541, 0 ),          # Sinister Strike third attack from Triple Threat conduit
             ( 328306, 0 ),          # Sepsis expiry direct damage hit
             ( 323660, 0 ),          # Slaughter instant damage
+            ( 323558, 0 ), ( 323559, 0 ), ( 323560, 0 ), # Echoing Reprimand buffs
         ),
 
         # Priest:
