59d96c4653985e51643f46b090f97383dd1d3e32
Dorovon <50294688+Dorovon@users.noreply.github.com>
2020-08-24 15:19:38 -0600

    Update reduced aoe damage calculation
    Spells that deal reduced damage to secondary targets do not have their damage reduced when they hit more than 20 targets.

--- engine/action/sc_action.cpp
+++ engine/action/sc_action.cpp
@@ -1231,8 +1231,8 @@ double action_t::calculate_direct_amount( action_state_t* state ) const
   amount *= composite_aoe_multiplier( state );
 
   // Spell goes over the maximum number of AOE targets - ignore for enemies
-  if ( !state->action->split_aoe_damage && state->n_targets > static_cast<size_t>( sim->max_aoe_enemies ) &&
-       !state->action->player->is_enemy() )
+  if ( !state->action->split_aoe_damage && !state->action->reduced_aoe_damage &&
+       state->n_targets > static_cast<size_t>( sim->max_aoe_enemies ) && !state->action->player->is_enemy() )
     amount *= sim->max_aoe_enemies / static_cast<double>( state->n_targets );
 
   // Record initial amount to state
