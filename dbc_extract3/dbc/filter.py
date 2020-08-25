c2495c39690771cda44fdd03ab190441e6d56c75
Mikhail Korolev <nuoHep@gmail.com>
2020-08-25 13:24:21 +0300

    [DBC] refactor TalentDataGenerator

--- dbc_extract3/dbc/filter.py
+++ dbc_extract3/dbc/filter.py
@@ -276,3 +276,18 @@ class CovenantAbilitySet(DataSet):
             _covenant_abilities.append(entry)
 
         return _covenant_abilities
+
+class TalentSet(DataSet):
+    def _filter(self, **kwargs):
+        talents = list()
+
+        for entry in self.db('Talent').values():
+            if entry.id_spell == 0:
+                continue
+
+            if entry.ref('id_spell').id != entry.id_spell:
+                continue
+
+            talents.append(entry)
+
+        return talents
