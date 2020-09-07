simulationcraft/simc

commit ca54e382d1cefe5e5ce79675144ea67b4d18c430

[Hunter] Update Lucid Dreams Proc Rate
* Based on long-duration log testing, the rate for Lucid minor appears to be 15% now, consistent with most other DPS specs

engine/class_modules/sc_hunter.cpp
@@ -535,7 +535,7 @@ public:
     std::string summon_pet_str = "turtle";
     timespan_t pet_attack_speed = 2.0_s;
     timespan_t pet_basic_attack_delay = 0.15_s;
-    double memory_of_lucid_dreams_proc_chance = 0.1;
+    double memory_of_lucid_dreams_proc_chance = 0.15;
   } options;
 
   hunter_t( sim_t* sim, const std::string& name, race_e r = RACE_NONE ) :
@@ -2394,8 +2394,7 @@ struct barbed_shot_t: public hunter_ranged_attack_t
 
   void init_finished() override
   {
-    pet_t* pets[] = { p() -> pets.main, p() -> pets.animal_companion };
-    for ( auto pet : pets )
+    for ( auto pet : p()->pet_list )
       add_pet_stats( pet, { "stomp" } );
 
     hunter_ranged_attack_t::init_finished();

commit 6fab4f4a0d030f3b0ed2781f16b9d2a9d79f0685

[Hunter] fix up Dire Beast & Stomp assert
We want to parent Stomp to Barbed Shot only for the main pet & Animal
Companion. Stomp performed by Dire Beast is not linked to Barbed Shot in
any way. Restore the old code to do it properly.

Fixes: ca54e382 "[Hunter] Update Lucid Dreams Proc Rate"
Closes: #5239

engine/class_modules/sc_hunter.cpp
@@ -3162,7 +3162,8 @@ struct barbed_shot_t: public hunter_ranged_attack_t
 
   void init_finished() override
   {
-    for ( auto pet : p()->pet_list )
+    pet_t* pets[] = { p() -> pets.main, p() -> pets.animal_companion };
+    for ( auto pet : pets )
       add_pet_stats( pet, { "stomp" } );
 
     hunter_ranged_attack_t::init_finished();