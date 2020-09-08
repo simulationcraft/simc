7cfa29137100abe720fb72b8eaae59f21e70436d
gastank <42421688+gastank@users.noreply.github.com>
2020-09-07 21:07:14 -0700

    Shadowlands enchants/consumables initial framework

--- engine/player/unique_gear_shadowlands.cpp
+++ engine/player/unique_gear_shadowlands.cpp
@@ -7,17 +7,79 @@
 
 #include "sim/sc_sim.hpp"
 #include "unique_gear.hpp"
+#include "unique_gear_helper.hpp"
 
 namespace unique_gear
 {
 namespace shadowlands
 {
+namespace consumables
+{
+void smothered_shank( special_effect_t& effect )
+{
+
+}
+
+void feast_of_gluttonous_hedonism( special_effect_t& effect )
+{
+
+}
+
+void potion_of_deadly_fixation( special_effect_t& effect )
+{
+
+}
+
+void potion_of_empowered_exorcism( special_effect_t& effect )
+{
+
+}
+
+void potion_of_phantom_fire( special_effect_t& effect )
+{
+
+}
+
+void embalmers_oil( special_effect_t& effect )
+{
+
+}
+
+void shadowcore_oil( special_effect_t& effect )
+{
+
+}
+}  // namespace consumables
+
+namespace enchants
+{
+void celestial_guidance( special_effect_t& effect )
+{
+
+}
+
+void lightless_force( special_effect_t& effect )
+{
+
+}
+
+void sinful_revelation( special_effect_t& effect )
+{
+
+}
+}  // namespace enchants
+
 void register_hotfixes()
 {
 }
+
 void register_special_effects()
 {
+    unique_gear::register_special_effect( 324747, enchants::celestial_guidance );
+    unique_gear::register_special_effect( 323932, enchants::lightless_force );
+    unique_gear::register_special_effect( 324250, enchants::sinful_revelation );
 }
+
 void register_target_data_initializers( sim_t& )
 {
 }
--- engine/player/unique_gear_shadowlands.hpp
+++ engine/player/unique_gear_shadowlands.hpp
@@ -6,11 +6,30 @@
 #pragma once
 
 struct sim_t;
+struct special_effect_t;
 
 namespace unique_gear
 {
 namespace shadowlands
 {
+namespace consumables
+{
+void smothered_shank( special_effect_t& );
+void feast_of_gluttonous_hedonism( special_effect_t& );
+void potion_of_deadly_fixation( special_effect_t& );
+void potion_of_empowered_exorcism( special_effect_t& );
+void potion_of_phantom_fire( special_effect_t& );
+void embalmers_oil( special_effect_t& );
+void shadowcore_oil( special_effect_t& );
+}
+
+namespace enchants
+{
+void celestial_guidance( special_effect_t& );
+void lightless_force( special_effect_t& );
+void sinful_revelation( special_effect_t& );
+}
+
 void register_hotfixes();
 void register_special_effects();
 void register_target_data_initializers( sim_t& );
