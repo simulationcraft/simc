1292cf984a788a57e72e678fc2667ea34a5c4cfc
gastank <42421688+gastank@users.noreply.github.com>
2020-09-10 22:27:40 -0700

    [Consumable] feast of gluttonous hedonism initial implementation

--- engine/player/unique_gear_shadowlands.cpp
+++ engine/player/unique_gear_shadowlands.cpp
@@ -59,6 +59,25 @@ struct SL_proc_spell_t : public proc_spell_t
   }
 };
 
+// feasts initialization helper
+void init_feast( special_effect_t& effect, std::initializer_list<std::pair<stat_e, int>> stat_map )
+{
+  effect.stat = effect.player->convert_hybrid_stat( STAT_STR_AGI_INT );
+  // TODO: Is this actually spec specific?
+  if ( effect.player->role == ROLE_TANK && !effect.player->sim->feast_as_dps )
+    effect.stat = STAT_STAMINA;
+
+  for ( auto&& stat : stat_map )
+  {
+    if ( stat.first == effect.stat )
+    {
+      effect.trigger_spell_id = stat.second;
+      break;
+    }
+  }
+  effect.stat_amount = effect.player->find_spell( effect.trigger_spell_id )->effectN( 1 ).average( effect.player );
+}
+
 namespace consumables
 {
 void smothered_shank( special_effect_t& effect )
@@ -84,7 +103,8 @@ void smothered_shank( special_effect_t& effect )
 
 void feast_of_gluttonous_hedonism( special_effect_t& effect )
 {
-
+  init_feast( effect,
+              {{STAT_STRENGTH, 327707}, {STAT_STAMINA, 327707}, {STAT_INTELLECT, 327708}, {STAT_AGILITY, 327709}} );
 }
 
 void potion_of_deathly_fixation( special_effect_t& effect )
@@ -284,6 +304,7 @@ void register_special_effects()
 {
     // Food
     unique_gear::register_special_effect( 308637, consumables::smothered_shank );
+    unique_gear::register_special_effect( 308462, consumables::feast_of_gluttonous_hedonism );
 
     // Potion
     unique_gear::register_special_effect( 307497, consumables::potion_of_deathly_fixation );
