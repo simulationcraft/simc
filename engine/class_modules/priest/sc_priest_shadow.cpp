de379f3e86a66fb7ed65fcbb451cf3ebe42ebbf7
Publik <sean.peters86@icloud.com>
2020-09-25 12:10:29 -0500

    [Priest] should roll over partials

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -1031,16 +1031,16 @@ struct devouring_plague_t final : public priest_spell_t
       double old_multiplier   = cast_state( old_s )->rolling_multiplier;
 
       // figure out how many old ticks to roll over
-      int num_full_ticks = as<int>( std::floor( ( old_remains - time_to_tick ) / old_tick ) );
+      double num_ticks = std::floor( ( old_remains - time_to_tick ) / old_tick );
 
       // find number of ticks in new DP
       double new_num_ticks = new_remains / new_tick;
 
-      sim->print_debug( "{} {} calculations - num_full_ticks: {}, new_num_ticks: {}", *player, *this, num_full_ticks,
+      sim->print_debug( "{} {} calculations - num_ticks: {}, new_num_ticks: {}", *player, *this, num_ticks,
                         new_num_ticks );
 
       // figure out the increase for each new tick of DP
-      double total_coefficient     = num_full_ticks * old_multiplier;
+      double total_coefficient     = num_ticks * old_multiplier;
       double increase_per_new_tick = total_coefficient / new_num_ticks;
 
       multiplier = 1 + increase_per_new_tick;
