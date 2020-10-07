d8ec99e7d6e8c551b9102e291b820a4cd7a3a88a
Serge Camille <sergecamille@gmail.com>
2020-10-07 21:15:19 +0200

    Change simc-support spec lookup functions and restrict fight-style
    - Use only DungeonSlice fight style for trinkets.
    - Let the updated simc-support library handle looking up spec for the way we format Class_Spec string.
    - throw if no SIMC_CLI_PATH is set.
    - print info when no profile can be found

--- tests/run.py
+++ tests/run.py
@@ -14,6 +14,8 @@ parser.add_argument('specialization', metavar='spec', type=str,
                     help='Simc specialization in the form of CLASS_SPEC, eg. Priest_Shadow')
 parser.add_argument('--no-trinkets', default=False, action='store_true',
                     help='Disable trinket tests. Trinket tests require simc-support library to be installed.')
+parser.add_argument('--trinkets-fight-style', default='DungeonSlice', type=str,
+                    help='Fight style used for trinket simulations.')
 args = parser.parse_args()
 
 klass = args.specialization
@@ -38,17 +40,19 @@ for profile, path in find_profiles(klass):
                  args=[('covenant', covenant.lower()), ('level', 60)])
 
     if not args.no_trinkets:
-        from simc_support.game_data.WowSpec import get_wow_spec
+        from simc_support.game_data.WowSpec import get_wow_spec_from_combined_simc_name
         from simc_support.game_data.Trinket import get_trinkets_for_spec
 
-        wow_class, wow_spec = klass.split('_')
-        trinkets = get_trinkets_for_spec(get_wow_spec(wow_class, wow_spec))
-        for fight_style in FIGHT_STYLES:
-            grp = TestGroup('{}/{}/trinkets'.format(profile, fight_style),
-                            fight_style=fight_style, profile=path)
-            tests.append(grp)
-            for trinket in trinkets:
-                Test('{} ({})'.format(trinket.name, trinket.item_id), group=grp, args=[
-                     ('trinket1', '{},id={},ilevel={}'.format(trinket.name, trinket.item_id, trinket.min_itemlevel))])
+        trinkets = get_trinkets_for_spec(
+            get_wow_spec_from_combined_simc_name(klass))
+        fight_style = args.trinkets_fight_style
+        grp = TestGroup('{}/{}/trinkets'.format(profile, fight_style),
+                        fight_style=fight_style, profile=path)
+        tests.append(grp)
+        for trinket in trinkets:
+            Test('{} ({})'.format(trinket.name, trinket.item_id), group=grp, args=[
+                    ('trinket1', '{},id={},ilevel={}'.format(trinket.name, trinket.item_id, trinket.min_itemlevel))])
+else:
+    print("No profile found for {}".format(klass))
 
 sys.exit(run(tests))
