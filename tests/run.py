09644c38e677a6a1b74e454fc3adf55acd83fb3d
Serge Camille <sergecamille@gmail.com>
2020-10-08 15:47:58 +0200

    Trinket tests opt-in, and skip when dependency can not be found.
    Change to --test-trinkets argument to make the test opt-in (and use it in our CI).
    On ImportError for simc-support, skip the trinket test with a info text.

--- tests/run.py
+++ tests/run.py
@@ -12,11 +12,31 @@ COVENANTS = ('Kyrian', 'Venthyr', 'Night_Fae', 'Necrolord')
 parser = argparse.ArgumentParser(description='Run simc tests')
 parser.add_argument('specialization', metavar='spec', type=str,
                     help='Simc specialization in the form of CLASS_SPEC, eg. Priest_Shadow')
-parser.add_argument('--no-trinkets', default=False, action='store_true',
-                    help='Disable trinket tests. Trinket tests require simc-support library to be installed.')
+parser.add_argument('--test-trinkets', default=False, action='store_true',
+                    help='Test trinkets. Trinket tests require simc-support library to be installed.')
 parser.add_argument('--trinkets-fight-style', default='DungeonSlice', type=str,
                     help='Fight style used for trinket simulations.')
 args = parser.parse_args()
+
+def test_trinkets(klass : str, path : str):
+    if not args.test_trinkets:
+        return
+    try:
+        from simc_support.game_data.WowSpec import get_wow_spec_from_combined_simc_name
+        from simc_support.game_data.Trinket import get_trinkets_for_spec
+    except ImportError:
+        print("Error importing simc-support library for trinket test. Skipping test.")
+        return
+
+    trinkets = get_trinkets_for_spec(
+        get_wow_spec_from_combined_simc_name(klass))
+    fight_style = args.trinkets_fight_style
+    grp = TestGroup('{}/{}/trinkets'.format(profile, fight_style),
+                    fight_style=fight_style, profile=path)
+    tests.append(grp)
+    for trinket in trinkets:
+        Test('{} ({})'.format(trinket.name, trinket.item_id), group=grp, args=[
+                ('trinket1', '{},id={},ilevel={}'.format(trinket.name, trinket.item_id, trinket.min_itemlevel))])
 
 klass = args.specialization
 
@@ -43,18 +63,7 @@ for profile, path in profiles:
             Test(covenant, group=grp,
                  args=[('covenant', covenant.lower()), ('level', 60)])
 
-    if not args.no_trinkets:
-        from simc_support.game_data.WowSpec import get_wow_spec_from_combined_simc_name
-        from simc_support.game_data.Trinket import get_trinkets_for_spec
+    test_trinkets(klass, path)
 
-        trinkets = get_trinkets_for_spec(
-            get_wow_spec_from_combined_simc_name(klass))
-        fight_style = args.trinkets_fight_style
-        grp = TestGroup('{}/{}/trinkets'.format(profile, fight_style),
-                        fight_style=fight_style, profile=path)
-        tests.append(grp)
-        for trinket in trinkets:
-            Test('{} ({})'.format(trinket.name, trinket.item_id), group=grp, args=[
-                    ('trinket1', '{},id={},ilevel={}'.format(trinket.name, trinket.item_id, trinket.min_itemlevel))])
 
 sys.exit(run(tests))
