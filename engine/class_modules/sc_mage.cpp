f5788f709b62b8946d3d4fa1cbbebff081921cca
Vít Šefl <vituscze@gmail.com>
2020-10-16 20:36:07 +0200

    [Mage] Implement SKB bug

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -2212,7 +2212,7 @@ struct hot_streak_spell_t : public fire_mage_spell_t
       p()->buffs.pyroclasm->trigger();
       p()->buffs.firemind->trigger();
 
-      trigger_legendary_buff( p()->buffs.sun_kings_blessing, p()->buffs.sun_kings_blessing_ready, 0 );
+      trigger_legendary_buff( p()->buffs.sun_kings_blessing, p()->buffs.sun_kings_blessing_ready, p()->bugs ? 0 : 1 );
 
       if ( rng().roll( p()->talents.pyromaniac->effectN( 1 ).percent() ) )
       {
