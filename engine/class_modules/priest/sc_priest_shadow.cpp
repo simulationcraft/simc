f36d40c590a9ec0841b124d9ad7ab00d0849751d
Publik <sean.peters86@icloud.com>
2020-10-17 12:55:08 -0500

    dont need to hard code speed

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -1550,7 +1550,6 @@ struct shadow_crash_t final : public priest_spell_t
     radius           = data().effectN( 1 ).radius();
     range            = data().max_range();
     cooldown->hasted = true;
-    travel_speed     = data().missile_speed();
 
     impact_action = new shadow_crash_damage_t( p );
     add_child( impact_action );
