02e48d495c3962a93b4b4ff71343fc53d501d164
Woliance <34073843+Woliance@users.noreply.github.com>
2020-10-25 12:05:15 +0100

    Dont do PRs while you just woke up
    You miss things

--- engine/class_modules/paladin/sc_paladin_protection.cpp
+++ engine/class_modules/paladin/sc_paladin_protection.cpp
@@ -935,7 +935,7 @@ void paladin_t::generate_action_prio_list_prot()
   
   def -> add_action( this, "Hammer of Wrath", "if=buff.avenging_wrath.up&target.health.pct>20&buff.avenging_wrath.remains<=floor(buff.avenging_wrath.remains%cooldown%attack_haste)*cooldown*attack_haste+gcd" ,"Tries to fit in an extra HoW into wings. It can attempt and fail if *just* below a haste breakpoint" );
 
-  def -> add_action( this, "Judgment", "cycle_targets=1,if=charges=2" );
+  def -> add_action( this, "Judgment", "cycle_targets=1,if=charges=2|!talent.crusadersjudgment.enabled" );
   def -> add_action( this, "Avenger's Shield", "if=debuff.vengeful_shock.down|!conduit.vengeful_shock.enabled" );
   def -> add_action( this, "Hammer of Wrath" );
   def -> add_action( this, "Avengers Shield" );
@@ -945,7 +945,7 @@ void paladin_t::generate_action_prio_list_prot()
   def -> add_action( this, "Divine Toll" );
   def -> add_talent( this, "Blessed Hammer", "strikes=2.4,if=charges=3" );
   def -> add_talent( this, "Ashen Hallow" );
-  def -> add_action( this, "Hammer of the Righteous", "if=charges=3" );
+  def -> add_action( this, "Hammer of the Righteous", "if=charges=2" );
   def -> add_action( this, "Word of Glory", "if=buff.vanquishers_hammer.up" );
   def -> add_action( this, "Blessed Hammer", "strikes=2.4" );
   def -> add_action( this, "Hammer of the Righteous" );
