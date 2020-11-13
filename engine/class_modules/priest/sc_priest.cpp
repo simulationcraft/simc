b96a504350c43a1c3bb13f0225df95baac005e72
Saeldur <5496498+Saeldur@users.noreply.github.com>
2020-11-13 20:43:58 +0000

    [Priest] Change mind_sear_cutoff to 2

--- engine/class_modules/priest/sc_priest.cpp
+++ engine/class_modules/priest/sc_priest.cpp
@@ -1971,7 +1971,7 @@ void priest_t::create_apl_precombat()
       if ( race == RACE_BLOOD_ELF )
         precombat->add_action( "arcane_torrent" );
       precombat->add_action( "use_item,name=azsharas_font_of_power" );
-      precombat->add_action( "variable,name=mind_sear_cutoff,op=set,value=1" );
+      precombat->add_action( "variable,name=mind_sear_cutoff,op=set,value=2" );
       precombat->add_action( this, "Vampiric Touch" );
       break;
   }
