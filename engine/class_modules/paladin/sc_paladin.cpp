90baccf165ed75b2fba108f639864bdee9535d23
OrcinusDrake <orcaone@gmail.com>
2020-11-16 09:24:11 +0800

    [Paladin] Move the aura assertion to init_finished

--- engine/class_modules/paladin/sc_paladin.cpp
+++ engine/class_modules/paladin/sc_paladin.cpp
@@ -596,10 +596,15 @@ struct paladin_aura_base_t : public paladin_spell_t
     aura_buff = nullptr;
   }
 
+  void init_finished() override
+  {
+    paladin_spell_t::init_finished();
+    assert( aura_buff != nullptr && "Paladin auras must have aura_buff set to their appropriate buff" );
+  }
+
   void execute() override
   {
     paladin_spell_t::execute();
-    assert( aura_buff != nullptr && "Paladin auras must have aura_buff set to their appropriate buff" );
     // If this aura is up, cancel it. Otherwise replace the current aura.
     if ( p() -> active_aura != nullptr )
     {
