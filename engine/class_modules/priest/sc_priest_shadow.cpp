5441c3f9d891f1a521cc25119b7b442b459cf3ee
Publik <sean.peters86@icloud.com>
2020-12-01 16:52:25 -0600

    fix order

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -1925,24 +1925,25 @@ void priest_t::generate_apl_shadow()
   default_list->add_run_action_list( main );
 
   // DMG Trinkets, specifically tied together with Hungering Void
-  dmg_trinkets->add_action( "use_item,name=dreadfire_vessel" );
-  dmg_trinkets->add_action( "use_item,name=glyph_of_assimilation" );
   dmg_trinkets->add_action( "use_item,name=darkmoon_deck__putrescence" );
   dmg_trinkets->add_action( "use_item,name=sunblood_amethyst" );
+  dmg_trinkets->add_action( "use_item,name=glyph_of_assimilation" );
+  dmg_trinkets->add_action( "use_item,name=dreadfire_vessel" );
 
   // Trinkets
   trinkets->add_action( "use_item,name=empyreal_ordnance,if=cooldown.void_eruption.remains<=12|buff.voidform.up",
                         "Use on CD ASAP to get DoT ticking and expire to line up better with Voidform" );
   trinkets->add_action( "use_item,name=inscrutable_quantum_device,if=buff.voidform.up", "Sync IQD with Voidform" );
   trinkets->add_action( "use_item,name=macabre_sheet_music,if=buff.voidform.up", "Sync Sheet Music with Voidform" );
-  trinkets->add_action(
-      "use_item,name=sinful_gladiators_badge_of_ferocity,if=buff.voidform.up|time>10&(!covenant.night_fae)",
-      "Use Badge inside of VF for the first use or on CD after the first use. With Night Fae hold for VF." );
   trinkets->add_action( "use_item,name=soulletting_ruby,if=buff.power_infusion.up,target_if=min:target.health.pct",
                         "Sync Ruby with Power Infusion usage, make sure to snipe the lowest HP target" );
+  trinkets->add_action(
+      "use_item,name=sinful_gladiators_badge_of_ferocity,if=buff.voidform.up|cooldown.void_eruption.remains>=10",
+      "Use Badge inside of VF for the first use or on CD after the first use. With Night Fae hold for VF." );
   trinkets->add_call_action_list(
       dmg_trinkets,
-      "if=(!talent.hungering_void.enabled|debuff.hungering_void.up)&(buff.voidform.up|buff.power_infusion.up)",
+      "if=(!talent.hungering_void.enabled|debuff.hungering_void.up)&(buff.voidform.up|buff.power_infusion.up|(cooldown."
+      "void_eruption.remains>20&cooldown.power_infusion.remains>20))",
       "Use list of on-use damage trinkets only if Hungering Void Debuff is active, or you are not talented into it." );
   trinkets->add_action( "use_items,if=buff.voidform.up|buff.power_infusion.up",
                         "Default fallback for usable items: Use on cooldown in order by trinket slot." );
