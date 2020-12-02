f6d9f561a213c5e85a7cda2252df752a6d694e90
khazakdk <joe.j.sommer@gmail.com>
2020-12-01 21:24:28 -0600

    [Item] Add clarifying comment to Spare Meat Hook

--- engine/player/unique_gear_shadowlands.hpp
+++ engine/player/unique_gear_shadowlands.hpp
@@ -47,7 +47,7 @@ void gluttonous_spike( special_effect_t& );
 void hateful_chain( special_effect_t& );
 void overflowing_anima_prison( special_effect_t& );
 void sunblood_amethyst( special_effect_t& );
-void spare_meat_hook( special_effect_t& );
+// void spare_meat_hook( special_effect_t& );  no further implementation needed as of 2020-12-1
 
 // Runecarves
 void echo_of_eonar( special_effect_t& );
