561341dfbe0b1e8dda4363d30a0c912915cbe33a
Azevara <Azevara.Duskbeam@gmail.com>
2020-12-03 18:28:18 -0500

    [Warlock] Wake Dreadstalkers back up if there's a proc for Dreadbite

--- engine/class_modules/warlock/sc_warlock_pets.cpp
+++ engine/class_modules/warlock/sc_warlock_pets.cpp
@@ -870,6 +870,8 @@ struct dreadstalker_melee_t : warlock_pet_melee_t
     {
       p()->dreadbite_executes++;
       p()->o()->procs.carnivorous_stalkers->occur();
+      event_t::cancel( p()->readying );
+      p()->schedule_ready();
     }
   }
 };
