simulationcraft/simc

commit bcd17f4f3cf31922e77aba0055efe4ac8f1d0009

Don't swap action targets, if the current target is still alive
In cases where target_if is used, the simulator should not retarget the
action unnecessarily, when a new "candidate target" can be selected.

engine/action/sc_action.cpp
@@ -4344,6 +4344,12 @@ void action_t::acquire_target( retarget_source /* event */, player_t* /* context
     target_cache.is_valid = false;
   }
 
+  // Don't swap targets if the action's current target is still alive
+  if ( !target->is_sleeping() )
+  {
+    return;
+  }
+
   // Don't change targets if they are not of the same generic type (both enemies, or both friendlies)
   if ( target && candidate_target && target->is_enemy() != candidate_target->is_enemy() )
   {

commit 749e373879d2984c281f685694b419a28bba292a

Fix targeting issue introduced in bcd17f4
Invulnerable targets are alive, but should not be the primary target,
unless nothing else can be found.

engine/action/sc_action.cpp
@@ -4344,12 +4344,6 @@ void action_t::acquire_target( retarget_source /* event */, player_t* /* context
     target_cache.is_valid = false;
   }
 
-  // Don't swap targets if the action's current target is still alive
-  if ( !target->is_sleeping() )
-  {
-    return;
-  }
-
   // Don't change targets if they are not of the same generic type (both enemies, or both friendlies)
   if ( target && candidate_target && target->is_enemy() != candidate_target->is_enemy() )
   {
@@ -4366,6 +4360,12 @@ void action_t::acquire_target( retarget_source /* event */, player_t* /* context
   // since if the retargeting event is due to the target demise, the channel has already been
   // interrupted before (due to the demise).
   if ( player->channeling && player->channeling == this )
+  {
+    return;
+  }
+
+  // Don't swap targets if the action's current target is still alive
+  if ( !target->is_sleeping() && !target->debuffs.invulnerable->check() )
   {
     return;
   }