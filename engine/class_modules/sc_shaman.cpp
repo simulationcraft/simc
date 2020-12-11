87c9cc76990c9f9a3b1fdf28c104ffb1668420d4
Jason <jasonlind96@gmail.com>
2020-12-11 12:24:23 -0500

    Shadowcore Oil Fix

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -8902,6 +8902,8 @@ std::string shaman_t::default_temporary_enchant() const
       if ( true_level >= 60 )
         return "main_hand:shadowcore_oil";
     case SHAMAN_ENHANCEMENT:
+      if ( true_level >= 60 )
+        return "disabled";
     case SHAMAN_RESTORATION:
       if ( true_level >= 60 )
         return "main_hand:shadowcore_oil";
