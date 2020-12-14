b812c1cd5ee3b9c96be4411000dabb7e129f08e7
Antoine Colin <antoine.colin1@hotmail.fr>
2020-12-14 04:14:23 +0100

    [APL Script} Add a condensed manual

--- engine/class_modules/apl/ConvertAPL.py
+++ engine/class_modules/apl/ConvertAPL.py
@@ -3,6 +3,12 @@ import getopt
 import os
 import shutil
 
+# Simple HowTo
+# Usage: >python ConvertAPL.py -i path/to/input/file -o path/to/output/file -s spec
+# Input file can be any type and location, but must only contain APL commands (actions=..., actions+=/..., etc.) and comments
+# Output file has to be an already existing file, with //spec_apl_start and //spec_apl_end comments around the APL method that you want to update
+# spec has to be a member of specList
+
 ignored_comments = [
     '# This default action priority list is automatically created based on your character.',
     '# It is a attempt to provide you with a action list that is both simple and practicable,',
@@ -98,7 +104,8 @@ def main(argv):
     inputFilePath = ''
     outputFilePath = ''
     specString = ''
-    specList = [ 'arcane', 'blood', 'fire', 'frost', 'unholy' ] #Add spec names here; these will be used to find the method in the apl file so should match your method name not necessarily tokenized spec name
+    # Add spec names here; these will be used to find the method in the apl file so should match your method name not necessarily tokenized spec name
+    specList = ['arcane', 'blood', 'fire', 'frost', 'unholy']
     try:
         opts, args = getopt.getopt(argv,"hi:o:s:",["input=","output=","spec="])
     except getopt.GetoptError:
