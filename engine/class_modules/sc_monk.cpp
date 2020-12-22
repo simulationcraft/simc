5cff09f3cae25890ed40f178e327d13842d4d75f
Timothy Seltman <tseltman@verizon.net>
2020-12-21 23:59:21 -0500

    [Monk] Clean up Weapons of Order Should fix #5578

--- engine/class_modules/sc_monk.cpp
+++ engine/class_modules/sc_monk.cpp
@@ -7887,9 +7887,10 @@ struct weapons_of_order_t : public monk_spell_t
   void execute() override
   {
     p()->buff.weapons_of_order->trigger();
-    if ( p()->specialization() == MONK_BREWMASTER ) {
+
+    if ( p()->specialization() == MONK_BREWMASTER )
       p()->cooldown.keg_smash->reset( true, 1 );
-    }
+
     monk_spell_t::execute();
   }
 };
