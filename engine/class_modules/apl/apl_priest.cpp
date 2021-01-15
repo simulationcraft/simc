bb8b10f1a9ef14f14b7dc9442065467b250c9e0e
Saeldur <5496498+Saeldur@users.noreply.github.com>
2021-01-15 15:24:10 +0000

    [Priest] APL Change for Kyrian If empyreal ordnance is equipped delay boon for empyreal ordnances int buff.

--- engine/class_modules/apl/apl_priest.cpp
+++ engine/class_modules/apl/apl_priest.cpp
@@ -160,12 +160,14 @@ void shadow( player_t* p )
       "15)&(buff.power_infusion.up|cooldown.power_infusion.remains>=10|!priest.self_power_infusion)&(!talent.hungering_"
       "void.enabled|debuff.hungering_void.up|!buff.voidform.up)",
       "Use Unholy Nova on CD, holding briefly to wait for power infusion or add spawns." );
-  cds->add_action( p, priest->covenant.boon_of_the_ascended, "boon_of_the_ascended",
-                   "if=!buff.voidform.up&!cooldown.void_eruption.up&spell_targets.mind_sear>1&!talent.searing_"
-                   "nightmare.enabled|(buff.voidform.up&spell_targets.mind_sear<2&!talent.searing_nightmare.enabled&"
-                   "prev_gcd.1.void_bolt)|(buff.voidform.up&talent.searing_nightmare.enabled)",
-                   "Use on CD but prioritise using Void Eruption first, if used inside of VF on ST use after a "
-                   "voidbolt for cooldown efficiency and for hungering void uptime if talented." );
+  cds->add_action(
+      p, priest->covenant.boon_of_the_ascended, "boon_of_the_ascended",
+      "if=!buff.voidform.up&!cooldown.void_eruption.up&spell_targets.mind_sear>1&!talent.searing_nightmare.enabled|("
+      "buff.voidform.up&spell_targets.mind_sear<2&!talent.searing_nightmare.enabled&(prev_gcd.1.void_bolt&!equipped."
+      "empyreal_ordnance|equipped.empyreal_ordnance&trinket.empyreal_ordnance.cooldown.remains<=162&debuff.hungering_"
+      "void.up))|(buff.voidform.up&talent.searing_nightmare.enabled)",
+      "Use on CD but prioritise using Void Eruption first, if used inside of VF on ST use after a "
+      "voidbolt for cooldown efficiency and for hungering void uptime if talented." );
   cds->add_call_action_list( trinkets );
 
   // APL to use when Boon of the Ascended is active
