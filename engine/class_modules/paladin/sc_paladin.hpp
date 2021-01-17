34875a8203b681abe23a5ddf68e674b6ddede3f6
Maxime Serrano <mserrano@alumni.cmu.edu>
2021-01-16 17:13:40 -0800

    [Paladin] I guess melees count as abilities

--- engine/class_modules/paladin/sc_paladin.hpp
+++ engine/class_modules/paladin/sc_paladin.hpp
@@ -911,8 +911,7 @@ public:
       paladin_td_t* td = this -> td( s -> target );
       if ( td -> debuff.reckoning -> up() )
         td -> debuff.reckoning -> expire();
-      // todo: triple-check whether ab::special is the right thing
-      if ( td -> debuff.execution_sentence -> check() && ab::special )
+      if ( td -> debuff.execution_sentence -> check() )
       {
         td -> debuff.execution_sentence -> accumulate_damage( s );
       }
