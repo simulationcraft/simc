263a2bbfe4636d846868dfba7fe50aee3a16ae83
Antoine Colin <antoine.colin1@hotmail.fr>
2021-01-23 21:46:51 +0100

    [ConvertAPL] Move the spec list to the top of the file That way the readme shouldn't need updating everytime the script is modified

--- engine/class_modules/apl/ConvertAPL.py
+++ engine/class_modules/apl/ConvertAPL.py
@@ -3,6 +3,12 @@ import getopt
 import os
 import shutil
 
+# Add spec names here; these will be used to find the method in the apl file so should match your method name not necessarily tokenized spec name
+# The script will only attempt to generate the APL of specs listed here
+specList = ['blood', 'frost', 'unholy',  # Death Knight
+            'arcane', 'fire', # Mage (frost is ignored because of duplicate name)
+           ]
+
 ignored_comments = [
     '# This default action priority list is automatically created based on your character.',
     '# It is a attempt to provide you with a action list that is both simple and practicable,',
@@ -92,8 +98,6 @@ def main(argv):
     inputFilePath = ''
     outputFilePath = ''
     specString = ''
-    # Add spec names here; these will be used to find the method in the apl file so should match your method name not necessarily tokenized spec name
-    specList = ['arcane', 'blood', 'fire', 'frost', 'unholy']
     try:
         opts, args = getopt.getopt(argv,"hi:o:s:",["input=","output=","spec="])
     except getopt.GetoptError:
