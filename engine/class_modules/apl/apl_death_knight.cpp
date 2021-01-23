115a85ab7687585a5bcc41abd3098144aa78c22f
nyterage <nyterage@gmail.com>
2021-01-23 10:29:43 -0800

    Update apl_death_knight.cpp

--- engine/class_modules/apl/apl_death_knight.cpp
+++ engine/class_modules/apl/apl_death_knight.cpp
@@ -349,7 +349,7 @@ void unholy( player_t* p )
   cooldowns->add_action( "potion,if=variable.major_cooldowns_active|fight_remains<26", "Potion" );
   cooldowns->add_action( "army_of_the_dead,if=cooldown.unholy_blight.remains<5&cooldown.dark_transformation.remains_guess<5&talent.unholy_blight|!talent.unholy_blight|fight_remains<35", "Cooldowns" );
   cooldowns->add_action( "soul_reaper,target_if=target.time_to_pct_35<5&target.time_to_die>5" );
-  cooldowns->add_action( "unholy_blight,if=variable.st_planning&(cooldown.apocalypse.remains_guess<5|cooldown.apocalypse.remains_guess>10)&(cooldown.dark_transformation.remains<gcd|buff.dark_transformation.up)", "Sync Blight with Apocalypse if the cooldown of Apocalypse is less than 10 seconds away, Otherwise, use with Dark Transformation." );
+  cooldowns->add_action( "unholy_blight,if=variable.st_planning&(cooldown.apocalypse.remains_guess<5|cooldown.apocalypse.remains_guess>10)&(cooldown.dark_transformation.remains<gcd|buff.dark_transformation.up)", "Holds Blight for up to 5 seconds to sync with Apocalypse, Otherwise, use with Dark Transformation." );
   cooldowns->add_action( "unholy_blight,if=active_enemies>=2|fight_remains<21" );
   cooldowns->add_action( "dark_transformation,if=variable.st_planning&(dot.unholy_blight_dot.remains|!talent.unholy_blight)" );
   cooldowns->add_action( "dark_transformation,if=active_enemies>=2|fight_remains<21" );
