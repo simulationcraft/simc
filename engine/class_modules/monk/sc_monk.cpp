331e1a2344646ef0c8468fce3ad90e6f5d14403b
Timothy Seltman <tseltman@verizon.net>
2021-03-11 23:07:21 -0500

    [Monk] Remove some extraneous comments

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -1841,7 +1841,6 @@ struct fists_of_fury_tick_t : public monk_melee_attack_t
     : monk_melee_attack_t( name, p, p->passives.fists_of_fury_tick )
   {
     background = true;
-    // Currently a bug where the it's a max of 5 targets; instead of primary + 5
     aoe        = 1 + (int)p->spec.fists_of_fury->effectN( 1 ).base_value();
     ww_mastery = true;
 
