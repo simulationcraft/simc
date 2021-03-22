05832fb847bba39a6fca9f883174f6cf3f3dfb5a
Timothy Seltman <tseltman@verizon.net>
2021-03-22 19:40:50 -0400

    [Monk] SCK Bug has been fixed.

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -1639,8 +1639,7 @@ struct sck_tick_action_t : public monk_melee_attack_t
   sck_tick_action_t( util::string_view name, monk_t* p, const spell_data_t* data )
     : monk_melee_attack_t( name, p, data ), charred_passions( new charred_passions_t( p ) )
   {
-    if ( !p->bugs )
-      ww_mastery    = true;
+    ww_mastery    = true;
     trigger_chiji = true;
 
     dual = background = true;
