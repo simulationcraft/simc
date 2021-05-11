7fe3e5f9b321fbe449eeb2b4933dd9cbc8a0021d
gastank <42421688+gastank@users.noreply.github.com>
2021-05-11 14:15:51 -0700

    [PTR] update spell data build 9.1.0.38627

--- dbc_extract3/generate.bat
+++ dbc_extract3/generate.bat
@@ -28,7 +28,7 @@ goto usage
 :okay
 
 py -3 %RUNFILE% -p %GTINPATH% -b %BUILD%                      %PTR% -t scale  -o %OUTPATH%\sc_scale_data%PTREXT%.inc
-py -3 %RUNFILE% -p %INPATH%   -b %BUILD% --hotfix=%CACHEFILE% %PTR% -t output %BATCHFILE%
+py -3 %RUNFILE% -p %INPATH%   -b %BUILD% %PTR% -t output %BATCHFILE%
 
 echo Done!
 
