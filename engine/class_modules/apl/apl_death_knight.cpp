4f6b482fdc30da1e64d114d67c15c992e00b6f6e
nyterage <nyterage@gmail.com>
2021-05-19 07:13:17 -0700

    Nothign to see here

--- engine/class_modules/apl/apl_death_knight.cpp
+++ engine/class_modules/apl/apl_death_knight.cpp
@@ -357,7 +357,7 @@ void unholy( player_t* p )
 
   aoe_burst->add_action( "clawing_shadows,if=active_enemies<=5", "AoE Burst" );
   aoe_burst->add_action( "clawing_shadows,if=active_enemies=6&death_knight.fwounded_targets>=3" );
-  aoe_burst->add_action( "wound_spender,if=talent.bursting_sores&(death_knight.fwounded_targets>=active_enemies|death_knight.fwounded_targets>=3)|talent.bursting_sores&talent.clawing_shadows&death_knight.fwounded_targets>=1" );
+  aoe_burst->add_action( "wound_spender,if=talent.bursting_sores&(death_knight.fwounded_targets=active_enemies|death_knight.fwounded_targets>=3)|talent.bursting_sores&talent.clawing_shadows&death_knight.fwounded_targets>=1" );
   aoe_burst->add_action( "death_coil,if=(buff.sudden_doom.react|!variable.pooling_runic_power)&(buff.dark_transformation.up&runeforge.deadliest_coil&active_enemies<=3|active_enemies=2)" );
   aoe_burst->add_action( "epidemic,if=runic_power.deficit<(10+death_knight.fwounded_targets*3)&death_knight.fwounded_targets<6&!variable.pooling_runic_power|buff.swarming_mist.up" );
   aoe_burst->add_action( "epidemic,if=runic_power.deficit<25&death_knight.fwounded_targets>5&!variable.pooling_runic_power" );
