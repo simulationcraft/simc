e5d1e1ce4fded25abffeb5fa0f7c4967d7f2e6a2
Publik <sean.peters86@icloud.com>
2021-05-31 11:41:31 -0500

    [Priest] add Void Torrent + Dark Thoughts bug

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -26,6 +26,7 @@ private:
   double mind_blast_insanity;
   const spell_data_t* mind_flay_spell;
   const spell_data_t* mind_sear_spell;
+  const spell_data_t* void_torrent_spell;
   bool only_cwc;
 
 public:
@@ -34,6 +35,7 @@ public:
       mind_blast_insanity( priest().specs.shadow_priest->effectN( 12 ).resource( RESOURCE_INSANITY ) ),
       mind_flay_spell( p.specs.mind_flay ),
       mind_sear_spell( p.specs.mind_sear ),
+      void_torrent_spell( p.talents.void_torrent ),
       only_cwc( false )
   {
     add_option( opt_bool( "only_cwc", only_cwc ) );
@@ -76,8 +78,10 @@ public:
         return false;
       if ( player->channeling == nullptr )
         return false;
+      // BUG: https://github.com/SimCMinMax/WoW-BugTracker/issues/761
       if ( player->channeling->data().id() == mind_flay_spell->id() ||
-           player->channeling->data().id() == mind_sear_spell->id() )
+           player->channeling->data().id() == mind_sear_spell->id() ||
+           player->channeling->data().id() == void_torrent_spell->id() )
         return priest_spell_t::ready();
       return false;
     }
@@ -1687,9 +1691,8 @@ struct dark_thought_t final : public priest_buff_t<buff_t>
     this->reactable = true;
 
     // Create a stack change callback to adjust the number of mindblast charges.
-    set_stack_change_callback( [ this ]( buff_t*, int old, int cur ) {
-      priest().cooldowns.mind_blast->adjust_max_charges( cur - old );
-    } );
+    set_stack_change_callback(
+        [ this ]( buff_t*, int old, int cur ) { priest().cooldowns.mind_blast->adjust_max_charges( cur - old ); } );
   }
 
   void expire_override( int expiration_stacks, timespan_t remaining_duration ) override
