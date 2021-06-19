b1b9cee422567981534d165a16054f53e90b12f9
basdehaan <26569338+basdehaan@users.noreply.github.com>
2021-06-19 04:50:08 +0200

    [Warlock] Improvements to Demonology APL and base profile (#5924)

--- engine/class_modules/warlock/sc_warlock.cpp
+++ engine/class_modules/warlock/sc_warlock.cpp
@@ -1201,8 +1201,12 @@ void warlock_t::apl_precombat()
 
   if ( specialization() == WARLOCK_DEMONOLOGY )
   {
-    precombat->add_action( "demonbolt" );
-    precombat->add_action( "variable,name=tyrant_ready,value=0" );
+    //TODO: uncomment when the 5% health buff from emeni works and dungeon slice is fixed
+    //precombat->add_action("fleshcraft");
+    precombat->add_action("demonbolt,if=buff.demonic_core.stack=0");
+    precombat->add_action("shadow_bolt,if=buff.demonic_core.stack>0");
+    //tested different values, even with gfg/vf its better to summon tyrant sooner in the opener
+    precombat->add_action("variable,name=first_tyrant_time,op=set,value=12");
   }
   if ( specialization() == WARLOCK_DESTRUCTION )
   {
