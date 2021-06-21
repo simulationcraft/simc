b6aa546aa316b150429b18d2e33e5ca45b97f92a
Publik <sean.peters86@icloud.com>
2021-06-21 16:08:41 -0500

    fix pet multipliers

--- engine/player/sc_pet.cpp
+++ engine/player/sc_pet.cpp
@@ -126,14 +126,9 @@ double pet_t::composite_player_target_multiplier( player_t* target, school_e sch
   if ( auto td = owner->find_target_data( target ) )
   {
     m *= 1.0 + td->debuff.condensed_lifeforce->check_value();
-
-    // These seem to apply only to "main" pets
-    if ( type == PLAYER_PET )
-    {
-      m *= 1 + td->debuff.adversary->check_value();
-      m *= 1 + td->debuff.plagueys_preemptive_strike->check_value();
-      m *= 1 + td->debuff.soulglow_spectrometer->check_value();
-    }
+    m *= 1 + td->debuff.adversary->check_value();
+    m *= 1 + td->debuff.plagueys_preemptive_strike->check_value();
+    m *= 1 + td->debuff.soulglow_spectrometer->check_stack_value();
   }
 
   return m;
