1c1923deb6c883e1c12abba7e536a827420da21a
Publik <sean.peters86@icloud.com>
2021-06-21 11:36:37 -0500

    start work on soulglow spectrometer

--- engine/player/sc_pet.cpp
+++ engine/player/sc_pet.cpp
@@ -132,6 +132,7 @@ double pet_t::composite_player_target_multiplier( player_t* target, school_e sch
     {
       m *= 1 + td->debuff.adversary->check_value();
       m *= 1 + td->debuff.plagueys_preemptive_strike->check_value();
+      m *= 1 + td->debuff.soulglow_spectrometer->check_value();
     }
   }
 
