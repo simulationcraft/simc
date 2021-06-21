8b5257a3c621a0301efff31d6e8102504c777eaa
Azevara <Azevara.Duskbeam@gmail.com>
2021-06-21 11:20:59 -0400

    Prune unused precombat line

--- engine/class_modules/warlock/sc_warlock.cpp
+++ engine/class_modules/warlock/sc_warlock.cpp
@@ -1299,8 +1299,7 @@ void warlock_t::apl_precombat()
   {
     //TODO: uncomment when the 5% health buff from emeni works and dungeon slice is fixed
     //precombat->add_action("fleshcraft");
-    precombat->add_action("demonbolt,if=buff.demonic_core.stack=0");
-    precombat->add_action("shadow_bolt,if=buff.demonic_core.stack>0");
+    precombat->add_action("demonbolt");
     //tested different values, even with gfg/vf its better to summon tyrant sooner in the opener
     precombat->add_action("variable,name=first_tyrant_time,op=set,value=12");
   }
