965dfaa860b11c836575c05a63d50e1026475f51
EvanMichaels <evan.michaels@gmail.com>
2021-06-24 19:54:21 -0400

    [DBC] Whitelist Pustule Eruption Soulbind Spells

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1286,6 +1286,7 @@ class SpellDataGenerator(DataGenerator):
          # 9.1 Soulbinds
          352881, # Bonded Hearts (night fae/niya)
          352918, 358404, # Newfound Resolve (kyrian/pelagos)
+         352086, 352095, # Pustule Eruption (Necrolord/Emeni)
          # Miniscule Mailemental in an Envelope
          352542,
          # Tome of Monstrous Constructions
