160c024c4cfa91a3fac3df366101c392424d77b6
Dorovon <50294688+Dorovon@users.noreply.github.com>
2021-06-28 19:22:14 -0600

    [Soulbinds] update Pointed Courage

--- engine/sim/sc_sim.cpp
+++ engine/sim/sc_sim.cpp
@@ -3810,7 +3810,7 @@ void sim_t::create_options()
 
   // Shadowlands
   add_option( opt_float( "shadowlands.combat_meditation_extend_chance", shadowlands_opts.combat_meditation_extend_chance, 0.0, 1.0 ) );
-  add_option( opt_uint( "shadowlands.pointed_courage_nearby", shadowlands_opts.pointed_courage_nearby, 0, 5 ) );
+  add_option( opt_uint( "shadowlands.pointed_courage_nearby", shadowlands_opts.pointed_courage_nearby, 0, 3 ) );
   add_option( opt_int( "shadowlands.lead_by_example_nearby", shadowlands_opts.lead_by_example_nearby, 0, 4 ) );
   add_option( opt_uint( "shadowlands.stone_legionnaires_in_party", shadowlands_opts.stone_legionnaires_in_party, 0, 4 ) );
   add_option( opt_uint( "shadowlands.crimson_choir_in_party", shadowlands_opts.crimson_choir_in_party, 0, 4 ) );
