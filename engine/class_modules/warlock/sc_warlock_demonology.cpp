1c67d28d63596636c61d0a07fb0073a7bc071921
basdehaan <26569338+basdehaan@users.noreply.github.com>
2021-07-08 15:38:36 +0200

    [Demonology] Fix decimating bolt timing for kevin (#6056)
    * added fleshcraft casts and cancels for kevins oozeling
    
    before: https://www.raidbots.com/simbot/report/iztJfNrVcG7yd1nhPrjuxS
    after: https://www.raidbots.com/simbot/report/pQ2c96mnreXMWZpssnwRjB
    
    * Update sc_warlock_demonology.cpp
    
    * different decimating bolt timing based on marileth traits
    
    * Update sc_warlock_demonology.cpp
    
    removed cancelaction

--- engine/class_modules/warlock/sc_warlock_demonology.cpp
+++ engine/class_modules/warlock/sc_warlock_demonology.cpp
@@ -1243,8 +1243,8 @@ void warlock_t::create_apl_demonology()
   cov->add_action( "soul_rot,if=soulbind.grove_invigoration&(cooldown.summon_demonic_tyrant.remains_expected<20|cooldown.summon_demonic_tyrant.remains_expected>30)" );
   cov->add_action( "soul_rot,if=soulbind.field_of_blossoms&pet.demonic_tyrant.active" );
   cov->add_action( "soul_rot,if=soulbind.wild_hunt_tactics" );
-  cov->add_action( "decimating_bolt,if=soulbind.lead_by_example&(pet.demonic_tyrant.active&soul_shard<2|cooldown.summon_demonic_tyrant.remains_expected>40)" );
-  cov->add_action( "decimating_bolt,if=!soulbind.lead_by_example&!pet.demonic_tyrant.active" );
+  cov->add_action( "decimating_bolt,if=(soulbind.lead_by_example|soulbind.kevins_oozeling)&(pet.demonic_tyrant.active&soul_shard<2|cooldown.summon_demonic_tyrant.remains_expected>40)" );
+  cov->add_action( "decimating_bolt,if=(soulbind.forgeborne_reveries|(soulbind.volatile_solvent&!soulbind.kevins_oozeling))&!pet.demonic_tyrant.active" );
   cov->add_action( "fleshcraft,if=soulbind.volatile_solvent,cancel_if=buff.volatile_solvent_humanoid.up" );
   cov->add_action( "scouring_tithe,if=soulbind.combat_meditation&pet.demonic_tyrant.active" );
   cov->add_action( "scouring_tithe,if=!soulbind.combat_meditation" );
