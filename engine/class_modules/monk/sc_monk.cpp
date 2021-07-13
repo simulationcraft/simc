ccf985ce7733461450500993f88bfc94ced7974c
Timothy Seltman <tseltman@verizon.net>
2021-07-13 18:23:15 -0400

    [Monk] Set Chi Burst Healing Targets to 8. - This is to better simulate being in a raid environment.

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -5739,7 +5739,7 @@ monk_t::monk_t( sim_t* sim, util::string_view name, race_e r )
   user_options.initial_chi              = 1;
   user_options.expel_harm_effectiveness = 0.25;
   user_options.faeline_stomp_uptime     = 1.0;
-  user_options.chi_burst_healing_targets = 1;
+  user_options.chi_burst_healing_targets = 8;
 }
 
 // monk_t::create_action ====================================================
