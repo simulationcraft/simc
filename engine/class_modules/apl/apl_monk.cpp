36f7102677081b1a9fb762b6f761fee76ff49a8e
Hurstilol <85832226+Hurstilol@users.noreply.github.com>
2021-08-18 12:48:01 +0200

    Keeping Volatile Solvent up
    Start Channel and interrupt after 1 global to keep Volatile Solvent up.
    
    example sim: https://www.raidbots.com/simbot/report/hJTLKzhUT1cuaCDgjZidm9

--- engine/class_modules/apl/apl_monk.cpp
+++ engine/class_modules/apl/apl_monk.cpp
@@ -537,7 +537,8 @@ void windwalker( player_t* p )
   cd_serenity->add_talent( p, "Serenity", "if=cooldown.rising_sun_kick.remains<2|fight_remains<15" );
   cd_serenity->add_action( "bag_of_tricks" );
   cd_serenity->add_action( "fleshcraft,if=soulbind.pustule_eruption&buff.serenity.down&debuff.bonedust_brew_debuff.down" );
-
+  cd_serenity->add_action( "fleshcraft,interrupt=1,if=soulbind.volatile_solvent&buff.serenity.down&debuff.bonedust_brew_debuff.down" );
+  
   // Storm, Earth and Fire Cooldowns
   cd_sef->add_action( p, "Invoke Xuen, the White Tiger", "if=!variable.hold_xuen&(cooldown.rising_sun_kick.remains<2|!covenant.kyrian)&(!covenant.necrolord|cooldown.bonedust_brew.remains<2)|fight_remains<25" );
 
@@ -626,7 +627,8 @@ void windwalker( player_t* p )
   }
 
   cd_sef->add_action( "fleshcraft,if=soulbind.pustule_eruption&buff.storm_earth_and_fire.down&debuff.bonedust_brew_debuff.down" );
-
+  cd_sef->add_action( "fleshcraft,interrupt=1,if=soulbind.volatile_solvent&buff.storm_earth_and_fire.down&debuff.bonedust_brew_debuff.down" );
+  
   // Serenity
   serenity->add_action( p, "Fists of Fury", "if=buff.serenity.remains<1" );
   serenity->add_action( p, "Spinning Crane Kick",
