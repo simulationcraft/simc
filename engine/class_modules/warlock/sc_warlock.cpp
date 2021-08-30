f15649df4d1e8941b75683963413cc8892706e69
basdehaan <26569338+basdehaan@users.noreply.github.com>
2021-08-30 22:50:45 +0200

    [Warlock] Shadowed orb of torment timing for Demonology (#6146)
    * Update sc_warlock_demonology.cpp
    
    old: https://www.raidbots.com/simbot/report/vDpuamyhpwJ4skrMoCGEXS
    new: https://www.raidbots.com/simbot/report/weCHDZp9SWdZUBwTnoRHgH
    its now before any setup for tyrant. makes more sense and less of a magic number.
    compared to current using it later in the setup is a dps loss: https://www.raidbots.com/simbot/report/bj7B9w6j9dxvHtT4mhm4eb
    https://www.raidbots.com/simbot/report/3qSLiSYzpupthhPgRcgBte
    using it once tyrant is up as well: https://www.raidbots.com/simbot/report/cBTncF5i4ir1mxoJemMear
    https://www.raidbots.com/simbot/report/rThi3huUqWX3LXXA4MzzN
    
    * Shadowed Orb of Torment precombat

--- engine/class_modules/warlock/sc_warlock.cpp
+++ engine/class_modules/warlock/sc_warlock.cpp
@@ -1303,6 +1303,7 @@ void warlock_t::apl_precombat()
     precombat->add_action( "demonbolt" );
     //tested different values, even with gfg/vf its better to summon tyrant sooner in the opener
     precombat->add_action( "variable,name=first_tyrant_time,op=set,value=12" );
+    precombat->add_action( "use_item,name=shadowed_orb_of_torment" );
   }
   if ( specialization() == WARLOCK_DESTRUCTION )
   {
