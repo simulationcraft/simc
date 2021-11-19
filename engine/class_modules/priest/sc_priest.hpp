2412c5f6a4671b066bb014cdd179eacd51066723
Publik <sean.peters86@icloud.com>
2021-11-18 22:26:33 -0600

    set bonus is 15% total not per dot

--- engine/class_modules/priest/sc_priest.hpp
+++ engine/class_modules/priest/sc_priest.hpp
@@ -947,11 +947,12 @@ struct priest_spell_t : public priest_action_t<spell_t>
     int dots                          = swp->is_ticking() + vt->is_ticking() + dp->is_ticking();
     double dark_thoughts_proc_percent = priest().specs.dark_thoughts->effectN( 1 ).percent();
 
-     if ( priest().sets->has_set_bonus( PRIEST_SHADOW, T28, B2 ) )
-     {
-       dark_thoughts_proc_percent *= 1.0 + priest().sets->set( PRIEST_SHADOW, T28, B2 )->effectN( 1 ).percent();
-     }
-      
+    // TODO: see how this is formatted in spell data
+    if ( priest().sets->has_set_bonus( PRIEST_SHADOW, T28, B2 ) )
+    {
+      dark_thoughts_proc_percent *= 1.0 + ( priest().sets->set( PRIEST_SHADOW, T28, B2 )->effectN( 1 ).percent() / 3 );
+    }
+
     // Currently Mind-Sear has 1/3 the proc rate of Mind Flay 3% -> 1%
     // https://github.com/SimCMinMax/WoW-BugTracker/issues/699
     if ( priest().bugs && action_id == mind_sear_id )
