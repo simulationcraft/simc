69d402eef49b8c75673ea4bf371ecf2188509141
Mikhail Korolev <nuoHep@gmail.com>
2021-11-19 16:50:16 +0300

    Drop ad-hoc void_t helpers
    Use c++17 std::void_t instead

--- engine/util/format.hpp
+++ engine/util/format.hpp
@@ -10,12 +10,11 @@
 #include "fmt/core.h"
 
 namespace util::fmt_detail {
-template <typename...> using void_t = void;
 template <typename T, typename = void> struct has_format_to : std::false_type {};
 template <typename T>
 struct has_format_to<T,
-  void_t<decltype( sc_format_to( std::declval<const T&>(),
-                              std::declval<::fmt::format_context::iterator>() ) ) > > : std::true_type {};
+  std::void_t<decltype( sc_format_to( std::declval<const T&>(),
+                                      std::declval<::fmt::format_context::iterator>() ) ) > > : std::true_type {};
 } // namespace util::fmt_detail
 
 namespace fmt {
