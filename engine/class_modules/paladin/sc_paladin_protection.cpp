b7453e9b0388f97e37b9467f6ead8aa5d4fbdbf4
xizbow <93213703+blculbertson@users.noreply.github.com>
2021-11-23 15:30:30 -0800

    added changes to class module
    as previously stated, increased vanq hammer prio and HOW prio in ashen. removed ashen with seraphim change, as it was pointed out that it is a slight loss

--- engine/class_modules/paladin/sc_paladin_protection.cpp
+++ engine/class_modules/paladin/sc_paladin_protection.cpp
@@ -956,12 +956,13 @@ void paladin_t::generate_action_prio_list_prot()
 
   std -> add_action( this, "Shield of the Righteous" , "if=debuff.judgment.up" );
   std -> add_action( this, "Shield of the Righteous" , "if=holy_power=5|buff.holy_avenger.up|holy_power=4&talent.sanctified_wrath.enabled&buff.avenging_wrath.up" );
+  std -> add_action( this, "Hammer of Wrath", "if=runeforge.the_mad_paragon|runeforge.vanguards_momentum|covenant.venthyr&cooldown.ashen_hallow.remains>210" );
   std -> add_action( this, "Judgment", "target_if=min:debuff.judgment.remains,if=charges=2|!talent.crusaders_judgment.enabled" );
   std -> add_action( this, "Hammer of Wrath" );
   std -> add_action( "blessing_of_the_seasons" );
+  std->add_action( "vanquishers_hammer", "if = buff.seraphim.up & runecarving.duty_bound_gavel | !runecarving.duty_bound_gavel | !talent.seraphim.enabled " );
   std -> add_action( this, "Avenger's Shield" );
   std -> add_action( this, "Judgment", "target_if=min:debuff.judgment.remains" );
-  std -> add_action( "vanquishers_hammer" );
   std -> add_action( this, "Consecration", "if=!consecration.up" );
   std -> add_action( "divine_toll" );
   std -> add_talent( this, "Blessed Hammer", "strikes=2.4,if=charges=3" );
