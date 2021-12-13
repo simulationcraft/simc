0a9da758a3e8e4df829102c69dc391b916a77911
Mikhail Korolev <nuoHep@gmail.com>
2021-12-13 22:32:28 +0300

    Generic: refactor accumulate()
    Drop all of the overloads and leave a single projection-enabled one. We
    don't need the BinaryOp-enabled overload at this time.
    
    Also, while at it open code it instead of dispatching through the std
    variant. Lets us do several things:
    - "back-port" the C++20 change of std::move()ing the accumulator
    - properly handle the common "gotcha" of summing fp values (or unwanted
      casting in general):
        `accumulate( (const double[]){ .25, .75 }, 0 )` will dtrt instead of
        returing 0 like the std version would
    - drop <numeric> include from the generic header
    
    The only usage of a BinaryOperation-enabled overload can be easily
    switched to the non-binary one. If we actually ever need one it's better
    to implement it then, it should be called fold anyway.

--- engine/player/player.cpp
+++ engine/player/player.cpp
@@ -12533,14 +12533,14 @@ void player_collected_data_t::collect_data( const player_t& p )
   absorb.add( p.iteration_absorb );
 
   // player + pet dmg
-  double total_iteration_dmg = range::accumulate_proj(p.pet_list, p.iteration_dmg, &player_t::iteration_dmg);
+  double total_iteration_dmg = range::accumulate(p.pet_list, p.iteration_dmg, &player_t::iteration_dmg);
 
-  double total_priority_iteration_dmg = range::accumulate_proj(p.pet_list, p.priority_iteration_dmg, &player_t::priority_iteration_dmg);
+  double total_priority_iteration_dmg = range::accumulate(p.pet_list, p.priority_iteration_dmg, &player_t::priority_iteration_dmg);
 
   // player + pet heal
-  double total_iteration_heal = range::accumulate_proj(p.pet_list, p.iteration_heal, &player_t::iteration_heal);
+  double total_iteration_heal = range::accumulate(p.pet_list, p.iteration_heal, &player_t::iteration_heal);
 
-  double total_iteration_absorb = range::accumulate_proj(p.pet_list, p.iteration_absorb, &player_t::iteration_absorb);
+  double total_iteration_absorb = range::accumulate(p.pet_list, p.iteration_absorb, &player_t::iteration_absorb);
 
   compound_dmg.add( total_iteration_dmg );
   prioritydps.add( uptime ? total_priority_iteration_dmg / uptime : 0 );
