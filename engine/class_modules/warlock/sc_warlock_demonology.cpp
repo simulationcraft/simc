581ee291a99d4517e133ec5c25d1c8968f0c5d9b
basdehaan <26569338+basdehaan@users.noreply.github.com>
2022-02-15 17:34:42 +0100

    [Demonology] Trinket logic and sync
    before: https://www.raidbots.com/simbot/report/npLDk2BRHy8C936tfuPNYk
    after: https://www.raidbots.com/simbot/report/v4c6XVHWbLWMmo8fdBREXx
    
    for the necro bolt build it syncs trinkets to decimating bolt. for the usual decon build it syncs to tyrant as before.

--- engine/class_modules/warlock/sc_warlock_demonology.cpp
+++ engine/class_modules/warlock/sc_warlock_demonology.cpp
@@ -1268,6 +1268,10 @@ void warlock_t::create_apl_demonology()
 
   def->add_action( "variable,name=next_tyrant_cd,op=set,value=cooldown.summon_demonic_tyrant.remains_expected,if=!soulbind.field_of_blossoms|cooldown.summon_demonic_tyrant.remains_expected>cooldown.soul_rot.remains_expected" );
   def->add_action( "variable,name=next_tyrant_cd,op=set,value=cooldown.soul_rot.remains_expected,if=soulbind.field_of_blossoms&cooldown.summon_demonic_tyrant.remains_expected<cooldown.soul_rot.remains_expected" );
+  def->add_action( "variable,name=next_tyrant_cd,op=set,value=cooldown.decimating_bolt.remains-10,if=variable.use_bolt_timings" );
+  def->add_action( "variable,name=next_tyrant_cd,op=set,value=variable.first_tyrant_time-time,if=time<variable.first_tyrant_time" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=variable.next_tyrant_cd,if=!variable.use_bolt_timings" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=cooldown.decimating_bolt.remains_expected,if=variable.use_bolt_timings" );
   def->add_action( "call_action_list,name=trinkets" );
   def->add_action( "call_action_list,name=ogcd,if=(!variable.use_bolt_timings&pet.demonic_tyrant.active)|(variable.use_bolt_timings&buff.shard_of_annihilation.up)" );
   def->add_action( "call_action_list,name=opener,if=time<variable.first_tyrant_time" );
@@ -1336,21 +1340,27 @@ void warlock_t::create_apl_demonology()
   ogcd->add_action( "fireblood" );
   ogcd->add_action( "use_items" );
 
-  trinks->add_action( "use_item,name=shadowed_orb_of_torment,if=variable.next_tyrant_cd<22" );
+  trinks->add_action( "use_item,name=shadowed_orb_of_torment,if=variable.buff_sync_cd<22" );
+  trinks->add_action( "use_item,name=scars_of_fraternal_strife" );
   trinks->add_action( "call_action_list,name=hp_trinks,if=talent.demonic_consumption.enabled&variable.next_tyrant_cd<20" );
   trinks->add_action( "call_action_list,name=5y_per_sec_trinkets", "Effects that travel slowly to target require additional, separate handling" );
   trinks->add_action( "use_item,name=overflowing_anima_cage,if=pet.demonic_tyrant.active" );
   trinks->add_action( "use_item,slot=trinket1,if=trinket.1.has_use_buff&pet.demonic_tyrant.active" );
   trinks->add_action( "use_item,slot=trinket2,if=trinket.2.has_use_buff&pet.demonic_tyrant.active" );
-  trinks->add_action( "call_action_list,name=pure_damage_trinks,if=time>variable.first_tyrant_time&variable.next_tyrant_cd>20" );
+  trinks->add_action( "call_action_list,name=pure_damage_trinks,if=time>variable.first_tyrant_time&variable.buff_sync_cd>20" );
 
-  five_y->add_action( "use_item,name=soulletting_ruby,if=variable.next_tyrant_cd<target.distance%5&time>variable.first_tyrant_time-(target.distance%5)" );
-  five_y->add_action( "use_item,name=sunblood_amethyst,if=variable.next_tyrant_cd<target.distance%5&time>variable.first_tyrant_time-(target.distance%5)" );
-  five_y->add_action( "use_item,name=empyreal_ordnance,if=variable.next_tyrant_cd<(target.distance%5)+12&variable.next_tyrant_cd>(((target.distance%5)+12)-15)&time>variable.first_tyrant_time-((target.distance%5)+12)", "Ordnance has a 12 second delay and is therefore skipped for first Tyrant to line up with the rest" );
+  five_y->add_action( "use_item,name=soulletting_ruby,if=variable.buff_sync_cd<target.distance%5" );
+  five_y->add_action( "use_item,name=sunblood_amethyst,if=variable.buff_sync_cd<target.distance%5" );
+  five_y->add_action( "use_item,name=empyreal_ordnance,if=variable.buff_sync_cd<(target.distance%5)+12" );
 
   hp->add_action( "use_item,name=sinful_gladiators_emblem" );
   hp->add_action( "use_item,name=sinful_aspirants_emblem" );
 
+
+  dmg->add_action( "use_item,name=grim_eclipse" );
+  dmg->add_action( "use_item,name=resonant_reservoir" );
+  dmg->add_action( "use_item,name=architects_ingenuity_core" );
+  dmg->add_action( "use_item,name=cosmic_gladiators_resonator" );
   dmg->add_action( "use_item,name=dreadfire_vessel" );
   dmg->add_action( "use_item,name=soul_igniter" );
   dmg->add_action( "use_item,name=glyph_of_assimilation,if=active_enemies=1" );
