9878801909670df70752228e709c0972cb2523aa
basdehaan <26569338+basdehaan@users.noreply.github.com>
2022-02-26 05:36:43 +0100

    [Warlock] Demonology APL Opener Improvement (#6407)
    * Decimating bolt timing update
    
    before: https://www.raidbots.com/simbot/report/2jn1na26Bsh5wZNsm1vnRx
    after: https://raidbots.com/simbot/report/QmucXTdgrVXcsAaDdn5Kp
    
    * potion usage
    
    before: https://www.raidbots.com/simbot/report/c2biqfvqux2Q6R1dZoRrTo
    after: https://www.raidbots.com/simbot/report/SixWSaCGyE8kNT3pBtzdd
    
    * [Warlock] opener update
    
    DL/FtS/GfG : https://www.raidbots.com/simbot/report/dcEczSi89JZdrD1CpHEg72
    DS/FtS/GfG : https://www.raidbots.com/simbot/report/cG4UcZWjeBS7PTHVFzsEmC
    DL/VT/GfG : https://www.raidbots.com/simbot/report/dA4duZCZWLjjn1CptD4XPo
    SL/FtS/SC : https://www.raidbots.com/simbot/report/ce2PoRFu1wagubp547xENu
    T28 profile https://www.raidbots.com/simbot/report/9jpzDeukAzmFBiw3JGqhR6
    Necro : https://www.raidbots.com/simbot/report/9QkbzLp1Wp8Dx5PzMtae2u

--- engine/class_modules/warlock/sc_warlock_demonology.cpp
+++ engine/class_modules/warlock/sc_warlock_demonology.cpp
@@ -1271,8 +1271,11 @@ void warlock_t::create_apl_demonology()
 
   def->add_action( "variable,name=next_tyrant_cd,op=set,value=cooldown.summon_demonic_tyrant.remains_expected,if=!soulbind.field_of_blossoms|cooldown.summon_demonic_tyrant.remains_expected>cooldown.soul_rot.remains_expected" );
   def->add_action( "variable,name=next_tyrant_cd,op=set,value=cooldown.soul_rot.remains_expected,if=soulbind.field_of_blossoms&cooldown.summon_demonic_tyrant.remains_expected<cooldown.soul_rot.remains_expected" );
-  def->add_action( "variable,name=next_tyrant_cd,op=set,value=variable.first_tyrant_time-time,if=time<variable.first_tyrant_time" );
-  def->add_action( "variable,name=buff_sync_cd,op=set,value=variable.next_tyrant_cd,if=!variable.use_bolt_timings" );
+  def->add_action( "variable,name=in_opener,op=set,value=0,if=pet.demonic_tyrant.active" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=variable.next_tyrant_cd,if=!variable.use_bolt_timings&!variable.in_opener" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=12,if=!variable.use_bolt_timings&variable.in_opener&!pet.dreadstalker.active" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=0,if=!variable.use_bolt_timings&variable.in_opener&pet.dreadstalker.active&buff.wild_imps.stack>0&!talent.vilefiend.enabled" );
+  def->add_action( "variable,name=buff_sync_cd,op=set,value=0,if=!variable.use_bolt_timings&variable.in_opener&pet.dreadstalker.active&prev_gcd.1.hand_of_guldan&talent.vilefiend.enabled" );
   def->add_action( "variable,name=buff_sync_cd,op=set,value=cooldown.decimating_bolt.remains_expected,if=variable.use_bolt_timings" );
   def->add_action( "call_action_list,name=trinkets" );
   def->add_action( "call_action_list,name=ogcd,if=(!variable.use_bolt_timings&pet.demonic_tyrant.active)|(variable.use_bolt_timings&buff.shard_of_annihilation.up&(!talent.power_siphon.enabled|buff.power_siphon.up))" );
