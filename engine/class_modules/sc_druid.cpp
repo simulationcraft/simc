64d879bf0991cc0258578d695a14cd9d4865b07f
gastank <42421688+gastank@users.noreply.github.com>
2022-02-28 11:14:33 -0800

    [Feral] refactor mastery spell data auto parsing

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -2064,7 +2064,7 @@ public:
   {
     const auto& eff = s_data->effectN( i );
     double val      = eff.percent();
-    bool mastery    = false;
+    bool mastery    = p()->find_mastery_spell( p()->specialization() ) == s_data;
 
     // TODO: more robust logic around 'party' buffs with radius
     if ( !( eff.type() == E_APPLY_AURA || eff.type() == E_APPLY_AREA_AURA_PARTY ) || eff.radius() )
@@ -2130,6 +2130,11 @@ public:
       p()->sim->print_debug( "buff-effects: {} ({}) {} modified by {}%{} with buff {} ({})", ab::name(), ab::id,
                              buffeffect_name, val * 100.0, mastery ? "+mastery" : "", buff->name(), buff->data().id() );
     }
+    else if ( mastery && !f )
+    {
+      p()->sim->print_debug( "mastery-effects: {} ({}) {} modified by {}%+mastery from {} ({})", ab::name(), ab::id,
+                             buffeffect_name, val * 100.0, s_data->name_cstr(), s_data->id() );
+    }
     else
     {
       p()->sim->print_debug( "conditional-effects: {} ({}) {} modified by {}% with condition from {} ({})", ab::name(),
@@ -2185,6 +2190,11 @@ public:
 
     for ( size_t i = 1 ; i <= spell->effect_count(); i++ )
       parse_buff_effect( nullptr, f, spell, i, false );
+  }
+
+  void parse_passive_effects( const spell_data_t* spell )
+  {
+    parse_conditional_effects( spell, nullptr );
   }
 
   double get_buff_effects_value( const std::vector<buff_effect_t>& buffeffects, bool flat = false,
@@ -2928,19 +2938,7 @@ public:
       snapshots.clearcasting =
           parse_persistent_buff_effects<S>( p->buff.clearcasting_cat, 0u, true, p->talent.moment_of_clarity );
 
-      if ( data().affected_by_all( p->mastery.razor_claws->effectN( 1 ) ) )
-      {
-        auto val = p->mastery.razor_claws->effectN( 1 ).percent();
-        da_multiplier_buffeffects.emplace_back( nullptr, val, false, true );
-        p->sim->print_debug( "buff-effects: {} ({}) direct damage modified by {}%+mastery", name(), id, val * 100.0 );
-      }
-
-      if ( data().affected_by_all( p->mastery.razor_claws->effectN( 2 ) ) )
-      {
-        auto val = p->mastery.razor_claws->effectN( 2 ).percent();
-        ta_multiplier_buffeffects.emplace_back( nullptr, val, false, true );
-        p->sim->print_debug( "buff-effects: {} ({}) tick damage modified by {}%+mastery", name(), id, val * 100.0 );
-      }
+      parse_passive_effects( p->mastery.razor_claws );
     }
   }
 
