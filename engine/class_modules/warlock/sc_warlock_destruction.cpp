a62d801a02698ed2011bdf85fcce6f90f75efb60
basdehaan <26569338+basdehaan@users.noreply.github.com>
2022-05-03 13:14:09 +0200

    [Destruction Warlock] 2 target rain of fire with 4p
    comparison: https://www.raidbots.com/simbot/report/pPyQ7Zvx125J7TBYv3Y3xN

--- engine/class_modules/warlock/sc_warlock_destruction.cpp
+++ engine/class_modules/warlock/sc_warlock_destruction.cpp
@@ -1292,7 +1292,7 @@ void warlock_t::create_apl_destruction()
   def->add_action( "fleshcraft,if=soulbind.volatile_solvent,cancel_if=buff.volatile_solvent_humanoid.up" );
   def->add_action( "conflagrate,if=talent.roaring_blaze.enabled&debuff.roaring_blaze.remains<1.5" );
   def->add_action( "cataclysm" );
-  def->add_action( "call_action_list,name=aoe,if=active_enemies>2" );
+  def->add_action( "call_action_list,name=aoe,if=active_enemies>2-set_bonus.tier28_4pc" );
   def->add_action( "soul_fire,cycle_targets=1,if=refreshable&soul_shard<=4&(!talent.cataclysm.enabled|cooldown.cataclysm.remains>remains)" );
   def->add_action( "immolate,cycle_targets=1,if=remains<3&(!talent.cataclysm.enabled|cooldown.cataclysm.remains>remains)" );
   def->add_action( "immolate,if=talent.internal_combustion.enabled&action.chaos_bolt.in_flight&remains<duration*0.5" );
@@ -1317,6 +1317,7 @@ void warlock_t::create_apl_destruction()
   def->add_action( "incinerate" );
 
   aoe->add_action( "rain_of_fire,if=pet.infernal.active&(!cooldown.havoc.ready|active_enemies>3)" );
+  aoe->add_action( "rain_of_fire,if=set_bonus.tier28_4pc" );
   aoe->add_action( "soul_rot" );
   aoe->add_action( "impending_catastrophe" );
   aoe->add_action( "channel_demonfire,if=dot.immolate.remains>cast_time" );
