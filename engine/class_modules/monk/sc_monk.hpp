4bd9a6b66e6233f73f4005a2685b5b42ec19be62
JeremyMonk <101205288+JeremyMonk@users.noreply.github.com>
2022-06-01 16:31:25 -0500

    [Monk] Fix SEF targetting issue (#6570)

--- engine/class_modules/monk/sc_monk.hpp
+++ engine/class_modules/monk/sc_monk.hpp
@@ -908,7 +908,7 @@ public:
   // Storm Earth and Fire targeting logic
   std::vector<player_t*> create_storm_earth_and_fire_target_list() const;
   void summon_storm_earth_and_fire( timespan_t duration );
-  void retarget_storm_earth_and_fire( pet_t* pet, std::vector<player_t*>& targets, size_t n_targets ) const;
+  void retarget_storm_earth_and_fire( pet_t* pet, std::vector<player_t*>& targets ) const;
   void retarget_storm_earth_and_fire_pets() const;
 
   void bonedust_brew_assessor( action_state_t* );
