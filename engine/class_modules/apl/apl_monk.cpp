4b6c7c118c03bcf1bcd0f40e94db4c186dc9a5dc
Timothy Seltman <tseltman@verizon.net>
2022-06-02 15:12:12 -0400

    [Monk] Minor fix to Brewmaster trinket APL

--- engine/class_modules/apl/apl_monk.cpp
+++ engine/class_modules/apl/apl_monk.cpp
@@ -188,7 +188,7 @@ void brewmaster( player_t* p )
             ",if=!buff.scars_of_fraternal_strife_4.up&time>1" );
       else if( item.name_str == "cache_of_acquired_treasures" )
           def->add_action( "use_item,name=" + item.name_str + ",if=buff.acquired_axe.up|fight_remains<25" );
-      if ( item.name_str == "jotungeirr_destinys_call" )
+      else if ( item.name_str == "jotungeirr_destinys_call" )
         continue;
       else
         def->add_action( "use_item,name=" + item.name_str );
