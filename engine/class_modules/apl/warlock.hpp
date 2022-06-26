350682ece0d40db9822f2f9d9e6a06e7ed833e08
Azevara <Azevara.Duskbeam@gmail.com>
2022-06-25 21:28:50 -0400

    Populate new warlock.hpp file

---
+++ engine/class_modules/apl/warlock.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+struct player_t;
+
+namespace warlock_apl{
+
+  std::string potion( const player_t* );
+  std:: string flask( const player_t* );
+  std::string food( const player_t* );
+  std::string rune( const player_t* );
+  std::string temporary_enchant( const player_t* );
+
+  void affliction( player_t* );
+  void demonology( player_t* );
+  void destruction( player_t* );
+
+} // namespace warlock_apl
