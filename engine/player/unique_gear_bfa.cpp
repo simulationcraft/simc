2e30f900d4e9c527c8fb59c8d691e47fc8e9f54d
Dorovon <50294688+Dorovon@users.noreply.github.com>
2022-08-02 20:22:24 -0600

    [Gear] update Hyperthread Wristwraps
    Hyperthread Wristwraps currently seem to be double dipping in all recharge rate modifiers.

--- engine/player/unique_gear_bfa.cpp
+++ engine/player/unique_gear_bfa.cpp
@@ -5497,8 +5497,9 @@ void items::hyperthread_wristwraps( special_effect_t& effect )
 
       for ( auto a : tracker->last_used )
       {
-        sim->print_debug( "Reducing cooldown of action {} by {} s.", a->name_str, reduction.total_seconds() );
-        a->cooldown->adjust( -reduction );
+        timespan_t adjusted_reduction = reduction / a->recharge_rate_multiplier( *a->cooldown );
+        sim->print_debug( "Reducing cooldown of action {} by {} s.", a->name_str, adjusted_reduction.total_seconds() );
+        a->cooldown->adjust( -adjusted_reduction );
       }
     }
 
