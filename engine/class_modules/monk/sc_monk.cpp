c9b582b633a20dc658f0e80de99d5204382226d2
JeremyMonk <101205288+JeremyMonk@users.noreply.github.com>
2022-08-26 08:48:46 -0500

    [Monk] Jade Ignition Bug Fix

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -2024,6 +2024,7 @@ struct chi_explosion_t : public monk_spell_t
   {
     dual = background = true;
     aoe               = -1;
+    school            = SCHOOL_NATURE;
   }
 
   double action_multiplier() const override
@@ -2031,7 +2032,7 @@ struct chi_explosion_t : public monk_spell_t
     double am = monk_spell_t::action_multiplier();
 
     if ( p()->buff.chi_energy->check() )
-      am += 1 + p()->buff.chi_energy->check_stack_value();
+      am *= 1 + p()->buff.chi_energy->check_stack_value();
 
     return am;
   }
