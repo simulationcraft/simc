52789a61652e86ffd0ddde42da02764edbaa1483
Arma <adam.shewchuk@gmail.com>
2022-09-07 20:55:21 -0700

    [Death Knight]  Add spell id

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1598,6 +1598,7 @@ class SpellDataGenerator(DataGenerator):
           ( 334895, 5 ), # Frenzied Monstrosity Buff that appears on the main ghoul pet (different from the player buff)
           ( 377588, 0 ), ( 377589, 0 ), # Ghoulish Frenzy buffs
           ( 193486, 0 ), # Runic Empowerment energize spell
+          ( 377656, 0 ), # Heartrend Buff
           ( 364197, 0 ), ( 366008, 0 ), ( 368938, 0 ), # T28 Endless Rune Waltz Blood Set Bonus
           ( 363885, 0 ), ( 364173, 0 ), ( 363887, 0 ), ( 367954, 0 ), # T28 Harvest Time Unholy Set Bonus
           ( 364384, 0 ), # T28 Arctic Assault Frost Set Bonus
