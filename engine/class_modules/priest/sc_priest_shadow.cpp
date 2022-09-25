414a1c12712c6c7d88e43b1fc7ae81f75802088e
Saeldur <5496498+Saeldur@users.noreply.github.com>
2022-09-25 20:49:27 +0100

    Revert "[Priest] Add Mind Flay Insanity to Scream"
    This reverts commit 542da9c46c56afb9d7efd5ca7197fcde7e2b37f1.

--- engine/class_modules/priest/sc_priest_shadow.cpp
+++ engine/class_modules/priest/sc_priest_shadow.cpp
@@ -2438,8 +2438,7 @@ bool priest_t::is_screams_of_the_void_up( player_t* target ) const
   if ( talents.shadow.screams_of_the_void.enabled() )
   {
     priest_td_t* td = get_target_data( target );
-    if ( td->dots.mind_flay->is_ticking() || td->dots.mind_flay_insanity->is_ticking() ||
-         td->dots.void_torrent->is_ticking() ||
+    if ( td->dots.mind_flay->is_ticking() || td->dots.void_torrent->is_ticking() ||
          talents.shadow.mind_sear.enabled() && channeling != nullptr &&
              channeling->id == talents.shadow.mind_sear->id() )
     {
