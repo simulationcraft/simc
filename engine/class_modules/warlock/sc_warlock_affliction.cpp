e39a5c76b32c0dcbf3b5253ebbc8ac50f578e311
Azevara <Azevara.Duskbeam@gmail.com>
2022-09-25 22:13:51 -0400

    Implement Nightfall for Drain Soul

--- engine/class_modules/warlock/sc_warlock_affliction.cpp
+++ engine/class_modules/warlock/sc_warlock_affliction.cpp
@@ -338,11 +338,80 @@ struct malefic_rapture_t : public affliction_spell_t
 // REMEMBER TO ADD NIGHTFALL TO THIS WHEN RETALENTIZING
 struct drain_soul_t : public affliction_spell_t
 {
+  struct drain_soul_state_t : public action_state_t
+  {
+    double tick_time_multiplier;
+    bool rounded_channel;
+
+    drain_soul_state_t( action_t* action, player_t* target )
+      : action_state_t( action, target ),
+      tick_time_multiplier( 1.0 ),
+      rounded_channel( false )
+    { }
+
+    void initialize() override
+    {
+      action_state_t::initialize();
+      tick_time_multiplier = 1.0;
+      rounded_channel = false;
+    }
+
+    std::ostringstream& debug_str( std::ostringstream& s ) override
+    {
+      action_state_t::debug_str( s ) << " tick_time_multiplier=" << tick_time_multiplier;
+      s << " rounded_channel=" << rounded_channel;
+      return s;
+    }
+
+    void copy_state( const action_state_t* s ) override
+    {
+      action_state_t::copy_state( s );
+      tick_time_multiplier = debug_cast<const drain_soul_state_t*>( s )->tick_time_multiplier;
+      rounded_channel = debug_cast<const drain_soul_state_t*>( s )->rounded_channel;
+    }
+  };
+
   drain_soul_t( warlock_t* p, util::string_view options_str )
     : affliction_spell_t( "drain_soul", p, p->talents.drain_soul.ok() ? p->talents.drain_soul_dot : spell_data_t::not_found() )
   {
     parse_options( options_str );
     channeled    = true;
+  }
+
+  action_state_t* new_state() override
+  { return new drain_soul_state_t( this, target ); }
+
+  void snapshot_state( action_state_t* s, result_amount_type rt ) override
+  {
+    debug_cast<drain_soul_state_t*>( s )->tick_time_multiplier = 1.0 + ( p()->buffs.nightfall->check() ? p()->buffs.nightfall->data().effectN( 3 ).percent() : 0 );
+    debug_cast<drain_soul_state_t*>( s )->rounded_channel = ( !td( s->target )->dots_drain_soul->is_ticking() && p()->buffs.nightfall->check() );
+    affliction_spell_t::snapshot_state( s, rt );
+  }
+
+  timespan_t composite_dot_duration( const action_state_t* s ) const override
+  {
+    auto dur = ( dot_duration * s->haste );
+
+    if ( debug_cast<const drain_soul_state_t*>( s )->rounded_channel )
+      dur =tick_time( s ) * std::ceil( dur / tick_time( s ) );
+
+    return dur;
+  }
+
+  timespan_t tick_time( const action_state_t* s ) const override
+  {
+    timespan_t t = affliction_spell_t::tick_time( s );
+
+    t *= debug_cast<const drain_soul_state_t*>( s )->tick_time_multiplier;
+
+    return t;
+  }
+
+  void execute() override
+  {
+    affliction_spell_t::execute();
+
+    p()->buffs.nightfall->decrement();
   }
 
   void tick( dot_t* d ) override
