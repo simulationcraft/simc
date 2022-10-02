45ed6cb2f7a571291cf7583a33dac10423c8c27f
Publik <sean.peters86@icloud.com>
2022-10-01 22:09:49 -0500

    [Player] typo

--- engine/player/player.hpp
+++ engine/player/player.hpp
@@ -126,7 +126,7 @@ struct player_t : public actor_t
   int          party;
   int          ready_type;
   specialization_e  _spec;
-  bool         bugs; // If true, include known InGame mechanics which are probably the cause of a bug and not inteded
+  bool         bugs; // If true, include known InGame mechanics which are probably the cause of a bug and not intended
   int          disable_hotfixes;
   bool         scale_player;
   double       death_pct; // Player will die if he has equal or less than this value as health-pct
