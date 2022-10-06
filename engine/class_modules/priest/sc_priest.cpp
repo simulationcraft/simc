bab2dc680be938c88515933dd887921cdbd125c1
Publik <sean.peters86@icloud.com>
2022-10-06 11:29:47 -0500

    [Priest] use spelldata for t29 set bonuses

--- engine/class_modules/priest/sc_priest.cpp
+++ engine/class_modules/priest/sc_priest.cpp
@@ -67,9 +67,9 @@ public:
       priest().buffs.mind_melt->expire();
     }
 
-    if ( priest().options.t29_2pc )
+    if ( priest().sets->has_set_bonus( PRIEST_SHADOW, T29, B2 ) )
     {
-      priest().buffs.t29_2pc->trigger();
+      priest().buffs.gathering_shadows->trigger();
     }
   }
 
@@ -2426,9 +2426,9 @@ double priest_t::composite_spell_haste() const
 {
   double h = player_t::composite_spell_haste();
 
-  if ( options.t29_4pc && buffs.t29_4pc->check() )
+  if ( sets->has_set_bonus( PRIEST_SHADOW, T29, B4 ) && buffs.dark_reveries->check() )
   {
-    h *= 1.0 / ( 1.0 + buffs.t29_4pc->current_value );
+    h *= 1.0 / ( 1.0 + buffs.dark_reveries->current_value );
   }
 
   return h;
@@ -3176,10 +3176,6 @@ void priest_t::create_options()
                        options.shadow_word_manipulation_seconds_remaining, 0, 8 ) );
   add_option( opt_int( "priest.pallid_command_allies", options.pallid_command_allies, 0, 50 ) );
   add_option( opt_bool( "priest.power_infusion_fiend", options.power_infusion_fiend ) );
-
-  // TEMP options
-  add_option( opt_bool( "priest.t29_2pc", options.t29_2pc ) );
-  add_option( opt_bool( "priest.t29_4pc", options.t29_4pc ) );
 }
 
 std::string priest_t::create_profile( save_e type )
