cae29eeaae1afbd19ab9c31c6abaa0ea20b70042
nyterage <nyterage@gmail.com>
2022-10-14 21:43:34 -0700

    [Unholy DK] Rotten Touch Bug (#6869)

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -5721,7 +5721,13 @@ struct death_coil_t : public death_knight_spell_t
         timespan_t::from_seconds( p() -> talent.unholy.eternal_agony -> effectN( 1 ).base_value() ) );
     }
 
-    if ( p() -> talent.unholy.rotten_touch.ok() && p() -> buffs.sudden_doom -> check() )
+    // Currently Rotten Touch only triggers on the main target
+    if ( !p() -> bugs && p() -> talent.unholy.rotten_touch.ok() && p() -> buffs.sudden_doom -> check() )
+    {
+      get_td( target ) -> debuff.rotten_touch -> trigger();
+    }
+    // Rotten touch is bugged as of 10/14/22, applying on every death coil cast.
+    if ( p() -> bugs && p() -> talent.unholy.rotten_touch.ok() )
     {
       get_td( target ) -> debuff.rotten_touch -> trigger();
     }
