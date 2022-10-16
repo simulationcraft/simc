d9e0e3dd416eb8c69a3b4ee22f46c042bfc64213
Azevara <46403881+Azevara@users.noreply.github.com>
2022-10-16 16:48:39 -0400

    [Warlock] Implement Chaos Incarnate (#6899)

--- engine/class_modules/warlock/sc_warlock.hpp
+++ engine/class_modules/warlock/sc_warlock.hpp
@@ -419,7 +419,7 @@ public:
     const spell_data_t* rain_of_chaos_buff;
     const spell_data_t* summon_infernal_roc; // Contains Rain of Chaos infernal duration
     // Grand Warlock's Design (formerly Wilfred's). Shared across all 3 specs
-    // DF - Chaos Incarnate (Choice against Dimensional Rift, maximum mastery value for some spells)
+    player_talent_t chaos_incarnate; // Maximum mastery value for some spells
     // DF - Dimensional Rift (Choice against Chaos Incarnate, charge cooldown instant spell which deals damage and grants fragments)
     // DF - Avatar of Destruction (Formerly SL Tier Bonus, summons Blasphemy when consuming Ritual of Ruin)
   } talents;
