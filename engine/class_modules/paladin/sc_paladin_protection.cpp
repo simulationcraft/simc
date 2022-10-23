617414b97fb4b5f6a427c410d1c3f9538d189735
Fluttershy <Sakayra@googlemail.com>
2022-10-23 20:56:33 +0200

    [Paladin] Gift of the Golden Valkyr (Not tested yet)

--- engine/class_modules/paladin/sc_paladin_protection.cpp
+++ engine/class_modules/paladin/sc_paladin_protection.cpp
@@ -98,6 +98,13 @@ struct avengers_shield_base_t : public paladin_spell_t
 
     if ( p()->talents.bulwark_of_righteous_fury->ok() )
       p()->buffs.bulwark_of_righteous_fury->trigger();
+
+    if ( p() ->talents.gift_of_the_golden_valkyr->ok())
+    {
+      // TODO: Test, if this works, after GoAK works correctly.
+      p()->cooldowns.guardian_of_ancient_kings->adjust(
+          timespan_t::from_millis(-1) * p()->talents.gift_of_the_golden_valkyr->effectN( 1 ).base_value() );
+    }
   }
 
 double recharge_multiplier( const cooldown_t& cd ) const override
