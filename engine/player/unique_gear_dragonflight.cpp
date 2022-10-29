8be096837db5766f8011c81d8683fb33d63cc973
nyterage <nyterage@gmail.com>
2022-10-29 15:07:06 -0700

    [Gear] Decoration of Flame Initial Implementation

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -1201,6 +1201,56 @@ void storm_eaters_boon( special_effect_t& effect )
   } );
 }
 
+// Decoration of Flame
+// TODO - Create a miss chance for the target list to better emulate in game behavior
+// TODO - Adjust absorb value based of enemies hit
+// 377449 Driver
+// 382058 Shield Buff
+// 394393 Damage and Shield Value
+// 397331 Fireball 1
+// 397347 - 397353 Fireballs 2-8
+void decoration_of_flame( special_effect_t& effect )
+{
+  buff_t* buff;
+  buff_t* shield;
+
+  buff = make_buff( effect.player, "decoration_of_flame", effect.player->find_spell( 377449 ) );
+  shield = make_buff<absorb_buff_t>( effect.player, "decoration_of_flame_shield", effect.player->find_spell( 382058 ) )
+      ->set_default_value( effect.player -> find_spell( 394393 ) -> effectN( 2 ).average( effect.item ) );
+
+  effect.custom_buff = buff;
+
+  struct decoration_of_flame_damage_t : public proc_spell_t
+  {
+    buff_t* buff;
+    decoration_of_flame_damage_t( const special_effect_t& e, buff_t* b ) :
+      proc_spell_t( "decoration_of_flame", e.player, e.player->find_spell( 377449 ), e.item ), buff( b )
+    {
+      background = true;
+      base_dd_min = base_dd_max = e.player->find_spell( 394393 )->effectN( 1 ).average(e.item);
+      aoe = e.driver()->effectN(3).base_value();
+      radius = 10;
+    }
+
+     double composite_da_multiplier( const action_state_t* s ) const override
+    {
+      double m = proc_spell_t::composite_da_multiplier( s );
+
+     // Damage increases by 10% per target based on in game testing
+      m *= 1.0 + ( n_targets() / 10 );
+
+      return m;
+    }
+  };
+
+  action_t* action = create_proc_action<decoration_of_flame_damage_t>( "decoration_of_flame", effect, buff );
+  buff->set_stack_change_callback( [ action, shield ](buff_t*, int, int ) 
+  {
+    action -> execute();
+    shield -> trigger();
+  } );
+}
+
 // Weapons
 void bronzed_grip_wrappings( special_effect_t& effect )
 {
@@ -1480,6 +1530,7 @@ void register_special_effects()
   register_special_effect( 384112, items::the_cartographers_calipers );
   register_special_effect( 377454, items::rumbling_ruby );
   register_special_effect( 377453, items::storm_eaters_boon );
+  register_special_effect( 377449, items::decoration_of_flame );
 
   // Weapons
   register_special_effect( 396442, items::bronzed_grip_wrappings );  // bronzed grip wrappings embellishment
