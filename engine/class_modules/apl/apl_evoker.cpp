0c18dae1dfe3ce45586a38a078126ad092beff31
nyterage <nyterage@gmail.com>
2022-11-03 07:42:50 -0700

    [Evoker] Formulaic on use Trinkets

--- engine/class_modules/apl/apl_evoker.cpp
+++ engine/class_modules/apl/apl_evoker.cpp
@@ -38,6 +38,8 @@ void devastation( player_t* p )
 {
   action_priority_list_t* default_ = p->get_action_priority_list( "default" );
   action_priority_list_t* precombat = p->get_action_priority_list( "precombat" );
+  action_priority_list_t* trinkets = p->get_action_priority_list( "trinkets" );
+  
 
   precombat->add_action( "flask" );
   precombat->add_action( "food" );
@@ -46,11 +48,14 @@ void devastation( player_t* p )
   precombat->add_action( "use_item,name=shadowed_orb_of_torment" );
   precombat->add_action( "firestorm,if=talent.firestorm" );
   precombat->add_action( "living_flame,if=!talent.firestorm" );
-
+  precombat->add_action( "variable,name=trinket_1_sync,op=setif,value=1,value_else=0.5,condition=trinket.1.has_use_buff&(trinket.1.cooldown.duration%%cooldown.dragonrage.duration=0)", "Evaluates both trinkets cooldowns to see if they can be evenly divided by the cooldown of Dragonrage, prioritizes trinkets that will sync with this cooldown" );
+  precombat->add_action( "variable,name=trinket_2_sync,op=setif,value=1,value_else=0.5,condition=trinket.2.has_use_buff&(trinket.2.cooldown.duration%%cooldown.dragonrage.duration=0)" );
+  precombat->add_action( "variable,name=trinket_priority,op=setif,value=2,value_else=1,condition=!trinket.1.has_use_buff&trinket.2.has_use_buff|trinket.2.has_use_buff&((trinket.2.cooldown.duration%trinket.2.proc.any_dps.duration)*(1.5+trinket.2.has_buff.intelligence)*(variable.trinket_2_sync))>((trinket.1.cooldown.duration%trinket.1.proc.any_dps.duration)*(1.5+trinket.1.has_buff.intelligence)*(variable.trinket_1_sync))", "Estimates a trinkets value by comparing the cooldown of the trinket, divided by the duration of the buff it provides. Has a intelligence modifier (currently 1.5x) to give a higher priority to intelligence trinkets. The intelligence modifier should be changed as intelligence priority increases or decreases. As well as a modifier for if a trinket will or will not sync with cooldowns." );
+ 
   default_->add_action( "potion,if=buff.dragonrage.up|time>=300&fight_remains<35" );
   default_->add_action( "use_item,name=shadowed_orb_of_torment" );
   default_->add_action( "use_item,name=crimson_aspirants_badge_of_ferocity,if=cooldown.dragonrage.remains>=55" );
-  default_->add_action( "use_items,if=buff.dragonrage.up" );
+  default_->add_action( "call_action_list,name=trinkets" );
   default_->add_action( "deep_breath,if=spell_targets.deep_breath>1&buff.dragonrage.down" );
   default_->add_action( "dragonrage,if=cooldown.eternity_surge.remains<=(buff.dragonrage.duration+6)&(cooldown.fire_breath.remains<=2*gcd.max|!talent.feed_the_flames)" );
   default_->add_action( "tip_the_scales,if=buff.dragonrage.up&(cooldown.eternity_surge.up|cooldown.fire_breath.up)&buff.dragonrage.remains<=gcd.max" );
@@ -72,6 +77,11 @@ void devastation( player_t* p )
   default_->add_action( "use_item,name=kharnalex_the_first_light,if=!debuff.shattering_star_debuff.up&!buff.dragonrage.up&spell_targets.pyre=1" );
   default_->add_action( "azure_strike,if=spell_targets.azure_strike>2|(talent.engulfing_blaze|talent.feed_the_flames)&buff.dragonrage.up" );
   default_->add_action( "living_flame" );
+  
+  trinkets->add_action( "use_item,slot=trinket1,if=buff.dragonrage.up&(!trinket.2.has_cooldown|trinket.2.cooldown.remains|variable.trinket_priority=1)|trinket.1.proc.any_dps.duration>=fight_remains", "The trinket with the highest estimated value, will be used first and paired with Dragonrage." );
+  trinkets->add_action( "use_item,slot=trinket2,if=buff.dragonrage.up&(!trinket.1.has_cooldown|trinket.1.cooldown.remains|variable.trinket_priority=2)|trinket.2.proc.any_dps.duration>=fight_remains" );
+  trinkets->add_action( "use_item,slot=trinket1,if=(!trinket.1.has_use_buff&(trinket.2.cooldown.remains|!trinket.2.has_use_buff)|cooldown.dragonrage.remains>20|!talent.dragonrage)", "If only one on use trinket provides a buff, use the other on cooldown. Or if neither trinket provides a buff, use both on cooldown." );
+  trinkets->add_action( "use_item,slot=trinket2,if=(!trinket.2.has_use_buff&(trinket.1.cooldown.remains|!trinket.1.has_use_buff)|cooldown.dragonrage.remains>20|!talent.dragonrage)" );
 }
 //devastation_apl_end
 
