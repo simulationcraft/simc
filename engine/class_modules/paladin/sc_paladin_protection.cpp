fdc2e5f6301925d193a90e828a22c9c87afd87f9
Fluttershy <Sakayra@googlemail.com>
2022-11-05 02:52:26 +0100

    [Paladin] Inspiring Vanguard, remove double allocation

--- engine/class_modules/paladin/sc_paladin_protection.cpp
+++ engine/class_modules/paladin/sc_paladin_protection.cpp
@@ -906,7 +906,7 @@ void paladin_t::trigger_grand_crusader()
   double gc_proc_chance = talents.grand_crusader->effectN( 1 ).percent();
   if ( talents.inspiring_vanguard -> ok() )
   {
-    gc_proc_chance = gc_proc_chance += talents.inspiring_vanguard->effectN( 2 ).percent();
+    gc_proc_chance += talents.inspiring_vanguard->effectN( 2 ).percent();
   }
 
   // The bonus from First Avenger is added after Inspiring Vanguard
