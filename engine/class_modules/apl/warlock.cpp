b0d65b68db37b5d00a35769e3e9f42f2f86e66f1
Facenomore23 <32964159+Facenomore23@users.noreply.github.com>
2022-11-09 21:18:06 +0100

    Destruction APL

--- engine/class_modules/apl/warlock.cpp
+++ engine/class_modules/apl/warlock.cpp
@@ -113,6 +113,9 @@ void destruction( player_t* p )
 {
   action_priority_list_t* default_ = p->get_action_priority_list( "default" );
   action_priority_list_t* precombat = p->get_action_priority_list( "precombat" );
+  action_priority_list_t* aoe = p->get_action_priority_list( "aoe" );
+  action_priority_list_t* cds = p->get_action_priority_list( "cds" );
+  action_priority_list_t* havoc = p->get_action_priority_list( "havoc" );
 
   precombat->add_action( "flask" );
   precombat->add_action( "food" );
@@ -124,21 +127,61 @@ void destruction( player_t* p )
   precombat->add_action( "cataclysm" );
   precombat->add_action( "incinerate" );
 
-  default_->add_action( "chaos_bolt,if=soul_shard=5" );
-  default_->add_action( "soul_fire,if=soul_shard<=4&(buff.backdraft.up|!talent.backdraft)" );
-  default_->add_action( "cataclysm" );
-  default_->add_action( "immolate,if=dot.immolate.remains<5" );
-  default_->add_action( "channel_demonfire" );
+  default_->add_action( "call_action_list,name=havoc,if=havoc_active&active_enemies>1&active_enemies<5-talent.inferno.enabled+(talent.inferno.enabled&talent.internal_combustion.enabled)&havoc_remains>gcd" );
+  default_->add_action( "variable,name=pool_soul_shards,value=active_enemies>1&(cooldown.havoc.remains<=10|talent.mayhem)" );
+  default_->add_action( "conflagrate,if=(talent.roaring_blaze.enabled&debuff.conflagrate.remains<1.5)|charges=max_charges" );
   default_->add_action( "use_items" );
-  default_->add_action( "summon_infernal" );
-  default_->add_action( "chaos_bolt,if=buff.backdraft.up" );
-  default_->add_action( "conflagrate,if=talent.backdraft&buff.backdraft.down" );
-  default_->add_action( "conflagrate,if=soul_shard<=4.5" );
-  default_->add_action( "chaos_bolt,if=soul_shard>=4.5|!talent.backdraft" );
-  default_->add_action( "immolate,if=dot.immolate.refreshable&(!talent.soul_fire&!talent.cataclysm)" );
+  default_->add_action( "cataclysm" );
+  default_->add_action( "channel_demonfire,if=talent.raging_demonfire" );
+  default_->add_action( "soul_fire,if=soul_shard<=4&!variable.pool_soul_shards", "We want to use soul_fire in a havoc window" );
+  default_->add_action( "dimensional_rift,if=soul_shard<4.5&(charges>2|time_to_die<cooldown.dimensional_rift.duration)" );
+  default_->add_action( "call_action_list,name=aoe,if=active_enemies>2" );
+  default_->add_action( "immolate,cycle_targets=1,if=((dot.immolate.refreshable&talent.internal_combustion)|(dot.immolate.remains<3&!talent.internal_combustion))&(!talent.cataclysm.enabled|cooldown.cataclysm.remains>remains)&(!talent.soul_fire|cooldown.soul_fire.remains>remains)", "TODO not cast immolate if havoc is up and soul_fire too (it will apply it)" );
+  default_->add_action( "call_action_list,name=cds,if=cooldown.summon_infernal.up" );
+  default_->add_action( "havoc,cycle_targets=1,if=!(target=self.target)" );
+  default_->add_action( "chaos_bolt,if=(pet.infernal.active|pet.blasphemy.active)|soul_shard>=4", "We don't want to cap soul_shards (might need to try with & too)" );
+  default_->add_action( "channel_demonfire,if=talent.ruin.rank>1&!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))", "There is some setups where using CDF is a loss" );
+  default_->add_action( "conflagrate,if=buff.backdraft.down&soul_shard>=1.5&!variable.pool_soul_shards" );
+  default_->add_action( "chaos_bolt,if=pet.infernal.active|buff.rain_of_chaos.remains>cast_time", "Some conditions to prio cast CB" );
+  default_->add_action( "chaos_bolt,if=buff.backdraft.up&!variable.pool_soul_shards" );
+  default_->add_action( "chaos_bolt,if=talent.eradication&!variable.pool_soul_shards&debuff.eradication.remains<cast_time&!action.chaos_bolt.in_flight" );
+  default_->add_action( "soul_fire,if=soul_shard<=4&talent.mayhem", "If we play mayhem, we still use Soul_fire outside of mayhem windows but with low prio" );
+  default_->add_action( "channel_demonfire,if=!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))", "There is some setups where using CDF is a loss" );
   default_->add_action( "dimensional_rift" );
-  default_->add_action( "shadowburn,if=target.health.pct<=20" );
+  default_->add_action( "shadowburn,if=charges>1&target.health.pct<20&(!talent.eradication&!talent.ashen_remains)&(talent.ruin.rank=2&talent.conflagration_of_chaos.rank=2)", "SB without refund is 99% of the time a dps loss" );
+  default_->add_action( "chaos_bolt,if=!variable.pool_soul_shards&talent.soul_conduit", "Low prio on CB cast actions+=/chaos_bolt,if=soul_shard>3.5" );
+  default_->add_action( "chaos_bolt,if=time_to_die<5&time_to_die>cast_time+travel_time" );
+  default_->add_action( "conflagrate,if=charges>(max_charges-1)|time_to_die<gcd" );
   default_->add_action( "incinerate" );
+
+  aoe->add_action( "rain_of_fire,if=pet.infernal.active&(!cooldown.havoc.ready|active_enemies>3)", "TODO :Has not been touched yet SL logic" );
+  aoe->add_action( "rain_of_fire,if=talent.avatar_of_destruction" );
+  aoe->add_action( "channel_demonfire,if=dot.immolate.remains>cast_time" );
+  aoe->add_action( "immolate,cycle_targets=1,if=active_enemies<5&dot.immolate.remains<5&(!talent.cataclysm.enabled|cooldown.cataclysm.remains>dot.immolate.remains)" );
+  aoe->add_action( "call_action_list,name=cds" );
+  aoe->add_action( "havoc,cycle_targets=1,if=!(target=self.target)&active_enemies<4" );
+  aoe->add_action( "rain_of_fire" );
+  aoe->add_action( "havoc,cycle_targets=1,if=!(self.target=target)" );
+  aoe->add_action( "incinerate,if=talent.fire_and_brimstone.enabled&buff.backdraft.up&soul_shard<5-0.2*active_enemies" );
+  aoe->add_action( "soul_fire" );
+  aoe->add_action( "conflagrate,if=buff.backdraft.down" );
+  aoe->add_action( "immolate,if=refreshable" );
+  aoe->add_action( "incinerate" );
+
+  cds->add_action( "summon_infernal", "TODO trinkets" );
+  cds->add_action( "potion,if=pet.infernal.active" );
+  cds->add_action( "berserking,if=pet.infernal.active" );
+  cds->add_action( "blood_fury,if=pet.infernal.active" );
+  cds->add_action( "fireblood,if=pet.infernal.active" );
+  cds->add_action( "use_items,if=pet.infernal.active|time_to_die<21" );
+
+  havoc->add_action( "conflagrate,if=buff.backdraft.down&soul_shard>=1&soul_shard<=4", "Havoc logic, did not touched at rain of fire yet" );
+  havoc->add_action( "soul_fire,if=cast_time<havoc_remains&soul_shard<3" );
+  havoc->add_action( "immolate,cycle_targets=1,if=dot.immolate.refreshable" );
+  havoc->add_action( "chaos_bolt,if=cast_time<havoc_remains&!(talent.avatar_of_destruction&active_enemies>1&talent.inferno.enabled)" );
+  havoc->add_action( "rain_of_fire,if=talent.avatar_of_destruction&active_enemies>1&talent.inferno.enabled" );
+  havoc->add_action( "shadowburn,if=target.health.pct<20&(!talent.eradication&!talent.ashen_remains)&(talent.ruin.rank=2&talent.conflagration_of_chaos.rank=2)" );
+  havoc->add_action( "incinerate,if=cast_time<havoc_remains" );
 }
 //destruction_apl_end
 
