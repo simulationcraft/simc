982b2d651fe86a1c2857255a8a4e19b1e4582612
Facenomore23 <32964159+Facenomore23@users.noreply.github.com>
2022-11-13 19:20:13 +0100

    [Warlock] Destruction APL

--- engine/class_modules/apl/warlock.cpp
+++ engine/class_modules/apl/warlock.cpp
@@ -113,32 +113,106 @@ void destruction( player_t* p )
 {
   action_priority_list_t* default_ = p->get_action_priority_list( "default" );
   action_priority_list_t* precombat = p->get_action_priority_list( "precombat" );
+  action_priority_list_t* aoe = p->get_action_priority_list( "aoe" );
+  action_priority_list_t* items = p->get_action_priority_list( "items" );
+  action_priority_list_t* ogcd = p->get_action_priority_list( "ogcd" );
+  action_priority_list_t* cleave = p->get_action_priority_list( "cleave" );
+  action_priority_list_t* havoc = p->get_action_priority_list( "havoc" );
 
   precombat->add_action( "flask" );
   precombat->add_action( "food" );
   precombat->add_action( "augmentation" );
   precombat->add_action( "summon_pet" );
+  precombat->add_action( "variable,name=cleave_apl,default=0,op=reset" );
   precombat->add_action( "grimoire_of_sacrifice,if=talent.grimoire_of_sacrifice.enabled" );
   precombat->add_action( "snapshot_stats" );
   precombat->add_action( "soul_fire" );
   precombat->add_action( "cataclysm" );
   precombat->add_action( "incinerate" );
 
-  default_->add_action( "chaos_bolt,if=soul_shard=5" );
-  default_->add_action( "soul_fire,if=soul_shard<=4&(buff.backdraft.up|!talent.backdraft)" );
+  default_->add_action( "call_action_list,name=cleave,if=active_enemies=2|variable.cleave_apl" );
+  default_->add_action( "call_action_list,name=aoe,if=active_enemies>=3" );
+  default_->add_action( "conflagrate,if=(talent.roaring_blaze&debuff.conflagrate.remains<1.5)|charges=max_charges" );
+  default_->add_action( "dimensional_rift,if=soul_shard<4.7&(charges>2|time_to_die<cooldown.dimensional_rift.duration)" );
   default_->add_action( "cataclysm" );
-  default_->add_action( "immolate,if=dot.immolate.remains<5" );
-  default_->add_action( "channel_demonfire" );
-  default_->add_action( "use_items" );
+  default_->add_action( "channel_demonfire,if=talent.raging_demonfire" );
+  default_->add_action( "soul_fire,if=soul_shard<=4" );
+  default_->add_action( "immolate,if=((dot.immolate.refreshable&talent.internal_combustion)|dot.immolate.remains<3)&(!talent.cataclysm|cooldown.cataclysm.remains>dot.immolate.remains)&(!talent.soul_fire|cooldown.soul_fire.remains>dot.immolate.remains)" );
+  default_->add_action( "chaos_bolt,if=pet.infernal.active|pet.blasphemy.active|soul_shard>=4" );
+  default_->add_action( "call_action_list,name=items" );
+  default_->add_action( "call_action_list,name=ogcd" );
   default_->add_action( "summon_infernal" );
+  default_->add_action( "channel_demonfire,if=talent.ruin.rank>1&!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))" );
+  default_->add_action( "conflagrate,if=buff.backdraft.down&soul_shard>=1.5" );
+  default_->add_action( "chaos_bolt,if=buff.rain_of_chaos.remains>cast_time" );
   default_->add_action( "chaos_bolt,if=buff.backdraft.up" );
-  default_->add_action( "conflagrate,if=talent.backdraft&buff.backdraft.down" );
-  default_->add_action( "conflagrate,if=soul_shard<=4.5" );
-  default_->add_action( "chaos_bolt,if=soul_shard>=4.5|!talent.backdraft" );
-  default_->add_action( "immolate,if=dot.immolate.refreshable&(!talent.soul_fire&!talent.cataclysm)" );
+  default_->add_action( "chaos_bolt,if=talent.eradication&debuff.eradication.remains<cast_time&!action.chaos_bolt.in_flight" );
+  default_->add_action( "chaos_bolt,if=buff.madness_cb.up" );
+  default_->add_action( "channel_demonfire,if=!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))" );
   default_->add_action( "dimensional_rift" );
-  default_->add_action( "shadowburn,if=target.health.pct<=20" );
+  default_->add_action( "chaos_bolt,if=soul_shard>3.5" );
+  default_->add_action( "chaos_bolt,if=talent.soul_conduit&!talent.madness_of_the_azjaqir|!talent.backdraft" );
+  default_->add_action( "chaos_bolt,if=time_to_die<5&time_to_die>cast_time+travel_time" );
+  default_->add_action( "conflagrate,if=charges>(max_charges-1)|time_to_die<gcd*charges" );
   default_->add_action( "incinerate" );
+
+  aoe->add_action( "rain_of_fire,if=pet.infernal.active" );
+  aoe->add_action( "rain_of_fire,if=talent.avatar_of_destruction" );
+  aoe->add_action( "channel_demonfire,if=dot.immolate.remains>cast_time" );
+  aoe->add_action( "immolate,cycle_targets=1,if=dot.immolate.remains<5&(!talent.cataclysm.enabled|cooldown.cataclysm.remains>dot.immolate.remains)" );
+  aoe->add_action( "call_action_list,name=items" );
+  aoe->add_action( "call_action_list,name=ogcd" );
+  aoe->add_action( "summon_infernal" );
+  aoe->add_action( "rain_of_fire" );
+  aoe->add_action( "havoc,cycle_targets=1,if=!(self.target=target)" );
+  aoe->add_action( "incinerate,if=talent.fire_and_brimstone.enabled&buff.backdraft.up" );
+  aoe->add_action( "soul_fire" );
+  aoe->add_action( "conflagrate,if=buff.backdraft.down|!talent.backdraft" );
+  aoe->add_action( "immolate,if=refreshable" );
+  aoe->add_action( "incinerate" );
+
+  items->add_action( "use_items,if=pet.infernal.active|!talent.summon_infernal|time_to_die<21" );
+
+  ogcd->add_action( "potion,if=pet.infernal.active|!talent.summon_infernal" );
+  ogcd->add_action( "berserking,if=pet.infernal.active|!talent.summon_infernal" );
+  ogcd->add_action( "blood_fury,if=pet.infernal.active|!talent.summon_infernal" );
+  ogcd->add_action( "fireblood,if=pet.infernal.active|!talent.summon_infernal" );
+
+  cleave->add_action( "call_action_list,name=havoc,if=havoc_active&havoc_remains>gcd" );
+  cleave->add_action( "variable,name=pool_soul_shards,value=cooldown.havoc.remains<=10|talent.mayhem" );
+  cleave->add_action( "conflagrate,if=(talent.roaring_blaze.enabled&debuff.conflagrate.remains<1.5)|charges=max_charges" );
+  cleave->add_action( "dimensional_rift,if=soul_shard<4.7&(charges>2|time_to_die<cooldown.dimensional_rift.duration)" );
+  cleave->add_action( "cataclysm" );
+  cleave->add_action( "channel_demonfire,if=talent.raging_demonfire" );
+  cleave->add_action( "soul_fire,if=soul_shard<=4&!variable.pool_soul_shards" );
+  cleave->add_action( "immolate,cycle_targets=1,if=((talent.internal_combustion&dot.immolate.refreshable)|dot.immolate.remains<3)&(!talent.cataclysm|cooldown.cataclysm.remains>remains)&(!talent.soul_fire|cooldown.soul_fire.remains>remains)" );
+  cleave->add_action( "chaos_bolt,if=pet.infernal.active|pet.blasphemy.active|soul_shard>=4" );
+  cleave->add_action( "call_action_list,name=items" );
+  cleave->add_action( "call_action_list,name=ogcd" );
+  cleave->add_action( "summon_infernal" );
+  cleave->add_action( "havoc,cycle_targets=1,if=!(target=self.target)" );
+  cleave->add_action( "channel_demonfire,if=talent.ruin.rank>1&!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))" );
+  cleave->add_action( "conflagrate,if=buff.backdraft.down&soul_shard>=1.5&!variable.pool_soul_shards" );
+  cleave->add_action( "chaos_bolt,if=buff.rain_of_chaos.remains>cast_time" );
+  cleave->add_action( "chaos_bolt,if=buff.backdraft.up&!variable.pool_soul_shards" );
+  cleave->add_action( "chaos_bolt,if=talent.eradication&!variable.pool_soul_shards&debuff.eradication.remains<cast_time&!action.chaos_bolt.in_flight" );
+  cleave->add_action( "chaos_bolt,if=buff.madness_cb.up" );
+  cleave->add_action( "soul_fire,if=soul_shard<=4&talent.mayhem" );
+  cleave->add_action( "channel_demonfire,if=!(talent.diabolic_embers&talent.avatar_of_destruction&(talent.burn_to_ashes|talent.chaos_incarnate))" );
+  cleave->add_action( "dimensional_rift" );
+  cleave->add_action( "shadowburn,if=charges>1&target.health.pct<20&(!talent.eradication&!talent.ashen_remains)&(talent.ruin.rank=2&talent.conflagration_of_chaos.rank=2)" );
+  cleave->add_action( "chaos_bolt,if=!variable.pool_soul_shards&(talent.soul_conduit&!talent.madness_of_the_azjaqir|!talent.backdraft)" );
+  cleave->add_action( "chaos_bolt,if=time_to_die<5&time_to_die>cast_time+travel_time" );
+  cleave->add_action( "conflagrate,if=charges>(max_charges-1)|time_to_die<gcd*charges" );
+  cleave->add_action( "incinerate" );
+
+  havoc->add_action( "conflagrate,if=talent.backdraft&buff.backdraft.down&soul_shard>=1&soul_shard<=4" );
+  havoc->add_action( "soul_fire,if=cast_time<havoc_remains&soul_shard<3" );
+  havoc->add_action( "immolate,cycle_targets=1,if=dot.immolate.refreshable" );
+  havoc->add_action( "chaos_bolt,if=cast_time<havoc_remains&!(talent.avatar_of_destruction&active_enemies>1&talent.inferno.enabled)" );
+  havoc->add_action( "rain_of_fire,if=talent.avatar_of_destruction&active_enemies>1&talent.inferno.enabled" );
+  havoc->add_action( "conflagrate,if=!talent.backdraft" );
+  havoc->add_action( "incinerate,if=cast_time<havoc_remains" );
 }
 //destruction_apl_end
 
