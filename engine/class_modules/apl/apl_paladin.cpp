0db75aa02d1dbceff0885d9eef5408af884f75f3
Woliance <34073843+Woliance@users.noreply.github.com>
2022-11-25 19:40:30 +0100

    Update apl_paladin.cpp

--- engine/class_modules/apl/apl_paladin.cpp
+++ engine/class_modules/apl/apl_paladin.cpp
@@ -263,12 +263,8 @@ void protection( player_t* p )
   action_priority_list_t* default_ = p->get_action_priority_list( "default" );
   action_priority_list_t* precombat = p->get_action_priority_list( "precombat" );
   action_priority_list_t* cooldowns = p->get_action_priority_list( "cooldowns" );
+  action_priority_list_t* trinkets = p->get_action_priority_list( "trinkets" );
   action_priority_list_t* standard = p->get_action_priority_list( "standard" );
-
-  default_->add_action( "auto_attack" );
-  default_->add_action( "variable,name=might_or_sentinel,value=(!talent.avenging_wrath.enabled&(!talent.avenging_wrath_might.enabled|talent.sentinel_enabled))" );
-  default_->add_action( "call_action_list,name=cooldowns" );
-  default_->add_action( "call_action_list,name=standard" );
 
   precombat->add_action( "flask" );
   precombat->add_action( "food" );
@@ -277,20 +273,35 @@ void protection( player_t* p )
   precombat->add_action( "lights_judgment" );
   precombat->add_action( "arcane_torrent" );
   precombat->add_action( "consecration" );
+  precombat->add_action( "variable,name=trinket_1_sync,op=setif,value=1,value_else=0.5,condition=trinket.1.has_use_buff&(talent.moment_of_glory&trinket.1.cooldown.duration%%cooldown.moment_of_glory.duration=0|(talent.avenging_wrath&!talent.moment_of_glory)&trinket.1.cooldown.duration%%cooldown.avenging_wrath.duration<=20)", "Evaluates a trinkets cooldown, divided by moment of glory or avenging wraths's cooldown. If it's value has no remainder return 1, else return 0.5." );
+  precombat->add_action( "variable,name=trinket_2_sync,op=setif,value=1,value_else=0.5,condition=trinket.2.has_use_buff&(talent.moment_of_glory&trinket.2.cooldown.duration%%cooldown.moment_of_glory.duration=0|(talent.avenging_wrath&!talent.moment_of_glory)&trinket.2.cooldown.duration%%cooldown.avenging_wrath.duration<=20)" );
+  precombat->add_action( "variable,name=trinket_priority,op=setif,value=2,value_else=1,condition=!trinket.1.has_use_buff&trinket.2.has_use_buff|trinket.2.has_use_buff&((trinket.2.cooldown.duration%trinket.2.proc.any_dps.duration)*(1.5+trinket.2.has_buff.strength)*(variable.trinket_2_sync))>((trinket.1.cooldown.duration%trinket.1.proc.any_dps.duration)*(1.5+trinket.1.has_buff.strength)*(variable.trinket_1_sync))" );
+  precombat->add_action( "variable,name=trinket_1_buffs,value=trinket.1.has_buff.strength|trinket.1.has_buff.mastery|trinket.1.has_buff.versatility|trinket.1.has_buff.haste|trinket.1.has_buff.crit" );
+  precombat->add_action( "variable,name=trinket_2_buffs,value=trinket.2.has_buff.strength|trinket.2.has_buff.mastery|trinket.2.has_buff.versatility|trinket.2.has_buff.haste|trinket.2.has_buff.crit" );
+
+  default_->add_action( "auto_attack" );
+  default_->add_action( "variable,name=might_or_sentinel,value=(!talent.avenging_wrath.enabled&(!talent.avenging_wrath_might.enabled|talent.sentinel_enabled))" );
+  default_->add_action( "call_action_list,name=cooldowns" );
+  default_->add_action( "call_action_list,name=trinkets" );
+  default_->add_action( "call_action_list,name=standard" );
 
   cooldowns->add_action( "seraphim" );
   cooldowns->add_action( "avenging_wrath,if=(buff.seraphim.up|!talent.seraphim.enabled)" );
-  cooldowns->add_action( "use_items,if=buff.avenging_wrath.up|variable.might_or_sentinel" );
   cooldowns->add_action( "potion,if=buff.avenging_wrath.up|variable.might_or_sentinel" );
-  cooldowns->add_action( "moment_of_glory,if=(buff.avenging_wrath.remains>15|(cooldown.avenging_wrath.remains>15))" );
+  cooldowns->add_action( "moment_of_glory,if=(buff.avenging_wrath.remains<15|(time>10|(cooldown.avenging_wrath.remains>15))&(cooldown.avengers_shield.remains&cooldown.judgment.remains&cooldown.hammer_of_wrath.remains))" );
   cooldowns->add_action( "holy_avenger,if=buff.avenging_wrath.up|variable.might_or_sentinel|cooldown.avenging_wrath.remains>60" );
   cooldowns->add_action( "bastion_of_light,if=buff.avenging_wrath.up" );
 
-  standard->add_action( "shield_of_the_righteous,if=(cooldown.seraphim.remains>=5|!talent.seraphim.enabled)&(buff.bastion_of_light.up|((holy_power=3&!buff.blessing_of_dusk.up&!buff.holy_avenger.up)|holy_power=5)|buff.divine_purpose.up)" );
-  standard->add_action( "divine_toll,if=time>20|(buff.seraphim.up&buff.avenging_wrath.up|(!talent.avenging_wrath.enabled&(!talent.avenging_wrath_might.enabled|talent.sentinel_enabled))&(buff.moment_of_glory.up|!talent.moment_of_glory.enabled))" );
+  trinkets->add_action( "use_item,slot=trinket1,if=buff.moment_of_glory.up&(!trinket.2.has_cooldown|trinket.2.cooldown.remains|variable.trinket_priority=1)|trinket.1.proc.any_dps.duration>=fight_remains" );
+  trinkets->add_action( "use_item,slot=trinket2,if=buff.moment_of_glory.up&(!trinket.1.has_cooldown|trinket.1.cooldown.remains|variable.trinket_priority=2)|trinket.2.proc.any_dps.duration>=fight_remains" );
+  trinkets->add_action( "use_item,slot=trinket1,if=!variable.trinket_1_buffs&(trinket.2.cooldown.remains|!variable.trinket_2_buffs|cooldown.moment_of_glory.remains>20)" );
+  trinkets->add_action( "use_item,slot=trinket2,if=!variable.trinket_2_buffs&(trinket.1.cooldown.remains|!variable.trinket_1_buffs|cooldown.moment_of_glory.remains>20)" );
+
+  standard->add_action( "shield_of_the_righteous,if=(cooldown.seraphim.remains>=5|!talent.seraphim.enabled)&(((holy_power=3&!buff.blessing_of_dusk.up&!buff.holy_avenger.up)|(holy_power=5)|buff.bastion_of_light.up|buff.divine_purpose.up))" );
   standard->add_action( "avengers_shield,if=buff.moment_of_glory.up|!talent.moment_of_glory.enabled" );
   standard->add_action( "hammer_of_wrath,if=buff.avenging_wrath.up|(!talent.avenging_wrath.enabled&(!talent.avenging_wrath_might.enabled|talent.sentinel_enabled))" );
   standard->add_action( "judgment,target_if=min:debuff.judgment.remains,if=charges=2|!talent.crusaders_judgment.enabled" );
+  standard->add_action( "divine_toll,if=time>20|((!talent.seraphim.enabled|buff.seraphim.up)&(buff.avenging_wrath.up|(!talent.avenging_wrath.enabled&(!talent.avenging_wrath_might.enabled|talent.sentinel_enabled)))&(buff.moment_of_glory.up|!talent.moment_of_glory.enabled))" );
   standard->add_action( "avengers_shield" );
   standard->add_action( "hammer_of_wrath" );
   standard->add_action( "judgment,target_if=min:debuff.judgment.remains" );
