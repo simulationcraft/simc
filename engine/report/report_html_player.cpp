e7341cf020ac051989162668641da07a30cada5d
Vít Šefl <vituscze@gmail.com>
2022-12-23 19:14:47 +0100

    Don't show talent iframes for multiactor sims

--- engine/report/report_html_player.cpp
+++ engine/report/report_html_player.cpp
@@ -1989,12 +1989,17 @@ void print_html_talents( report::sc_html_stream& os, const player_t& p )
      << "<h3 class=\"toggle\">Talents</h3>\n"
      << "<div class=\"toggle-content hide\">\n";
 
-  auto w_ = raidbots_talent_render_width( p.specialization(), 600 );
-  os.format( "<iframe src=\"{}\" width=\"{}\" height=\"650\"></iframe>\n",
-             raidbots_talent_render_src( p.talents_str, p.true_level, w_, false, p.dbc->ptr ), w_ );
+  int num_players = p.sim->players_by_name.size();
+  if ( num_players == 1 )
+  {
+    auto w_ = raidbots_talent_render_width( p.specialization(), 600 );
+    os.format( "<iframe src=\"{}\" width=\"{}\" height=\"650\"></iframe>\n",
+               raidbots_talent_render_src( p.talents_str, p.true_level, w_, false, p.dbc->ptr ), w_ );
 
-  os << "<h3 class=\"toggle\">Talent Tables</h3>\n"
-     << "<div class=\"toggle-content hide\">\n";
+    // Hide the talent table only if the Raidbots talent iframe is present.
+    os << "<h3 class=\"toggle\">Talent Tables</h3>\n"
+       << "<div class=\"toggle-content hide\">\n";
+  }
 
   os.format( "<table class=\"sc\"><tr><th>Row</th><th>{} Talents [{}]</th></tr>\n",
              util::player_type_string_long( p.type ),
@@ -2039,8 +2044,11 @@ void print_html_talents( report::sc_html_stream& os, const player_t& p )
   }
   os << "</table>\n";
 
+  // Close the talent table div only if it exists.
+  if ( num_players == 1 )
+    os << "</div>\n";
+
   os << "</div>\n"
-     << "</div>\n"
      << "</div>\n";
 }
 
@@ -3991,10 +3999,13 @@ void print_html_player_results_spec_gear( report::sc_html_stream& os, const play
   {
     os << "<div>\n";
 
-    auto w_ = raidbots_talent_render_width( p.specialization(), 125 );
-    os.format(
-      "<iframe src=\"{}\" width=\"{}\" height=\"125\" style=\"float: left; margin-right: 10px; margin-top: 5px;\"></iframe>\n",
-      raidbots_talent_render_src( p.talents_str, p.true_level, w_, true, p.dbc->ptr ), w_ );
+    if ( p.sim->players_by_name.size() == 1 )
+    {
+      auto w_ = raidbots_talent_render_width( p.specialization(), 125 );
+      os.format(
+        "<iframe src=\"{}\" width=\"{}\" height=\"125\" style=\"float: left; margin-right: 10px; margin-top: 5px;\"></iframe>\n",
+        raidbots_talent_render_src( p.talents_str, p.true_level, w_, true, p.dbc->ptr ), w_ );
+    }
 
     os << "<table class=\"sc spec\">\n";
 
