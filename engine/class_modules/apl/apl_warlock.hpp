3a97142ea017a3e069aaf3107b5a64b401958536
Denis Craig <admin@deniscraig.com>
2023-01-10 12:27:46 +0000

    Fix spacing

--- engine/class_modules/apl/apl_warlock.hpp
+++ engine/class_modules/apl/apl_warlock.hpp
@@ -6,13 +6,13 @@ struct player_t;
 
 namespace warlock_apl
 {
-	std::string potion( const player_t* );
+  std::string potion( const player_t* );
   std:: string flask( const player_t* );
   std::string food( const player_t* );
   std::string rune( const player_t* );
   std::string temporary_enchant( const player_t* );
 
-	void affliction( player_t* );
-	void demonology( player_t* );
+  void affliction( player_t* );
+  void demonology( player_t* );
   void destruction( player_t* );
 }  // namespace warlock_apl
