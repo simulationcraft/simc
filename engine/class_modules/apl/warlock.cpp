70b1644fd13fb19ca0c2914e6071b96dfcd59fe5
Denis Craig <admin@deniscraig.com>
2023-01-11 02:26:53 +0000

    [Warlock] Affliction APL: Shuffle priorities of DoTs & Shadow Embrace (#7254)
    * Migrate warlock APL to python generation
    
    * Fix spacing
    
    * Formatting
    
    * Fix scripts simc file locations
    
    * Use Raszageth trinkets when available, otherwise wait for CDs. Add PI invoke
    
    * Add missing call to variables action list
    
    * Update PS to handle Soul Rot & Darkglare talents, update Darkglare to only run when DoTs & PI up
    
    * Update malefic rapture actions to better handle buffs & procs
    
    * Move priority of shadow embrace DS/SB, clean up DoT order
    
    Co-authored-by: Azevara <46403881+Azevara@users.noreply.github.com>

--- engine/class_modules/apl/warlock.cpp
+++ engine/class_modules/apl/warlock.cpp
@@ -63,13 +63,14 @@ void affliction( player_t* p )
   default_->add_action( "call_action_list,name=aoe,if=active_enemies>3" );
   default_->add_action( "call_action_list,name=ogcd" );
   default_->add_action( "call_action_list,name=items" );
-  default_->add_action( "haunt" );
   default_->add_action( "soul_swap,if=dot.unstable_affliction.remains<5" );
   default_->add_action( "unstable_affliction,if=remains<5" );
   default_->add_action( "agony,if=remains<5" );
-  default_->add_action( "siphon_life,if=remains<5" );
   default_->add_action( "corruption,if=remains<5" );
-  default_->add_action( "soul_tap" );
+  default_->add_action( "siphon_life,if=remains<5" );
+  default_->add_action( "haunt" );
+  default_->add_action( "drain_soul,if=talent.shadow_embrace&(debuff.shadow_embrace.stack<3|debuff.shadow_embrace.remains<3)" );
+  default_->add_action( "shadow_bolt,if=talent.shadow_embrace&(debuff.shadow_embrace.stack<3|debuff.shadow_embrace.remains<3)" );
   default_->add_action( "phantom_singularity,if=!talent.soul_rot|cooldown.soul_rot.remains<=execute_time|!talent.summon_darkglare" );
   default_->add_action( "vile_taint" );
   default_->add_action( "soul_rot,if=variable.ps_up&variable.vt_up|!talent.summon_darkglare" );
@@ -77,15 +78,16 @@ void affliction( player_t* p )
   default_->add_action( "malefic_rapture,if=soul_shard>4|(talent.tormented_crescendo&buff.tormented_crescendo.stack=1&soul_shard>3)" );
   default_->add_action( "malefic_rapture,if=talent.dread_touch&talent.malefic_affliction&debuff.dread_touch.remains<2&buff.malefic_affliction.stack=3" );
   default_->add_action( "malefic_rapture,if=talent.malefic_affliction&buff.malefic_affliction.stack<3" );
-  default_->add_action( "malefic_rapture,if=talent.tormented_crescendo&buff.tormented_crescendo.up&!debuff.dread_touch.up" );
+  default_->add_action( "malefic_rapture,if=talent.tormented_crescendo&buff.tormented_crescendo.react&!debuff.dread_touch.react" );
   default_->add_action( "malefic_rapture,if=talent.tormented_crescendo&buff.tormented_crescendo.stack=2" );
   default_->add_action( "malefic_rapture,if=variable.cd_dots_up" );
-  default_->add_action( "malefic_rapture,if=talent.tormented_crescendo&talent.nightfall&buff.tormented_crescendo.up&buff.nightfall.up" );
-  default_->add_action( "drain_soul,if=buff.nightfall.react|talent.shadow_embrace&(debuff.shadow_embrace.stack<3|debuff.shadow_embrace.remains<2)" );
-  default_->add_action( "shadow_bolt,if=buff.nightfall.react|talent.shadow_embrace&(debuff.shadow_embrace.stack<3|debuff.shadow_embrace.remains<2)" );
+  default_->add_action( "malefic_rapture,if=talent.tormented_crescendo&talent.nightfall&buff.tormented_crescendo.react&buff.nightfall.react" );
   default_->add_action( "drain_life,if=buff.inevitable_demise.stack>48|buff.inevitable_demise.stack>20&time_to_die<4" );
+  default_->add_action( "drain_soul,if=buff.nightfall.react" );
+  default_->add_action( "shadow_bolt,if=buff.nightfall.react" );
   default_->add_action( "agony,if=refreshable" );
   default_->add_action( "corruption,if=refreshable" );
+  default_->add_action( "soul_tap" );
   default_->add_action( "drain_soul,interrupt=1" );
   default_->add_action( "shadow_bolt" );
 
@@ -94,7 +96,7 @@ void affliction( player_t* p )
   variables->add_action( "variable,name=sr_up,op=set,value=dot.soul_rot.ticking|!talent.soul_rot" );
   variables->add_action( "variable,name=cd_dots_up,op=set,value=variable.ps_up&variable.vt_up&variable.sr_up" );
   variables->add_action( "variable,name=has_cds,op=set,value=talent.phantom_singularity|talent.vile_taint|talent.soul_rot|talent.summon_darkglare" );
-  variables->add_action( "variable,name=cds_active,op=set,value=!variable.has_cds|(pet.darkglare.active|variable.cd_dots_up|buff.power_infusion.up)" );
+  variables->add_action( "variable,name=cds_active,op=set,value=!variable.has_cds|(pet.darkglare.active|variable.cd_dots_up|buff.power_infusion.react)" );
 
   aoe->add_action( "call_action_list,name=ogcd" );
   aoe->add_action( "call_action_list,name=items" );
