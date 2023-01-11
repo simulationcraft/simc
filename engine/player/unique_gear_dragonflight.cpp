d2205aad55b4cfaa9c4f0ee6a43bd337f8dff774
gastank <42421688+gastank@users.noreply.github.com>
2023-01-11 12:18:14 -0800

    [Gear] voidmender's shadowgem procs on all druid casts

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -1554,7 +1554,7 @@ void voidmenders_shadowgem( special_effect_t& effect )
   stacking_driver->custom_buff        = stacking_buff;
 
   // TODO: Check this. As of 28/12/22 every single spell on shadow procs this item for some reason.
-  if ( effect.player->specialization() == PRIEST_SHADOW )
+  if ( effect.player->specialization() == PRIEST_SHADOW || effect.player->type == player_e::DRUID )
   {
     stacking_driver->proc_flags_ |= PF_ALL_DAMAGE;
     stacking_driver->proc_flags2_ |= PF2_CAST | PF2_CAST_DAMAGE;
