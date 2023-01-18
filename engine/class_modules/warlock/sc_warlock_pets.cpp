dd007f9c397bb8f01f1bc3229da626074ba22b17
Saeldur <5496498+Saeldur@users.noreply.github.com>
2023-01-18 20:49:48 +0000

    [Affliction] Fix Grim Reach use Result_amount not result_total. Checked debug log and they do different things.

--- engine/class_modules/warlock/sc_warlock_pets.cpp
+++ engine/class_modules/warlock/sc_warlock_pets.cpp
@@ -2205,7 +2205,7 @@ struct eye_beam_t : public warlock_pet_spell_t
 
   void impact( action_state_t* s ) override
   {
-    auto raw_damage = s->result_total;
+    auto raw_damage = s->result_amount;
 
     warlock_pet_spell_t::impact( s );
 
