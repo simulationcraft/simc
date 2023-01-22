8fa8c2e27a2e417215e9d26fd39321cf223a16c6
0xjc <5281913+0xjc@users.noreply.github.com>
2023-01-22 01:01:36 -0500

    [Warlock] Pit Lord is not affected by The Expendables

--- engine/class_modules/warlock/sc_warlock.cpp
+++ engine/class_modules/warlock/sc_warlock.cpp
@@ -2060,6 +2060,10 @@ void warlock_t::expendables_trigger_helper( warlock_pet_t* source )
     if ( lock_pet == source )
       continue;
 
+    // Pit Lord is not affected by The Expendables
+    if ( lock_pet->pet_type == PET_PIT_LORD )
+      continue;
+
     lock_pet->buffs.the_expendables->increment();
   }
 }
