98d6135668424e3c12cc8f1acfe8bbc80281a42d
srliao <906239+srliao@users.noreply.github.com>
2023-01-31 20:58:07 -0500

    refactor warrior apl into sep file

---
+++ engine/class_modules/apl/apl_warrior.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+struct player_t;
+
+namespace warrior_apl {
+
+void protection( player_t* );
+void fury( player_t* );
+void arms( player_t* );
+void default_precombat( player_t* );
+
+} // namespace warrior_apl
