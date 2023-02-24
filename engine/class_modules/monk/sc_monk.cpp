879537d33992b54c070e2cc32ee24387539a3533
JeremyMonk <101205288+JeremyMonk@users.noreply.github.com>
2023-02-24 07:27:45 -0600

    whitespace

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -3533,7 +3533,7 @@ namespace monk
       struct roll_t : public monk_spell_t
       {
         roll_t( monk_t *player, util::string_view options_str )
-          : monk_spell_t( "roll", player, ( player->talent.general.chi_torpedo->ok() ? spell_data_t::not_found() : player->spec.roll ))
+          : monk_spell_t( "roll", player, ( player->talent.general.chi_torpedo->ok() ? spell_data_t::not_found() : player->spec.roll ) )
         {
           cast_during_sck = true;
 
