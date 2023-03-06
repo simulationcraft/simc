735c56a2bea8c2fba17cf27a9b5ccfa5185b2374
Tom Herbert <taherbert@gmail.com>
2023-03-05 17:16:20 -0800

    force shadow covenant buff on halo and DS

--- engine/class_modules/priest/sc_priest.cpp
+++ engine/class_modules/priest/sc_priest.cpp
@@ -241,6 +241,9 @@ struct divine_star_spell_t final : public priest_spell_t
 
     // This is not found in the affected spells for Dark Ascension, overriding it manually
     force_buff_effect( p.buffs.dark_ascension, 1 );
+    // This is not found in the affected spells for Shadow Covenant, overriding it manually
+    // Final two params allow us to override the 25% damage buff when twilight corruption is selected (25% -> 35%)
+    force_buff_effect( p.buffs.shadow_covenant, 1, false, true );
   }
 
   // Hits twice, but only if you are at the correct distance
@@ -355,6 +358,9 @@ struct halo_spell_t final : public priest_spell_t
 
     // This is not found in the affected spells for Dark Ascension, overriding it manually
     force_buff_effect( p.buffs.dark_ascension, 1 );
+    // This is not found in the affected spells for Shadow Covenant, overriding it manually
+    // Final two params allow us to override the 25% damage buff when twilight corruption is selected (25% -> 35%)
+    force_buff_effect( p.buffs.shadow_covenant, 1, false, true );
   }
 };
 
@@ -2421,7 +2427,7 @@ struct priest_module_t final : public module_t
   void init( player_t* p ) const override
   {
     p->buffs.guardian_spirit  = make_buff( p, "guardian_spirit",
-                                          p->find_spell( 47788 ) );  // Let the ability handle the CD
+                                           p->find_spell( 47788 ) );  // Let the ability handle the CD
     p->buffs.pain_suppression = make_buff( p, "pain_suppression",
                                            p->find_spell( 33206 ) );  // Let the ability handle the CD
   }
