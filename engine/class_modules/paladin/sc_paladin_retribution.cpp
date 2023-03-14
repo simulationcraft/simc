58c21c156bc229093012260bd233566ac50ea5c3
Maxime Serrano <mserrano@alumni.cmu.edu>
2023-03-13 20:27:38 -0700

    [Paladin] Apply Judgment ret talents to Divine Toll judgments

--- engine/class_modules/paladin/sc_paladin_retribution.cpp
+++ engine/class_modules/paladin/sc_paladin_retribution.cpp
@@ -998,6 +998,21 @@ struct judgment_ret_t : public judgment_t
     {
       seal_of_wrath = new seal_of_wrath_t( p );
     }
+
+    if ( p -> is_ptr() )
+    {
+      if ( p -> talents.boundless_judgment -> ok() )
+      {
+        holy_power_generation += p -> talents.boundless_judgment -> effectN( 1 ).base_value();
+      }
+
+      // we don't do the blessed champion stuff here; DT judgments do not seem to cleave
+
+      if ( p -> talents.judge_jury_and_executioner -> ok() )
+      {
+        base_crit += p -> talents.judge_jury_and_executioner -> effectN( 1 ).percent();
+      }
+    }
   }
 
   void init() override
