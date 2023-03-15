8330039ef8264d1f5ec2fc8c1810e8af022c535c
Maxime Serrano <mserrano@alumni.cmu.edu>
2023-03-14 17:50:14 -0700

    [Paladin] Better implementation of Adjudication's Blessed Hammers

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1458,6 +1458,7 @@ class SpellDataGenerator(DataGenerator):
             ( 387178, 0 ),          # Empyrean Legacy buff
             ( 224239, 0 ),          # Tempest divine storm
             ( 384810, 0 ),          # Seal of Clarity buff
+            ( 404140, 0 ),          # Blessed Hammers for Adjudication
         ),
 
         # Hunter:
@@ -1568,7 +1569,7 @@ class SpellDataGenerator(DataGenerator):
             ( 394757, 0 ),          # Flagellation talent damage spell
             ( 394758, 0 ),          # Flagellation talent persist buff
             ( 385948, 0 ), ( 385949, 0 ), ( 385951, 0 ), # Finality talent buffs
-            ( 385960, 0 ), ( 386081, 0 ), # Lingering Shadow talent background spells 
+            ( 385960, 0 ), ( 386081, 0 ), # Lingering Shadow talent background spells
             ( 393724, 0 ), ( 393725, 0 ), # T29 Assassination Set Bonus Spells
             ( 393727, 0 ), ( 393728, 0 ), ( 394879, 0 ), ( 394888, 0 ), # T29 Outlaw Set Bonus Spells
             ( 393729, 0 ), ( 393730, 0 ), # T29 Subtlety Set Bonus Spells
