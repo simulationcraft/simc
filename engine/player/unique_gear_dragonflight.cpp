dcca5571a0d6c19e9553eb21ca381bfe08579e5e
nyterage <nyterage@gmail.com>
2023-03-30 15:50:31 -0700

    [Gear] Voice of the Silent Star Adjustment
    Actually increase the rppm by 50%, rather than 150%

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -4205,8 +4205,8 @@ void voice_of_the_silent_star( special_effect_t& effect )
   effect.proc_flags_ = PF_ALL_DAMAGE;
   effect.proc_flags2_ = PF2_ALL_HIT;
   // 30-3-2023, This effect appears to be proccing ~1.5x more than spell data suggests. 
-  // Modifiying RPPM data by 1.5x to emulate this behavior, will need to be rechecked on launch.
-  effect.rppm_modifier_ = effect.driver() -> real_ppm() * 1.5; 
+  // Modifiying RPPM data by 50% to emulate this behavior, will need to be rechecked on launch.
+  effect.rppm_modifier_ = effect.driver() -> real_ppm() * 0.5; 
   new dbc_proc_callback_t( effect.player, effect );
 }
 
