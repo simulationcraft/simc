4e76001892a558620db14f5c6f258ec4c4b913ca
nyterage <nyterage@gmail.com>
2023-04-04 06:45:49 -0700

    [Gear] Simplify Elementum Pocket Anvil

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -3628,8 +3628,7 @@ void elementium_pocket_anvil_use ( special_effect_t& e )
 
   struct elementium_pocket_anvil_use_damage_t : public generic_aoe_proc_t
   {
-    buff_t* buff;
-    elementium_pocket_anvil_use_damage_t( const special_effect_t& e, buff_t* b ) : generic_aoe_proc_t( e, "anvil_strike_damage", e.player -> find_spell( 408578 ) ), buff( b )
+    elementium_pocket_anvil_use_damage_t( const special_effect_t& e ) : generic_aoe_proc_t( e, "anvil_strike_damage", e.player -> find_spell( 408578 ) )
     {
       background = true;
       split_aoe_damage = false;
@@ -3639,7 +3638,7 @@ void elementium_pocket_anvil_use ( special_effect_t& e )
     void execute() override
     {
       generic_aoe_proc_t::execute();
-      buff -> trigger();
+      player -> buffs.anvil_strike -> trigger();
     }
   };
 
@@ -3649,8 +3648,7 @@ void elementium_pocket_anvil_use ( special_effect_t& e )
     action_t* damage;
 
     elementium_pocket_anvil_use_t( const special_effect_t& e ) : generic_proc_t( e, "anvil_strike", e.driver() ),
-        buff( buff_t::find( e.player, "anvil_strike" ) ),
-        damage( create_proc_action<elementium_pocket_anvil_use_damage_t>( "anvil_strike_damage", e, buff ) )
+        damage( create_proc_action<elementium_pocket_anvil_use_damage_t>( "anvil_strike_damage", e ) )
     {
       add_child( damage );
     }
