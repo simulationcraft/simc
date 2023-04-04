e21e40c0f24075e4197d4fec2d181c1cebb3365a
bees <78449839+hotbees@users.noreply.github.com>
2023-04-04 10:01:00 -0400

    [Havoc] Expose T30 fury tracking to the APL (#7442)

--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -5973,6 +5973,28 @@ std::unique_ptr<expr_t> demon_hunter_t::create_expression( util::string_view nam
       return this->get_target_data(*primary_idx)->dots.fiery_brand->is_ticking();
     });
   }
+  else if ( util::str_compare_ci( name_str, "seething_fury_threshold" ) )
+  {
+    return expr_t::create_constant( "seething_fury_threshold",
+                                    this->set_bonuses.t30_havoc_2pc->effectN( 1 ).base_value() );
+  } 
+  else if ( util::str_compare_ci( name_str, "seething_fury_spent" ) )
+  {
+    return make_mem_fn_expr( "seething_fury_spent", this->set_bonuses,
+                             &demon_hunter_t::set_bonuses_t::t30_havoc_2pc_fury_tracker );
+  }
+  else if ( util::str_compare_ci( name_str, "seething_fury_deficit" ) )
+  {
+    if ( this->set_bonuses.t30_havoc_2pc->ok() )
+    {
+      return make_fn_expr( "seething_fury_deficit", [ this ] {
+        return this->set_bonuses.t30_havoc_2pc->effectN( 1 ).base_value() - this->set_bonuses.t30_havoc_2pc_fury_tracker;
+      } );
+    } 
+    else {
+      return expr_t::create_constant( "seething_fury_deficit", 0.0 );
+    }
+  }
 
   return player_t::create_expression( name_str );
 }
