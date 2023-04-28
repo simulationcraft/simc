43ab50fa1e9af03d1ac71d270be6ea21a7e25985
Timothy Seltman <tseltman@verizon.net>
2023-04-28 16:37:16 -0400

    [Monk] Minor adjustment to last commit.

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -2890,6 +2890,9 @@ namespace monk
           if ( p()->buff.serenity->check() )
             am *= 1 + p()->talent.windwalker.serenity->effectN( 7 ).percent();
 
+          if ( p()->buff.hit_combo->check() )
+            am *= 1 + p()->passives.hit_combo->effectN( 3 ).percent();
+
           return am;
         }
 
@@ -2971,17 +2974,6 @@ namespace monk
           }
 
           trigger_gcd = timespan_t::zero();
-        }
-
-        double action_multiplier() const override
-        {
-          double am = monk_melee_attack_t::action_multiplier();
-
-          am *= 1 + p()->buff.hit_combo->check() * p()->passives.hit_combo->effectN( 3 ).percent();
-
-          am *= 1 + p()->buff.storm_earth_and_fire->check() * p()->talent.windwalker.storm_earth_and_fire->effectN( 3 ).percent();
-
-          return am;
         }
 
         bool ready() override
