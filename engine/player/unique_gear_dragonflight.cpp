bfc1fa95929e310085d8fa5842cf15e0dfe44b59
Khazak <joe.j.sommer@gmail.com>
2023-05-03 23:14:36 -0500

    [Gear] Implement Drogbar Rocks and Underlight Globe (#7636)

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -4529,6 +4529,27 @@ void heart_of_thunder ( special_effect_t& e )
   new dbc_proc_callback_t( e.player, e );
 }
 
+// 407895 driver
+// 407896 damage
+void drogbar_rocks( special_effect_t& effect ) {
+  effect.proc_flags2_ = PF2_CRIT;
+  
+  auto proc = create_proc_action<generic_proc_t>( "drogbar_rocks", effect, "drogbar_rocks", effect.trigger() );
+  effect.execute_action = proc;
+  new dbc_proc_callback_t( effect.player, effect );
+}
+
+// 408607 driver
+// 408984 spawned moth
+// 408983 primary stat buff
+void underlight_globe( special_effect_t& effect )
+{
+  effect.custom_buff = create_buff<stat_buff_t>( effect.player, effect.player -> find_spell( 408983 ) )
+    ->add_stat_from_effect( 1, effect.driver() -> effectN(1).average( effect.item ) );
+
+  new dbc_proc_callback_t( effect.player, effect );
+}
+
 // Weapons
 void bronzed_grip_wrappings( special_effect_t& effect )
 {
@@ -6659,6 +6680,8 @@ void register_special_effects()
   register_special_effect( 395175, items::treemouths_festering_splinter );
   register_special_effect( 401395, items::vessel_of_searing_shadow );
   register_special_effect( 413419, items::heart_of_thunder );
+  register_special_effect( 407895, items::drogbar_rocks );
+  register_special_effect( 408607, items::underlight_globe );
 
   // Weapons
   register_special_effect( 396442, items::bronzed_grip_wrappings );             // bronzed grip wrappings embellishment
