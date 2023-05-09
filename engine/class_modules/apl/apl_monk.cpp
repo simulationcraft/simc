5d5ca87306cfc978b3d198ab5e0b44aa6901458e
Kate Martin <51387586+renanthera@users.noreply.github.com>
2023-05-08 22:09:47 -0600

    [Monk] djaruun syncing

--- engine/class_modules/apl/apl_monk.cpp
+++ engine/class_modules/apl/apl_monk.cpp
@@ -209,7 +209,7 @@ namespace monk_apl
         "(debuff.weapons_of_order_debuff.up)|"                                                                                                                      // 4. if woo is up and it won't put other trinket on bad shared cd
         "((cooldown.dragonfire_bomb_dispenser.charges_fractional+1)*10+(trinket.1.ready_cooldown<fight_remains)*20<fight_remains)"                                  // 5. If you can't spend all current charges of Dragonfire Bomb Dispenser + other on-use trinket if it will be available before end of fight, start spending immediately.
         ")" },
-      { "djaruun_pillar_of_the_elder_flame_main_hand", "" },
+      { "djaruun_pillar_of_the_elder_flame_main_hand", ",if=debuff.weapons_of_order_debuff.stack>3" },
 
       // Defaults by slot:
       { "trinket1", ",if=debuff.weapons_of_order_debuff.stack>3" },
@@ -345,9 +345,10 @@ namespace monk_apl
     cooldowns_niuzao_woo->add_action( "invoke_external_buff,name=power_infusion,if=buff.weapons_of_order.remains<=20",
       "Use <a href='https://www.wowhead.com/spell=10060/power-infusion'>Power Infusion</a> when <a href='https://www.wowhead.com/spell=387184/weapons-of-order'>Weapons of Order</a> reaches 4 stacks." );
     cooldowns_niuzao_woo->add_action( "weapons_of_order,if="
-                                      "(talent.weapons_of_order.enabled)&"
+                                      "(talent.weapons_of_order.enabled)&("
                                       "((equipped.beacon_to_the_beyond&trinket.beacon_to_the_beyond.cooldown.remains<30)|"
-                                      "(!equipped.beacon_to_the_beyond))");
+                                      "(!equipped.beacon_to_the_beyond))"
+                                      ")");
     cooldowns_niuzao_woo->add_action( "bonedust_brew,if=!buff.bonedust_brew.up&debuff.weapons_of_order_debuff.stack>3" );
     cooldowns_niuzao_woo->add_action( "bonedust_brew,if=!buff.bonedust_brew.up&!buff.weapons_of_order.up&cooldown.weapons_of_order.remains>10" );
     cooldowns_niuzao_woo->add_action( "exploding_keg,if=buff.bonedust_brew.up" );
