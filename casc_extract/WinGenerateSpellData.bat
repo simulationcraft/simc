ba0878b6301d15e47ce9653fa616aeb1f98193f9
Khazak <joe.j.sommer@gmail.com>
2023-05-11 01:25:19 -0500

    Revert change to casc extract script

--- casc_extract/WinGenerateSpellData.bat
+++ casc_extract/WinGenerateSpellData.bat
@@ -9,6 +9,6 @@ set curr=%cd%
 cd dbc_extract3
 robocopy "C:\Program Files (x86)\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
 robocopy "C:\Program Files (x86)\Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
-robocopy "F:\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
+robocopy "C:\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
 call generate.bat %wowdir% "%curr%\casc_extract\wow"
 pause
