566dbe741a487249fb953752531e4c20a16aacf3
Khazak <joe.j.sommer@gmail.com>
2023-05-12 20:28:47 -0500

    [Gear] Increase Phial of Corrupting Rage uptime to 80%

--- engine/sim/sim.hpp
+++ engine/sim/sim.hpp
@@ -492,7 +492,7 @@ struct sim_t : private sc_thread_t
     // Chance for nearby enemies to move out of range for Allied Wristguards of Companionship
     double allied_wristguards_ally_leave_chance = 0.05;
     // Corrupting Rages Average Uptime
-    double corrupting_rage_uptime = 0.5;
+    double corrupting_rage_uptime = 0.80;
     // Hood of Surging Time proc chance when the period is set
     double hood_of_surging_time_chance = 0.0;
     // Hood of Surging Time proc period
