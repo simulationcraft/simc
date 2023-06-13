b4cf078458598f7edaf2c8eaab68e8b36c3fdc1d
nyterage <nyterage@gmail.com>
2023-06-12 21:27:03 -0700

    Move bool to player, disable for warlock

--- engine/player/player.hpp
+++ engine/player/player.hpp
@@ -744,6 +744,8 @@ struct player_t : public actor_t
     bool is_default() { return current_value == default_value; }
   };
 
+  bool pet_stat_delay;
+
   struct shadowlands_opt_t
   {
     /// Type stat gained from So'leah's Secret Technique
