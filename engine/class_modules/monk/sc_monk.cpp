99e4395d95781bcae4d938b3e79f67f99fe6ad78
Timothy Seltman <tseltman@verizon.net>
2023-06-14 00:48:35 -0400

    [Monk] Parse the Press the Advantage buff.

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -150,6 +150,7 @@ namespace monk
         //    parse_buff_effects( p()->buff.hit_scheme );
         //    parse_buff_effects( p()->buff.mighty_pour );
         //    parse_buff_effects( p()->buff.purified_chi );
+        parse_buff_effects( p()->buff.press_the_advantage );
         // Mistweaver
         parse_buff_effects( p()->buff.invoke_chiji_evm, true, true );
         parse_buff_effects( p()->buff.lifecycles_enveloping_mist );
