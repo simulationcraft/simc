532efddf574491f1f1c45cb10ac79630b6c8f13b
nyterage <nyterage@gmail.com>
2023-07-12 13:13:50 -0700

    Fix for dungeonroute

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -6381,11 +6381,13 @@ void demonsbane( special_effect_t& e )
   auto dot     = create_proc_action<generic_proc_t>( "warchiefs_rend", e, "warchiefs_rend", 419262 );
   dot->base_td = e.driver()->effectN( 1 ).average( e.item );
 
+  e.player->callbacks.register_callback_trigger_function( e.driver() -> id(), dbc_proc_callback_t::trigger_fn_type::CONDITION,
+      [ dot ]( const dbc_proc_callback_t*, action_t*, action_state_t* s ) {
+        return s->target->race == RACE_DEMON;
+      } );
+
   e.execute_action = dot;
-  if ( e.player->target->race == RACE_DEMON )
-  {
-    new dbc_proc_callback_t( e.player, e );
-  }
+  new dbc_proc_callback_t( e.player, e );
 }
 
 }  // namespace items
