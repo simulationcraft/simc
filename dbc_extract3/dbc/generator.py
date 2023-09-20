acdb76fdb21368bc8d51891895ef40a05e8b9507
Publik <sean.peters86@icloud.com>
2023-09-20 11:15:55 -0500

    [DBC] add buffs for Pinch of Dream Magic

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -1442,6 +1442,7 @@ class SpellDataGenerator(DataGenerator):
          426827, 427059, 426834, 427037, 427058, 427057, 427056, 426840, 427047, # Coiled Serpent Idol
          423124, 426564, 426553, # Augury of the Primal Flame
          426114, # Bandolier of Twisted Blades
+         424228, 424276, 424274, 424272, 424275, # Pinch of Dream Magic: Shapeshift Buffs
         ),
 
         # Warrior:
