c71ec5e40f2539960a3a6d7e4b928eb4898751ec
nyterage <nyterage@gmail.com>
2023-10-25 22:26:17 -0700

    [Gear] Gift of Ursine Vengance, adjust proc system

--- engine/sim/sim.cpp
+++ engine/sim/sim.cpp
@@ -4070,7 +4070,6 @@ void sim_t::create_options()
   add_option( opt_uint( "dragonflight.verdant_embrace_allies", dragonflight_opts.verdant_embrace_allies, 0, 2 ) );
   add_option( opt_float( "dragonflight.embersoul_dire_chance", dragonflight_opts.embersoul_dire_chance, 0.0, 1.0 ) );
   add_option( opt_timespan( "dragonflight.embersoul_dire_interval", dragonflight_opts.embersoul_dire_interval, 0_s, timespan_t::max() ) );
-  add_option( opt_timespan( "dragonflight.gift_of_ursine_vengeance_period", dragonflight_opts.gift_of_ursine_vengeance_period, 0_s, timespan_t::max() ) );
 }
 
 // sim_t::parse_option ======================================================
