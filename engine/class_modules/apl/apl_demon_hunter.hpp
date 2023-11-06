b2940a529ba70ba91168f62331ddfb05b3a59370
Richard Harrah <topplethenunnery@gmail.com>
2023-11-06 10:21:27 -0500

    [Demon Hunter] finish cleaning up after PTR module merge

--- engine/class_modules/apl/apl_demon_hunter.hpp
+++ engine/class_modules/apl/apl_demon_hunter.hpp
@@ -14,9 +14,6 @@ std::string food( const player_t* );
 std::string rune( const player_t* );
 std::string temporary_enchant_havoc( const player_t* );
 std::string temporary_enchant_vengeance( const player_t* );
-std::string temporary_enchant_vengeance_ptr( const player_t* );
 void havoc( player_t* );
-void havoc_ptr( player_t* );
 void vengeance( player_t* );
-void vengeance_ptr( player_t* );
 }  // namespace demon_hunter_apl
