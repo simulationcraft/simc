76258cfe2cbfde3a30c49d95d0dea6970e991c7f
SheffyWoW <59845533+SheffyWoW@users.noreply.github.com>
2023-11-07 08:15:36 +0100

    [Elemental] Update T31 Profile + Regen (#8331)
    * [Elemental] Update T31 Profile
    
    https://mimiron.raidbots.com/simbot/report/oJE6rix8FnA2Nu7TZxZvcq
    
    - updated default phial (Iced Phial of Corrupting Rage)
    - updates to embellishments and trinkets
    
    * Update sc_shaman.cpp
    
    * [Shaman] Regenerate Profile
    
    * [Elemental] Add correct bonus_ids for Sea Star (Thanks Topple)
    
    ---------
    
    Co-authored-by: Fluttershy <Sakayra@googlemail.com>

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -10988,7 +10988,7 @@ std::string shaman_t::default_potion() const
 
 std::string shaman_t::default_flask() const
 {
-  std::string elemental_flask = ( true_level >= 61 ) ? "phial_of_elemental_chaos_3" :
+  std::string elemental_flask = ( true_level >= 61 ) ? "iced_phial_of_corrupting_rage_3" :
                                 ( true_level >= 51 ) ? "spectral_flask_of_power" :
                                 ( true_level >= 45 ) ? "greater_flask_of_endless_fathoms" :
                                 "disabled";
