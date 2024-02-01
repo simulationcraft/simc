c5e5264fe37980df995287b6d44664278c84af2d
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-02-01 03:41:24 +0000

    [Augmentation] Fix Dungeon APL Bug

--- engine/class_modules/apl/apl_evoker.cpp
+++ engine/class_modules/apl/apl_evoker.cpp
@@ -177,7 +177,7 @@ void augmentation( player_t* p )
   precombat->add_action( "blistering_scales,target_if=target.role.tank" );
   precombat->add_action( "living_flame" );
 
-  ebon_logic->add_action( "ebon_might,if=raid_event.adds.remains>10|raid_event.adds.in>20" );
+  ebon_logic->add_action( "ebon_might" );
 
   opener_filler->add_action( "variable,name=opener_delay,value=variable.opener_delay>?variable.minimum_opener_delay,if=!variable.opener_cds_detected&evoker.allied_cds_up>0" );
   opener_filler->add_action( "variable,name=opener_delay,value=variable.opener_delay-1" );
@@ -199,7 +199,7 @@ void augmentation( player_t* p )
 
   default_->add_action( "variable,name=temp_wound,value=debuff.temporal_wound.remains,target_if=max:debuff.temporal_wound.remains" );
   default_->add_action( "prescience,target_if=min:debuff.prescience.remains+1000*(target=self&active_allies>2)+1000*target.spec.augmentation,if=(full_recharge_time<=gcd.max*3|cooldown.ebon_might.remains<=gcd.max*3&(buff.ebon_might_self.remains-gcd.max*3)<=buff.ebon_might_self.duration*0.4|variable.temp_wound>=(gcd.max+action.eruption.cast_time)|fight_remains<=30)&(buff.trembling_earth.stack+evoker.prescience_buffs)<=(5+(full_recharge_time<=gcd.max*3))" );
-  default_->add_action( "call_action_list,name=ebon_logic,if=(buff.ebon_might_self.remains-cast_time)<=buff.ebon_might_self.duration*0.4&(active_enemies>0|raid_event.adds.in<=3)&(evoker.prescience_buffs>=2&time<=10|evoker.prescience_buffs>=3|buff.ebon_might_self.remains>=action.ebon_might.cast_time|active_allies<=2)" );
+  default_->add_action( "call_action_list,name=ebon_logic,if=(buff.ebon_might_self.remains-cast_time)<=buff.ebon_might_self.duration*0.4&(active_enemies>0|raid_event.adds.in<=3)&(evoker.prescience_buffs>=2&time<=10|evoker.prescience_buffs>=3|fight_style.dungeonroute|fight_style.dungeonslice|buff.ebon_might_self.remains>=action.ebon_might.cast_time|active_allies<=2)" );
   default_->add_action( "run_action_list,name=opener_filler,if=variable.opener_delay>0&!fight_style.dungeonroute" );
   default_->add_action( "potion,if=debuff.temporal_wound.up&buff.ebon_might_self.up" );
   default_->add_action( "call_action_list,name=items" );
