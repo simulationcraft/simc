54c9ac3fdca1ce3ff959a4c95267a2b677c5c313
Khazak <joe.j.sommer@gmail.com>
2024-02-19 15:31:53 -0600

    Figure out directories for ci

---
+++ generate_profiles_ci.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+SIMC=${SIMC:-../../builddir}
+
+if [ ! -d "profiles" ]; then
+  echo 'You must run this script with simc root as current working directory.'
+  exit 0
+fi
+cd 'profiles'
+# PreRaids doesn't match the typical pattern
+PROFDIR='PreRaids'
+echo "---$PROFDIR---"
+if [ -d $PROFDIR ]; then
+  cd $PROFDIR/
+  ${SIMC} "../generators/$PROFDIR/PR_Generate.simc"
+  cd ../
+else
+  echo 'Skipped PreRaids, directory not found.'
+fi
+# DungeonSlice doesn't match the typical pattern
+# PROFDIR='DungeonSlice'
+# echo "---$PROFDIR---"
+# if [ -d $PROFDIR ]; then
+#   cd $PROFDIR/
+#   ../../engine/simc "../generators/$PROFDIR/DS_Generate.simc"
+#   cd ../
+# else
+#   echo 'Skipped DungeonSlice, directory not found.'
+# fi
+# TierXX profiles generation
+for tier in 29 30 31
+do
+  PROFDIR="Tier$tier"
+  echo "---$PROFDIR---"
+  if [ ! -d $PROFDIR ]; then
+    echo "Skipped $PROFDIR, directory not found."
+    continue
+  fi
+  cd $PROFDIR/
+  ${SIMC} '../generators/Tier'$tier'/T'$tier'_Generate.simc'
+  cd ../
+done
+echo 'done'
