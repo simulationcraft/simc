382c6a2b4169b3d09577a5b87e36b4d9f71b36eb
Khazak <joe.j.sommer@gmail.com>
2024-02-19 22:08:30 -0600

    Try independent linux action

--- generate_profiles_ci.sh
+++ generate_profiles_ci.sh
@@ -1,34 +1,22 @@
 #!/usr/bin/env sh
 set -e
 
-SIMC=${SIMC:-../../builddir/simc}
+#SIMC=${SIMC:-../../builddir/simc}
 
 if [ ! -d "profiles" ]; then
   echo 'You must run this script with simc root as current working directory.'
   exit 0
 fi
 cd 'profiles'
-# PreRaids doesn't match the typical pattern
 PROFDIR='PreRaids'
 echo "---$PROFDIR---"
 if [ -d $PROFDIR ]; then
   cd $PROFDIR/
-  ${SIMC} "../generators/$PROFDIR/PR_Generate.simc"
+  ${SIMC_CLI_PATH} "../generators/$PROFDIR/PR_Generate.simc"
   cd ../
 else
   echo 'Skipped PreRaids, directory not found.'
 fi
-# DungeonSlice doesn't match the typical pattern
-# PROFDIR='DungeonSlice'
-# echo "---$PROFDIR---"
-# if [ -d $PROFDIR ]; then
-#   cd $PROFDIR/
-#   ../../engine/simc "../generators/$PROFDIR/DS_Generate.simc"
-#   cd ../
-# else
-#   echo 'Skipped DungeonSlice, directory not found.'
-# fi
-# TierXX profiles generation
 for tier in 29 30 31
 do
   PROFDIR="Tier$tier"
@@ -38,7 +26,7 @@ do
     continue
   fi
   cd $PROFDIR/
-  ${SIMC} '../generators/Tier'$tier'/T'$tier'_Generate.simc'
+  ${SIMC_CLI_PATH} '../generators/Tier'$tier'/T'$tier'_Generate.simc'
   cd ../
 done
 echo 'done'
