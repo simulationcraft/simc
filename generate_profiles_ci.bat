1a7a66d184c5e4243aa4850cb8d681a71e17cb93
Khazak <joe.j.sommer@gmail.com>
2024-02-19 20:24:09 -0600

    run simc from builddir

---
+++ generate_profiles_ci.bat
@@ -0,0 +1,32 @@
+@echo off
+if exist simc_cache.dat del /s simc_cache.dat
+
+if exist simc.exe goto 32
+goto exit
+
+:32
+cd "profiles"
+:: PreRaids doesn't match the typical pattern
+cd "PreRaids"
+echo Running PR_Generate.simc in %cd%
+"%~dp0builddir\simc.exe" PR_Generate.simc
+cd ..\
+:: DungeonSlice doesn't match the typical pattern
+:: cd "DungeonSlice"
+:: echo Running DS_Generate.simc in %cd%
+:: "%~dp0builddir\simc.exe" DS_Generate.simc
+:: cd ..\
+:: TierXX profiles generation
+for %%g in (29 30 31) do (
+  cd Tier%%g
+  echo Running T%%g_Generate.simc in %cd%
+  "%~dp0builddir\simc.exe" T%%g_Generate.simc
+  cd ..\
+)
+goto pause
+
+:exit
+echo "This file requires the CLI (simc.exe) to be located in the same folder"
+
+:pause
+pause
