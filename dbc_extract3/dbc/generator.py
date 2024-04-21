eea8d94d08f8afb47db992343a37b0e489d92887
Timothy Seltman <tseltman@verizon.net>
2024-04-21 10:20:25 -0400

    [Monk] Add Combat Wisdom's Expel Harm

--- dbc_extract3/dbc/generator.py
+++ dbc_extract3/dbc/generator.py
@@ -2123,6 +2123,7 @@ class SpellDataGenerator(DataGenerator):
           ( 393565, 3 ), # Thunderfist buff
           ( 395413, 3 ), # Fae Exposure Healing Buff
           ( 395414, 3 ), # Fae Exposure Damage Debuff
+          ( 451968, 3 ), # Combat Wisdom Expel Harm
 
           # Covenant
           ( 325217, 0 ), # Necrolord Bonedust Brew damage
