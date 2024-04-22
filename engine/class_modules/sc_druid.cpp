d4622a42bb21a71e125e8953a65f8b2075469056
gastank <42421688+gastank@users.noreply.github.com>
2024-04-21 19:58:17 -0700

    [Druid] arcane affinity initial implementation

--- engine/class_modules/sc_druid.cpp
+++ engine/class_modules/sc_druid.cpp
@@ -13252,6 +13252,7 @@ void druid_t::apply_affecting_auras( action_t& action )
   action.apply_affecting_aura( talent.soul_of_the_forest_cat );
 
   // Hero talents
+  action.apply_affecting_aura( talent.arcane_affinity );
   action.apply_affecting_aura( talent.astral_insight );
   action.apply_affecting_aura( talent.early_spring );
   action.apply_affecting_aura( talent.groves_inspiration );
