d3a5388511f7eca4dcc185f4bc57441c513ab77a
Dave Hendler <sollaires@gmail.com>
2024-05-01 10:26:57 -0700

    [dbc] Remove incorrect key hotfix record

--- dbc_extract3/dbc/__init__.py
+++ dbc_extract3/dbc/__init__.py
@@ -124,7 +124,6 @@ KEY_FIELD_HOTFIX_RECORD = {
     'TraitTree'                   : 'unk_3',
     'TraitEdge'                   : 'id',
     'ModifiedCraftingSpellSlot'   : 'id_spell',
-    'ItemLogicalCost'             : 'id_item_extended_cost',
     'CharacterLoadoutItem'        : 'id_loadout',
 }
 
