9af7330acfb85c5bbd206cf3b876a35c84d8b333
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-05-08 09:07:29 +0100

    [Augmentation] Set default ilvl for characters to 518

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -226,7 +226,7 @@ struct simplified_player_t : public player_t
   // Options
   struct options_t
   {
-    int item_level = 512;
+    int item_level = 518;
     std::string variant = "default";
   } option;
 
