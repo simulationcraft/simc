109839ae2caae66e75be2a7a532a445829b4d5f3
Wesley Wigham <wwigham@gmail.com>
2024-05-10 06:59:39 -0700

    [damage_buff_t] Allow overriding the base scaling quantity in addition to the per-stack scaling (#8834)

--- engine/buff/buff.cpp
+++ engine/buff/buff.cpp
@@ -3839,12 +3839,13 @@ buff_t* damage_buff_t::apply_affecting_effect( const spelleffect_data_t& effect 
 
 damage_buff_t* damage_buff_t::set_buff_mod( damage_buff_modifier_t& mod, double multiplier )
 {
-  return set_buff_mod( mod, spell_data_t::nil(), 0, multiplier );
+  return set_buff_mod( mod, spell_data_t::nil(), 0, multiplier, 1.0 );
 }
 
-damage_buff_t* damage_buff_t::set_buff_mod( damage_buff_modifier_t& mod, const spell_data_t* s, size_t effect_idx, double multiplier )
+damage_buff_t* damage_buff_t::set_buff_mod( damage_buff_modifier_t& mod, const spell_data_t* s, size_t effect_idx, double multiplier, double initial_multiplier )
 {
   assert( mod.s_data == nullptr && mod.effect_idx == 0 );
+  mod.initial_multiplier = initial_multiplier;
 
   if( multiplier != 0.0 )
     mod.multiplier = 1.0 + multiplier;
@@ -3863,26 +3864,26 @@ damage_buff_t* damage_buff_t::set_buff_mod( damage_buff_modifier_t& mod, const s
 damage_buff_t* damage_buff_t::set_direct_mod( double multiplier )
 { return set_direct_mod( spell_data_t::nil(), 0, multiplier ); }
 
-damage_buff_t* damage_buff_t::set_direct_mod( const spell_data_t* s, size_t effect_idx, double multiplier )
-{ return set_buff_mod( direct_mod, s, effect_idx, multiplier ); }
+damage_buff_t* damage_buff_t::set_direct_mod( const spell_data_t* s, size_t effect_idx, double multiplier, double initial_multiplier )
+{ return set_buff_mod( direct_mod, s, effect_idx, multiplier, initial_multiplier ); }
 
 damage_buff_t* damage_buff_t::set_periodic_mod( double multiplier )
 { return set_periodic_mod( spell_data_t::nil(), 0, multiplier ); }
 
-damage_buff_t* damage_buff_t::set_periodic_mod( const spell_data_t* s, size_t effect_idx, double multiplier )
-{ return set_buff_mod( periodic_mod, s, effect_idx, multiplier ); }
+damage_buff_t* damage_buff_t::set_periodic_mod( const spell_data_t* s, size_t effect_idx, double multiplier, double initial_multiplier )
+{ return set_buff_mod( periodic_mod, s, effect_idx, multiplier, initial_multiplier ); }
 
 damage_buff_t* damage_buff_t::set_auto_attack_mod( double multiplier )
 { return set_auto_attack_mod( spell_data_t::nil(), 0, multiplier ); }
 
-damage_buff_t* damage_buff_t::set_auto_attack_mod( const spell_data_t* s, size_t effect_idx, double multiplier )
-{ return set_buff_mod( auto_attack_mod, s, effect_idx, multiplier ); }
+damage_buff_t* damage_buff_t::set_auto_attack_mod( const spell_data_t* s, size_t effect_idx, double multiplier, double initial_multiplier )
+{ return set_buff_mod( auto_attack_mod, s, effect_idx, multiplier, initial_multiplier ); }
 
 damage_buff_t* damage_buff_t::set_crit_chance_mod( double multiplier )
 { return set_crit_chance_mod( spell_data_t::nil(), 0, multiplier ); }
 
 damage_buff_t* damage_buff_t::set_crit_chance_mod( const spell_data_t* s, size_t effect_idx, double multiplier )
-{ return set_buff_mod( crit_chance_mod, s, effect_idx, multiplier ); }
+{ return set_buff_mod( crit_chance_mod, s, effect_idx, multiplier, 1.0 ); }
 
 bool damage_buff_t::is_affecting( const spell_data_t* s )
 {
