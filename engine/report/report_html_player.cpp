2b2a7f98d5145848559c515ebf390e40ce92c950
Wesley Wigham <wwigham@gmail.com>
2024-05-10 18:35:11 -0700

    Show overridden base initial bamage buff multipliers in report

--- engine/report/report_html_player.cpp
+++ engine/report/report_html_player.cpp
@@ -3513,11 +3513,11 @@ void print_html_player_buff( report::sc_html_stream& os, const buff_t& b, int re
                   "<li><span class=\"label\">crit_chance:</span>%.2f</li>\n"
                   "<li><span class=\"label\">is_stacking:</span>true</li>\n"
                   "</ul>\n",
-                  1.0,
+                  damage_buff->direct_mod.initial_multiplier,
                   damage_buff->direct_mod.multiplier - 1.0,
-                  1.0,
+                  damage_buff->periodic_mod.initial_multiplier,
                   damage_buff->periodic_mod.multiplier - 1.0,
-                  1.0,
+                  damage_buff->auto_attack_mod.initial_multiplier,
                   damage_buff->auto_attack_mod.multiplier - 1.0,
                   damage_buff->crit_chance_mod.multiplier );
       }
