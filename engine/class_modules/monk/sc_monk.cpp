788c1c2a246c8ccfab087669940390477941afb4
Jeremy <101205288+JeremyMonk@users.noreply.github.com>
2024-06-07 10:37:46 -0500

    [Monk] Correct spell data

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -1449,6 +1449,9 @@ struct tiger_palm_t : public monk_melee_attack_t
   bool counterstrike;
   bool combat_wisdom;
 
+  // Tier 33
+  const spell_data_t *ww_set_effect;
+
   tiger_palm_t( monk_t *p, util::string_view options_str )
     : monk_melee_attack_t( "tiger_palm", p, p->spec.tiger_palm ),
       face_palm( false ),
@@ -1459,8 +1462,9 @@ struct tiger_palm_t : public monk_melee_attack_t
 
     if ( p->sets->has_set_bonus( MONK_WINDWALKER, TWW1, B4 ) )
     {
+      ww_set_effect       = p->find_spell( 454505 );
       aoe                 = -1;
-      reduced_aoe_targets = p->sets->set( MONK_WINDWALKER, TWW1, B4 )->effectN( 2 ).base_value();
+      reduced_aoe_targets = ww_set_effect->effectN( 2 ).base_value();
     }
 
     ww_mastery       = true;
@@ -1483,7 +1487,7 @@ struct tiger_palm_t : public monk_melee_attack_t
 
     if ( p()->sets->has_set_bonus( MONK_WINDWALKER, TWW1, B4 ) )
       if ( target != p()->target )
-        m *= p()->sets->set( MONK_WINDWALKER, TWW1, B4 )->effectN( 1 ).percent();
+        m *= ww_set_effect->effectN( 1 ).percent();
 
     return m;
   }
