8cb32f9ffb6ab2fad98e27fd9af929f302368a80
gastank <42421688+gastank@users.noreply.github.com>
2024-06-14 12:57:47 -0700

    dump bonus id's with spell data dump

--- SpellDataDump/spelldatadump.bat
+++ SpellDataDump/spelldatadump.bat
@@ -8,3 +8,5 @@ simc display_build="0" spell_query="spell.class=%%i">spelldatadump/%%i.txt
 simc display_build="0" spell_query="spell">spelldatadump/allspells.txt
 
 simc display_build="2">spelldatadump/build_info.txt
+
+simc display_build="0" show_bonus_ids="1">spelldatadump/bonus_ids.txt
