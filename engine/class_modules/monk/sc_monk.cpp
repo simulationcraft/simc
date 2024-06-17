cddea3142d928d66c7d8902ed5559cfb27770069
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-06-17 20:44:46 +0100

    [Monk] Memory of the Monastery changes * Jeremy requested a couple of changes

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -2146,14 +2146,6 @@ struct blackout_kick_t : charred_passions_t<monk_melee_attack_t>
       if ( p()->talent.brewmaster.spirit_of_the_ox->ok() && p()->rppm.spirit_of_the_ox->trigger() )
         p()->buff.gift_of_the_ox->trigger();
 
-      int stacks = p()->buff.teachings_of_the_monastery->current_stack;
-
-      if ( p()->talent.windwalker.memory_of_the_monastery.enabled() && stacks > 0 )
-      {
-        p()->buff.memory_of_the_monastery->expire();
-        p()->buff.memory_of_the_monastery->trigger( stacks );
-      }
-
       p()->buff.teachings_of_the_monastery->expire();
 
       if ( p()->buff.blackout_reinforcement->up() )
@@ -2181,11 +2173,23 @@ struct blackout_kick_t : charred_passions_t<monk_melee_attack_t>
 
       int stacks = p()->buff.teachings_of_the_monastery->current_stack;
 
+      if ( p()->talent.windwalker.memory_of_the_monastery.enabled() && p()->bugs )
+      {
+        // TODO: Confirm proper mechanics for this. Tested 17/06/2024 and behaviour has it expire previous stacks before
+        // triggering new which feels like a bug.
+        p()->buff.memory_of_the_monastery->expire();
+      }
+
       for ( int i = 0; i < stacks; i++ )
       {
-        // Transfer the power triggers from ToTM hits but only on the primary target
+        // Transfer the power and Memory of the Monastery triggers from ToTM hits but only on the primary target
         if ( s->chain_target == 0 )
+        {
           p()->buff.transfer_the_power->trigger();
+
+          if ( p()->talent.windwalker.memory_of_the_monastery.enabled() )
+            p()->buff.memory_of_the_monastery->trigger();
+        }
 
         bok_totm_proc->execute();
       }
