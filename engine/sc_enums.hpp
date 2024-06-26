225eaede6b6b689d43b42fdd96548c8535cf121b
nyterage <nyterage@gmail.com>
2024-06-25 19:27:53 -0700

    [DK] Fix Undeath Cloning

--- engine/sc_enums.hpp
+++ engine/sc_enums.hpp
@@ -423,8 +423,9 @@ enum dot_behavior_e
 
 enum dot_copy_e
 {
-  DOT_COPY_START,
-  DOT_COPY_CLONE
+  DOT_COPY_START,          // Start a new DoT with the same remaining duration
+  DOT_COPY_CLONE,          // Clone everything from source DoT (tick time, stacks, remaining duration, etc)
+  DOT_COPY_CLONE_NO_STACKS // Clone everything except source DoT stacks
 };
 
 enum attribute_e
