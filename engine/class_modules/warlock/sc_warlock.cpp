f583d0e251d5b8986c24c045e2f67eb13e4c1bba
Azevara <Azevara.Duskbeam@gmail.com>
2024-07-03 13:58:44 -0400

    Reorder target data

--- engine/class_modules/warlock/sc_warlock.cpp
+++ engine/class_modules/warlock/sc_warlock.cpp
@@ -40,6 +40,13 @@ warlock_td_t::warlock_td_t( player_t* target, warlock_t& p )
                           ->set_default_value( p.tier.infirmity->effectN( 1 ).percent() )
                           ->add_invalidate( CACHE_PLAYER_DAMAGE_MULTIPLIER );
 
+  // Demonology
+  debuffs_the_houndmasters_stratagem = make_buff( *this, "the_houndmasters_stratagem", p.talents.the_houndmasters_stratagem_debuff )
+                                           ->set_default_value_from_effect( 1 );
+
+  debuffs_fel_sunder = make_buff( *this, "fel_sunder", p.talents.fel_sunder_debuff )
+                           ->set_default_value( p.talents.fel_sunder->effectN( 1 ).percent() );
+
   // Destruction
   dots_immolate = target->get_dot( "immolate", &p );
 
@@ -76,13 +83,6 @@ warlock_td_t::warlock_td_t( player_t* target, warlock_t& p )
 
                         range::for_each( p.havoc_spells, []( action_t* a ) { a->target_cache.is_valid = false; } );
                       } );
-
-  // Demonology
-  debuffs_the_houndmasters_stratagem = make_buff( *this, "the_houndmasters_stratagem", p.talents.the_houndmasters_stratagem_debuff )
-                                           ->set_default_value_from_effect( 1 );
-
-  debuffs_fel_sunder = make_buff( *this, "fel_sunder", p.talents.fel_sunder_debuff )
-                           ->set_default_value( p.talents.fel_sunder->effectN( 1 ).percent() );
 
   target->register_on_demise_callback( &p, [ this ]( player_t* ) { target_demise(); } );
 }
