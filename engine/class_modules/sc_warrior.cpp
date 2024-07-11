f14475106b91341f512dfd714455f0310a07da6e
Arma <adam.shewchuk@gmail.com>
2024-07-11 08:45:20 -0700

    [Warrior] Fix crushing blow tier proc (#9012)

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -4561,6 +4561,9 @@ struct crushing_blow_t : public warrior_attack_t
         cooldown->reset( true );
         if ( p()->talents.slayer.opportunist->ok() )
           p()->buff.opportunist->trigger();
+
+        if ( p()->sets->has_set_bonus( WARRIOR_FURY, TWW1, B4 ) )
+          p()->buff.deep_thirst->trigger();
       }
     }
     else if ( p()->talents.fury.improved_raging_blow->ok() && rng().roll( cd_reset_chance ) )
@@ -4568,6 +4571,9 @@ struct crushing_blow_t : public warrior_attack_t
       cooldown->reset( true );
       if ( p()->talents.slayer.opportunist->ok() )
         p()->buff.opportunist->trigger();
+
+      if ( p()->sets->has_set_bonus( WARRIOR_FURY, TWW1, B4 ) )
+          p()->buff.deep_thirst->trigger();
     }
 
     p()->buff.crushing_blow->decrement();
