be1c38439a839804989c25f4cc3861b055f4194e
Timothy Seltman <tseltman@verizon.net>
2024-07-11 23:07:19 -0400

    [Monk] Remove Heart of the Jade whitelist

--- engine/class_modules/monk/sc_monk.cpp
+++ engine/class_modules/monk/sc_monk.cpp
@@ -174,8 +174,8 @@ void monk_action_t<Base>::apply_buff_effects()
 
   // Conduit of the Celestials
   parse_effects( p()->buff.august_dynasty );
-  parse_effects( p()->buff.heart_of_the_jade_serpent_cdr, 0b0110000U );
-  parse_effects( p()->buff.heart_of_the_jade_serpent_cdr_celestial, 0b0110000U );
+  parse_effects( p()->buff.heart_of_the_jade_serpent_cdr );
+  parse_effects( p()->buff.heart_of_the_jade_serpent_cdr_celestial );
   parse_effects( p()->buff.jade_sanctuary );
   parse_effects( p()->buff.strength_of_the_black_ox );
 
