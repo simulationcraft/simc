113b57b82a4a6db4f1f9f2d1dc58aa6c638086cc
Kate Martin <51387586+renanthera@users.noreply.github.com>
2024-07-14 06:38:17 -0600

    [monk] Fix Invoker's Delight, autoparse ID, Momentum Boost, Memory of the Monastery. Use more `make_buff_fallback` to improve startup time.

--- engine/class_modules/monk/sc_monk.hpp
+++ engine/class_modules/monk/sc_monk.hpp
@@ -1455,7 +1455,6 @@ public:
   std::string default_rune() const override;
   std::string default_temporary_enchant() const override;
   action_t *create_action( util::string_view name, util::string_view options ) override;
-  double composite_melee_auto_attack_speed() const override;
   double composite_attack_power_multiplier() const override;
   double composite_attribute( attribute_e ) const override;
   double composite_dodge() const override;
