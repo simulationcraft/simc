e772a5d25039f4ca099d62fbe2001a3fc245c179
Richard Harrah <topplethenunnery@gmail.com>
2024-07-13 23:38:57 -0400

    [DH] trigger Art of the Glaive in-line

--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -2122,8 +2122,7 @@ struct art_of_the_glaive_trigger_t : public BASE
 
       if ( BASE::p()->talent.aldrachi_reaver.fury_of_the_aldrachi->ok() )
       {
-        make_event<delayed_execute_event_t>( *BASE::sim, BASE::p(), BASE::p()->active.art_of_the_glaive,
-                                             BASE::p()->target, 0_ms );
+        BASE::p()->active.art_of_the_glaive->execute_on_target( BASE::target );
       }
 
       BASE::p()->buff.glaive_flurry->expire();
