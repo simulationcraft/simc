5888c789cd2e04d3beff2f72a1091e77b511e34c
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-08-02 01:03:30 +0100

    Revert "[Warlock] Apply affecting the destruction aura to Rituals"
    This reverts commit 229aa8672d753305cdd127d93126f6ceac87fcad.

--- engine/class_modules/warlock/sc_warlock_init.cpp
+++ engine/class_modules/warlock/sc_warlock_init.cpp
@@ -727,34 +727,34 @@ namespace warlock
   void warlock_t::create_buffs_diabolist()
   {
     buffs.ritual_overlord = make_buff( this, "diabolic_ritual_overlord", hero.ritual_overlord )
-                                ->set_stack_change_callback( [ this ]( buff_t*, int, int cur ) {
-                                  if ( cur == 0 )
+                                ->set_stack_change_callback( [ this ]( buff_t*, int, int cur )
                                   {
-                                    make_event( sim, 0_ms, [ this ] { buffs.art_overlord->trigger(); } );
-                                    diabolic_ritual = 1;
-                                  }
-                                } )
-                                ->apply_affecting_aura( warlock_base.destruction_warlock );
+                                    if ( cur == 0 )
+                                    {
+                                      make_event( sim, 0_ms, [ this ] { buffs.art_overlord->trigger(); } );
+                                      diabolic_ritual = 1;
+                                    }
+                                  } );
 
     buffs.ritual_mother = make_buff( this, "diabolic_ritual_mother_of_chaos", hero.ritual_mother )
-                              ->set_stack_change_callback( [ this ]( buff_t*, int, int cur ) {
-                                if ( cur == 0 )
+                              ->set_stack_change_callback( [ this ]( buff_t*, int, int cur )
                                 {
-                                  make_event( sim, 0_ms, [ this ] { buffs.art_mother->trigger(); } );
-                                  diabolic_ritual = 2;
-                                }
-                              } )
-                              ->apply_affecting_aura( warlock_base.destruction_warlock );
-
-    buffs.ritual_pit_lord = make_buff( this, "diabolic_ritual_pit_lord", hero.ritual_pit_lord )
-                                ->set_stack_change_callback( [ this ]( buff_t*, int, int cur ) {
                                   if ( cur == 0 )
                                   {
-                                    make_event( sim, 0_ms, [ this ] { buffs.art_pit_lord->trigger(); } );
-                                    diabolic_ritual = 0;
+                                    make_event( sim, 0_ms, [ this ] { buffs.art_mother->trigger(); } );
+                                    diabolic_ritual = 2;
                                   }
-                                } )
-                                ->apply_affecting_aura( warlock_base.destruction_warlock );
+                                } );
+
+    buffs.ritual_pit_lord = make_buff( this, "diabolic_ritual_pit_lord", hero.ritual_pit_lord )
+                                ->set_stack_change_callback( [ this ]( buff_t*, int, int cur )
+                                  {
+                                    if ( cur == 0 )
+                                    {
+                                      make_event( sim, 0_ms, [ this ] { buffs.art_pit_lord->trigger(); } );
+                                      diabolic_ritual = 0;
+                                    }
+                                  } );
 
     buffs.art_overlord = make_buff( this, "demonic_art_overlord", hero.art_overlord )
                              ->set_stack_change_callback( [ this ]( buff_t*, int, int cur )
