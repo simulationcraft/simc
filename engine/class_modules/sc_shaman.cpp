c9fc8a036a2df33fc2877a0d8958457ada6dfe16
Will Farrington <wcfarrington@gmail.com>
2024-08-04 16:03:10 -0400

    [shaman] Fix Ancestor Elemental Blast casts
    It seems a recent spell data change resulted in simc considering this
    spell power coefficient to be 0. This seems to roughly align with the
    Ancestor Elemental Blast spell (447427) receiving a change that resulted
    in its tooltop showing a `0` value rather than the spell power
    coefficient.
    
    Thankfully it still has a Dummy effect holding the correct spell power
    coefficient, so we can use that with an override.

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -3692,7 +3692,10 @@ struct ancestor_t : public shaman_pet_t
   struct elemental_blast_t : public pet_spell_t<ancestor_t>
   {
     elemental_blast_t( ancestor_t* p ) : super( p, "elemental_blast", p->find_spell( 447427 ) )
-    { background = true; }
+    {
+        background = true;
+        spell_power_mod.direct = data().effectN( 1 ).sp_coeff();
+    }
   };
 
   ancestor_t( shaman_t* owner ) : shaman_pet_t( owner, "ancestor", true, false ),
