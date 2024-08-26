be813d37767fde885cbcd63136ee00d67fb7b31f
Dave Hendler <sollaires@gmail.com>
2024-08-26 13:06:09 -0700

    [casc] Actually log available info on failure

--- casc_extract/casc_extract.py
+++ casc_extract/casc_extract.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
 
 				result = blte.extract_buffer_to_file(data, os.path.join(output_path, file_name.replace('\\', '/')))
 				if not result:
-					print('Failed to extract data for URL %s' % file_keys[0], file=sys.stderr)
+					print('Failed to extract data for %s %s' % (opts.locale, file_name), file=sys.stderr)
 					sys.exit(1)
 
 	elif opts.mode == 'unpack':
