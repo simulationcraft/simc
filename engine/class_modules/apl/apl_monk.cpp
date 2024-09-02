5ebbc19c0a84d354e4ea60acf2b4d90fa03206b1
Alex <alex@jintronix.com>
2024-09-01 23:30:19 -0400

    apparently jfs does more damage than sck

--- engine/class_modules/apl/apl_monk.cpp
+++ engine/class_modules/apl/apl_monk.cpp
@@ -278,7 +278,10 @@ void mistweaver( player_t *p )
   def->add_action( "rising_sun_kick,if=talent.secret_infusion&buff.thunder_focus_tea.up" );
   def->add_action( "spinning_crane_kick,if=buff.dance_of_chiji.up" );
   def->add_action( "chi_burst,if=active_enemies>=2" );
+
+  def->add_action( "jadefire_stomp,if=active_enemies>=4" );
   def->add_action( "spinning_crane_kick,if=active_enemies>=4" );
+
   def->add_action( "jadefire_stomp,if=buff.jadefire_stomp.down" );
   def->add_action( "rising_sun_kick,if=active_enemies<=2" );
   def->add_action(
