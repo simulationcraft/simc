e3f0a500f9b0cb0ad55d3cb2a6fbfb4213587378
Saeldur <5496498+Saeldur@users.noreply.github.com>
2024-09-03 13:12:35 +0100

    [Evoker] Stop Mass Disintegrate Over Applying on AOE

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -4160,7 +4160,7 @@ struct disintegrate_t : public essence_spell_t
   {
     essence_spell_t::impact( s );
 
-    if ( p()->talent.scalecommander.bombardments.ok() && p()->buff.mass_disintegrate_stacks->check() )
+    if ( p()->talent.scalecommander.bombardments.ok() && p()->buff.mass_disintegrate_stacks->check() && s->chain_target == 0 )
     {
       auto td = p()->get_target_data( s->target );
       td->debuffs.bombardments->trigger();
