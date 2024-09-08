bcdc490996a82be38bbdf1bdb7af36cf696b0d25
Will Farrington <wcfarrington@gmail.com>
2024-09-08 15:53:02 -0400

    use `py` and add a batch file for windows folks

--- engine/class_modules/apl/shaman/generate_shaman.sh
+++ engine/class_modules/apl/shaman/generate_shaman.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Converting Elemental"
-python3 '../ConvertAPL.py' -i elemental.simc -o '../apl_shaman.cpp' -s elemental
-python3 '../ConvertAPL.py' -i elemental_ptr.simc -o '../apl_shaman.cpp' -s elemental_ptr
+py '../ConvertAPL.py' -i elemental.simc -o '../apl_shaman.cpp' -s elemental
+py '../ConvertAPL.py' -i elemental_ptr.simc -o '../apl_shaman.cpp' -s elemental_ptr
 
 echo Done!
 read -rsp $'Press enter to continue...\n'
