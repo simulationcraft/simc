405e0184db0caf8fc2a06f7a6c54dcdde3137788
EvanMichaels <evan.michaels@gmail.com>
2024-09-08 21:52:48 -0400

    [Rogue] Fatebound Coin (Tails) now appears to be able to proc poisons

--- engine/class_modules/sc_rogue.cpp
+++ engine/class_modules/sc_rogue.cpp
@@ -7046,6 +7046,9 @@ struct fatebound_coin_tails_t : public rogue_attack_t
 
   bool procs_blade_flurry() const override
   { return true; }
+
+  bool procs_poison() const override
+  { return true; }
 };
 
 struct fatebound_lucky_coin_t : public rogue_attack_t
@@ -12256,6 +12259,25 @@ void rogue_t::init_items()
 void rogue_t::init_special_effects()
 {
   player_t::init_special_effects();
+  
+  if ( unique_gear::find_special_effect( this, 448000 ) )
+  {
+    std::vector<unsigned> poison_ids = {
+      spell.instant_poison->effectN( 1 ).trigger()->id(),
+      spell.crippling_poison->effectN( 1 ).trigger()->id(),
+      spell.wound_poison->effectN( 1 ).trigger()->id(),
+      talent.rogue.atrophic_poison->effectN( 1 ).trigger()->id(),
+      talent.assassination.deadly_poison->effectN( 1 ).trigger()->id(),
+      talent.assassination.amplifying_poison->effectN( 3 ).trigger()->id()
+    };
+    range::erase_remove( poison_ids, 0 );
+
+    callbacks.register_callback_trigger_function(
+      448000, dbc_proc_callback_t::trigger_fn_type::CONDITION,
+      [ poison_ids ]( const dbc_proc_callback_t*, action_t* a, const action_state_t* ) {
+        return !a->special || range::contains( poison_ids, a->data().id() );
+    } );
+  }
 
   if ( weapon_data[ WEAPON_MAIN_HAND ].item_data[ WEAPON_SECONDARY ] )
   {
