c337716195977b6c7b773231ba5ff0c3c5dbaa27
Putro <29204244+Pewtro@users.noreply.github.com>
2024-09-09 17:43:05 +0200

    [Hunter] Squash warnings about precise shots

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -7852,7 +7852,7 @@ void hunter_t::create_buffs()
   buffs.precise_shots =
     make_buff( this, "precise_shots", talents.precise_shots_buff )
       -> set_default_value( talents.precise_shots->effectN( 1 ).percent() )
-      -> set_initial_stack( talents.precise_shots_buff->max_stacks() )
+      -> set_initial_stack( talents.precise_shots.ok() ? talents.precise_shots_buff->max_stacks() : 1 )
       -> set_chance( talents.precise_shots.ok() );
 
   buffs.streamline =
