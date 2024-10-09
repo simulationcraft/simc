1fd739a3228bfcdbf3760cdcbb630341802fb31f
Arma <adam.shewchuk@gmail.com>
2024-10-08 17:37:19 -0700

    [Warrior] Thunder Blast gives 8 rage for protection

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -4013,7 +4013,10 @@ struct thunder_blast_t : public warrior_attack_t
     energize_type = action_energize::NONE;
 
     if ( p->spec.protection_warrior->ok() )
+    {
       rage_gain += p->spec.protection_warrior->effectN( 20 ).resource( RESOURCE_RAGE );
+      rage_gain += p->talents.mountain_thane.thunder_blast->effectN( 3 ).resource( RESOURCE_RAGE );
+    }
 
     if ( p->talents.mountain_thane.crashing_thunder->ok() && p->specialization() == WARRIOR_FURY )
       rage_gain += p->talents.mountain_thane.crashing_thunder->effectN( 4 ).resource( RESOURCE_RAGE );
