c1d9952d4cd6a2306ad9fba2da995026a5c1e6ed
Navv <navv1234@gmail.com>
2024-10-20 09:06:32 -0400

    [Shaman] Trigger Flowing Spirits on cast

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -2476,6 +2476,11 @@ public:
     {
       p()->buff.flurry->decrement();
     }
+
+    if ( may_proc_flowing_spirits )
+    {
+      p()->trigger_flowing_spirits( execute_state );
+    }
   }
 
   void impact( action_state_t* state ) override
@@ -2489,11 +2494,6 @@ public:
     p()->trigger_windfury_weapon( state );
     p()->trigger_flametongue_weapon( state );
     p()->trigger_hot_hand( state );
-
-    if ( may_proc_flowing_spirits )
-    {
-      p()->trigger_flowing_spirits( state );
-    }
   }
 
   virtual double stormbringer_proc_chance() const
@@ -2805,17 +2805,11 @@ struct shaman_spell_t : public shaman_spell_base_t<spell_t>
       p()->trigger_fusion_of_elements( execute_state );
     }
 
-
     p()->trigger_earthen_rage( execute_state );
-  }
-
-  void impact( action_state_t* state ) override
-  {
-    base_t::impact( state );
 
     if ( may_proc_flowing_spirits )
     {
-      p()->trigger_flowing_spirits( state );
+      p()->trigger_flowing_spirits( execute_state );
     }
   }
 
