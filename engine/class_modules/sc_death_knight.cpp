5ad5a2631cd2849f48d5bf6721408c2528099afb
nyterage <nyterage@gmail.com>
2024-10-22 17:48:13 -0700

    [DK] Silence another warning

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -5878,9 +5878,8 @@ struct melee_t : public death_knight_melee_attack_t
 
     if ( p->talent.frost.icy_death_torrent.ok() )
     {
-      idt_chance = p->talent.frost.icy_death_torrent->proc_chance() * p->main_hand_weapon.group() == WEAPON_2H
-                       ? 1.0
-                       : 1.0 / ( p->talent.frost.icy_death_torrent->effectN( 1 ).base_value() * 0.1 );
+      idt_chance = p->main_hand_weapon.group() == WEAPON_2H ? 1.0 : 1.0 / ( p->talent.frost.icy_death_torrent->effectN( 1 ).base_value() * 0.1 ) *
+                             p->talent.frost.icy_death_torrent->proc_chance();
     }
 
     apply_action_effects();
