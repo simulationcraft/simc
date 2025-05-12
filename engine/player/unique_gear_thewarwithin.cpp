1ea0693cbe5ea16b5fb8e9cbac88d2befc25e0c2
Kate Martin <51387586+renanthera@users.noreply.github.com>
2025-05-12 04:40:53 -0600

    [unique_gear] Preliminary implementation for Critical Chain, Spark Burst and Static Charge.

--- engine/player/unique_gear_thewarwithin.cpp
+++ engine/player/unique_gear_thewarwithin.cpp
@@ -10418,10 +10418,6 @@ void roaring_warqueens_citrine( special_effect_t& effect )
 
 namespace durable_information_securing_chamber
 {
-/*
- * TODO:
- *  - Use belt
- */
 void charged_bolts( special_effect_t& effect )
 {
   constexpr unsigned driver_id = 1236109;
@@ -10440,7 +10436,7 @@ void charged_bolts( special_effect_t& effect )
       auto hit_scaling_effect = damage_spell_data->effectN( 2 ).trigger()->effectN( 1 );
       damage = create_proc_action<generic_proc_t>( util::tokenize_fn( damage_spell_data->name_cstr() ), effect,
                                                    damage_spell_data );
-      damage->base_dd_min = damage->base_dd_max = hit_scaling_effect.average( effect.player->items[ SLOT_WAIST ] );
+      damage->base_dd_min = damage->base_dd_max = hit_scaling_effect.average( player->items[ SLOT_WAIST ] );
       damage->base_multiplier *= role_mult( effect.player, effect.player->find_spell( 1236108 ) );
       tick_callback = [ & ]( buff_t*, int, timespan_t ) { damage->execute(); };
     }
@@ -10454,27 +10450,102 @@ void charged_bolts( special_effect_t& effect )
 
   new dbc_proc_callback_t( effect.player, effect );
 }
+
 void cauterizing_bolts( special_effect_t& effect )
 {
+  // NYI: Healing effect lol
 }
+
 void critical_chain( special_effect_t& effect )
 {
+  constexpr unsigned driver_id = 1236272;
+  auto driver                  = effect.player->find_spell( driver_id );
+  auto trigger_buff = driver->effectN( 1 ).trigger();
+  auto stack_buff = trigger_buff->effectN( 1 ).trigger();
+
+  struct critical_overload_t : public buff_t
+  {
+    buff_t* stacking_crit;
+
+    critical_overload_t( player_t* player, std::string_view name, const spell_data_t* spell_data )
+      : buff_t( player, name, spell_data ), stacking_crit( nullptr )
+    {
+      auto stack_buff_data = spell_data->effectN( 1 ).trigger();
+
+      stacking_crit = create_buff<stat_buff_t>( player, stack_buff_data )
+        ->add_stat_from_effect_type( A_MOD_RATING, stack_buff_data->effectN( 2 ).trigger()->effectN( 3 ).average( player->items[ SLOT_WAIST ] ) );
+
+      quiet = true;
+      tick_callback = [ & ](buff_t*, int current_tick, timespan_t ){
+        if ( current_tick == 0 )
+          stacking_crit->expire();
+        stacking_crit->increment();
+      };
+      expire_callback = [ & ]( buff_t*, int, timespan_t ) {
+        stacking_crit->expire();
+      };
+    }
+  };
+
+  effect.spell_id     = driver_id;
+  effect.proc_flags_  = driver->proc_flags() | PF_ALL_DAMAGE;
+  effect.proc_flags2_ = PF2_ALL_HIT;
+  effect.ppm_         = driver->_rppm;
+  effect.custom_buff = create_buff<critical_overload_t>( effect.player, util::tokenize_fn( driver->name_cstr() ), trigger_buff );
+
+  new dbc_proc_callback_t( effect.player, effect );
 }
+
 void spark_burst( special_effect_t& effect )
 {
+  constexpr unsigned driver_id = 1236273;
+  auto driver                  = effect.player->find_spell( driver_id );
+  auto stat_buff = driver->effectN( 1 ).trigger();
+
+  effect.spell_id     = driver_id;
+  effect.proc_flags_  = driver->proc_flags() | PF_ALL_DAMAGE;
+  effect.proc_flags2_ = PF2_ALL_HIT;
+  effect.ppm_         = driver->_rppm;
+  effect.custom_buff = create_buff<stat_buff_t>( effect.player, util::tokenize_fn( driver->name_cstr() ), stat_buff )
+    ->add_stat_from_effect_type( A_MOD_RATING, stat_buff->effectN( 2 ).trigger()->effectN( 4 ).average( effect.player->items[ SLOT_WAIST ] ) );
+
+  new dbc_proc_callback_t( effect.player, effect );
 }
+
 void static_charge( special_effect_t& effect )
 {
+  constexpr unsigned driver_id = 1236275;
+  auto driver                  = effect.player->find_spell( driver_id );
+  auto stat_buff = driver->effectN( 1 ).trigger();
+
+  effect.spell_id     = driver_id;
+  effect.proc_flags_  = driver->proc_flags() | PF_ALL_DAMAGE;
+  effect.proc_flags2_ = PF2_ALL_HIT;
+  effect.ppm_         = driver->_rppm;
+  effect.cooldown_ = driver->cooldown();
+  effect.custom_buff = create_buff<stat_buff_t>( effect.player, util::tokenize_fn( driver->name_cstr() ), stat_buff )
+    ->add_stat_from_effect_type( A_MOD_RATING, stat_buff->effectN( 2 ).trigger()->effectN( 5 ).average( effect.player->items[ SLOT_WAIST ] ) )
+    ->set_reverse( true )
+    ->set_reverse_stack_count( 1 );
+
+  // TODO: dbc_proc_callback_t on PF2_ALL_DAMAGE/PF2_ALL_HIT with 1s cd that decrements effect.custom_buff
+
+  new dbc_proc_callback_t( effect.player, effect );
 }
+
 void electric_current( special_effect_t& effect )
 {
 }
+
 void charged_touch( special_effect_t& effect )
 {
+  // NYI: Healing effect lol
 }
+
 void energy_shield( special_effect_t& effect )
 {
 }
+
 void charged_crystal( special_effect_t& effect )
 {
 }
@@ -10710,9 +10781,9 @@ void register_special_effects()
   // durable_information_securing_chamber
   register_special_effect( 999999 - 1, durable_information_securing_chamber::charged_bolts );
   // register_special_effect( 999999 - 2, durable_information_securing_chamber::cauterizing_bolts );
-  // register_special_effect( 999999 - 3, durable_information_securing_chamber::critical_chain );
-  // register_special_effect( 999999 - 4, durable_information_securing_chamber::spark_burst );
-  // register_special_effect( 999999 - 5, durable_information_securing_chamber::static_charge );
+  register_special_effect( 999999 - 3, durable_information_securing_chamber::critical_chain );
+  register_special_effect( 999999 - 4, durable_information_securing_chamber::spark_burst );
+  register_special_effect( 999999 - 5, durable_information_securing_chamber::static_charge );
   // register_special_effect( 999999 - 6, durable_information_securing_chamber::electric_current );
   // register_special_effect( 999999 - 7, durable_information_securing_chamber::charged_touch );
   // register_special_effect( 999999 - 8, durable_information_securing_chamber::energy_shield );
