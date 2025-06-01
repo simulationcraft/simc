dc78975f9e8ae25ff016810d5f9a80f8b39ee1a5
Kate Martin <51387586+renanthera@users.noreply.github.com>
2025-06-01 02:46:23 -0600

    [player] Swap parameter type to avoid issues with narrowing with ctor overloads.

--- engine/player/player.hpp
+++ engine/player/player.hpp
@@ -125,7 +125,7 @@ struct parsed_assisted_combat_rule_t
   parsed_assisted_combat_rule_t( std::string expr, bool show_diff )
     : expr( expr ), comment( {} ), show_diff( show_diff ) {}
 
-  parsed_assisted_combat_rule_t( std::string expr, std::string comment )
+  parsed_assisted_combat_rule_t( std::string expr, const char* comment )
     : expr( expr ), comment( comment ), show_diff( true ) {}
 
   parsed_assisted_combat_rule_t( std::string expr, std::string comment, bool show_diff )
