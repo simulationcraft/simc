d394f0da6991fc1bef847022fdb2c03b40bd5c29
Mlklock <118071530+Mlklock@users.noreply.github.com>
2025-07-28 15:53:29 -0300

    Update sc_warlock_pets.cpp

--- engine/class_modules/warlock/sc_warlock_pets.cpp
+++ engine/class_modules/warlock/sc_warlock_pets.cpp
@@ -2579,7 +2579,7 @@ namespace diabolist
   /// Infernal Fragment Begin
 
   infernal_fragment_t::infernal_fragment_t( warlock_t* owner, util::string_view name )
-    : warlock_pet_t( owner, name )
+    : warlock_pet_t( owner, name , PET_WARLOCK_FRAGMENT, true )
   {
     type = FRAG;
     owner_coeff.ap_from_sp *= owner->hero.abyssal_dominion->effectN( 4 ).percent();
