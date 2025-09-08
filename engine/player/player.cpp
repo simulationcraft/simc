40f166587eb3ad4769a46a9680be4bfcbc5b175f
gastank <42421688+gastank@users.noreply.github.com>
2025-09-08 08:54:54 -0700

    fix stuck + add resource match assert

--- engine/player/player.cpp
+++ engine/player/player.cpp
@@ -10794,41 +10794,48 @@ struct pool_resource_t : public action_t
     if ( next_action )
     {
       // cache current resource amount while we do evaluations
-      double current   = player->resources.current[ resource ];
-      double remaining = amount_expr ? amount_expr->eval() : 0.0;
+      double current = player->resources.current[ resource ];
+      double extra = amount_expr ? amount_expr->eval() : 0.0;
 
-      if ( current < remaining )
-        return true;
-
-      double deducted = std::min( current, remaining );
-
-      player->resources.current[ resource ] -= deducted;
-      bool next_is_ready                    = next_action->action_ready();
-      player->resources.current[ resource ] = current;
-
-      // if next action is ready and we'll have enough resource remaining after cost, we don't need to pool
-      if ( next_is_ready )
-        return false;
-
-      // check against maximum resource to see if we're resource limited
-      player->resources.current[ resource ] = player->resources.max[ resource ];
-      bool next_is_resource_limited         = next_action->action_ready();
-      player->resources.current[ resource ] = current;
-
-      // if next action still can't be cast with max resources, we're not resource limited and don't need to pool
-      if ( !next_is_resource_limited )
-        return false;
-
-      // force_wait suspends APL re-evaluation until the next ability can be cast
-      if ( force_wait )
+      // must at least have more current resources than the extra amount
+      if ( current >= extra )
       {
-        // we don't know what the cost of the action is, so the issue where cost
-        // may return a value not related to the resource we intend is not fixable
-        double next_cost = next_action->cost();
-        double diff      = next_cost + remaining - current;
-        double regen     = player->resource_regen_per_second( resource );
-        wait             = timespan_t::from_seconds( std::max( 0.0, diff ) / regen ) + player->available();
-        return true;
+        // check if we can cast next ability after accounting for the extra amount required
+        player->resources.current[ resource ] -= extra;
+        bool next_is_ready = next_action->action_ready();
+        player->resources.current[ resource ] = current;
+
+        // if next action is ready, we'll have enough resource remaining after cost so we don't need to pool
+        if ( next_is_ready )
+          return false;
+
+        // check against maximum resource to see if we're resource limited
+        player->resources.current[ resource ] = player->resources.max[ resource ];
+        bool next_is_resource_limited = next_action->action_ready();
+        player->resources.current[ resource ] = current;
+
+        // if next action still can't be cast with max resources, we're not resource limited and don't need to pool
+        if ( !next_is_resource_limited )
+          return false;
+
+        // force_wait suspends APL re-evaluation until the next ability can be cast
+        if ( force_wait )
+        {
+          assert( next_action->current_resource() == resource &&
+                  "Cannot pool to force_wait with mismatched resources." );
+
+          double next_cost = next_action->cost();
+          double diff = next_cost + extra - current;
+          double regen = player->resource_regen_per_second( resource );
+
+          wait = timespan_t::from_seconds( std::max( 0.0, diff ) / regen ) + player->available();
+          return true;
+        }
+      }
+      else
+      {
+        // not enough resources to cover the extra, even if next action has 0 cost
+        return false;
       }
     }
 
