0ee95efc95704c59bece904ca0d074267dd46c53
gastank <42421688+gastank@users.noreply.github.com>
2025-10-01 23:11:49 -0700

    add error message to track down assert

--- engine/buff/buff.cpp
+++ engine/buff/buff.cpp
@@ -244,6 +244,13 @@ struct expiration_t : public buff_event_t
 
   void execute() override
   {
+#ifndef NDEBUG
+    if ( buff->expiration.empty() )
+    {
+      sim().error( error_level_e::SEVERE, "{} {} could not find buff_expiration!", *buff->player, *buff );
+      sim().cancel();
+    }
+#endif
     assert( !buff->expiration.empty() );
 
     // For non-async buffs, this is always unconditionally the "last tick" since we expire the buff
