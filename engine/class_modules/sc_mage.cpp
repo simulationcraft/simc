5fb76ea06951f95823ac89b7b5a0110cd6cfdd1b
Vít Šefl <vituscze@gmail.com>
2025-10-06 15:19:29 +0200

    [Mage] Remove accidental double dip

--- engine/class_modules/sc_mage.cpp
+++ engine/class_modules/sc_mage.cpp
@@ -1530,8 +1530,6 @@ struct meteorite_impact_t final : public arcane_phoenix_spell_t
     aoe = -1;
     reduced_aoe_targets = 8; // TODO: Verify this
     is_mage_spell = true;
-    if ( !bug )
-      base_dd_multiplier *= 1.0 + o()->spec.arcane_mage->effectN( 10 ).percent();
   }
 };
 
