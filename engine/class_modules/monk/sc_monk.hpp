f7ab848468d89b794f3b7a4a82762619a67c867f
Kate Martin <51387586+renanthera@users.noreply.github.com>
2026-01-24 18:01:39 -0700

    [monk] Resolve name for Rushing Wind Kick to Rising Sun Kick, as action override is implicit and filtered explicitly by rule.

--- engine/class_modules/monk/sc_monk.hpp
+++ engine/class_modules/monk/sc_monk.hpp
@@ -1116,6 +1116,7 @@ public:
   void init_blizzard_action_list() override;
   void parse_assisted_combat_step( const assisted_combat_step_data_t &step,
                                    action_priority_list_t *assisted_combat ) override;
+  std::vector<std::string> action_names_from_spell_id( unsigned int spell_id ) const override;
   std::string aura_expr_from_spell_id( unsigned int spell_id, bool on_self = true ) const override;
   parsed_assisted_combat_rule_t parse_assisted_combat_rule( const assisted_combat_rule_data_t &rule,
                                                             const assisted_combat_step_data_t &step ) const override;
