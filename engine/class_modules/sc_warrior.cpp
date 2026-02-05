685b689572b7da48d4591e47b9c406751909ef4b
Arma <adam.shewchuk@gmail.com>
2026-02-05 01:35:59 -0800

    [Warrior] Update slayers malice

--- engine/class_modules/sc_warrior.cpp
+++ engine/class_modules/sc_warrior.cpp
@@ -7478,6 +7478,10 @@ void warrior_t::init_spells()
     specialization() == WARRIOR_FURY ? effect_mask_t( true ).disable( 3 )
                                      : effect_mask_t( true ) );
 
+  register_passive_effect_mask( talents.slayer.slayers_malice,
+    specialization() == WARRIOR_ARMS ? effect_mask_t( true ).disable( 2 )
+                                     : effect_mask_t( true ).disable( 1 ) );
+
   // Armor Handled Manually for ATTT
   register_passive_effect_mask( talents.protection.armor_specialization, effect_mask_t( false ).enable( 1, 2 ) );
   register_passive_effect_mask( talents.protection.focused_vigor, effect_mask_t( false ).enable( 2 ) );
