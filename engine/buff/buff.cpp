0e30e8a53a7e9d7cb67bc4fb2853ba3c164770ff
nyterage <nyterage@gmail.com>
2026-02-09 22:56:58 -0800

    [Buff] Set Async buff behavior from spell flag

--- engine/buff/buff.cpp
+++ engine/buff/buff.cpp
@@ -752,6 +752,9 @@ buff_t::buff_t( sim_t* sim, player_t* target, player_t* source, util::string_vie
     ignore_time_modifier = true;
   set_can_cancel( !s_data->flags( spell_attribute::SX_NO_CANCEL ) );
 
+  if ( s_data->flags( spell_attribute::SX_ASYNCRONOUS_STACKING_BUFF ) && _max_stack > 1 )
+    set_stack_behavior( buff_stack_behavior::ASYNCHRONOUS );
+
   update_trigger_calculations();
 }
 
