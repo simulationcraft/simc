3dbf4d1eee82231b9e61ab471b1559e112baccc4
nyterage <nyterage@gmail.com>
2026-02-13 18:59:37 -0800

    [Core] Fix culling

--- engine/sim/sim.cpp
+++ engine/sim/sim.cpp
@@ -2162,7 +2162,7 @@ void sim_t::analyze_error()
       {
         double mean_error = sim_t::distribution_mean_error( *this, cd.target_metric );
         current_error = mean_error / current_mean;
-        current_standard_error = cd.target_metric.std_dev / sqrt(cd.target_metric.count());
+        current_standard_error = cd.target_metric.std_dev / sqrt( cd.target_metric.count() );
       }
     }
   }
@@ -2195,6 +2195,7 @@ void sim_t::analyze_error()
              if ( error > current_error )
               current_error = error;
           }
+          current_standard_error = cd.target_metric.std_dev / sqrt( cd.target_metric.count() );
           mean_total += mean;
           mean_count++;
         }
