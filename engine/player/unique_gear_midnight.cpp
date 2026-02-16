0e06295986a645e0b264097f06444ab138ef3c85
NN0323 <noah@dimse.dk>
2026-02-16 22:46:04 +0100

    Added trinket Vaelgors final stare

--- engine/player/unique_gear_midnight.cpp
+++ engine/player/unique_gear_midnight.cpp
@@ -2011,6 +2011,113 @@ void magisters_alchemist_stone( special_effect_t& e )
   new dbc_proc_callback_t( e.player, e );
 }
 
+// Vaelgor's Final Stare
+// 1259293 Driver
+// 1260459 Nullsight buff
+void vaelgors_final_stare( special_effect_t& effect )
+{
+  struct vaelgors_buff_t final : public stat_buff_t
+  {
+    const special_effect_t& effect;
+    const spell_data_t* value_spell;
+    int current_tick;
+    double buff_val;
+    double decrease;
+
+    vaelgors_buff_t( const special_effect_t& e )
+      : stat_buff_t( e.player, "vaelgors_final_stare", e.driver() ),
+        effect( e ),
+        value_spell( nullptr ),
+        current_tick( 0 ),
+        buff_val( 0 ),
+        decrease( 0 )
+    {
+      auto n_ticks = e.driver()->duration() / e.driver()->effectN( 3 ).period();
+      value_spell  = e.player->find_spell( 1260459 );
+      buff_val     = value_spell->effectN( 1 ).average( e );
+      decrease     = buff_val / n_ticks;
+
+      set_stat_from_effect( 1, value_spell->effectN( 1 ).average( e ) );
+      set_tick_callback( [ this ]( buff_t*, int, timespan_t ) { recalculate(); } );
+    }
+
+    double current_value()
+    {
+      double value = buff_val - ( decrease * current_tick );
+
+      return value;
+    }
+
+    void recalculate()
+    {
+      current_tick++;
+      for ( auto& buff_stat : stats )
+      {
+        double delta = buff_stat.current_value - current_value();
+
+        if ( delta > 0 )
+        {
+          player->stat_loss( 
+            buff_stat.stat, 
+            decrease, 
+            stat_gain, 
+            nullptr, 
+            buff_duration() > timespan_t::zero() 
+          );
+        }
+        else if ( delta < 0 )
+        {
+          player->stat_gain( 
+            buff_stat.stat, 
+            std::fabs( delta ), 
+            stat_gain, nullptr,               
+            buff_duration() > timespan_t::zero() 
+          );
+        }
+      }
+    }
+
+    void recalculate_expiry()
+    {
+      for ( auto& buff_stat : stats )
+      {
+        double delta = current_value();
+        if ( delta > 0 )
+        {
+          player->stat_loss( buff_stat.stat, delta, stat_gain, nullptr, buff_duration() > timespan_t::zero() );
+        }
+        else if ( delta < 0 )
+        {
+          player->stat_gain( 
+            buff_stat.stat,
+            std::fabs( delta ), 
+            stat_gain, 
+            nullptr,
+            buff_duration() > timespan_t::zero() 
+          );
+        }
+        buff_stat.current_value = 0;
+      }
+    }
+
+    void expire_override( int s, timespan_t d ) override
+    {
+      // Skip stat_buff_t::expire_override() since we are manually handling stat changes.
+      buff_t::expire_override( s, d );
+      recalculate_expiry();
+      current_tick = 0;
+    }
+
+    void reset() override
+    {
+      stat_buff_t::reset();
+      current_tick = 0;
+    }
+  };
+
+  effect.custom_buff = make_buff<vaelgors_buff_t>( effect );
+}
+
 }  // namespace trinkets
 
 namespace weapons
