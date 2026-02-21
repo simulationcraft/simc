1b0c45d12457981ff63cd943728eefc68df91cc4
NN0323 <noah@dimse.dk>
2026-02-21 01:27:40 +0100

    buff spell = e.driver()

--- engine/player/unique_gear_midnight.cpp
+++ engine/player/unique_gear_midnight.cpp
@@ -2067,7 +2067,7 @@ void nullsight( special_effect_t& e )
   unsigned equip_id = 1259293;
   auto equip = find_special_effect( e.player, equip_id );
   assert( equip && "Vaelgor's final stance missing equip effect" );
-  auto buff_spell = e.player->find_spell(1260459);
+  auto buff_spell = e.driver();
   int stacks = buff_spell->duration() / buff_spell->effectN( 3 ).period();
   double buff_val = equip->driver()->effectN(1).average( e );
   double buff_stacks = buff_val/stacks;
