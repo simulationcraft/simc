212276846308e3b8a2d57b1027e9aa2d8767a868
Tom Herbert <18316812+taherbert@users.noreply.github.com>
2026-03-12 11:42:39 -0700

    [VDH] Allow sigil_of_spite in precombat actions
    Sigil of Spite was missing the precombat overrides that Sigil of Flame
    already had, causing an error when added to precombat action lists.

--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -5348,6 +5348,17 @@ struct sigil_of_spite_t : public demon_hunter_spell_t
     }
   }
 
+  void init_finished() override
+  {
+    demon_hunter_spell_t::init_finished();
+    harmful = !is_precombat;  // Do not count towards the precombat hostile action limit
+  }
+
+  bool usable_precombat() const override
+  {
+    return true;  // Has virtual travel time due to Sigil activation delay
+  }
+
   void execute() override
   {
     demon_hunter_spell_t::execute();
