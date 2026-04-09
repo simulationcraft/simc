2246e0f1a64a44c8c5b8a2fda45df6122690b6b6
Xerfall <xerfallwow@gmail.com>
2026-04-09 23:14:16 +0200

    [Warlock] More updates to profile and APL headers formatting fix

--- engine/class_modules/apl/warlock.hpp
+++ engine/class_modules/apl/warlock.hpp
@@ -7,7 +7,7 @@ struct player_t;
 namespace warlock_apl{
 
   std::string potion( const player_t* );
-  std:: string flask( const player_t* );
+  std::string flask( const player_t* );
   std::string food( const player_t* );
   std::string rune( const player_t* );
   std::string temporary_enchant( const player_t* );
