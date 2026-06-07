6f41342d825da49fd9c42716330d586f886614fb
Kate Martin <51387586+renanthera@users.noreply.github.com>
2026-06-07 00:38:24 -0600

    [report] Remove deferral for talent tree iframe to mitigate flashbang.

--- engine/report/report_html_player.cpp
+++ engine/report/report_html_player.cpp
@@ -2049,17 +2049,14 @@ void print_html_talents( report::sc_html_stream& os, const player_t& p )
   }
 
   os << "<div class=\"player-section talents\">\n";
-  os.printf( "<h3 class=\"toggle\" id=\"player%d_talents_toggle\">Talents</h3>\n", p.index );
+  os.printf( "<h3 class=\"toggle\">Talents</h3>\n", p.index );
   os << "<div class=\"toggle-content hide\">\n";
-  os.printf( "<script type=\"text/x-deferred-html\" data-toggle=\"player%d_talents_toggle\">\n", p.index );
 
   auto max_col = class_columns( p.specialization(), p.is_ptr() ) + spec_columns( p.specialization(), p.is_ptr() );
   auto h_ = static_cast<int>( 1165 - max_col * 28 );
   os.format( R"(<iframe src="{}" width="1165" height="{}" style="background:#160f0b"></iframe>)",
               raidbots_talent_render_src( p.talents_str, p.true_level, 1165, false, p.dbc->ptr ), h_ );
-  os << "</script>\n";
 
-  // Hide the talent table only if the Raidbots talent iframe is present.
   os.printf( "<h3 class=\"toggle\" id=\"player%d_talent_tables_toggle\">Talent Tables</h3>\n", p.index );
   os << "<div class=\"toggle-content hide\">\n";
   os.printf( "<script type=\"text/x-deferred-html\" data-toggle=\"player%d_talent_tables_toggle\">\n", p.index );
