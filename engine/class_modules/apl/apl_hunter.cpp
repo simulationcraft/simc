502b77383d7bd96746d84292be06a356c5134a04
Aykeron <mzuev97@gmail.com>
2026-06-30 12:37:20 +0200

    [Hunter] Update BM PL AoE APL

--- engine/class_modules/apl/apl_hunter.cpp
+++ engine/class_modules/apl/apl_hunter.cpp
@@ -102,7 +102,7 @@ void beast_mastery( player_t* p )
   cleave->add_action( "wild_thrash,if=talent.beast_cleave" );
   cleave->add_action( "bestial_wrath,if=!prev.wild_thrash" );
   cleave->add_action( "wild_thrash,if=!talent.beast_cleave" );
-  cleave->add_action( "kill_command,if=buff.natures_ally.react|talent.master_handler&(active_enemies>3|howl_summon.ready)" );
+  cleave->add_action( "kill_command,if=cooldown.bestial_wrath.remains>gcd&(buff.natures_ally.react|talent.master_handler&(active_enemies>3|howl_summon.ready))" );
   cleave->add_action( "cobra_shot,if=cooldown.wild_thrash.remains>gcd&buff.hogstrider.up&active_enemies<4" );
   cleave->add_action( "barbed_shot,target_if=min:dot.barbed_shot.remains|max_prio_damage" );
   cleave->add_action( "cobra_shot,if=talent.beast_cleave&cooldown.wild_thrash.remains>gcd|!talent.beast_cleave" );
