e0afb41bca74b25cd8cef911aa9c83773671e91e
Kate Martin <51387586+renanthera@users.noreply.github.com>
2026-07-03 16:48:11 -0600

    For some reason, `try_emplace` causes a memory leak I cannot locally replicate.

--- engine/sim/profileset_control.cpp
+++ engine/sim/profileset_control.cpp
@@ -105,8 +105,10 @@ void profileset_controller_data_wrapper_t::construct_controller( sim_t* sim )
 
 bool profileset_controller_t::register_controller( std::string key, profileset_controller_t::factory_fn_pair_t&& value )
 {
-  const auto val = factory.try_emplace( key, std::move( value ) );
-  return val.second;
+  if ( factory.find( key ) == factory.end() )
+    return false;
+  factory.emplace( key, std::move( value ) );
+  return true;
 }
 
 bool profileset_controller_t::controller_exists( std::string key )
