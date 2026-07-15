5c3faf3567fe815c0f82155d60fceda44a02b02e
Pewtro <29204244+Pewtro@users.noreply.github.com>
2026-07-15 10:04:01 +0200

    [Hunter] Remove flamefang pitch from the APL

--- engine/class_modules/apl/apl_hunter.cpp
+++ engine/class_modules/apl/apl_hunter.cpp
@@ -532,7 +532,6 @@ void survival_ptr( player_t* p )
   plst->add_action( "kill_command,if=buff.tip_of_the_spear.stack<2&howl_summon.ready", "ST - PL" );
   plst->add_action( "kill_command,if=cooldown.takedown.remains<gcd&buff.tip_of_the_spear.stack<2&!talent.twin_fangs" );
   plst->add_action( "takedown,if=buff.tip_of_the_spear.stack>0&!talent.twin_fangs|buff.tip_of_the_spear.stack=0&talent.twin_fangs" );
-  plst->add_action( "flamefang_pitch" );
   plst->add_action( "wildfire_bomb,if=buff.tip_of_the_spear.up&(talent.lethal_calibration&full_recharge_time<4+gcd|!talent.lethal_calibration)" );
   plst->add_action( "boomstick,if=buff.tip_of_the_spear.up" );
   plst->add_action( "raptor_strike,if=(buff.tip_of_the_spear.up|!buff.raptor_swipe.up)" );
@@ -547,7 +546,6 @@ void survival_ptr( player_t* p )
   sentst->add_action( "takedown,if=buff.tip_of_the_spear.stack>0&!talent.twin_fangs|buff.tip_of_the_spear.stack=0&talent.twin_fangs" );
   sentst->add_action( "boomstick,if=buff.tip_of_the_spear.up" );
   sentst->add_action( "moonlight_chakram,if=buff.tip_of_the_spear.up" );
-  sentst->add_action( "flamefang_pitch" );
   sentst->add_action( "raptor_strike,if=buff.tip_of_the_spear.up|!buff.raptor_swipe.up" );
   sentst->add_action( "kill_command,if=cooldown.takedown.remains" );
   sentst->add_action( "takedown" );
@@ -555,7 +553,6 @@ void survival_ptr( player_t* p )
   plcleave->add_action( "kill_command,if=buff.tip_of_the_spear.stack<2&(buff.howl_of_the_pack_leader_wyvern.remains|buff.howl_of_the_pack_leader_boar.remains|buff.howl_of_the_pack_leader_bear.remains)", "AOE - PL" );
   plcleave->add_action( "kill_command,if=cooldown.takedown.remains<gcd&buff.tip_of_the_spear.stack<2&!talent.twin_fangs" );
   plcleave->add_action( "takedown,if=buff.tip_of_the_spear.stack>0&!talent.twin_fangs|buff.tip_of_the_spear.stack=0&talent.twin_fangs" );
-  plcleave->add_action( "flamefang_pitch" );
   plcleave->add_action( "wildfire_bomb,if=full_recharge_time<gcd" );
   plcleave->add_action( "boomstick,if=buff.tip_of_the_spear.up" );
   plcleave->add_action( "wildfire_bomb,if=buff.tip_of_the_spear.up" );
@@ -571,7 +568,6 @@ void survival_ptr( player_t* p )
   sentcleave->add_action( "kill_command,if=cooldown.takedown.remains<gcd&buff.tip_of_the_spear.stack<2&!talent.twin_fangs" );
   sentcleave->add_action( "takedown,if=buff.tip_of_the_spear.up" );
   sentcleave->add_action( "moonlight_chakram,if=buff.tip_of_the_spear.up" );
-  sentcleave->add_action( "flamefang_pitch,if=talent.flamefang_pitch&buff.tip_of_the_spear.up" );
   sentcleave->add_action( "raptor_strike,if=buff.tip_of_the_spear.up&buff.raptor_swipe.up|!buff.raptor_swipe.up" );
   sentcleave->add_action( "kill_command" );
 }
