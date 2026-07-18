c2322b54438b6797d2924d619161bcfb3e3705fa
HawkCorrigan <david.groelle@protonmail.com>
2026-07-18 14:24:03 +0200

    [ele] potm rework

--- engine/class_modules/sc_shaman.cpp
+++ engine/class_modules/sc_shaman.cpp
@@ -7341,7 +7341,10 @@ struct lightning_bolt_t : public shaman_spell_t
       {
         p()->summon_ancestor();
       }
+<<<<<<< HEAD
 
+=======
+>>>>>>> c08abd55f7 ([ele] potm rework)
       if ( rng().roll( p()->talent.power_of_the_maelstrom->effectN( 1 ).percent() ) && p()->is_ptr() &&
            p()->talent.power_of_the_maelstrom->ok() )
       {
