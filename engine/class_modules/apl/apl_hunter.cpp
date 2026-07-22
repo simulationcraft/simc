79f7ff8a1dd0953a4df9ea8dbabb27eb6978251c
Azortharion <azortharion72@gmail.com>
2026-07-22 18:57:37 +0200

    [Hunter] MM APL Updates

--- engine/class_modules/apl/apl_hunter.cpp
+++ engine/class_modules/apl/apl_hunter.cpp
@@ -360,7 +360,7 @@ void marksmanship_ptr( player_t* p )
   cds->add_action( "potion,if=buff.trueshot.up&(buff.bloodlust.up|fight_remains<120-30*talent.calling_the_shots)|fight_remains<31" );
 
   drst->add_action( "black_arrow,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up" );
-  drst->add_action( "explosive_shot" );
+  drst->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   drst->add_action( "volley" );
   drst->add_action( "aimed_shot,if=buff.trueshot.up&buff.precise_shots.down&cooldown.black_arrow.ready|full_recharge_time<gcd+cast_time" );
   drst->add_action( "trueshot,if=variable.trueshot_ready&(raid_event.pull.remains>30|talent.calling_the_shots|!raid_event.pull.exists)" );
@@ -371,7 +371,7 @@ void marksmanship_ptr( player_t* p )
   drst->add_action( "black_arrow" );
   drst->add_action( "steady_shot" );
 
-  draoe->add_action( "explosive_shot" );
+  draoe->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   draoe->add_action( "volley" );
   draoe->add_action( "aimed_shot,target_if=max:debuff.spotters_mark.up|max_prio_damage,if=buff.trick_shots.remains>cast_time&full_recharge_time<gcd+cast_time" );
   draoe->add_action( "black_arrow,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up" );
@@ -384,7 +384,7 @@ void marksmanship_ptr( player_t* p )
   draoe->add_action( "black_arrow" );
   draoe->add_action( "steady_shot" );
 
-  sentaoe->add_action( "explosive_shot" );
+  sentaoe->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   sentaoe->add_action( "volley" );
   sentaoe->add_action( "trueshot,if=variable.trueshot_ready&(raid_event.pull.remains>30|talent.calling_the_shots|!raid_event.pull.exists)" );
   sentaoe->add_action( "moonlight_chakram,if=buff.moonlight_chakram.remains<gcd.max" );
@@ -392,20 +392,18 @@ void marksmanship_ptr( player_t* p )
   sentaoe->add_action( "rapid_fire" );
   sentaoe->add_action( "aimed_shot,target_if=max:debuff.sentinels_mark.up|max_prio_damage,if=buff.trick_shots.remains>cast_time" );
   sentaoe->add_action( "moonlight_chakram" );
-  sentaoe->add_action( "multishot,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up&talent.windrunner_quiver" );
   sentaoe->add_action( "steady_shot" );
 
-  sentst->add_action( "explosive_shot" );
+  sentst->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   sentst->add_action( "volley" );
   sentst->add_action( "trueshot,if=variable.trueshot_ready&(raid_event.pull.remains>30|talent.calling_the_shots|!raid_event.pull.exists)" );
   sentst->add_action( "moonlight_chakram,if=buff.moonlight_chakram.remains<gcd.max" );
   sentst->add_action( "rapid_fire,if=debuff.spotters_mark_rapid_fire.up|!apex.3" );
   sentst->add_action( "kill_shot,target_if=max:debuff.sentinels_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up&(buff.trueshot.up&prev_gcd.1.aimed_shot|!buff.trueshot.up)" );
   sentst->add_action( "arcane_shot,target_if=max:debuff.sentinels_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up&(buff.trueshot.up&prev_gcd.1.aimed_shot|!buff.trueshot.up)" );
-  sentst->add_action( "rapid_fire,if=(buff.bulletstorm.remains<action.aimed_shot.execute_time|talent.unload&target.health.pct<20)" );
+  sentst->add_action( "rapid_fire" );
   sentst->add_action( "aimed_shot,target_if=max:debuff.sentinels_mark.up|max_prio_damage" );
   sentst->add_action( "moonlight_chakram" );
-  sentst->add_action( "rapid_fire" );
   sentst->add_action( "steady_shot" );
 
   trinkets->add_action( "use_items,check_existing=0,slots=trinket1:trinket2,if=this_trinket.has_use_buff&this_trinket.cooldown.duration%%cooldown.trueshot.duration=0&(buff.trueshot.remains>14|this_trinket.is.algethar_puzzle_box&variable.trueshot_ready&cooldown.trueshot.remains<5)", "A buff trinket that lines up cleanly with Trueshot; use with Trueshot." );
