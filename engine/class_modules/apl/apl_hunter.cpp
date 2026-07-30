9bbb40e51efd05fb4668ef98b04b40c7b2ecfc70
Azortharion <azortharion72@gmail.com>
2026-07-30 18:10:29 +0200

    [Hunter] SV APL Update
    Implement Thyminde's earlier improvement properly

--- engine/class_modules/apl/apl_hunter.cpp
+++ engine/class_modules/apl/apl_hunter.cpp
@@ -568,13 +568,12 @@ void survival_ptr( player_t* p )
   plcleave->add_action( "takedown" );
 
   sentcleave->add_action( "kill_command,if=buff.tip_of_the_spear.stack=0", "AOE - Sent" );
-  sentcleave->add_action( "wildfire_bomb,if=talent.wildfire_shells&(buff.tip_of_the_spear.up&!debuff.sentinels_mark.remains&cooldown.boomstick.remains<11&cooldown.boomstick.remains>1)" );
   sentcleave->add_action( "boomstick,if=buff.tip_of_the_spear.up" );
-  sentcleave->add_action( "wildfire_bomb,if=buff.tip_of_the_spear.up" );
+  sentcleave->add_action( "wildfire_bomb,target_if=max:debuff.sentinels_mark.remains,if=buff.tip_of_the_spear.up" );
   sentcleave->add_action( "kill_command,if=cooldown.takedown.remains<gcd&buff.tip_of_the_spear.stack<2&!talent.twin_fangs" );
-  sentcleave->add_action( "takedown,if=buff.tip_of_the_spear.up" );
-  sentcleave->add_action( "moonlight_chakram,if=buff.tip_of_the_spear.up" );
-  sentcleave->add_action( "raptor_strike,if=buff.tip_of_the_spear.up&buff.raptor_swipe.up|!buff.raptor_swipe.up" );
+  sentcleave->add_action( "takedown,target_if=min:debuff.sentinels_mark.remains,if=buff.tip_of_the_spear.up" );
+  sentcleave->add_action( "moonlight_chakram,target_if=min:debuff.sentinels_mark.remains,if=buff.tip_of_the_spear.up" );
+  sentcleave->add_action( "raptor_strike,target_if=min:debuff.sentinels_mark.remains,if=buff.tip_of_the_spear.up&buff.raptor_swipe.up|!buff.raptor_swipe.up" );
   sentcleave->add_action( "kill_command" );
 }
 //survival_ptr_apl_end
