61cd1d0a09d56fe4340c544a5e94749f286ab831
Publik <sean.peters86@icloud.com>
2026-08-04 20:56:18 -0500

    [Priest] adjust combat potion

--- engine/class_modules/apl/apl_priest.cpp
+++ engine/class_modules/apl/apl_priest.cpp
@@ -13,7 +13,7 @@ namespace priest_apl
 {
 std::string potion( const player_t* p )
 {
-  return ( p->true_level > 80 ) ? "lights_potential_2" : "tempered_potion_3";
+  return ( p->true_level > 80 ) ? "potion_of_recklessness_2" : "tempered_potion_3";
 }
 
 std::string flask( const player_t* p )
