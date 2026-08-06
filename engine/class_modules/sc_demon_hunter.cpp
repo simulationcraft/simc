421f7d4014882cac0d060b4983c6ae2751c809ec
Aezeor <matthewcolejackson@gmail.com>
2026-08-06 18:41:53 -0400

    felblade also does not shatter

--- engine/class_modules/sc_demon_hunter.cpp
+++ engine/class_modules/sc_demon_hunter.cpp
@@ -5829,7 +5829,7 @@ struct consume_t : public consume_base_t
 
 struct voidblade_base_t : public voidrush_trigger_t<hungering_slash_trigger_t<demon_hunter_spell_t>>
 {
-  struct voidblade_damage_t : public burning_blades_trigger_t<shattered_souls_trigger_t<demon_hunter_spell_t>>
+  struct voidblade_damage_t : public burning_blades_trigger_t<demon_hunter_spell_t>
   {
     voidblade_damage_t( util::string_view name, demon_hunter_t* p ) : base_t( name, p, p->spec.voidblade )
     {
