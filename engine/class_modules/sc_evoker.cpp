4be392d08765ab2783d5e5ac9439b1d62bcb0dc9
Saeldur <5496498+Saeldur@users.noreply.github.com>
2026-08-08 17:51:04 +0100

    [Dev] Star no mastery

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -4460,6 +4460,7 @@ private:
   using ab = evoker_action_t<spell_t>;
 
 public:
+  bool affected_by_giantkiller = true;
   evoker_spell_t( std::string_view name, evoker_t* player, const spell_data_t* spell = spell_data_t::nil(),
                   std::string_view options_str = {} )
     : ab( name, player, spell )
@@ -4512,7 +4513,7 @@ public:
 
     // Preliminary testing shows this is linear with target hp %.
     // TODO: confirm this applies only to all evoker offensive spells
-    if ( p()->specialization() == EVOKER_DEVASTATION )
+    if ( p()->specialization() == EVOKER_DEVASTATION && affected_by_giantkiller )
     {
       if ( use_full_mastery() )
         tm *= 1.0 + p()->cache.mastery_value();
@@ -5399,6 +5400,7 @@ struct shattering_star_t : public evoker_spell_t
 {
   shattering_star_t( evoker_t* p, std::string_view name ) : evoker_spell_t( name, p, p->talent.shattering_star_spell )
   {
+    affected_by_giantkiller = false;
   }
 };
 
