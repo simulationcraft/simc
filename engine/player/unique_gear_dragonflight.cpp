90c3fb8dc35873f24601d9d47e5912798889e1e7
gastank <42421688+gastank@users.noreply.github.com>
2026-08-11 14:56:24 -0700

    [Gear] fix crash with ruby whelp triggering on friendly

--- engine/player/unique_gear_dragonflight.cpp
+++ engine/player/unique_gear_dragonflight.cpp
@@ -3895,6 +3895,10 @@ void ruby_whelp_shell( special_effect_t& effect )
 
     void trigger_random_whelp_proc( player_t* t )
     {
+      // only trigger damage if the target is an enemy, otherwise ignore for 'heal'
+      if ( !t->is_enemy() )
+        return;
+
       int choice = rng().range( static_cast<int>( RUBY_WHELP_TYPE_MAX ) );
       trigger_whelp_proc( static_cast<ruby_whelp_type_e>( choice ), t );
     }
