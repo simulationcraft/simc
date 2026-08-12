120ae3b4cd56a7fe3d3427785c85f7d24071975e
Saeldur <5496498+Saeldur@users.noreply.github.com>
2026-08-12 12:08:25 +0100

    [Aug] Boost default simplified ilvl

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -369,7 +369,7 @@ struct simplified_player_t : public player_t
   // Options
   struct options_t
   {
-    int item_level      = 281;
+    int item_level      = 298;
     std::string variant = "default";
     double skill         = 1.0;
   } option;
