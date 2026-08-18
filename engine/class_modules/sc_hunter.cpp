d2bf3eb240d8c6a68b9d466b822c958777a80816
ZingusWoW <zinguswow@gmail.com>
2026-08-18 18:44:08 +0100

    [Hunter] remove hotfixes

--- engine/class_modules/sc_hunter.cpp
+++ engine/class_modules/sc_hunter.cpp
@@ -9141,18 +9141,6 @@ struct hunter_module_t: public module_t
         .operation( hotfix::HOTFIX_SET )
         .modifier( 25 )
         .verification_value( 15 );
-
-    hotfix::register_effect( "Hunter", "2026-08-16", "Cobra Cleave Percent", 1323597, hotfix::HOTFIX_FLAG_LIVE )
-        .field( "base_value" )
-        .operation( hotfix::HOTFIX_SET )
-        .modifier( 30 )
-        .verification_value( 20 );
-
-    hotfix::register_effect( "Hunter", "2026-08-16", "Cobra Fang Percent", 1323599, hotfix::HOTFIX_FLAG_LIVE )
-        .field( "base_value" )
-        .operation( hotfix::HOTFIX_SET )
-        .modifier( 20 )
-        .verification_value( 15 );
   }
 
   void register_actor_initializers( sim_t* ) const override {}
