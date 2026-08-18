689eb01f03ff71cc0f1135795abfdd28693e1dd8
gastank <42421688+gastank@users.noreply.github.com>
2026-08-18 08:03:09 -0700

    [Gear] differentiate proc rppms by driver spell id

--- engine/action/dbc_proc_callback.cpp
+++ engine/action/dbc_proc_callback.cpp
@@ -288,7 +288,8 @@ void dbc_proc_callback_t::initialize()
   // prioritizes RPPM > PPM > proc chance.
   if ( effect.rppm() > 0 && effect.rppm_scale() != RPPM_DISABLE )
   {
-    rppm = listener->get_rppm( effect.name(), effect.rppm(), effect.rppm_modifier(), effect.rppm_scale() );
+    rppm = listener->get_rppm( fmt::format( "proc_{}_{}_rppm", effect.name(), effect.spell_id ), effect.rppm(),
+                               effect.rppm_modifier(), effect.rppm_scale() );
     rppm->set_blp_state( static_cast<real_ppm_t::blp>( effect.rppm_blp_ ) );
   }
   else if ( effect.ppm() > 0 )
