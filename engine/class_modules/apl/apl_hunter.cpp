cd25e2c6fdb7cb7df94ce70ad840e96d902b5a6c
Azortharion <azortharion72@gmail.com>
2026-08-25 11:21:07 +0200

    [Hunter] MM APL Cleanup/Re-simplifications

--- engine/class_modules/apl/apl_hunter.cpp
+++ engine/class_modules/apl/apl_hunter.cpp
@@ -261,7 +261,7 @@ void marksmanship( player_t* p )
 
   drst->add_action( "black_arrow,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up" );
   drst->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=set_bonus.mid2_4pc&active_enemies>1&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up)" );
-  drst->add_action( "explosive_shot,if=(!talent.tactical_reload|!buff.lock_and_load.up)&(!fight_style.dungeonroute|active_enemies=1|active_dot.explosive_shot=0|buff.unstable_trigger.up)" );
+  drst->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   drst->add_action( "volley" );
   drst->add_action( "aimed_shot,if=buff.trueshot.up&buff.precise_shots.down&cooldown.black_arrow.ready|full_recharge_time<gcd+cast_time" );
   drst->add_action( "trueshot,if=variable.trueshot_ready" );
@@ -274,13 +274,13 @@ void marksmanship( player_t* p )
   drst->add_action( "steady_shot" );
 
   draoe->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=set_bonus.mid2_4pc&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up)" );
-  draoe->add_action( "explosive_shot,if=(!talent.tactical_reload|!buff.lock_and_load.up)&(!fight_style.dungeonroute|active_dot.explosive_shot=0|buff.unstable_trigger.up)" );
+  draoe->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   draoe->add_action( "volley" );
   draoe->add_action( "aimed_shot,target_if=max:debuff.spotters_mark.up|max_prio_damage,if=buff.trick_shots.remains>cast_time&full_recharge_time<gcd+cast_time" );
   draoe->add_action( "black_arrow,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up" );
   draoe->add_action( "multishot,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.trick_shots.down" );
   draoe->add_action( "trueshot,if=variable.trueshot_ready" );
-  draoe->add_action( "rapid_fire,if=buff.trick_shots.remains>execute_time&(buff.bulletstorm.remains<action.aimed_shot.execute_time|talent.unload)" );
+  draoe->add_action( "rapid_fire,if=buff.trick_shots.remains>execute_time" );
   draoe->add_action( "wailing_arrow,if=!cooldown.black_arrow.ready" );
   draoe->add_action( "multishot,target_if=max:debuff.spotters_mark.down|action.aimed_shot.in_flight_to_target|max_prio_damage,if=buff.precise_shots.up" );
   draoe->add_action( "aimed_shot,target_if=max:debuff.spotters_mark.up|max_prio_damage,if=buff.trick_shots.remains>cast_time" );
@@ -288,8 +288,8 @@ void marksmanship( player_t* p )
   draoe->add_action( "multishot,if=focus>cost+action.aimed_shot.cost&!max_prio_damage" );
   draoe->add_action( "steady_shot" );
 
-  sentaoe->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=(set_bonus.mid2_4pc&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up))" );
-  sentaoe->add_action( "explosive_shot,if=((!talent.tactical_reload|!buff.lock_and_load.up)&(!fight_style.dungeonroute|active_dot.explosive_shot=0|buff.unstable_trigger.up))" );
+  sentaoe->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=set_bonus.mid2_4pc&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up)" );
+  sentaoe->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   sentaoe->add_action( "volley" );
   sentaoe->add_action( "trueshot,if=variable.trueshot_ready" );
   sentaoe->add_action( "moonlight_chakram,if=buff.moonlight_chakram.remains<gcd.max" );
@@ -302,8 +302,8 @@ void marksmanship( player_t* p )
   sentaoe->add_action( "multishot,if=focus>cost+action.aimed_shot.cost&!max_prio_damage" );
   sentaoe->add_action( "steady_shot" );
 
-  sentst->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=(set_bonus.mid2_4pc&active_enemies>1&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up))" );
-  sentst->add_action( "explosive_shot,if=((!talent.tactical_reload|!buff.lock_and_load.up)&(!fight_style.dungeonroute|active_enemies=1|active_dot.explosive_shot=0|buff.unstable_trigger.up))" );
+  sentst->add_action( "explosive_shot,target_if=min:dot.explosive_shot.remains,if=set_bonus.mid2_4pc&active_enemies>1&(!max_prio_damage|!fight_style.dungeonroute)&(!talent.tactical_reload|!buff.lock_and_load.up)" );
+  sentst->add_action( "explosive_shot,if=!talent.tactical_reload|!buff.lock_and_load.up" );
   sentst->add_action( "volley" );
   sentst->add_action( "trueshot,if=variable.trueshot_ready" );
   sentst->add_action( "moonlight_chakram,if=buff.moonlight_chakram.remains<5" );
