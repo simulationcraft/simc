5766d078a55616fb48b271ef4eed64f9a0c1b706
Richard Harrah <topplethenunnery@gmail.com>
2026-08-29 22:15:37 -0400

    setup actions

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://topplethenun.github.io',
+	base: '/simc',
 	integrations: [
 		starlight({
 			title: 'SimulationCraft',
