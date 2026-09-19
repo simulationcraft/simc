6ade30e343b1f4097db11524d557d375f4f55ebc
Saeldur <5496498+Saeldur@users.noreply.github.com>
2026-09-19 12:06:32 +0100

    [Evoker] Increase default simplified player ilvl to 328

--- engine/class_modules/sc_evoker.cpp
+++ engine/class_modules/sc_evoker.cpp
@@ -369,7 +369,7 @@ struct simplified_player_t : public player_t
   // Options
   struct options_t
   {
-    int item_level      = 323;
+    int item_level      = 328;
     std::string variant = "default";
     double skill         = 1.0;
   } option;
