be6b62434b52ae2bdf1a99d664633741418e6752
gastank <42421688+gastank@users.noreply.github.com>
2026-09-20 10:46:13 -0700

    [DBC] initial format/conf cleanup

--- casc_extract/WinGenerateSpellData.bat
+++ casc_extract/WinGenerateSpellData.bat
@@ -1,4 +1,4 @@
-call py -3 casc_extract.py -m batch --cdn -o wow
+call py -3 casc_extract.py -m batch --cdn --product=wow_classic_beta -o wow
 cd wow
 dir /b /a:D /O:-D>hi.txt
 set /p wowdir=<hi.txt
@@ -7,8 +7,5 @@ cd ..
 cd ..
 set curr=%cd%
 cd dbc_extract3
-robocopy "C:\Program Files (x86)\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
-robocopy "C:\Program Files (x86)\Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
-robocopy "C:\World of Warcraft\_retail_\Cache\ADB\enUS" "%cd%\cache\live" DBCache.bin
 call generate.bat %wowdir% "%curr%\casc_extract\wow"
 pause
