9ddf9427499858b538f1aed39333a907e204bb0c
nyterage <nyterage@gmail.com>
2026-09-23 07:57:54 -0700

    [Unholy] Fix blightfall error

--- engine/class_modules/sc_death_knight.cpp
+++ engine/class_modules/sc_death_knight.cpp
@@ -16525,7 +16525,7 @@ bool death_knight_t::validate_actor()
 
   if ( talent.unholy.blightfall.ok() )
     sim->errorf(
-        "Player {} has Blightfall talent enabled. Sim results WILL NOT be accurate. This talent is bugged in game, and "
+        "Player %s has Blightfall talent enabled. Sim results WILL NOT be accurate. This talent is bugged in game, and "
         "we have yet to figure out how. Sims assume Blightfall is working as expected, leading to substantially more "
         "DPS than expected.",
         name() );
